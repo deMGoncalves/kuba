b66aaa014117f5da075bc4e90c3d2dc11bc6dfa8
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-04-20 18:15:37 -0300

    Refatoracao, remocao do modulo que comparava se os tagName eram diferentes

--- src/h/reflow/differentTagName.js
+++
@@ -1,5 +0,0 @@
-import equals from 'ramda/src/equals'
-import not from 'ramda/src/not'
-
-export default (element, vElement) =>
-  not(equals(element.tagName, vElement.tagName))
--- src/h/reflow/reflow.js
+++ src/h/reflow/reflow.js
@@ -1,8 +1,8 @@
 import cond from 'ramda/src/cond'
+import equals from 'ramda/src/equals'
 import not from 'ramda/src/not'
 import T from 'ramda/src/T'
 import appendChild from './appendChild'
-import differentTagName from './differentTagName'
 import isTextNode from './isTextNode'
 import remove from './remove'
 import replaceChild from './replaceChild'
@@ -12,7 +12,7 @@ import replaceTextNode from './replaceTextNode'
 export default cond([
   [(element) => not(!!element), appendChild],
   [(_, vElement) => not(!!vElement), remove],
-  [differentTagName, replaceChild],
+  [(element, vElement) => not(equals(element.tagName, vElement.tagName)), replaceChild],
   [isTextNode, replaceTextNode],
   [T, replaceElement]
 ])
