a60339cfe0c78e48d43d04102673e389dd296140
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-04-21 00:12:51 -0300

    Melhorando as descricoes dos testes

--- src/h/__tests__/h.spec.js
+++ src/h/__tests__/h.spec.js
@@ -1,43 +1,46 @@
 import h from '../'
 
-test('Retornar um elemento div quando executar a funcao h passando parametro "div"', () => {
+test('Retorna um elemento div quando executar a funcao h passando parametro "div"', () => {
   expect(h('div')).toEqual(<div />)
 })
 
-test('Retornar um elemento p quando executar a funcao h passando o parametro "p"', () => {
+test('Retorna um elemento p quando executar a funcao h passando o parametro "p"', () => {
   expect(h('p')).toEqual(<p />)
 })
 
-test('Retornar um elemento div com o id egual a johzao quando executar a funcao h passando o segundo parametro um objeto com o id', () => {
+test('Retorna um elemento div com o id egual a johzao quando executar a funcao h passando um objeto com o id', () => {
   expect(h('div', { id: 'johzao' })).toEqual(<div id='johzao' />)
 })
 
-test('Retornar um elemento p com a classe egual a johzao quando executar a funcao h passando o segundo parametro um objeto com a classe', () => {
+test('Retorna um elemento p com a classe egual a johzao quando executar a funcao h passando um objeto com a classe', () => {
   expect(h('p', { class: 'johzao' })).toEqual(<p class='johzao' />)
 })
 
-test('Deve retornar um DIV com o dataset.name johzao quando executar a funcao h passando o atributo data-name johzao', () => {
+test('Retorna um elemento div com o dataset.name egual johzao quando executar a funcao h passando o atributo data-name johzao', () => {
   expect(h('div', { 'data-name': 'johzao' })).toEqual(<div data-name='johzao' />)
 })
 
-test('Deve retornar um DIV quando executar a funcao h passando null como atributo', () => {
+test('Retorna um elemento div quando executar a funcao h passando null como atributo', () => {
   expect(h('div', null)).toEqual(<div />)
 })
 
-test('Deve retornar um DIV quando executar a funcao h passando qualquer valor como atributo', () => {
+test('Retorna um elemento div quando executar a funcao h passando qualquer valor como atributo', () => {
   expect(h('div', new Date())).toEqual(<div />)
 })
 
-test('Deve retornar um DIV com o evento de click quando executar a funcao h passando o evento onClick', () => {
-  const clickHere = () => { expect(true).toBeTruthy() }
-  h('div', { onClick: clickHere }).dispatchEvent(new Event('click'))
+test('Retorna um elemento div com o evento de click quando executar a funcao h passando o evento onClick', () => {
+  const listener = () => {
+    expect(true).toBeTruthy()
+  }
+
+  h('div', { onClick: listener }).dispatchEvent(new Event('click'))
 })
 
-test('Deve retornar um P com o text Johzao quando executar a funcao h passando o texto Johzao', () => {
+test('Retorna um elemento p com o texto Johzao quando executar a funcao h passando o texto Johzao', () => {
   expect(h('p', null, 'Johzao')).toEqual(<p>Johzao</p>)
 })
 
-test('Deve retornar um P com o nome completo do Johzao quando executar a funcao h passando  o nome completo', () => {
+test('Retorna um elemento p com o nome completo do Johzao quando executar a funcao h passando o nome completo', () => {
   expect(h('p', null, h('strong', null, 'Johzao'), ' da Silva'))
     .toEqual(
       <p>
@@ -46,7 +49,7 @@ test('Deve retornar um P com o nome completo do Johzao quando executar a funcao 
     )
 })
 
-test('Deve retornar um UL com as frutas de uma lista quando executar a funcao h passando a colecao ["abacaxi", "morango"]', () => {
+test('Retorna um elemento ul com as frutas de uma lista quando executar a funcao h passando a colecao ["abacaxi", "morango"]', () => {
   expect(h('ul', null, ['abacaxi', 'morango'].map(fruta => h('li', null, fruta))))
     .toEqual(
       <ul>
@@ -56,7 +59,7 @@ test('Deve retornar um UL com as frutas de uma lista quando executar a funcao h 
     )
 })
 
-test('Deve retornar um Component quando executar a funcao h passando uma funcao como parametro', () => {
+test('Retorna um Component quando executar a funcao h passando uma funcao como parametro', () => {
   const Name = (props) =>
     h('p', null, props.name)
 
