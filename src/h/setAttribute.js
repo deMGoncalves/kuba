95daa170d9aa07e74474168fa3a6829fe70f678f
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-04-23 18:12:59 -0300

    Testando o modulo de atribuicao de atributos para um elemento

--- src/h/setAttribute.js
+++ src/h/setAttribute.js
@@ -1,6 +1,8 @@
 import curry from 'ramda/src/curry'
 
-const setAttribute = (element, key, value) =>
+const setAttribute = (element, key, value) => {
   element.setAttribute(key, value)
+  return element
+}
 
 export default curry(setAttribute)
