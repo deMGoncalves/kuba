8dbd6449a2b8fa29b7282c1c4f68968eac326661
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-01 02:19:44 -0300

    Refatoracao na funcao h, removendo dependencia que nao sera0 mais usados, simplificacao do codigo e documentacao

--- src/infra/h/h.js
+++ src/infra/h/h.js
@@ -1,14 +1,22 @@
-import cond from 'ramda/src/cond'
 import flatten from 'ramda/src/flatten'
-import T from 'ramda/src/T'
 import createElement from './createElement'
 import executeComponent from './executeComponent'
 import isTagName from './isTagName'
 
-const h = cond([
-  [isTagName, createElement],
-  [T, executeComponent]
-])
+/**
+ * Hyperscript, syntaxe javascript para construir elementos HTML
+ *
+ * @name h
+ * @function
+ * @access public
+ * @args {string|function} tagNameOrComponent Nome do tag HTML ou referencia do componente
+ * @args {object} attributes Atributos do elemento HTML ou componente
+ * @args {array} children Elementos filhos
+ * @returns {Node} Elemento HTML
+ */
+const h = (tagNameOrComponent, attributes, ...children) =>
+  (isTagName(tagNameOrComponent)
+    ? createElement
+    : executeComponent)(tagNameOrComponent, { ...attributes }, flatten(children))
 
-export default (tagNameOrComponent, attributes, ...children) =>
-  h(tagNameOrComponent, { ...attributes }, flatten(children))
+export default h
