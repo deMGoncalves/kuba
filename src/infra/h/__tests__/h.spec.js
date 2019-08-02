e3f168f48076b816bfef014ef33de7ea095c7a8a
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-02 01:22:05 -0300

    Deve criar um elemento quanto o parametro tagNameOrComponent for um string

--- src/infra/h/__tests__/h.spec.js
+++ src/infra/h/__tests__/h.spec.js
@@ -1,5 +1,18 @@
 import h from '../'
+import isTagName from '../isTagName'
+
+jest.mock('../isTagName.js')
 
 test('h', () => {
   expect(h).toBeDefined()
+})
+
+test('Deve criar um elemento quando o parametro tagNameOrComponent for um string', () => {
+  isTagName.mockReturnValue(true)
+
+  expect(h('div')).toEqual(document.createElement('div'))
+
+  expect(isTagName).toHaveBeenCalled()
+  expect(isTagName).toHaveBeenCalledTimes(1)
+  expect(isTagName).toHaveBeenCalledWith('div')
 })
