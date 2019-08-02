b98ddd9544163bf58aa813cf6630424b4f4966b7
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-02 01:59:44 -0300

    Deve retornar o resultado do component quando o parametro tagNameOrComponent for uma funcao

--- src/infra/h/__tests__/h.spec.js
+++ src/infra/h/__tests__/h.spec.js
@@ -1,18 +1,53 @@
 import h from '../'
+import createElement from '../createElement.js'
+import executeComponent from '../executeComponent.js'
 import isTagName from '../isTagName'
 
+jest.mock('../createElement.js')
+jest.mock('../executeComponent.js')
 jest.mock('../isTagName.js')
 
 test('h', () => {
   expect(h).toBeDefined()
 })
 
-test('Deve criar um elemento quando o parametro tagNameOrComponent for um string', () => {
+test('Deve criar um elemento quando o parametro tagNameOrComponent for uma string', () => {
+  const element = document.createElement('div')
+
+  isTagName.mockReset()
   isTagName.mockReturnValue(true)
 
-  expect(h('div')).toEqual(document.createElement('div'))
+  createElement.mockReset()
+  createElement.mockReturnValue(element)
+
+  expect(h('div')).toEqual(element)
 
   expect(isTagName).toHaveBeenCalled()
   expect(isTagName).toHaveBeenCalledTimes(1)
   expect(isTagName).toHaveBeenCalledWith('div')
+
+  expect(createElement).toHaveBeenCalled()
+  expect(createElement).toHaveBeenCalledTimes(1)
+  expect(createElement).toHaveBeenCalledWith('div', {}, [])
+})
+
+test('Deve retornar o resultado do component quando o parametro tagNameOrComponent for uma funcao', () => {
+  const component = () => {}
+  const element = document.createElement('div')
+
+  isTagName.mockReset()
+  isTagName.mockReturnValue(false)
+
+  executeComponent.mockReset()
+  executeComponent.mockReturnValue(element)
+
+  expect(h(component)).toEqual(element)
+
+  expect(isTagName).toHaveBeenCalled()
+  expect(isTagName).toHaveBeenCalledTimes(1)
+  expect(isTagName).toHaveBeenCalledWith(component)
+
+  expect(executeComponent).toHaveBeenCalled()
+  expect(executeComponent).toHaveBeenCalledTimes(1)
+  expect(executeComponent).toHaveBeenCalledWith(component, {}, [])
 })
