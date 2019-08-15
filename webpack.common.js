92734c37af588d5c3dc66db4a3a5986e0556ad64
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-15 18:24:45 -0300

    Configuracao do css-loader

--- webpack.common.js
+++ webpack.common.js
@@ -35,7 +35,6 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              importLoaders: 1,
               modules: true
             }
           },
