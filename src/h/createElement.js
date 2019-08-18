4a0dc46548380351eb1f7c30230af5065373383d
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-18 14:18:49 -0300

    Suporte para customElements.define(..., { extends })

--- src/h/createElement.js
+++ src/h/createElement.js
@@ -3,4 +3,4 @@ import appendChildren from './appendChildren'
 import extendAttributes from './extendAttributes'
 
 export default (tagName, attributes, children) =>
-  compose(appendChildren(children), extendAttributes(attributes))(document.createElement(tagName))
+  compose(appendChildren(children), extendAttributes(attributes))(document.createElement(tagName, { is: attributes.is }))
