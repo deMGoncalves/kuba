daf1af53c3f10733a87f2a5e79205c32c3ff700c
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-21 18:30:37 -0300

    Nao preciso mais do lazy load, hoje o chrome me faz isso na tag image com o atributo loading="lazy"

--- src/defineLazyLoad.js
+++
@@ -1,9 +0,0 @@
-class LazyLoad extends HTMLImageElement {
-  connectedCallback () {
-    this.dataset.src = this.getAttribute('src')
-    this.setAttribute('src', '')
-    return this
-  }
-}
-
-customElements.define('lazy-load', LazyLoad, { extends: 'img' })
