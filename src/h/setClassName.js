fc097a15fd53b495ababa4349a1b605d5fa2a164
Cleber Goncalves <cleber.goncalves@cea.com.br>
2019-08-23 15:00:59 -0300

    Implementacao de classes por um array

--- src/h/setClassName.js
+++ src/h/setClassName.js
@@ -1,6 +1,6 @@
 import curry from 'ramda/src/curry'
 
 const setClassName = (element, className, target) =>
-  (element[className] = target)
+  (element[className] = [].concat(target).join(' '))
 
 export default curry(setClassName)
