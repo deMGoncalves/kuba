6a2de90bea0503fb90456a5da2246421b339f640
Cleber Goncalves <cleber.goncalves@cea.com.br>
2019-08-27 08:29:54 -0300

    Teste do stop nos eventos

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -6,7 +6,11 @@ import style from './style.css'
 gps(/^\/$/, () =>
   document.body.appendChild(
     <main className={style.home}>
-      <img is='lazy-load' className={style.home__logo} src={RexJS} alt='Rex.JS' />
+      <img
+        className={style.home__logo}
+        onClick_stop={() => console.log('%c Rex.JS - O bom e velho amigo do programador!', 'font-size: 60px;')}
+        src={RexJS}
+        alt='Rex.JS' />
       <h1>Rex.JS</h1>
       <p>Simples, pequeno e imperfeito. Uma visão além da programação</p>
     </main>
