e5b7f32d96bd9d070c74c4da903595a76f2c2199
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-28 18:22:19 -0300

    Teste com o scheme para seo

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -5,14 +5,15 @@ import style from './style.css'
 
 gps(/^\/$/, () =>
   document.body.appendChild(
-    <main className={style.home}>
+    <main className={style.home} itemScope itemType='https://schema.org/Product'>
       <img
         className={style.home__logo}
         onClick_stop={() => console.log('%c Rex.JS - O bom e velho amigo do programador!', 'font-size: 60px;')}
         src={RexJS}
-        alt='Rex.JS' />
-      <h1>Rex.JS</h1>
-      <p>Simples, pequeno e imperfeito. Uma visão além da programação</p>
+        alt='Rex.JS'
+        itemProp='image' />
+      <h1 itemProp='name'>Rex.JS</h1>
+      <p itemProp='description'>Simples, pequeno e imperfeito. Uma visão além da programação</p>
     </main>
   )
 )
