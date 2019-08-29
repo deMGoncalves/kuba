f635ece24a1c555fc5fb3a43ceecb417e603c3a2
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-29 06:08:46 -0300

    Inclusao do itemprop url

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -6,9 +6,11 @@ import style from './style.css'
 gps(/^\/$/, () =>
   document.body.appendChild(
     <main className={style.home} itemScope itemType='https://schema.org/Organization'>
-      <img className={style.home__logo} src={RexJS} alt='Rex.JS' itemProp='logo' />
-      <h1 itemProp='name'>Rex.JS</h1>
-      <p itemProp='description'>Simples, pequeno e imperfeito. Uma visão além da programação</p>
+      <a href='/' itemProp='url'>
+        <img className={style.home__logo} src={RexJS} alt='Rex.JS' itemProp='logo' />
+        <h1 itemProp='name'>Rex.JS</h1>
+        <p itemProp='description'>Simples, pequeno e imperfeito. Uma visão além da programação</p>
+      </a>
     </main>
   )
 )
