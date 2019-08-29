03dd9ac8cabe67e467196e949198a350dd806cbb
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-29 06:02:39 -0300

    Configuracao do microdata de organizacao

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -5,13 +5,8 @@ import style from './style.css'
 
 gps(/^\/$/, () =>
   document.body.appendChild(
-    <main className={style.home} itemScope itemType='https://schema.org/Product'>
-      <img
-        className={style.home__logo}
-        onClick_stop={() => console.log('%c Rex.JS - O bom e velho amigo do programador!', 'font-size: 60px;')}
-        src={RexJS}
-        alt='Rex.JS'
-        itemProp='image' />
+    <main className={style.home} itemScope itemType='https://schema.org/Organization'>
+      <img className={style.home__logo} src={RexJS} alt='Rex.JS' itemProp='logo' />
       <h1 itemProp='name'>Rex.JS</h1>
       <p itemProp='description'>Simples, pequeno e imperfeito. Uma visão além da programação</p>
     </main>
