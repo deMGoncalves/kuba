99b041ea6998c8ad41f961a0e107d8dfa8d5cf07
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-30 18:37:42 -0300

    Doc e refatoracao de codigo

--- src/h/executeComponent.js
+++ src/h/executeComponent.js
@@ -1,2 +1,15 @@
-export default (component, attributes, children) =>
+/**
+ * Executa um component function
+ *
+ * @name executeComponent
+ * @function
+ * @access private
+ * @param {Function} component Um componente customizado
+ * @param {Object} attributes Atributos do componente
+ * @param {Array} children Uma matriz de elementos filhos
+ * @returns {HTMLElement} Um elemento html
+ */
+const executeComponent = (component, attributes, children) =>
   component(attributes, children)
+
+export default executeComponent
