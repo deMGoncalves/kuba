a8b86abe77679523f54b946d4a6fca14af8c1c8b
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-30 18:16:30 -0300

    Refatoracao do registerSW, indicado pelo plato

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,10 +1,3 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration)
-      registration.pushManager.subscribe({ userVisibleOnly: true })
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError)
-    })
-  })
+  window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js'))
 }
