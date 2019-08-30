b1b7ce3baf5aaa5c479f02c511cd57111f79b261
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-30 06:07:55 -0300

    REfatoracao para o funcionamento correto do render

--- src/render/render.js
+++ src/render/render.js
@@ -1,5 +1,6 @@
-const render = (element) =>
-  (Target) =>
-    () => document.querySelector(element).appenChild(<Target />)
+import stub from './stub'
+
+const render = (element, Component) =>
+  (document.querySelector(element) || stub).appenChild(<Component />)
 
 export default render
---
+++ src/render/stub.js
@@ -0,0 +1,3 @@
+const stub = { appendChild () {} }
+
+export default stub
