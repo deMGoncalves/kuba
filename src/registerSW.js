00387fe8c3f8e72471a3ea42946c2edb5ff887fe
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-08-30 19:12:05 -0300

    Service workier esta causando um erro, depis que fiz uma modificacao

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,3 +1,10 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js'))
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration)
+      registration.pushManager.subscribe({ userVisibleOnly: true })
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError)
+    })
+  })
 }
