ee1a852d722468b46fa2f006ebf39ff6095a3194
Cleber Goncalves <cleber.goncalves@cea.com.br>
2019-09-02 14:28:32 -0300

    Agora o service worker so ira funcionar quando estiver em producao

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,10 +1,12 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('SW registered: ', registration)
-      registration.pushManager.subscribe({ userVisibleOnly: true })
-    }).catch(registrationError => {
-      console.log('SW registration failed: ', registrationError)
+if (process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        console.log('SW registered: ', registration)
+        registration.pushManager.subscribe({ userVisibleOnly: true })
+      }).catch(registrationError => {
+        console.log('SW registration failed: ', registrationError)
+      })
     })
-  })
+  }
 }
