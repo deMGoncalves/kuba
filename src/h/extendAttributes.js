2e13180eb35089afa73a3fffdae81e0f7a9573fc
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-02 18:51:11 -0300

    Removendo de vez o moculo curry do ramda

--- src/h/extendAttributes.js
+++ src/h/extendAttributes.js
@@ -1,5 +1,4 @@
 import * as f from '@f'
-import curry from 'ramda/src/curry'
 import forEach from 'ramda/src/forEach'
 import toPairs from 'ramda/src/toPairs'
 import test from 'ramda/src/test'
@@ -10,15 +9,16 @@ import addEventListenerWithStopAndPrevent from './addEventListenerWithStopAndPre
 import setAttribute from './setAttribute'
 import setClassName from './setClassName'
 
-const extendAttributes = curry((element, args) =>
-  f.cond(
-    [test(/^on[A-Z][a-z]+$/), addEventListener(element)],
-    [test(/^on[A-Z][a-z]+_stop$/), addEventListenerWithStop(element)],
-    [test(/^on[A-Z][a-z]+_prevent$/), addEventListenerWithPrevent(element)],
-    [test(/^on[A-Z][a-z]+_(stop|prevent)_(stop|prevent)$/), addEventListenerWithStopAndPrevent(element)],
-    [test(/^className$/), setClassName(element)],
-    [f.T, setAttribute(element)]
-  )(...args))
+const extendAttributes = (element) =>
+  (args) =>
+    f.cond(
+      [test(/^on[A-Z][a-z]+$/), addEventListener(element)],
+      [test(/^on[A-Z][a-z]+_stop$/), addEventListenerWithStop(element)],
+      [test(/^on[A-Z][a-z]+_prevent$/), addEventListenerWithPrevent(element)],
+      [test(/^on[A-Z][a-z]+_(stop|prevent)_(stop|prevent)$/), addEventListenerWithStopAndPrevent(element)],
+      [test(/^className$/), setClassName(element)],
+      [f.T, setAttribute(element)]
+    )(...args)
 
 export default (attributes, element) =>
   f.always(element)(forEach(extendAttributes(element), toPairs(attributes)))
