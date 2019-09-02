bbb61eb7ea412c8009f81c1894e3085cb373891b
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-02 18:23:55 -0300

    Removendo o condo do ramda e alterando para no meu cond

--- src/h/extendAttributes.js
+++ src/h/extendAttributes.js
@@ -1,4 +1,4 @@
-import cond from 'ramda/src/cond'
+import * as f from '@f'
 import curry from 'ramda/src/curry'
 import forEach from 'ramda/src/forEach'
 import toPairs from 'ramda/src/toPairs'
@@ -12,14 +12,14 @@ import setAttribute from './setAttribute'
 import setClassName from './setClassName'
 
 const extendAttributes = curry((element, args) =>
-  cond([
+  f.cond(
     [test(/^on[A-Z][a-z]+$/), addEventListener(element)],
     [test(/^on[A-Z][a-z]+_stop$/), addEventListenerWithStop(element)],
     [test(/^on[A-Z][a-z]+_prevent$/), addEventListenerWithPrevent(element)],
     [test(/^on[A-Z][a-z]+_(stop|prevent)_(stop|prevent)$/), addEventListenerWithStopAndPrevent(element)],
     [test(/^className$/), setClassName(element)],
     [T, setAttribute(element)]
-  ])(...args))
+  )(...args))
 
 export default curry((attributes, element) => {
   forEach(extendAttributes(element), toPairs(attributes))
