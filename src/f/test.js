a27f06c3aa8143b9c78adc5dbec72397730c84e9
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-04 06:23:38 -0300

    Testa de o valor fornecido no teste do parttern

---
+++ src/f/test.js
@@ -0,0 +1,16 @@
+import curry from './curry'
+
+/**
+ * Teste se o valor alvo passa no teste de expressao regular fornecida
+ *
+ * @name test
+ * @function
+ * @access public
+ * @param {RegExp} Expressao regular havaliadora
+ * @param {Strinmg} Valor a ser havaliado
+ * @returns {Boolean} Verdadeiro se o valor passar no teste da expressao regular
+ */
+const test = (pattern, target) =>
+  pattern.test(target)
+
+export default curry(test)
