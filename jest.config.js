55413f3e12a4a257fcce8b28696036f41128a5de
Cleber Goncalves <cleber.goncalves@cea.com.br>
2019-09-04 11:21:35 -0300

    ajuste para o funcionamento do teste corretamente e para escluir o arquivo test.js do modulo f

--- jest.config.js
+++ jest.config.js
@@ -160,7 +160,7 @@ module.exports = {
   // The glob patterns Jest uses to detect test files
   testMatch: [
     "**/__tests__/**/*.js?(x)",
-    "**/?(*.)+(spec|test).js?(x)"
+    "**/?(*.)+(spec).js?(x)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
