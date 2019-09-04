c9a1dc1289e03e07eb0171d1b059d74df6d99fcb
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-04 06:08:00 -0300

    Criacao do modulo gap

--- src/f/index.js
+++ src/f/index.js
@@ -2,14 +2,18 @@ import always from './always'
 import arity from './arity'
 import cond from './cond'
 import flatten from './flatten'
+import gap from './gap'
 import is from './is'
 import T from './T'
+import test from './test'
 
 export {
   always,
   arity,
   cond,
   flatten,
+  gap,
   is,
-  T
+  T,
+  test
 }
