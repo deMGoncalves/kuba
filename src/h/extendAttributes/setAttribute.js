51cda97e4c63f0cbfc65de84c9107e09c82e701a
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-06 19:13:38 -0300

    Refatoracao, inclsaao da boa praticas que eu acreto que seja otima

--- src/h/extendAttributes/setAttribute.js
+++ src/h/extendAttributes/setAttribute.js
@@ -1,8 +1,16 @@
-import curry from 'ramda/src/curry'
+import * as f from '@f'
 
-const setAttribute = (element, key, value) => {
-  element.setAttribute(key, value)
-  return element
-}
-
-export default curry(setAttribute)
+/**
+ * Adiciona um atributo no elemento
+ *
+ * @name setAttribute
+ * @function
+ * @access private
+ * @param {HTMLElement} element Elemento html alvo
+ * @param {String} key Nome da atributo
+ * @param {String} value Valor do atributo
+ * @returns {HTMLElement} O elemento alvo com o novo atributo
+ */
+export default (element) =>
+  (key, value) =>
+    f.always(element)(element.setAttribute(key, value))
