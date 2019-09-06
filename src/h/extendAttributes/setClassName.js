2063c24527a7e04d130db6d176f2873353384568
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-06 18:39:05 -0300

    Deixando a funcao de mais legivel

--- src/h/extendAttributes/setClassName.js
+++ src/h/extendAttributes/setClassName.js
@@ -1,6 +1,13 @@
-import curry from 'ramda/src/curry'
-
-const setClassName = (element, className, target) =>
-  (element[className] = [].concat(target).join(' '))
-
-export default curry(setClassName)
+/**
+ * Adiciona a(s) classe(s) css no element
+ *
+ * @name setClassName
+ * @function
+ * @access private
+ * @param {HTMLElement} element Elemento html que tera a(s) classe css adicionado
+ * @param {String|Array} className Nome de uma classe ou matrix de nomes de classes css
+ * @returns {HTMLElement} Element html com as class css adicionado
+ */
+export default (element) =>
+  (_, className) =>
+    (element.className = [].concat(className).join(' '))
