aabc99a2508fbf305a4243883b115b341d247b47
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-06 19:29:53 -0300

    firebase deploy

---
+++ public/precache-manifest.4528355acb6e0736e420d0026cd5c446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67fc4d8c5c503dabbbbf844e3045466",
+    "url": "/../node_modules/.cache/bundle-stats/baseline.json"
+  },
+  {
+    "url": "/3b09e82c36058ff0664dc9426ab3d6b3.png"
+  },
+  {
+    "url": "/app.f464975500e2a0f26c56.js"
+  },
+  {
+    "revision": "530628c54d309d9a8a285f8bdf039c13",
+    "url": "/bundle-stats.html"
+  },
+  {
+    "revision": "3b09e82c36058ff0664dc9426ab3d6b3",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "aecb2482ef0a132ca6f2a55fddd8a34e",
+    "url": "/index.html"
+  },
+  {
+    "url": "/runtime.f464975500e2a0f26c56.js"
+  },
+  {
+    "url": "/vendors.f464975500e2a0f26c56.js"
+  }
+]);
