3304fbde8f1765dedc8ce1617aef25ad9d51108f
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-07 20:11:00 -0300

    A pagina home nao tem mais css

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,14 +1,9 @@
 import h from '@h'
-import RexJS from '../favicon.png'
-import style from './style.css'
+import Header from '@/header'
 
 const component = () =>
-  <main className={style.home} itemScope itemType='https://schema.org/Organization'>
-    <a className={style.home__link} href='/' itemProp='url'>
-      <img className={style.home__logo} src={RexJS} alt='Rex.JS' itemProp='logo' />
-      <h1 itemProp='name'>Rex.JS</h1>
-      <p itemProp='description'>Simples, pequeno e imperfeito. Uma visão além da programação</p>
-    </a>
+  <main>
+    <Header />
   </main>
 
 export default component
