67ea9e6a9cec2e87bff503a4580c0265b670a66c
Cleber Goncalves <cleber.goncalves@cea.com.br>
2019-09-10 15:27:23 -0300

    havalia se o elemento é um component

---
+++ src/h/isComponent.js
@@ -0,0 +1,12 @@
+import * as f from '@f'
+
+/**
+ * Havalia se o elemento eh um component
+ *
+ * @name isComponent
+ * @function
+ * @access private
+ * @param {*} element Elemento ha ser havaliado
+ * @returns {Boolean} Verdadeiro se o elemento eh um component
+ */
+export default f.is(Function, f.__)
