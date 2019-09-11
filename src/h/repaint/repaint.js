2a93c269bc0b6fda1d246cf465bfbd62d89832ae
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-11 18:45:03 -0300

    Documentacao e refatoracao com a utilizacao do @f

--- src/h/repaint/repaint.js
+++ src/h/repaint/repaint.js
@@ -1,11 +1,22 @@
-export default (target, _, descriptor) => {
-  const originalMethod = descriptor.value
+import * as f from '@f'
 
-  Object.assign(descriptor, {
+/**
+ * Dispara o processo repaint do component
+ *
+ * @name default
+ * @function
+ * @access public
+ * @args {Class} _target Classe que tera seu metodo encapsulado
+ * @args {String} _method Nome do metodo encapsulado
+ * @args {Object} descriptor Descricao da definicao do metodo
+ * @returns {*} O retorno do metodo encapsulado
+ */
+export default (_target, _method, descriptor) => {
+  const { value } = descriptor
+
+  f.assign(descriptor, {
     value (...args) {
-      const result = originalMethod.call(this, ...args)
-      this.__reflow__ && this.__reflow__()
-      return result
+      return f.always(value.call(this, ...args))(this.__reflow__ && this.__reflow__())
     }
   })
 
