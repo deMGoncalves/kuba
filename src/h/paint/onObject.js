fba76073a53ac4755f8ea3e04990401094217058
Cleber Goncalves <cleber.goncalves@cea.com.br>
2019-09-13 10:35:50 -0300

    removendo as dependencias do ramda e documentacao

--- src/h/paint/onObject.js
+++ src/h/paint/onObject.js
@@ -1,19 +1,35 @@
-import is from 'ramda/src/is'
-import prop from 'ramda/src/prop'
-import proxyOverComponent from './onComponent'
+import * as f from '@f'
+import onComponent from './onComponent'
 
-export default (target, Component) => {
-  return new Proxy(
-    (_, children) =>
-      proxyOverComponent(target, Component, children),
+/**
+ * Sera feito um wrapper sobro a instancia da classe para podermos fazer
+ * as tratativas do get, set e para quando usar a instancia como component
+ *
+ * @name onObject
+ * @function
+ * @access private
+ * @param {Object} context Instancia da classe que foi encapsulado
+ * @param {Function} component Component representativo da classe
+ * @returns {Proxy} Um proxy que fara a orquestracao para um funcionamento hibrido
+ */
+export default (context, component) =>
+  new Proxy(
+
+    /**
+     * Sera entregue uma funcao ao invez do contexto, com isso posso
+     * fazer a renderizacao do component quando esta instancia for colocado
+     * no DOM
+     */
+    (_, children) => onComponent(context, component, children),
+
+    /**
+     * Remap das keys para o contexto, senao o get sera feito sobre
+     * wrapper do component
+     */
     {
-      get (_, property) {
-        const propertyOrMethod = prop(property, target)
-
-        return is(Function, propertyOrMethod)
-          ? propertyOrMethod.bind(target)
-          : propertyOrMethod
+      get (_, key) {
+        const target = context[key]
+        return f.is(Function, target) ? target.bind(context) : target
       }
     }
   )
-}
