93210cb0483e3e0fc153f27d1b1e4883caa8bf75
Cleber Goncalves <cleber.goncalves@cea.com.br>
2019-09-16 12:40:46 -0300

    Documentacao completa do repaint

--- src/h/paint/onComponent.js
+++ src/h/paint/onComponent.js
@@ -3,6 +3,18 @@ import createHookReflow from './createHookReflow'
 import createVElementToCompare from './createVElementToCompare'
 import hasElementInTarget from './hasElementInTarget'
 
+/**
+ * Se o contexto tiver um elemento sera criado apenas outro vDom
+ * para fins de comparacao, caso trontario sera criado o primeiro elemento
+ *
+ * @name onComponent
+ * @function
+ * @access private
+ * @param {Object} context Instancia da classe para encapsulado pelo component
+ * @param {Function} component Web component
+ * @param {Array} children Elemento filhos que seram incluidos no web component
+ * @return {HTMLElement} Elemento html quer fora gerado pelo component
+ */
 export default f.cond(
   [hasElementInTarget, createVElementToCompare],
   [f.T, createHookReflow]
