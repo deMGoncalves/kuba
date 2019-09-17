7428abe0f4f731dbea7a339f685f753dee8c465d
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-17 07:01:31 -0300

    Documentacao e refatoracao, implementacao do isNil

--- src/h/reflow/notHasElement.js
+++ src/h/reflow/notHasElement.js
@@ -1,4 +1,13 @@
-import not from 'ramda/src/not'
+import * as f from '@f'
 
+/**
+ * Valida a existencia do elemento real
+ *
+ * @name noHasElement
+ * @function
+ * @access private
+ * @param {HTMLElement} element Elmento real que esta no DOM
+ * @return {Boolean} Verdadeiro se o elemento real existir
+ */
 export default (element) =>
-  not(!!element)
+  f.isNil(element)
