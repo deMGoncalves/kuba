3937a057f36ac04f002f98d28c9c27f37868843f
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-18 17:54:40 -0300

    inclusao da funcao de remover atributos

--- src/h/reflow/restrictAttributes.js
+++ src/h/reflow/restrictAttributes.js
@@ -1,9 +1,10 @@
 import removeEventListener from './removeEventListener'
+import removeAttributes from './removeAttributes'
 import addEventListener from './addEventListener'
 import setAttribute from './setAttribute'
 
 export default (element, vElement) => {
-  removeEventListener(element)
+  removeAttributes(removeEventListener(element))
   addEventListener(element, vElement)
   setAttribute(element, vElement)
   return element
