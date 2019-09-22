43a991031d7b6a24fbfebefe9a950f198c56a808
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-22 00:24:11 -0300

    Deve retornar undefined na segunda execucao

--- src/f/__tests__/always.spec.js
+++ src/f/__tests__/always.spec.js
@@ -1,13 +1,18 @@
 import always from '../always'
 
 describe('always', () => {
-  test('Deve retorna uma funcao que sempre ao ser executado ira devolver o parametro da primeira execucao', () => {
+  test('Deve retorna 3.14 na segunda execucao', () => {
     const pi = always(3.14)
     expect(pi()).toBe(3.14)
   })
 
-  test('Deve retornar o parametro da primeira chamada mesmo passando um novo parametro', () => {
+  test('Deve retornar 3.14 na segunda exeucao mesmo passando um novo parametro', () => {
     const pi = always(3.14)
     expect(pi(4.13)).toBe(3.14)
+  })
+
+  test('Deve retornar undefined na segunca execucao', () => {
+    const pi = always()
+    expect(pi()).toBeUndefined()
   })
 })
