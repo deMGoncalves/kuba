f4bb31d883467421ba18f1ac7de5bb87c5ed035c
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-22 00:40:57 -0300

    Deve retornar falso quando o primeiro parametro for falso

--- src/f/__tests__/and.spec.js
+++ src/f/__tests__/and.spec.js
@@ -1,7 +1,11 @@
 import and from '../and'
 
 describe('and', () => {
-  test('Deve retornar verdadeiro quando os dois parametros forem verdadeiros', () => {
+  test('Deve retornar verdadeiro apenas quando os dois parametros forem verdadeiros', () => {
     expect(and(true, true)).toBeTruthy()
+  })
+
+  test('Deve retornar falso quando primeiro parametro for falso', () => {
+    expect(and(false, true)).toBeFalsy()
   })
 })
