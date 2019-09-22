25a25f3d5c130d2f44ffcd157671738328770176
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-22 00:18:44 -0300

    Deve retornar o parametro da primeira chamada mesmo passando um novo parametro

--- src/f/__tests__/always.spec.js
+++ src/f/__tests__/always.spec.js
@@ -1,8 +1,13 @@
 import always from '../always'
 
 describe('always', () => {
-  test('Deve retorna uma funcao que ao ser executado retorna o valor fornecido na primeira chamada', () => {
+  test('Deve retorna uma funcao que sempre ao ser executado ira devolver o parametro da primeira execucao', () => {
     const pi = always(3.14)
     expect(pi()).toBe(3.14)
+  })
+
+  test('Deve retornar o parametro da primeira chamada mesmo passando um novo parametro', () => {
+    const pi = always(3.14)
+    expect(pi(4.13)).toBe(3.14)
   })
 })
