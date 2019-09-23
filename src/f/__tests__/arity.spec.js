fb7bce80bc6a3a3d0f90db0c5b25b1b28c665199
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-23 06:34:59 -0300

    Deve retornar NaN quanto executar a funcao add passando os parametros 1, 2

--- src/f/__tests__/arity.spec.js
+++ src/f/__tests__/arity.spec.js
@@ -1,8 +1,17 @@
 import arity from '../arity'
-import and from '../and'
+
+var add = (x, y) => x + y
+
+beforeEach(() => {
+  add = arity(1, add)
+})
 
 describe('arity', () => {
   test('Deve redefinir de 2 para 1 parametro a funcao and', () => {
-    expect(arity(1, and)).toHaveLength(1)
+    expect(add).toHaveLength(1)
+  })
+
+  test('Deve retornar NaN quando executar a funcao add passando os parametros 1, 2', () => {
+    expect(add(1, 2)).toBeNaN()
   })
 })
