4538580a58ebf24f633ebffb149e0beb72074fc1
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-26 18:09:04 -0300

    Deve retornar o erro "Nao pode alterar o nome para Lupy.JS" quando alterar a propriedade nome

--- src/h/paint/__tests__/paint.spec.js
+++ src/h/paint/__tests__/paint.spec.js
@@ -4,4 +4,10 @@ describe('paint', () => {
   test('Deve retornar "Rex.JS" quando acessar a propriedade nome', () => {
     expect(Rex.nome).toBe('Rex.JS')
   })
+
+  test('Deve retornar o erro "Nao pode alterar o nome para Lupy.JS" quando alterar a propriedade nome', () => {
+    const expression = () => (Rex.nome = 'Lupy.JS')
+    const error = new Error('Nao pode alterar o nome para Lupy.JS')
+    expect(expression).toThrowError(error)
+  })
 })
