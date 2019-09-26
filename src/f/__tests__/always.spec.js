92b6883c1881800f3bc5fb6e47632711397e16e0
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-26 18:21:28 -0300

    Deve retornar 3.14 na segunda execucao

--- src/f/__tests__/always.spec.js
+++ src/f/__tests__/always.spec.js
@@ -1,7 +1,7 @@
 import always from '../always'
 
 describe('always', () => {
-  test('Deve retorna 3.14 na segunda execucao', () => {
+  test('Deve retornar 3.14 na segunda execucao', () => {
     const pi = always(3.14)
     expect(pi()).toBe(3.14)
   })
