4addaf2d796e74da838631fa0997fec7888e59fa
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-09-27 06:51:06 -0300

    deve retornar a instancia de rex quando executar a instancia com o operador new

--- src/h/paint/__tests__/paint.spec.js
+++ src/h/paint/__tests__/paint.spec.js
@@ -1,6 +1,17 @@
+import onObject from '../onObject'
+import onComponent from '../onComponent'
+import component from './component'
 import Rex from './Rex'
 
+jest.mock('../onObject.js')
+jest.mock('../onComponent.js')
+
 describe('paint', () => {
+  beforeEach(() => {
+    onObject.mockReset()
+    onComponent.mockReset()
+  })
+
   test('Deve retornar "Rex.JS" quando acessar a propriedade nome', () => {
     expect(Rex.nome).toBe('Rex.JS')
   })
@@ -13,5 +24,15 @@ describe('paint', () => {
 
   test('Deve cantar NaNNaNNaNNaNNaN Batman! quando executar o metodo cante', () => {
     expect(Rex.cante()).toBe('NaNNaNNaNNaNNaN Batman!')
+  })
+
+  test('Deve retornar a instancia de Rex quando executar a instrucao com o operador new', () => {
+    onObject.mockReturnValue({})
+
+    expect(new Rex()).toEqual({})
+
+    expect(onObject).toHaveBeenCalled()
+    expect(onObject).toHaveBeenCalledTimes(1)
+    expect(onObject).toHaveBeenCalledWith({}, component)
   })
 })
