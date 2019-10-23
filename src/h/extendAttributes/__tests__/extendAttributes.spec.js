927f426a01373b82fef93e3df06f9dc34e1ec166
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-10-23 18:15:53 -0300

    Deve retornar uma colecao de chave e valor de todos os atributos

--- src/h/extendAttributes/__tests__/extendAttributes.spec.js
+++ src/h/extendAttributes/__tests__/extendAttributes.spec.js
@@ -2,10 +2,14 @@ import * as f from '@f'
 import extendAttributes from '../extendAttributes'
 
 jest.mock('../../../f/always.js')
+jest.mock('../../../f/forEach.js')
+jest.mock('../../../f/toPairs.js')
 
 describe('extendAttributes', () => {
   beforeEach(() => {
     f.always.mockReset()
+    f.forEach.mockReset()
+    f.toPairs.mockReset()
   })
 
   test('Deve retornar o mesmo elemento depois que executar o extendAttributes', () => {
@@ -20,5 +24,25 @@ describe('extendAttributes', () => {
     expect(f.always).toHaveBeenCalled()
     expect(f.always).toHaveBeenCalledTimes(1)
     expect(f.always).toHaveBeenCalledWith(element)
+  })
+
+  test('Deve retornar uma colecao de chave e valor de todos os atributos', () => {
+    const attributes = { id: 'rex-js' }
+    const element = document.createElement('div')
+    const always = () => element
+    const toPairs = ['id', 'rex-js']
+
+    f.always.mockReturnValue(always)
+    f.toPairs.mockReturnValue(toPairs)
+
+    expect(extendAttributes(attributes, element)).toEqual(element)
+
+    expect(f.always).toHaveBeenCalled()
+    expect(f.always).toHaveBeenCalledTimes(1)
+    expect(f.always).toHaveBeenCalledWith(element)
+
+    expect(f.toPairs).toHaveBeenCalled()
+    expect(f.toPairs).toHaveBeenCalledTimes(1)
+    expect(f.toPairs).toHaveBeenCalledWith(attributes)
   })
 })
