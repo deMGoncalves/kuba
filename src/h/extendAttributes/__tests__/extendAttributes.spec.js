2ad71883519cd9cf1b052e2d6bf3708c77fd533b
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-10-23 17:54:51 -0300

    Deve retornar o mesmo elemento depois que executar o extendAttributes

--- src/h/extendAttributes/__tests__/extendAttributes.spec.js
+++ src/h/extendAttributes/__tests__/extendAttributes.spec.js
@@ -1,5 +1,24 @@
+import * as f from '@f'
+import extendAttributes from '../extendAttributes'
+
+jest.mock('../../../f/always.js')
+
 describe('extendAttributes', () => {
-  test('extendAttributes', () => {
-    expect(true).toBeTruthy()
+  beforeEach(() => {
+    f.always.mockReset()
+  })
+
+  test('Deve retornar o mesmo elemento depois que executar o extendAttributes', () => {
+    const attributes = {}
+    const element = document.createElement('div')
+    const always = () => element
+
+    f.always.mockReturnValue(always)
+
+    expect(extendAttributes(attributes, element)).toEqual(element)
+
+    expect(f.always).toHaveBeenCalled()
+    expect(f.always).toHaveBeenCalledTimes(1)
+    expect(f.always).toHaveBeenCalledWith(element)
   })
 })
