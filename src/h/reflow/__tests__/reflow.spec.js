1603e02edda023587269450f5c5937f0d02028a3
Cleber de Moraes Gonçalves <cleber.engineer@hotmail.com>
2019-10-24 06:57:09 -0300

    Deve fazer nada quando o elemento e vElemento nao existir

--- src/h/reflow/__tests__/reflow.spec.js
+++ src/h/reflow/__tests__/reflow.spec.js
@@ -1,5 +1,44 @@
+import * as f from '@f'
+import appendChild from '../appendChild'
+import elementsAreDifferent from '../elementsAreDifferent'
+import elementsIsTextNode from '../elementsIsTextNode'
+import doNothing from '../doNothing'
+import notHasElement from '../notHasElement'
+import notHasElementAndVElement from '../notHasElementAndVElement'
+import notHasVElement from '../notHasVElement'
+import otherObject from '../otherObject'
+import reflow from '../reflow'
+import remove from '../remove'
+import replaceChild from '../replaceChild'
+import replaceComponent from '../replaceComponent'
+import replaceElement from '../replaceElement'
+import sameObject from '../sameObject'
+import setTextContent from '../setTextContent'
+
+jest.mock('../doNothing.js')
+jest.mock('../notHasElementAndVElement.js')
+
 describe('reflow', () => {
-  test('reflow', () => {
-    expect(true).toBeTruthy()
+  beforeEach(() => {
+    doNothing.mockReset()
+    notHasElementAndVElement.mockReset()
+  })
+
+  test('Deve fazer nada quando o element e vElement nao existir', () => {
+    const element = undefined
+    const vElement = undefined
+
+    doNothing.mockReturnValue(null)
+    notHasElementAndVElement.mockReturnValue(true)
+
+    expect(reflow(element, vElement)).toBe(null)
+
+    expect(notHasElementAndVElement).toHaveBeenCalled()
+    expect(notHasElementAndVElement).toHaveBeenCalledTimes(1)
+    expect(notHasElementAndVElement).toHaveBeenCalledWith(element, vElement)
+
+    expect(doNothing).toHaveBeenCalled()
+    expect(doNothing).toHaveBeenCalledTimes(1)
+    expect(doNothing).toHaveBeenCalledWith(element, vElement)
   })
 })
