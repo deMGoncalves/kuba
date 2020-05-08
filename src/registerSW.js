d9a6756f9599bfb25e7bce6df20629a045ddf91b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-08 09:38:23 -0300

    ajuste do service worker

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,11 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-        console.log('SW registered: ', registration)
-      }).catch((registrationError) => {
-        console.log('SW registration failed: ', registrationError)
-      })
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((registration) => console.log('SW registered: ', registration))
+        .catch((registrationError) => console.log('SW registration failed: ', registrationError))
     })
   }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,12 @@
 const common = require('./webpack.common.js')
 const merge = require('webpack-merge')
-const WorkboxPlugin = require('workbox-webpack-plugin')
+
+const { GenerateSW } = require('workbox-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new WorkboxPlugin.GenerateSW({
+    new GenerateSW({
       clientsClaim: true,
       skipWaiting: true
     })
