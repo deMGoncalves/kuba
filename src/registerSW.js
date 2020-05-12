744375b3d36402dc64887cc7b0036b2a73da2d34
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-12 14:30:02 -0300

    ajuste no stilo do codigo

--- src/registerSW.js
+++ src/registerSW.js
@@ -1,9 +1,10 @@
 if (process.env.NODE_ENV === 'production') {
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
+    window.addEventListener('load', () =>
+      navigator
+        .serviceWorker
+        .register('/service-worker.js')
         .then((registration) => console.log('SW registered: ', registration))
-        .catch((registrationError) => console.log('SW registration failed: ', registrationError))
-    })
+        .catch((registrationError) => console.log('SW registration failed: ', registrationError)))
   }
 }
