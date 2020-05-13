1e22ad9661e19348fd21a4bf18846154ac7a7659
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-13 10:40:19 -0300

    configuracao o mini extract css

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: {
@@ -17,6 +18,10 @@ module.exports = {
     ]),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'template/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css'
     })
   ],
   module: {
@@ -34,7 +39,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: true,
+              hmr: process.env.NODE_ENV === 'development',
+              reloadAll: true
+            },
+          },
           {
             loader: 'css-loader',
             options: {
