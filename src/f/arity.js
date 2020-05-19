d0d4f24667d2fe7aced7b3ad85deeee9de593f60
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-19 11:33:40 -0300

    devemos usar a sintaxe da funcao classica para eu poder ter acesso ao arguments

--- src/f/arity.js
+++ src/f/arity.js
@@ -13,9 +13,9 @@ import curry from './curry'
 const arity = (n, target) => {
   switch (n) {
     case 0: return () => target()
-    case 1: return (a0) => target(a0)
-    case 2: return (a0, a1) => target(a0, a1)
-    case 3: return (a0, a1, a2) => target(a0, a1, a2)
+    case 1: return function (_a0) { return target(...arguments) }
+    case 2: return function (_a0, _a1) { return target(...arguments) }
+    case 3: return function (_a0, _a1, _a2) { return target(...arguments) }
     default: throw new Error('O primeiro argumento para arity deve ser um número inteiro não negativo e não superior a três')
   }
 }
