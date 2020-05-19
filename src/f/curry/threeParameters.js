0106bde0423589d252179b94a6058ed3041823c3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-19 11:38:20 -0300

    a funcao de retorno precisa ter um nome (f3) para ser retornado quando os parametros forem um gap e devemos executar o target com call para manter o contexto da funcao

--- src/f/curry/threeParameters.js
+++ src/f/curry/threeParameters.js
@@ -11,37 +11,37 @@ import { isGap } from '../gap'
  * @return {Function} A função curried.
  */
 export default (target) =>
-  function (a0, a1, a2) {
+  function f3 (a0, a1, a2) {
     switch (arguments.length) {
       case 0:
-        return target
+        return f3
       case 1:
         return isGap(a0)
-          ? target
-          : twoParameters((_a1, _a2) => target(a0, _a1, _a2))
+          ? f3
+          : twoParameters((...args) => target.call(this, a0, ...args))
       case 2:
         return isGap(a0) && isGap(a1)
-          ? target
+          ? f3
           : isGap(a0)
-            ? twoParameters((_a0, _a2) => target(_a0, a1, _a2))
+            ? twoParameters((_a0, ...args) => target.call(this, _a0, a1, ...args))
             : isGap(a1)
-              ? twoParameters((_a1, _a2) => target(a0, _a1, _a2))
-              : oneParameter((_a2) => target(a0, a1, _a2))
+              ? twoParameters((...args) => target.call(this, a0, ...args))
+              : oneParameter((...args) => target.call(this, a0, a1, ...args))
       default:
         return isGap(a0) && isGap(a1) && isGap(a2)
-          ? target
+          ? f3
           : isGap(a0) && isGap(a1)
-            ? twoParameters((_a0, _a1) => target(_a0, _a1, a2))
+            ? twoParameters((_a0, _a1, ...args) => target.call(this, _a0, _a1, a2, ...args))
             : isGap(a0) && isGap(a2)
-              ? twoParameters((_a0, _a2) => target(_a0, a1, _a2))
+              ? twoParameters((_a0, ...args) => target.call(this, _a0, a1, ...args))
               : isGap(a1) && isGap(a2)
-                ? twoParameters((_a1, _a2) => target(a0, _a1, _a2))
+                ? twoParameters((...args) => target.call(this, a0, ...args))
                 : isGap(a0)
-                  ? oneParameter((_a0) => target(_a0, a1, a2))
+                  ? oneParameter((_a0, ...args) => target.call(this, _a0, a1, a2, ...args))
                   : isGap(a1)
-                    ? oneParameter((_a1) => target(a0, _a1, a2))
+                    ? oneParameter((_a1, ...args) => target.call(this, a0, _a1, a2, ...args))
                     : isGap(a2)
-                      ? oneParameter((_a2) => target(a0, a1, _a2))
-                      : target(a0, a1, a2)
+                      ? oneParameter((...args) => target.call(this, a0, a1, ...args))
+                      : target.call(this, ...arguments)
     }
   }
