5bf15be936f4f2cd5b120e293c5d36d11be6c9b4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-22 19:53:20 -0300

    teste do meu workflow

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import router from '@router'
 import '@sw'
 import './reset.css'
 
+document.body.appendChild(<h1>Venus</h1>)
+
 router(/\/calculadora/, async () => {
   const { default: Calculadora } = await import(/* webpackChunkName: "calculadora" */ './calculadora')
   document.body.appendChild(<Calculadora />)
