cfd8df99b564ef19eaa290e00d0ae71fbc8a7f73
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-23 10:37:27 -0300

    contrucao do footer

---
+++ src/footer/component.jsx
@@ -0,0 +1,33 @@
+export default () =>
+  <footer>
+    <nav>
+      <ul>
+        <li>
+          Mundo JRMod
+          <ul>
+            <li>Novidades</li>
+            <li>Curiosidades</li>
+            <li>Dicas</li>
+            <li>Certificado e Garantia JRMod</li>
+          </ul>
+        </li>
+        <li>
+          Chutando Latas
+          <ul>
+            <li>Quem Somos</li>
+            <li>Depoimentos</li>
+            <li>O que fazemos?</li>
+          </ul>
+        </li>
+        <li>
+          Parceiros e Representantes
+          <ul>
+            <li>Monte Allums</li>
+            <li>General Guitar Gadgets</li>
+            <li>Guitar Tone</li>
+          </ul>
+        </li>
+        <li>Contato</li>
+      </ul>
+    </nav>
+  </footer>
