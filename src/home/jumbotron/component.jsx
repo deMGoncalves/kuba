48c83e4d977ad65edc609efd42e7609fc7b13c3f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-24 20:38:53 -0300

    ajuste nos elementos do bloco

--- src/home/jumbotron/component.jsx
+++ src/home/jumbotron/component.jsx
@@ -5,12 +5,12 @@ export default () =>
   <section className={style.jumbotron}>
     <div className={style.jumbotron__container}>
       <picture className={style.jumbotron__picture}>
-        <source srcSet='https://dummyimage.com/767x400/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
-        <source srcSet='https://dummyimage.com/959x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
-        <source srcSet='https://dummyimage.com/1200x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
+        <source srcSet='https://dummyimage.com/767x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
+        <source srcSet='https://dummyimage.com/959x500/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
+        <source srcSet='https://dummyimage.com/1200x500/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
         <img className={style.jumbotron__img} />
       </picture>
-      <h1 className={style.jumbotron__title}>Aqui você encontra timbres e soluções</h1>
-      <a className={style.jumbotron__link} href='#'>saiba mais</a>
+      <h1 className={style.jumbotron__h1}>Aqui você encontra timbres e soluções</h1>
+      <a className={style.jumbotron__a} href='#'>saiba mais</a>
     </div>
   </section>
--- src/main/header/nav/component.jsx
+++ src/main/header/nav/component.jsx
@@ -3,8 +3,8 @@ import style from './style.css'
 
 export default (attr) =>
   <nav className={[style.nav, attr.className]}>
-    <a className={style.nav__link} href='./efeitos'>Efeitos</a>
-    <a className={style.nav__link} href='./pedal-custom'>Pedal Custom</a>
-    <a className={style.nav__link} href='./mod-kits'>Mod Kits</a>
-    <a className={style.nav__link} href='./souvenier'>Souvenier</a>
+    <a className={style.nav__a} href='./efeitos'>Efeitos</a>
+    <a className={style.nav__a} href='./pedal-custom'>Pedal Custom</a>
+    <a className={style.nav__a} href='./mod-kits'>Mod Kits</a>
+    <a className={style.nav__a} href='./souvenier'>Souvenier</a>
   </nav>
