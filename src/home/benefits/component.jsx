97dfaf6c24365abc0e65c67c18b2792ca91d47a3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-24 20:39:42 -0300

    contrucao do beneficios

---
+++ src/home/benefits/component.jsx
@@ -0,0 +1,9 @@
+import h from '@h'
+import style from 'style.css'
+
+export default () =>
+  <section className={style.benefits}>
+    <div className={style.benefits__container}>
+      beneficios
+    </div>
+  </section>
