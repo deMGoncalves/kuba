2e9c1b8dcefde4d757bbde8393fea0a5ce9add9b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-24 21:23:03 -0300

    ajuste do alinhamento do beneficios

--- src/home/benefits/component.jsx
+++ src/home/benefits/component.jsx
@@ -1,9 +1,26 @@
 import h from '@h'
-import style from 'style.css'
+import style from './style.css'
 
 export default () =>
   <section className={style.benefits}>
     <div className={style.benefits__container}>
-      beneficios
+      <ul className={style.benefits__ul}>
+        <li className={style.benefits__li}>
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <a href='#' className={style.benefits__a}>Atualizações gratuitas disponíveis para nossas mods “Serie A”</a>
+        </li>
+        <li className={style.benefits__li}>
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <a href='#' className={style.benefits__a}>Representante Exclusivo Oficial General Guitar Gadgets – America do Sul</a>
+        </li>
+        <li className={style.benefits__li}>
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <a href='#' className={style.benefits__a}>Representante e Instalador Oficial dos produtos Monte Allums no Brasil</a>
+        </li>
+        <li className={style.benefits__li}>
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <a href='#' className={style.benefits__a}>“Não instala” oficialmente modificações de livros Públicos (WAMPLER) ou Internet</a>
+        </li>
+      </ul>
     </div>
   </section>
--- src/home/component.jsx
+++ src/home/component.jsx
@@ -1,8 +1,10 @@
 import h from '@h'
+import Benefits from './benefits'
 import Jumbotron from './jumbotron'
 import Main from '@main'
 
 export default () =>
   <Main>
     <Jumbotron />
+    <Benefits />
   </Main>
