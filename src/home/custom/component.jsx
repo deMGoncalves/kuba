bba050a30ea3ef54f19ababe8910b51ba0c78210
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-25 19:03:36 -0300

    configuracao do modulo de pedais custom

--- src/home/custom/component.jsx
+++ src/home/custom/component.jsx
@@ -7,9 +7,9 @@ export default () =>
     <s.Container>
       <h2 className={style.custom__h2}>Pedais Custom</h2>
       <picture className={style.custom__picture}>
-        <source srcSet='' media='(max-width: 767px)' />
-        <source srcSet='' media='(min-width: 768px) and (max-width: 959px)' />
-        <source srcSet='' media='(min-width: 960px)' />
+        <source srcSet='https://dummyimage.com/767x400/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
+        <source srcSet='https://dummyimage.com/959x300/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
+        <source srcSet='https://dummyimage.com/1200x200/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
         <img className={style.custom__img} alt='custom' />
       </picture>
       <a className={style.custom__a} href='#'>ver mais</a>
