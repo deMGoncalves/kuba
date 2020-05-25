04dc8e54165e162e6c1c79ce02eae3908d7cdfde
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-25 19:14:27 -0300

    ajuste no texto pedido pelo nick

--- src/home/benefits/component.jsx
+++ src/home/benefits/component.jsx
@@ -8,19 +8,19 @@ export default () =>
       <ul className={style.benefits__ul}>
         <li className={style.benefits__li}>
           <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
-          <a href='#' className={style.benefits__a}>Atualização e Garantia vitalícia dos pedais da Série A</a>
+          <a href='#' className={style.benefits__a}>atualização e Garantia vitalícia dos pedais da série A</a>
         </li>
         <li className={style.benefits__li}>
           <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
-          <a href='#' className={style.benefits__a}>Pedais absolutamente fiéis aos seus propósitos e cumprem 100% de suas propostas</a>
+          <a href='#' className={style.benefits__a}>pedais absolutamente fiéis e cumprindo com 100% de efetividade o seu papel</a>
         </li>
         <li className={style.benefits__li}>
           <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
-          <a href='#' className={style.benefits__a}>Parceria com os grandes representantes globais em Modificações/Customizações de pedais</a>
+          <a href='#' className={style.benefits__a}>parceria com os grandes representantes globais em modificações/customizações de pedais</a>
         </li>
         <li className={style.benefits__li}>
           <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
-          <a href='#' className={style.benefits__a}>Produto confeccionado com primor e coração ou paixão ou outra qualidade profunda</a>
+          <a href='#' className={style.benefits__a}>produto confeccionado com primor e coração ou paixão ou outra qualidade profunda</a>
         </li>
       </ul>
     </s.Container>
