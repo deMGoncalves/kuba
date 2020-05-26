eabef509966ee6a648076aef9591b5dde030b25c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-26 19:30:03 -0300

    estrutura html e css do footer

--- src/main/footer/component.jsx
+++ src/main/footer/component.jsx
@@ -1,35 +1,44 @@
 import h from '@h'
+import * as s from '@share'
+import style from './style.css'
 
 export default () =>
-  <footer>
-    <nav>
-      <ul>
-        <li>
-          Mundo JRMod
-          <ul>
-            <li>Novidades</li>
-            <li>Curiosidades</li>
-            <li>Dicas</li>
-            <li>Certificado e Garantia JRMod</li>
+  <footer className={style.footer}>
+    <s.Container>
+      <nav className={style.footer__nav}>
+        <div className={style.footer__div}>
+          <h3 className={style.footer__h3}>mundo jrmod</h3>
+          <ul className={style.footer__ul}>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Novidades</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Curiosidades</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Dicas</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Certificado e Garantia JRMod</a></li>
           </ul>
-        </li>
-        <li>
-          Chutando Latas
-          <ul>
-            <li>Quem Somos</li>
-            <li>Depoimentos</li>
-            <li>O que fazemos?</li>
+        </div>
+        <div className={style.footer__div}>
+          <h3 className={style.footer__h3}>chutando latas</h3>
+          <ul className={style.footer__ul}>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Quem Somos</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Depoimentos</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>O que fazemos?</a></li>
           </ul>
-        </li>
-        <li>
-          Parceiros e Representantes
-          <ul>
-            <li>Monte Allums</li>
-            <li>General Guitar Gadgets</li>
-            <li>Guitar Tone</li>
+        </div>
+        <div className={style.footer__div}>
+          <h3 className={style.footer__h3}>parceiros e representantes</h3>
+          <ul className={style.footer__ul}>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Monte Allums</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>General Guitar Gadgets</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Guitar Tone</a></li>
           </ul>
-        </li>
-        <li>Contato</li>
-      </ul>
-    </nav>
+        </div>
+        <div className={style.footer__div}>
+          <h3 className={style.footer__h3}>contato</h3>
+          <ul className={style.footer__ul}>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Quem somos</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>Depoimentos</a></li>
+            <li className={style.footer__li}><a className={style.footer__a} href='#'>O que fazemos?</a></li>
+          </ul>
+        </div>
+      </nav>
+    </s.Container>
   </footer>
