f62268ddb28489115f367b5c7b5fae8ff697374a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-27 15:00:41 -0300

    vamos focar na home por hora

--- src/index.jsx
+++ src/index.jsx
@@ -7,13 +7,3 @@ router(/^\/$/, async () => {
   const { default: Home } = await import(/* webpackChunkName: "home" */ './home')
   document.body.appendChild(<Home />)
 })
-
-router(/^\/([\w-]+)$/, async (departamento) => {
-  const { default: Departamento } = await import(/* webpackChunkName: "departamento" */ './departamento')
-  document.body.appendChild(<Departamento {...{ departamento }} />)
-})
-
-router(/^\/([\w-]+)\/([\w-]+)$/, async (departamento, categoria) => {
-  const { default: Categoria } = await import(/* webpackChunkName: "categoria" */ './categoria')
-  document.body.appendChild(<Categoria {...{ departamento, categoria }} />)
-})
