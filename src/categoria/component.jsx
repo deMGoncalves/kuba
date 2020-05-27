2c9fb3a0c3efa9554f6e4e6d39fbda72699c4339
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-27 15:02:13 -0300

    removendo as pagina que nao temos foco hoje

--- src/categoria/component.jsx
+++
@@ -1,9 +0,0 @@
-import h from '@h'
-import Main from '@main'
-
-export default (attr) =>
-  <Main>
-    <section>
-      <h1>{ attr.departamento } / { attr.categoria }</h1>
-    </section>
-  </Main>
--- src/departamento/component.jsx
+++
@@ -1,9 +0,0 @@
-import h from '@h'
-import Main from '@main'
-
-export default (attr) =>
-  <Main>
-    <section>
-      <h1>{ attr.departamento }</h1>
-    </section>
-  </Main>
