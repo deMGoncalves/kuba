406201c442d25136a54368b77473c632250e7de5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-27 16:31:27 -0300

    inclusao da configuracao do sitemap

---
+++ paths.js
@@ -0,0 +1,8 @@
+module.exports = [
+  {
+    path: '/',
+    lastmod: '2020-01-05',
+    priority: '1',
+    changefreq: 'monthly'
+  }
+]
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const common = require('./webpack.common.js')
 const merge = require('webpack-merge')
+const paths = require('./paths')
 
 const { GenerateSW } = require('workbox-webpack-plugin')
+const SitemapPlugin = require('sitemap-webpack-plugin').default
 
 module.exports = merge.smart(common, {
   mode: 'production',
@@ -10,7 +12,8 @@ module.exports = merge.smart(common, {
     new GenerateSW({
       clientsClaim: true,
       skipWaiting: true
-    })
+    }),
+    new SitemapPlugin('https://jrmod.net', paths)
   ],
   optimization: {
     moduleIds: 'hashed',
