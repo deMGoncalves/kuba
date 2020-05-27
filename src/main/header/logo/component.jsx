3b5d16b39f963707db6f99951f388e27c87cb72f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-27 11:07:36 -0300

    implementando carregamento lazy no logo

--- src/main/header/logo/component.jsx
+++ src/main/header/logo/component.jsx
@@ -6,7 +6,7 @@ export default (attr) =>
     <source srcSet='https://dummyimage.com/120x40/1A1A1A/1A1A1A.png' media='(max-width: 469px)' />
     <source srcSet='https://dummyimage.com/60x60/1A1A1A/1A1A1A.png' media='(min-width: 470px) and (max-width: 589px)' />
     <source srcSet='https://dummyimage.com/180x60/1A1A1A/1A1A1A.png' media='(min-width: 590px)' />
-    <img className='logo__img' onClick={() => location.assign(location.origin)} alt='JRMod' itemProp='logo' />
+    <img className='logo__img' loading='lazy' onClick={() => location.assign(location.origin)} alt='JRMod' itemProp='logo' />
     <meta itemProp='url' content='https://www.jrmod.net' />
     <meta itemProp='name' content='JRMod' />
   </picture>
