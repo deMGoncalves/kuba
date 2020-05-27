b1b3fe84bb57fdd16ce86b7186c54f71d679bfcd
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-27 15:54:35 -0300

    construcao da rota para o site map dinamico

--- src/index.jsx
+++ src/index.jsx
@@ -7,3 +7,7 @@ router(/^\/$/, async () => {
   const { default: Home } = await import(/* webpackChunkName: "home" */ './home')
   document.body.appendChild(<Home />)
 })
+
+router(/^\/sitemap$/, async () => {
+  document.body.appendChild(<ul><li><a href='https://jrmod.net'>home</a></li></ul>)
+})
