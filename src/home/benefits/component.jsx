470f6c6336bb65a13da38d5ada39e0b33b2473bb
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-27 11:10:58 -0300

    implementando o carregamento lazy nos icones do modulo de beneficio

--- src/home/benefits/component.jsx
+++ src/home/benefits/component.jsx
@@ -7,19 +7,19 @@ export default () =>
     <s.Container>
       <ul className={style.benefits__ul}>
         <li className={style.benefits__li}>
-          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='benefits' />
           <a href='#' className={style.benefits__a}>atualização e Garantia vitalícia dos pedais da série A</a>
         </li>
         <li className={style.benefits__li}>
-          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='benefits' />
           <a href='#' className={style.benefits__a}>pedais absolutamente fiéis e cumprindo com 100% de efetividade o seu papel</a>
         </li>
         <li className={style.benefits__li}>
-          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='benefits' />
           <a href='#' className={style.benefits__a}>parceria com os grandes representantes globais em modificações/customizações de pedais</a>
         </li>
         <li className={style.benefits__li}>
-          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' alt='benefits' />
+          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='benefits' />
           <a href='#' className={style.benefits__a}>produto confeccionado com primor e coração ou paixão ou outra qualidade profunda</a>
         </li>
       </ul>
