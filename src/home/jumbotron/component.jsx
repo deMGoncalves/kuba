5fde5ff813b7abd1e8b289d6b3eb335b514eff36
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-27 14:47:27 -0300

    ajustando o tamanho do banner para caber o conteudo

--- src/home/jumbotron/component.jsx
+++ src/home/jumbotron/component.jsx
@@ -6,9 +6,9 @@ export default () =>
   <section className={style.jumbotron}>
     <s.Container>
       <picture className={style.jumbotron__picture}>
-        <source srcSet='https://dummyimage.com/767x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
+        <source srcSet='https://dummyimage.com/767x600/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
         <source srcSet='https://dummyimage.com/959x500/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
-        <source srcSet='https://dummyimage.com/1200x500/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
+        <source srcSet='https://dummyimage.com/1200x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
         <img className={style.jumbotron__img} loading='lazy' alt='Aqui você encontra timbres e soluções' />
       </picture>
       <h1 className={style.jumbotron__h1}>aqui você encontra timbres e soluções</h1>
