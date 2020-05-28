cd59ab4490d7c69233485124dfa10d5f507cad88
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-28 16:10:13 -0300

    ajuste na estilizacao do logo do header

--- src/main/header/logo/component.jsx
+++ src/main/header/logo/component.jsx
@@ -3,7 +3,7 @@ import style from './style.css'
 
 export default (attr) =>
   <picture className={[style.logo, attr.className]} itemScope itemType='http://schema.org/Organization'>
-    <source srcSet='https://dummyimage.com/120x40/1A1A1A/1A1A1A.png' media='(max-width: 469px)' />
+    <source srcSet='https://dummyimage.com/120x60/1A1A1A/1A1A1A.png' media='(max-width: 469px)' />
     <source srcSet='https://dummyimage.com/60x60/1A1A1A/1A1A1A.png' media='(min-width: 470px) and (max-width: 589px)' />
     <source srcSet='https://dummyimage.com/180x60/1A1A1A/1A1A1A.png' media='(min-width: 590px)' />
     <img className='logo__img' loading='lazy' onClick={() => location.assign(location.origin)} alt='JRMod' itemProp='logo' />
