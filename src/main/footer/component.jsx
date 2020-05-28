43d0a8de6fb11d88f8b17bb49d1864d9bcde2ef7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-28 15:00:43 -0300

    relayout do footer

--- src/main/footer/component.jsx
+++ src/main/footer/component.jsx
@@ -4,41 +4,32 @@ import style from './style.css'
 
 export default () =>
   <footer className={style.footer}>
-    <s.Container>
-      <nav className={style.footer__nav}>
-        <div className={style.footer__div}>
-          <h3 className={style.footer__h3}>mundo jrmod</h3>
-          <ul className={style.footer__ul}>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Novidades</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Curiosidades</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Dicas</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Certificado e Garantia JRMod</a></li>
-          </ul>
-        </div>
-        <div className={style.footer__div}>
-          <h3 className={style.footer__h3}>chutando latas</h3>
-          <ul className={style.footer__ul}>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Quem Somos</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Depoimentos</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>O que fazemos?</a></li>
-          </ul>
-        </div>
-        <div className={style.footer__div}>
-          <h3 className={style.footer__h3}>parceiros e representantes</h3>
-          <ul className={style.footer__ul}>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Monte Allums</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>General Guitar Gadgets</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Guitar Tone</a></li>
-          </ul>
-        </div>
-        <div className={style.footer__div}>
-          <h3 className={style.footer__h3}>contato</h3>
-          <ul className={style.footer__ul}>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Quem somos</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>Depoimentos</a></li>
-            <li className={style.footer__li}><a className={style.footer__a} href='#'>O que fazemos?</a></li>
-          </ul>
-        </div>
-      </nav>
+    <s.Container className={style.footer__container}>
+      <div className={style.footer__div}>
+        <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='mundo jrmod' />
+        <h3 className={style.footer__h3}>mundo jrmod</h3>
+        <p className={style.footer__p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+        <a className={style.footer__a}>saiba mais</a>
+      </div>
+      <div className={style.footer__div}>
+        <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='chutando latas' />
+        <h3 className={style.footer__h3}>chutando latas</h3>
+        <p className={style.footer__p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+        <a className={style.footer__a}>saiba mais</a>
+      </div>
+      <div className={style.footer__div}>
+        <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='parceiros e representantes' />
+        <h3 className={style.footer__h3}>parceiros</h3>
+        <p className={style.footer__p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+        <a className={style.footer__a}>saiba mais</a>
+      </div>
+      <div className={style.footer__div}>
+        <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='contato' />
+        <h3 className={style.footer__h3}>contato</h3>
+        <p className={style.footer__p}>Rua Ministro Edmundo Lins, 199</p>
+        <p className={style.footer__p}>Bairro Ferreira, SP - Brasil</p>
+        <p className={style.footer__p}>+55 11 3881-9937</p>
+        <p className={style.footer__p}>rossettijrmod@hotmail.com</p>
+      </div>
     </s.Container>
   </footer>
