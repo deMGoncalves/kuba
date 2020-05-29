410c4bdbc30f47b6c40ef62591c8abb0d4da5f91
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-29 14:05:50 -0300

    carregamento do firebase deve ser somento depois do load da pagina

--- src/analytics/register.jsx
+++ src/analytics/register.jsx
@@ -1,17 +1,13 @@
 import config from './config.json'
 
-window.addEventListener('load', () => {
-  const init = { mode: 'no-cors' }
-  const app = fetch('https://www.gstatic.com/firebasejs/7.14.5/firebase-app.js', init).then(response => response.text())
-  const analytics = fetch('https://www.gstatic.com/firebasejs/7.14.5/firebase-analytics.js', init).then(response => response.text())
+window.addEventListener('load', async () => {
+  const header = { mode: 'no-cors' }
+  const app = await fetch('https://www.gstatic.com/firebasejs/7.14.5/firebase-app.js', header).then(response => response.text())
+  const analytics = await fetch('https://www.gstatic.com/firebasejs/7.14.5/firebase-analytics.js', header).then(response => response.text())
 
-  Promise
-    .all(app, analytics)
-    .then(([app, analytics]) => {
-      document.body.appendChild(<script>{ app }</script>)
-      document.body.appendChild(<script>{ analytics }</script>)
+  document.body.appendChild(<script>{ app }</script>)
+  document.body.appendChild(<script>{ analytics }</script>)
 
-      window.firebase.initializeApp(config)
-      window.firebase.analytics()
-    })
+  window.firebase.initializeApp(config)
+  window.firebase.analytics()
 })
