58df27fb01833250f79a2bed0980bffefb06a41f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-29 11:20:43 -0300

    ajuste para o funcionamento do banner com um aspect ratio que nao cubra a arte

--- src/home/custom/component.jsx
+++ src/home/custom/component.jsx
@@ -7,7 +7,7 @@ export default () =>
     <s.Container>
       <h2 className={style.custom__h2}>pedais custom</h2>
       <picture className={style.custom__picture}>
-        <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
+        <source srcSet='https://dummyimage.com/747x400/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
         <source srcSet='https://dummyimage.com/939x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
         <source srcSet='https://dummyimage.com/1200x200/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
         <img className={style.custom__img} loading='lazy' alt='custom' />
