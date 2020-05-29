d5ff5c4a9f45eaca3bc0f26a6894d32b59dd2a71
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-29 20:17:30 -0300

    mudanca de nomenclatura

--- src/main/header/nav/component.jsx
+++ src/main/header/nav/component.jsx
@@ -3,8 +3,8 @@ import style from './style.css'
 
 export default () =>
   <nav className={style.nav}>
-    <a className={style.nav__a} href='./efeitos'>Efeitos</a>
-    <a className={style.nav__a} href='./pedal-custom'>Pedal Custom</a>
-    <a className={style.nav__a} href='./mod-kits'>Mod Kits</a>
-    <a className={style.nav__a} href='./souvenier'>Souvenier</a>
+    <a className={style.nav__a} href='./modificados'>modificados</a>
+    <a className={style.nav__a} href='./custom-shop'>custom shop</a>
+    <a className={style.nav__a} href='./kits'>kits</a>
+    <a className={style.nav__a} href='./souvenier'>souvenier</a>
   </nav>
