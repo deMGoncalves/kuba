4380858061606d9abeb0b10688ede4e6d00d7550
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-29 11:18:32 -0300

    ajuste para o funcionamento do banner com um aspect ratio que nao cubra a arte

--- src/home/jumbotron/component.jsx
+++ src/home/jumbotron/component.jsx
@@ -6,7 +6,8 @@ export default () =>
   <section className={style.jumbotron}>
     <s.Container>
       <picture className={style.jumbotron__picture}>
-        <source srcSet='https://dummyimage.com/747x600/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
+        <source srcSet='https://dummyimage.com/404x500/1A1A1A/1A1A1A.png' media='(max-width: 424px)' />
+        <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(min-width: 425px) and (max-width: 767px)' />
         <source srcSet='https://dummyimage.com/939x500/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
         <source srcSet='https://dummyimage.com/1200x500/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
         <img className={style.jumbotron__img} loading='lazy' alt='Aqui você encontra timbres e soluções' />
