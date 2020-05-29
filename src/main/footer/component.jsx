2cb9f2293254c2890f17ba3cb3a7e4a6ad441681
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-29 16:06:18 -0300

    corrigindo semantica

--- src/main/footer/component.jsx
+++ src/main/footer/component.jsx
@@ -5,25 +5,25 @@ import style from './style.css'
 export default () =>
   <footer className={style.footer}>
     <s.Container className={style.footer__container}>
-      <div className={style.footer__div}>
+      <section className={style.footer__section}>
         <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='mundo jrmod' />
         <h3 className={style.footer__h3}>mundo jrmod</h3>
         <p className={style.footer__p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
         <a className={style.footer__a}>saiba mais</a>
-      </div>
-      <div className={style.footer__div}>
+      </section>
+      <section className={style.footer__section}>
         <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='chutando latas' />
         <h3 className={style.footer__h3}>chutando latas</h3>
         <p className={style.footer__p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
         <a className={style.footer__a}>saiba mais</a>
-      </div>
-      <div className={style.footer__div}>
+      </section>
+      <section className={style.footer__section}>
         <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='parceiros e representantes' />
         <h3 className={style.footer__h3}>parceiros</h3>
         <p className={style.footer__p}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
         <a className={style.footer__a}>saiba mais</a>
-      </div>
-      <address className={style.footer__div} itemProp='address' itemScope itemType='http://schema.org/PostalAddress'>
+      </section>
+      <address className={style.footer__section} itemProp='address' itemScope itemType='http://schema.org/PostalAddress'>
         <img className={style.footer__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='contato' />
         <h3 className={style.footer__h3}>contato</h3>
         <p className={style.footer__p} itemProp='streetAddress'>Rua Ministro Edmundo Lins, 199</p>
