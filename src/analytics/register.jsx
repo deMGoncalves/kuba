8c0445380b56fa3cc4f13940937ec59437318cae
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-29 14:59:33 -0300

    simplificando o script e garantindo que cada elemento sera esecutado no seu tempo

--- src/analytics/register.jsx
+++ src/analytics/register.jsx
@@ -1,9 +1,11 @@
 import h from '@h'
-import * as firebase from 'firebase/app'
 import config from './config.json'
-import 'firebase/analytics'
 
 if (process.env.NODE_ENV === 'production') {
-  firebase.initializeApp(config)
-  firebase.analytics()
+  (async () => {
+    const { default: firebase } = await import(/* webpackChunkName: "firebase" */ 'firebase')
+
+    firebase.initializeApp(config)
+    firebase.analytics()
+  })()
 }
