90397a678a586ad03a78fc570a93acd0f5f91718
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-30 16:19:14 -0300

    banner para o distortion

--- src/home/modificados/component.jsx
+++ src/home/modificados/component.jsx
@@ -6,6 +6,10 @@ import bannerL from './banner_1200x400.jpg'
 import bannerM from './banner_939x400.jpg'
 import bannerS from './banner_747x500.jpg'
 
+import distortionL from './distortion_393x400.jpg'
+import distortionM from './distortion_464x400.jpg'
+import distortionS from './distortion_747x500.jpg'
+
 export default () =>
   <section className={style.modificados}>
     <s.Container className={style.modificados__container}>
@@ -21,9 +25,9 @@ export default () =>
       </div>
       <div className={[style.modificados__div, style.half]}>
         <picture className={[style.modificados__picture, style.half]}>
-          <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
-          <source srcSet='https://dummyimage.com/464.5x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
-          <source srcSet='https://dummyimage.com/393.3x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
+          <source srcSet={distortionS} media='(max-width: 767px)' />
+          <source srcSet={distortionM} media='(min-width: 768px) and (max-width: 959px)' />
+          <source srcSet={distortionL} media='(min-width: 960px)' />
           <img className={style.modificados__img} loading='lazy' alt='distortion / overdrive / fuzz' />
         </picture>
         <s.Link className={style.modificados__a} href='#'>distortion</s.Link>
