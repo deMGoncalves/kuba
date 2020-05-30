f184242e02499f5edfdb737c688f490de63df18a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-30 18:19:41 -0300

    inclusao dos banner para o modulo kit

--- src/home/kits/component.jsx
+++ src/home/kits/component.jsx
@@ -2,15 +2,19 @@ import h from '@h'
 import * as s from '@share'
 import style from './style.css'
 
+import bannerL from './banner_593x400.jpg'
+import bannerM from './banner_939x400.jpg'
+import bannerS from './banner_747x400.jpg'
+
 export default () =>
   <section className={style.kits}>
     <s.Container className={style.kits__container}>
       <h2 className={style.kits__h2}>parceria com os grandes representantes globais</h2>
       <div className={style.kits__div}>
         <picture className={style.kits__picture}>
-          <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
-          <source srcSet='https://dummyimage.com/939x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
-          <source srcSet='https://dummyimage.com/593.4x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
+          <source srcSet={bannerS} media='(max-width: 767px)' />
+          <source srcSet={bannerM} media='(min-width: 768px) and (max-width: 959px)' />
+          <source srcSet={bannerL} media='(min-width: 960px)' />
           <img className={style.kits__img} loading='lazy' alt='mod kits' />
         </picture>
         <s.Link className={style.kits__a} href='#'>ver mais</s.Link>
