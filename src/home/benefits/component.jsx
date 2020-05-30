ea91a161ac711907bcde40abcccc5483681d7621
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-30 10:06:21 -0300

    inclusao do icones

--- src/home/benefits/component.jsx
+++ src/home/benefits/component.jsx
@@ -7,6 +7,10 @@ export default () =>
     <s.Container>
       <ul className={style.benefits__ul}>
         <li className={style.benefits__li}>
+          <s.Icon className={style.benefits__icon}>supervisor_account</s.Icon>
+          <a href='#' className={style.benefits__a}>parceria com os grandes representantes globais em modificações e customizações de pedais</a>
+        </li>
+        <li className={style.benefits__li}>
           <s.Icon className={style.benefits__icon}>admin_panel_settings</s.Icon>
           <a href='#' className={style.benefits__a}>atualização e garantia vitalícia dos pedais da série a</a>
         </li>
@@ -15,11 +19,7 @@ export default () =>
           <a href='#' className={style.benefits__a}>pedais absolutamente fiéis e cumprindo com 100% de efetividade o seu papel</a>
         </li>
         <li className={style.benefits__li}>
-          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='benefits' />
-          <a href='#' className={style.benefits__a}>parceria com os grandes representantes globais em modificações/customizações de pedais</a>
-        </li>
-        <li className={style.benefits__li}>
-          <img className={style.benefits__img} src='https://dummyimage.com/40x40/1A1A1A/1A1A1A.png' loading='lazy' alt='benefits' />
+          <s.Icon className={style.benefits__icon}>loyalty</s.Icon>
           <a href='#' className={style.benefits__a}>produto confeccionado com primor e coração ou paixão ou outra qualidade profunda</a>
         </li>
       </ul>
