4225aa4231ad4767c7447fa3c122ea4e4e1c0520
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-30 08:23:27 -0300

    troca do nome de classe de distortion para modificados

--- src/home/modificados/component.jsx
+++ src/home/modificados/component.jsx
@@ -3,71 +3,71 @@ import * as s from '@share'
 import style from './style.css'
 
 export default () =>
-  <section className={style.distortion}>
-    <s.Container className={style.distortion__container}>
-      <h2 className={style.distortion__h2}>pedais fiéis e cumprindo 100% de efetividade o seu papel</h2>
-      <div className={style.distortion__div}>
-        <picture className={style.distortion__picture}>
+  <section className={style.modificados}>
+    <s.Container className={style.modificados__container}>
+      <h2 className={style.modificados__h2}>pedais fiéis e cumprindo 100% de efetividade o seu papel</h2>
+      <div className={style.modificados__div}>
+        <picture className={style.modificados__picture}>
           <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
           <source srcSet='https://dummyimage.com/939x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet='https://dummyimage.com/1200x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
-          <img className={style.distortion__img} loading='lazy' alt='distortion' />
+          <img className={style.modificados__img} loading='lazy' alt='distortion' />
         </picture>
-        <a className={style.distortion__a} href='#'>ver mais</a>
+        <a className={style.modificados__a} href='#'>ver mais</a>
       </div>
-      <div className={[style.distortion__div, style.half]}>
-        <picture className={[style.distortion__picture, style.half]}>
+      <div className={[style.modificados__div, style.half]}>
+        <picture className={[style.modificados__picture, style.half]}>
           <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
           <source srcSet='https://dummyimage.com/464.5x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet='https://dummyimage.com/393.3x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
-          <img className={style.distortion__img} loading='lazy' alt='distortion / overdrive / fuzz' />
+          <img className={style.modificados__img} loading='lazy' alt='distortion / overdrive / fuzz' />
         </picture>
-        <a className={style.distortion__a} href='#'>distortion</a>
+        <a className={style.modificados__a} href='#'>distortion</a>
       </div>
-      <div className={[style.distortion__div, style.half]}>
-        <picture className={[style.distortion__picture, style.half]}>
+      <div className={[style.modificados__div, style.half]}>
+        <picture className={[style.modificados__picture, style.half]}>
           <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
           <source srcSet='https://dummyimage.com/464.5x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet='https://dummyimage.com/393.3x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
-          <img className={style.distortion__img} loading='lazy' alt='compressor' />
+          <img className={style.modificados__img} loading='lazy' alt='compressor' />
         </picture>
-        <a className={style.distortion__a} href='#'>compressor</a>
+        <a className={style.modificados__a} href='#'>compressor</a>
       </div>
-      <div className={[style.distortion__div, style.half]}>
-        <picture className={[style.distortion__picture, style.half]}>
+      <div className={[style.modificados__div, style.half]}>
+        <picture className={[style.modificados__picture, style.half]}>
           <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
           <source srcSet='https://dummyimage.com/464.5x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet='https://dummyimage.com/393.3x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
-          <img className={style.distortion__img} loading='lazy' alt='equalizer' />
+          <img className={style.modificados__img} loading='lazy' alt='equalizer' />
         </picture>
-        <a className={style.distortion__a} href='#'>equalizer</a>
+        <a className={style.modificados__a} href='#'>equalizer</a>
       </div>
-      <div className={[style.distortion__div, style.half]}>
-        <picture className={[style.distortion__picture, style.half]}>
+      <div className={[style.modificados__div, style.half]}>
+        <picture className={[style.modificados__picture, style.half]}>
           <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
           <source srcSet='https://dummyimage.com/464.5x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet='https://dummyimage.com/393.3x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
-          <img className={style.distortion__img} loading='lazy' alt='preamp' />
+          <img className={style.modificados__img} loading='lazy' alt='preamp' />
         </picture>
-        <a className={style.distortion__a} href='#'>preamp</a>
+        <a className={style.modificados__a} href='#'>preamp</a>
       </div>
-      <div className={[style.distortion__div, style.half]}>
-        <picture className={[style.distortion__picture, style.half]}>
+      <div className={[style.modificados__div, style.half]}>
+        <picture className={[style.modificados__picture, style.half]}>
           <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
           <source srcSet='https://dummyimage.com/464.5x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet='https://dummyimage.com/393.3x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
-          <img className={style.distortion__img} loading='lazy' alt='expression' />
+          <img className={style.modificados__img} loading='lazy' alt='expression' />
         </picture>
-        <a className={style.distortion__a} href='#'>expression</a>
+        <a className={style.modificados__a} href='#'>expression</a>
       </div>
-      <div className={[style.distortion__div, style.half]}>
-        <picture className={[style.distortion__picture, style.half]}>
+      <div className={[style.modificados__div, style.half]}>
+        <picture className={[style.modificados__picture, style.half]}>
           <source srcSet='https://dummyimage.com/747x500/1A1A1A/1A1A1A.png' media='(max-width: 767px)' />
           <source srcSet='https://dummyimage.com/464.5x400/1A1A1A/1A1A1A.png' media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet='https://dummyimage.com/393.3x400/1A1A1A/1A1A1A.png' media='(min-width: 960px)' />
-          <img className={style.distortion__img} loading='lazy' alt='modulation' />
+          <img className={style.modificados__img} loading='lazy' alt='modulation' />
         </picture>
-        <a className={style.distortion__a} href='#'>modulation</a>
+        <a className={style.modificados__a} href='#'>modulation</a>
       </div>
     </s.Container>
   </section>
