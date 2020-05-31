5144982372b75f7126eee6b573efe92b64e25c52
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-31 19:50:07 -0300

    construcao do secao do o que fazemos

--- src/pages/mundo-jrmod/component.jsx
+++ src/pages/mundo-jrmod/component.jsx
@@ -4,9 +4,16 @@ import style from './style.css'
 
 export default () =>
   <s.Template>
-    <s.Container className={style.mundoJRMod__container}>
-      <h1 className={style.mundoJRMod__h1}>mundo JRMod</h1>
-      <p className={style.mundoJRMod__p}>Neste site você encontrará as melhores modificações disponíveis para pedais de diversas marcas e modelos, modificações (MODS) estas que vão desde uma simples troca de CHIP as mais elaboradas e rebuscadas em busca do timbre perfeito! Aqui você encontrará todas modificações elaboradas e desenvolvidas por MONTE ALLUMS, JRMOD e também algumas de outras empresas, consulte a disponibilidade!</p>
-      <p>A JRMOD é a única INSTALADORA OFICIAL de MONTE ALLUMS no Brasil e todos os componentes para suas mods são enviados pessoalmente por MONTE ALLUMS, diretamente dos USA. Nas modificações JRMOD são usados componentes de acordo com o padrão selecionado pelo cliente, são estes “SERIE A” ou “Standard”… os padrões se distinguem por suas qualidades e valores (detalhes logo abaixo). Importante salientar que em nossas modificações “SÉRIE A” não entram nem são instalados componentes adquiridos na famosa Sta Ifigênia (componentes estes adquiridos no Brasil, vindos da China). Sou conhecido por ser um músico extremamente detalhista com relação a timbres, por ser nada virtuoso, busco a perfeição em cada nota e em cada efeito! Tudo que você encontrar aqui, são anos e anos de pesquisa e trabalho árduo, para se obter o melhor em cada detalhe!</p>
-    </s.Container>
+    <section className={style.mundoJRMod__section}>
+      <s.Container className={style.mundoJRMod__container}>
+        <h1 className={style.mundoJRMod__h1}>mundo JRMod</h1>
+        <p className={style.mundoJRMod__p}>Neste site você encontrará as melhores modificações disponíveis para pedais de diversas marcas e modelos, modificações (MODS) estas que vão desde uma simples troca de CHIP as mais elaboradas e rebuscadas em busca do timbre perfeito! Aqui você encontrará todas modificações elaboradas e desenvolvidas por MONTE ALLUMS, JRMOD e também algumas de outras empresas, consulte a disponibilidade!</p>
+        <p className={style.mundoJRMod__p}>A JRMOD é a única INSTALADORA OFICIAL de MONTE ALLUMS no Brasil e todos os componentes para suas mods são enviados pessoalmente por MONTE ALLUMS, diretamente dos USA. Nas modificações JRMOD são usados componentes de acordo com o padrão selecionado pelo cliente, são estes “SERIE A” ou “Standard”… os padrões se distinguem por suas qualidades e valores (detalhes logo abaixo). Importante salientar que em nossas modificações “SÉRIE A” não entram nem são instalados componentes adquiridos na famosa Sta Ifigênia (componentes estes adquiridos no Brasil, vindos da China). Sou conhecido por ser um músico extremamente detalhista com relação a timbres, por ser nada virtuoso, busco a perfeição em cada nota e em cada efeito! Tudo que você encontrar aqui, são anos e anos de pesquisa e trabalho árduo, para se obter o melhor em cada detalhe!</p>
+      </s.Container>
+    </section>
+    <section className={style.mundoJRMod__section}>
+      <s.Container>
+        <h2 className={style.mundoJRMod__h2}>o que fazemos</h2>
+      </s.Container>
+    </section>
   </s.Template>
