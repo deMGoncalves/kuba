7b9ab75b99a29b5a6efdea6c09d072e31595595b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-31 10:46:43 -0300

    voltando o que ja havia desenvolvido

--- src/index.jsx
+++ src/index.jsx
@@ -1,2 +1,9 @@
-import '@pages'
+import h from '@h'
+import router from '@router'
 import '@sw'
+import './reset.css'
+
+router(/^\/$/, async () => {
+  const { default: Home } = await import(/* webpackChunkName: "home" */ './home')
+  document.body.appendChild(<Home />)
+})
--- src/pages/index.jsx
+++
@@ -1,6 +0,0 @@
-import h from '@h'
-import router from '@router'
-import Home from './home'
-import '@/reset.css'
-
-router(/^\/$/, () => document.body.appendChild(<Home />))
