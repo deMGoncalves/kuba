fc2e4b0fd62b0c76815f2357aa7e6d17297ed48d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-05-31 10:58:08 -0300

    o arquivo nao tem mais html

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: './src/index.jsx'
+    app: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(),
