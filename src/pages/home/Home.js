9a2678d2fd07a51a0fce1682745abe4b6541080d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-02 14:51:57 -0300

    inclusao do open graph no objeto home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,11 @@
 import { paint } from '@h'
 import metadata from '@metadata'
+import opengraph from '@opengraph'
 import component from './component'
 
 @paint(component)
 @metadata
+@opengraph
 class Home {
 }
 
