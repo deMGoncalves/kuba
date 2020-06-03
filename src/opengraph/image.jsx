70299976c2b0de002cf78e39105f225dadefaaa0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-03 11:07:02 -0300

    o tamanho minimo da imagem deve ser 200 x 200

--- src/opengraph/image.jsx
+++ src/opengraph/image.jsx
@@ -2,7 +2,7 @@ import h from '@h'
 import * as f from '@f'
 
 const __image__ = Symbol('image')
-const defaultUrl = 'https://dummyimage.com/192x192/1a1a1a/1a1a1a.png'
+const defaultUrl = 'https://dummyimage.com/200x200/1a1a1a/1a1a1a.png'
 
 export default (page) =>
   []
