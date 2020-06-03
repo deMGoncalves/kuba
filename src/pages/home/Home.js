f3efce6f33f676b79d56bb6ee294d06e97112a6c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-03 12:30:20 -0300

    agora a home tera os dados estruturado base

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import { paint } from '@h'
 import metadata from '@metadata'
 import opengraph from '@opengraph'
+import structureddata from '@structureddata'
 import twittercard from '@twittercard'
 import component from './component'
 
 @paint(component)
 @metadata
 @opengraph
+@structureddata
 @twittercard
 class Home {
 }
