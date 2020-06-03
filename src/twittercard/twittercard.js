a7a6086c77313c8c705aba70c6e648e48d6ee7b3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-03 12:06:13 -0300

    inclusao do description do twitter card

--- src/twittercard/twittercard.js
+++ src/twittercard/twittercard.js
@@ -1,3 +1,4 @@
+import description from './description'
 import site from './site'
 import summary from './summary'
 import title from './title'
@@ -7,6 +8,7 @@ export default (Klass) =>
     const page = new Klass(...arguments)
 
     setImmediate(() => {
+      description(page)
       site(page)
       summary(page)
       title(page)
