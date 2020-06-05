7d738616ed499219f45cc1f8f7aa60c0d9414a9f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 19:00:54 -0300

    removendo a rota do sitemap

--- paths.js
+++ paths.js
@@ -4,11 +4,5 @@ module.exports = [
     lastmod: '2020-05-01',
     priority: '1',
     changefreq: 'monthly'
-  },
-  {
-    path: '/mundo-jrmod',
-    lastmod: '2020-05-01',
-    priority: '1',
-    changefreq: 'monthly'
   }
 ]
