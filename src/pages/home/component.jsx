6adc7bd5f8c02278b8f190e1d2fe92aa268ca9da
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 10:27:22 -0300

    mudando de nome o modulo para o contexto

--- src/pages/home/component.jsx
+++ src/pages/home/component.jsx
@@ -1,7 +1,7 @@
 import h from '@h'
 import * as s from '@share'
 import Banner from './highlight-banner'
-import Benefits from './benefits'
+import Benefits from './highlight-benefits'
 import HalfBanner from './highlight-half-banner'
 import Kits from './kits'
 import Modificados from './modificados'
