f1fc36ede10fef63379595de6c0f7f2ad5bd3937
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 16:46:58 -0300

    ajuste na semantica e no css do card de produto

--- src/pages/home/display-products/component.jsx
+++ src/pages/home/display-products/component.jsx
@@ -5,15 +5,92 @@ import style from './style.css'
 const banner = 'https://dummyimage.com/293x400/1a1a1a/1a1a1a'
 
 export default () =>
-  <section className={style.banner}>
-    <s.Container className={style.banner__container}>
-      <h2 className={style.banner__h2}>recomendamos alguns produtos</h2>
-      <div className={style.banner__div}>
-        <picture className={style.banner__picture}>
-          <img className={style.banner__img} src={banner} loading='lazy' alt='product' />
+  <section className={style.product}>
+    <s.Container className={style.product__container}>
+      <h2 className={style.product__h2}>recomendamos alguns produtos</h2>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
         </picture>
-        <p className={style.banner__p}>product namd</p>
-        <data className={style.banner__data} value='999.99'>R$ 999,99</data>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
+      </div>
+      <div className={style.product__div}>
+        <picture className={style.product__picture}>
+          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
+        </picture>
+        <p className={style.product__p}>product name</p>
+        <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
     </s.Container>
   </section>
