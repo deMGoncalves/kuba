6afa4274eed757eca7fa1601ef84dbb0baf771ca
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 12:25:47 -0300

    ajuste de layout

--- src/pages/home/display-products/component.jsx
+++ src/pages/home/display-products/component.jsx
@@ -2,44 +2,18 @@ import h from '@h'
 import * as s from '@share'
 import style from './style.css'
 
-const bannerS = 'https://dummyimage.com/747x500/1a1a1a/1a1a1a'
-const bannerM = 'https://dummyimage.com/939x400/1a1a1a/1a1a1a'
-const bannerL = 'https://dummyimage.com/593x400/1a1a1a/1a1a1a'
-
-const halfBannerS = 'https://dummyimage.com/368x500/1a1a1a/1a1a1a'
-const halfBannerM = 'https://dummyimage.com/464x400/1a1a1a/1a1a1a'
-const halfBannerL = 'https://dummyimage.com/293x400/1a1a1a/1a1a1a'
+const banner = 'https://dummyimage.com/293x400/1a1a1a/1a1a1a'
 
 export default () =>
   <section className={style.banner}>
     <s.Container className={style.banner__container}>
-      <h2 className={style.banner__h2}>separamos algumas tendências para você</h2>
+      <h2 className={style.banner__h2}>recomendamos alguns produtos</h2>
       <div className={style.banner__div}>
         <picture className={style.banner__picture}>
-          <source srcSet={bannerS} media='(max-width: 767px)' />
-          <source srcSet={bannerM} media='(min-width: 768px) and (max-width: 959px)' />
-          <source srcSet={bannerL} media='(min-width: 960px)' />
-          <img className={style.banner__img} loading='lazy' alt='aspiration' />
+          <img className={style.banner__img} src={banner} loading='lazy' alt='product' />
         </picture>
-        <s.Link className={style.banner__a} href='#'>ver mais</s.Link>
-      </div>
-      <div className={[style.banner__div, style.half]}>
-        <picture className={[style.banner__picture, style.half]}>
-          <source srcSet={halfBannerS} media='(max-width: 767px)' />
-          <source srcSet={halfBannerM} media='(min-width: 768px) and (max-width: 959px)' />
-          <source srcSet={halfBannerL} media='(min-width: 960px)' />
-          <img className={style.banner__img} loading='lazy' alt='aspiration' />
-        </picture>
-        <s.Link className={style.banner__a} href='#'>ver mais</s.Link>
-      </div>
-      <div className={[style.banner__div, style.half]}>
-        <picture className={[style.banner__picture, style.half]}>
-          <source srcSet={halfBannerS} media='(max-width: 767px)' />
-          <source srcSet={halfBannerM} media='(min-width: 768px) and (max-width: 959px)' />
-          <source srcSet={halfBannerL} media='(min-width: 960px)' />
-          <img className={style.banner__img} loading='lazy' alt='aspiration' />
-        </picture>
-        <s.Link className={style.banner__a} href='#'>ver mais</s.Link>
+        <p className={style.banner__p}>product namd</p>
+        <data className={style.banner__data} value='999.99'>R$ 999,99</data>
       </div>
     </s.Container>
   </section>
