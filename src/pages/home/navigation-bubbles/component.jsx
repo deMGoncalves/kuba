e3234a2d59ec901a5d28498822c8bc0d6067b281
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 11:02:25 -0300

    padrao de nomenclatura de css

--- src/pages/home/navigation-bubbles/component.jsx
+++ src/pages/home/navigation-bubbles/component.jsx
@@ -4,57 +4,57 @@ import carousel from './carousel'
 import style from './style.css'
 
 export default carousel(() =>
-  <section className={style.navigation}>
-    <s.Container className={style.navigation__container}>
-      <h2 className={style.navigation__h2}>navegue pelo mundo JRMod</h2>
-      <nav className={style.navigation__nav}>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+  <section className={style.bubble}>
+    <s.Container className={style.bubble__container}>
+      <h2 className={style.bubble__h2}>navegue pelo mundo JRMod</h2>
+      <nav className={style.bubble__nav}>
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
+        <div className={style.bubble__div}>
+          <picture className={style.bubble__picture}>
+            <img className={style.bubble__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
           </picture>
-          <a className={style.navigation__a}>navigation</a>
+          <a className={style.bubble__a}>navigation</a>
         </div>
       </nav>
     </s.Container>
