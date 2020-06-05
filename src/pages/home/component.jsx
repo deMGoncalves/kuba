ec312ca4af7e2482aa8747d73bb4f2749dfb4b63
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 20:41:54 -0300

    o modulo de highlight half banner fica no contexto de share

--- src/pages/home/component.jsx
+++ src/pages/home/component.jsx
@@ -1,6 +1,5 @@
 import h from '@h'
 import * as s from '@pages/share'
-import HighlightHalfBanner from './highlight-half-banner'
 import NavigationBubbles from './navigation-bubbles'
 import PromoThreeBanners from './promo-three-banners'
 
@@ -8,7 +7,7 @@ export default () =>
   <s.Main>
     <s.HighlightBanner />
     <s.HighlightBenefits />
-    <HighlightHalfBanner />
+    <s.HighlightHalfBanner />
     <NavigationBubbles />
     <s.AspirationThreeBanners />
     <s.DisplayProducts />
