63d7c02c52d096935256e701999778a7ab9eb4f4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 20:55:48 -0300

    copia da home para o contexto de departamento

---
+++ src/pages/departament/component.jsx
@@ -0,0 +1,13 @@
+import h from '@h'
+import * as s from '@pages/share'
+
+export default () =>
+  <s.Main>
+    <s.HighlightBanner />
+    <s.HighlightBenefits />
+    <s.HighlightHalfBanner />
+    <s.NavigationBubbles />
+    <s.AspirationThreeBanners />
+    <s.DisplayProducts />
+    <s.PromoThreeBanners />
+  </s.Main>
