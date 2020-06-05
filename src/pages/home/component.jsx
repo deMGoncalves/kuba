8047b3015dec821afae177418fd6261d68d2a1b8
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 11:10:53 -0300

    linguagem ubiquoa para os nomes de modulos

--- src/pages/home/component.jsx
+++ src/pages/home/component.jsx
@@ -1,19 +1,21 @@
 import h from '@h'
 import * as s from '@share'
-import Banner from './highlight-banner'
-import Benefits from './highlight-benefits'
-import HalfBanner from './highlight-half-banner'
+import AspirationThreeBanners from './aspiration-three-banners'
+import HighlightBanner from './highlight-banner'
+import HighlightBenefits from './highlight-benefits'
+import HighlightHalfBanner from './highlight-half-banner'
 import Kits from './kits'
 import Modificados from './modificados'
-import Bubbles from './navigation-bubbles'
+import NavigationBubbles from './navigation-bubbles'
 import Souvenier from './souvenier'
 
 export default () =>
   <s.Template>
-    <Banner />
-    <Benefits />
-    <HalfBanner />
-    <Bubbles />
+    <HighlightBanner />
+    <HighlightBenefits />
+    <HighlightHalfBanner />
+    <NavigationBubbles />
+    <AspirationThreeBanners />
     <Modificados />
     <Souvenier />
     <Kits />
