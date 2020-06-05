f349b0ed6b619041a7aa06321de0cffe2c0ad76c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 10:39:42 -0300

    vamos manter apenas 8 itens

--- src/pages/home/navigation-bubbles/component.jsx
+++ src/pages/home/navigation-bubbles/component.jsx
@@ -56,18 +56,6 @@ export default carousel(() =>
           </picture>
           <a className={style.navigation__a}>navigation</a>
         </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
-          </picture>
-          <a className={style.navigation__a}>navigation</a>
-        </div>
-        <div className={style.navigation__div}>
-          <picture className={style.navigation__picture}>
-            <img className={style.navigation__img} src='https://dummyimage.com/130x130/1a1a1a/1a1a1a' loading='lazy' alt='navigation' />
-          </picture>
-          <a className={style.navigation__a}>navigation</a>
-        </div>
       </nav>
     </s.Container>
   </section>
