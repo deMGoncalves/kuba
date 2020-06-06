4c2cecaa1687d3c4fccef29acfd67795ea3d8ed5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-06 17:53:04 -0300

    o link deve repassar o href para o tag a

--- src/pages/share/link/component.jsx
+++ src/pages/share/link/component.jsx
@@ -1,5 +1,6 @@
 import h from '@h'
+import * as f from '@f'
 import style from './style.css'
 
 export default (attr, child) =>
-  <a className={[style.link, attr.className]} href='#'>{ child }</a>
+  <a className={[style.link, attr.className]} href={f.or(attr.href, '#')}>{ child }</a>
