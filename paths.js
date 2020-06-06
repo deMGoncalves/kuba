853939624d4152f01ae27f9050f751401fe6474d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-05 21:34:12 -0300

    inclusao das rotas de departamento

--- paths.js
+++ paths.js
@@ -4,5 +4,29 @@ module.exports = [
     lastmod: '2020-05-01',
     priority: '1',
     changefreq: 'monthly'
+  },
+  {
+    path: '/modificados',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/custom-shop',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/kits',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/souvenier',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
   }
 ]
