9b344ffdd8853b6840f1fc70908e1f080189bf91
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-07 21:58:31 -0300

    usando o modulo de imagem

--- src/pages/share/aspiration-three-banners/component.jsx
+++ src/pages/share/aspiration-three-banners/component.jsx
@@ -15,30 +15,27 @@ export default () =>
     <s.Container className={style.banner__container}>
       <s.Title>separamos algumas tendências para você</s.Title>
       <div className={style.banner__div}>
-        <picture className={style.banner__picture}>
+        <s.Image className={style.banner__s.Image}>
           <source srcSet={bannerS} media='(max-width: 767px)' />
           <source srcSet={bannerM} media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet={bannerL} media='(min-width: 960px)' />
-          <img className={style.banner__img} loading='lazy' alt='aspiration' />
-        </picture>
+        </s.Image>
         <s.Link className={style.banner__a} href='#'>ver mais</s.Link>
       </div>
       <div className={[style.banner__div, style.half]}>
-        <picture className={[style.banner__picture, style.half]}>
+        <s.Image className={[style.banner__s.Image, style.half]}>
           <source srcSet={halfBannerS} media='(max-width: 767px)' />
           <source srcSet={halfBannerM} media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet={halfBannerL} media='(min-width: 960px)' />
-          <img className={style.banner__img} loading='lazy' alt='aspiration' />
-        </picture>
+        </s.Image>
         <s.Link className={style.banner__a} href='#'>ver mais</s.Link>
       </div>
       <div className={[style.banner__div, style.half]}>
-        <picture className={[style.banner__picture, style.half]}>
+        <s.Image className={[style.banner__image, style.half]}>
           <source srcSet={halfBannerS} media='(max-width: 767px)' />
           <source srcSet={halfBannerM} media='(min-width: 768px) and (max-width: 959px)' />
           <source srcSet={halfBannerL} media='(min-width: 960px)' />
-          <img className={style.banner__img} loading='lazy' alt='aspiration' />
-        </picture>
+        </s.Image>
         <s.Link className={style.banner__a} href='#'>ver mais</s.Link>
       </div>
     </s.Container>
