99dbe5c5afd5af091970d052fefcde74d703a256
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-08 13:10:04 -0300

    usando o modulo base e removendo dados duplicado

--- src/pages/share/navigation-bubbles/component.jsx
+++ src/pages/share/navigation-bubbles/component.jsx
@@ -6,7 +6,7 @@ import style from './style.css'
 const banner = 'https://dummyimage.com/132x132/1a1a1a/1a1a1a'
 
 export default carousel(() =>
-  <section className={style.bubble}>
+  <s.Module>
     <s.Container className={style.bubble__container}>
       <s.Title>navegue pelo mundo JRMod</s.Title>
       <nav className={style.bubble__nav}>
@@ -60,5 +60,5 @@ export default carousel(() =>
         </div>
       </nav>
     </s.Container>
-  </section>
+  </s.Module>
 )
