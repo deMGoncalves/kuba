0f1212989813d3454fc356a762d6332d329f6a6b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-07 22:42:41 -0300

    usando o modulo de imagem para nao repetir codigo

--- src/pages/share/display-products/component.jsx
+++ src/pages/share/display-products/component.jsx
@@ -9,86 +9,86 @@ export default () =>
     <s.Container className={style.product__container}>
       <s.Title>recomendamos alguns produtos</s.Title>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
       <div className={style.product__div}>
-        <picture className={style.product__picture}>
-          <img className={style.product__img} src={banner} loading='lazy' alt='product' />
-        </picture>
+        <s.Image className={style.product__image}>
+          <source srcSet={banner} />
+        </s.Image>
         <p className={style.product__p}>product name</p>
         <data className={style.product__data} value='999.99'>R$ 999,99</data>
       </div>
