e5c887f786da0d57b03efcab77bee1e373aea845
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-07 23:37:09 -0300

    adicoa de novas url no path

--- paths.js
+++ paths.js
@@ -18,7 +18,55 @@ module.exports = [
     changefreq: 'monthly'
   },
   {
+    path: '/custom-shop/distortion-overdrive-fuzz',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/custom-shop/compressor-limiter',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/custom-shop/equalizer',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/custom-shop/preamp',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/custom-shop/expression-wah',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/custom-shop/modulacao',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
     path: '/kits',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/kits/overdrive',
+    lastmod: '2020-05-01',
+    priority: '1',
+    changefreq: 'monthly'
+  },
+  {
+    path: '/kits/distortion',
     lastmod: '2020-05-01',
     priority: '1',
     changefreq: 'monthly'
