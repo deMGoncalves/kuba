45bb807386f59596682a2800a03b863e53c4a8da
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 13:08:35 -0300

    removendo os mapeamento de rotas

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,13 +5,3 @@ router(/^\/$/, async () => {
   const { default: Home } = await import(/* webpackChunkName: "home" */ '@pages/home')
   document.body.appendChild(<Home />)
 })
-
-router(/^\/([\w-]+)$/, async (departament) => {
-  const { default: Departament } = await import(/* webpackChunkName: "departament" */ '@pages/departament')
-  document.body.appendChild(<Departament title={departament} />)
-})
-
-router(/^\/([\w-]+)\/([\w-]+)$/, async (_, category) => {
-  const { default: Category } = await import(/* webpackChunkName: "category" */ '@pages/category')
-  document.body.appendChild(<Category title={category} />)
-})
