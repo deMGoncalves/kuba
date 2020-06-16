bfc6c0883bb9a14c68ba578b4c43a6e3b4567874
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 14:55:42 -0300

    reutilizando o endereco

--- src/structureddata/organization.js
+++ src/structureddata/organization.js
@@ -1,19 +1,9 @@
+import address from './address'
+
 export default {
   '@context': 'http://schema.org',
   '@type': 'Organization',
-  address: {
-    '@type': 'PostalAddress',
-    postalCode: '07135-313',
-    streetAddress: 'Rua Floro de Oliveira, 361',
-    addressRegion: 'SP',
-    addressLocality: 'Guarulhos, Jd. Adriana',
-    addressCountry: {
-      '@type': 'Country',
-      name: 'BR'
-    },
-    telephone: '+55 11 94631-4083',
-    email: 'cleber.engineer@hotmail.com'
-  },
+  address,
   logo: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
   name: 'Zuen',
   sameAs: 'https://github.com/oneOffJS/zuen',
