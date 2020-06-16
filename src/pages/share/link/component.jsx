fde3062ef12b2769029002b9fc2f803006dfbb65
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 13:18:45 -0300

    removendo o moudulo agregador de link

--- src/pages/share/link/component.jsx
+++
@@ -1,6 +0,0 @@
-import h from '@h'
-import * as f from '@f'
-import style from './style.css'
-
-export default (attr, child) =>
-  <a className={[style.link, attr.className]} href={f.or(attr.href, '#')}>{ child }</a>
