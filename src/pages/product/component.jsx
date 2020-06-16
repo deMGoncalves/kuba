d8aba5656be6dbb64918ee2f6ebd81df25f1d4d3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 13:42:12 -0300

    removendo modulso que nao existe no contexto share e product

--- src/pages/product/component.jsx
+++ src/pages/product/component.jsx
@@ -2,12 +2,4 @@ import h from '@h'
 import * as s from '@pages/share'
 
 export default () =>
-  <s.Main>
-    <s.HighlightBanner />
-    <s.HighlightBenefits />
-    <s.HighlightHalfBanner />
-    <s.NavigationBubbles />
-    <s.AspirationThreeBanners />
-    <s.DisplayProducts />
-    <s.PromoThreeBanners />
-  </s.Main>
+  <s.Main />
