6988b736ad214f07e8dafd931152f66e3031b5be
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 13:35:22 -0300

    removendo o modulo base para criar modulos

--- src/pages/share/index.js
+++ src/pages/share/index.js
@@ -2,7 +2,6 @@ import Container from './container'
 import Icon from './icon'
 import Image from './image'
 import Main from './main'
-import Module from './module'
 import ModuleTitle from './moduleTitle'
 import NavigationBubbles from './navigation-bubbles'
 import PromoThreeBanners from './promo-three-banners'
@@ -12,7 +11,6 @@ export {
   Icon,
   Image,
   Main,
-  Module,
   ModuleTitle,
   NavigationBubbles,
   PromoThreeBanners
