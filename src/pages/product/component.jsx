9a19be4153a872514b3548c4c9d8bf3be4528fd0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 18:14:51 -0300

    inclusao do titulo e preco na pagina

--- src/pages/product/component.jsx
+++ src/pages/product/component.jsx
@@ -5,7 +5,9 @@ export default (page) =>
   <s.Main>
     <s.Container>
       <s.Image>
+        <h1>{ page.title }</h1>
         <source srcSet={page.image} />
+        <data value='1329.05'>R$ 1.329,05</data>
       </s.Image>
     </s.Container>
   </s.Main>
