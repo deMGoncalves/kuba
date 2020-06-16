a667e63217b91039dfbc3c61177ee4d2e0dc8e3d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 14:57:38 -0300

    inclusao do endereco e de dados complementares

--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -1,11 +1,15 @@
-import h from '@h'
+import address from './address'
 
 export default {
   '@context': 'http://schema.org',
   '@type': 'Website',
   author: {
     '@type': 'Person',
-    name: 'Cleber de Moraes Gonçalves'
+    address,
+    image: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
+    name: 'Cleber de Moraes Gonçalves',
+    sameAs: 'https://github.com/oneOffJS',
+    url: 'https://zuen.store'
   },
   image: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
   name: 'Zuen',
