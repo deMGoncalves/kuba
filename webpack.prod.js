c8e746bd3e9ce94b6e12a9b3016aef3ad55aa301
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 16:21:31 -0300

    fazendo o sitemap na mao

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const common = require('./webpack.common.js')
 const merge = require('webpack-merge')
-const paths = require('./paths')
 
 const { GenerateSW } = require('workbox-webpack-plugin')
 
