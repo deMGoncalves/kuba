b3360a2c5066f45fdfaf381128be6d0113c45167
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 13:45:18 -0300

    reemovendo os modulos de header e footer

--- src/pages/share/main/component.jsx
+++ src/pages/share/main/component.jsx
@@ -1,10 +1,4 @@
 import h from '@h'
-import * as s from '@pages/share'
 import './reset.css'
 
-export default (_, chield) =>
-  <main>
-    <s.Header />
-    { chield }
-    <s.Footer />
-  </main>
+export default (_, chield) => <main>{ chield }</main>
