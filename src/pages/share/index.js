9dc934fd8eeae07fd4bed80f7222b34ee65ed212
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 13:36:10 -0300

    removendo o modulo de promo

--- src/pages/share/index.js
+++ src/pages/share/index.js
@@ -2,12 +2,10 @@ import Container from './container'
 import Icon from './icon'
 import Image from './image'
 import Main from './main'
-import PromoThreeBanners from './promo-three-banners'
 
 export {
   Container,
   Icon,
   Image,
-  Main,
-  PromoThreeBanners
+  Main
 }
