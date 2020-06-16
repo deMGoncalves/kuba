33d355a84525466abfeaf39e33385a786e48e4ad
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 18:20:02 -0300

    erro no html

--- src/pages/product/component.jsx
+++ src/pages/product/component.jsx
@@ -4,10 +4,10 @@ import * as s from '@pages/share'
 export default (page) =>
   <s.Main>
     <s.Container>
+      <h1>{ page.title }</h1>
       <s.Image alt={page.title}>
-        <h1>{ page.title }</h1>
         <source srcSet={page.image} />
-        <data value='1329.05'>R$ 1.329,05</data>
       </s.Image>
+      <data value='1329.05'>R$ 1.329,05</data>
     </s.Container>
   </s.Main>
