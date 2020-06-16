a4b699bd64c008c9b4a741e12313d359b78fce6f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 15:50:06 -0300

    vou fazer o teste depois

--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -1,5 +1,4 @@
 import organization from './organization'
-import product from './product'
 
 export default {
   '@context': 'http://schema.org',
@@ -8,11 +7,6 @@ export default {
   image: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
   isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
   name: 'Zuen',
-  offers: {
-    '@context': 'http://schema.org',
-    '@type': 'Demand',
-    itemOffered: product
-  },
   sameAs: 'https://github.com/oneOffJS',
   url: 'https://zuen.store'
 }
