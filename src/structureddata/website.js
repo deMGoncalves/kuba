3820a05e1cb059b835b28da01c573d218631854c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 15:11:40 -0300

    usando a organizacao domo author

--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -1,17 +1,11 @@
-import address from './address'
+import organization from './organization'
 
 export default {
   '@context': 'http://schema.org',
   '@type': 'Website',
-  author: {
-    '@type': 'Person',
-    address,
-    image: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
-    name: 'Cleber de Moraes Gonçalves',
-    sameAs: 'https://github.com/oneOffJS',
-    url: 'https://zuen.store'
-  },
+  author: organization,
   image: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
+  isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
   name: 'Zuen',
   sameAs: 'https://github.com/oneOffJS',
   url: 'https://zuen.store'
