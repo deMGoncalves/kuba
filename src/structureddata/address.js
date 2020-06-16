66a319005df7c8c9dcb626a9ae67e61761539b74
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 14:53:57 -0300

    construcao do endereco

---
+++ src/structureddata/address.js
@@ -0,0 +1,14 @@
+export default {
+  '@context': 'http://schema.org',
+  '@type': 'PostalAddress',
+  email: 'cleber.engineer@hotmail.com',
+  addressCountry: {
+    '@type': 'Country',
+    name: 'BR'
+  },
+  addressLocality: 'Guarulhos, Jd. Adriana',
+  addressRegion: 'SP',
+  postalCode: '07135-313',
+  streetAddress: 'Rua Floro de Oliveira, 361',
+  telephone: '+55 11 94631-4083'
+}
