d1f0cfb705dece8f1e5c92380223c7c31b76c04b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 14:41:45 -0300

    construcao do modulo website

---
+++ src/structureddata/website.js
@@ -0,0 +1,14 @@
+import h from '@h'
+
+export default {
+  '@context': 'http://schema.org',
+  '@type': 'Website',
+  author: {
+    '@type': 'Person',
+    name: 'Cleber de Moraes Gonçalves'
+  },
+  image: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
+  name: 'Zuen',
+  sameAs: 'https://github.com/oneOffJS',
+  url: 'https://zuen.store'
+}
