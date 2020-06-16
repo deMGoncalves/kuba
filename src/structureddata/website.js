fa49cc13b8ecc7d5d0b1a0dd815c04aa02d11eae
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 15:25:19 -0300

    teste para estar a construca unica de uma unico dado estruturado

--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -1,4 +1,5 @@
 import organization from './organization'
+import product from './product'
 
 export default {
   '@context': 'http://schema.org',
@@ -7,6 +8,11 @@ export default {
   image: 'https://dummyimage.com/112x112/1a1a1a/1a1a1a',
   isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
   name: 'Zuen',
+  offers: {
+    '@context': 'http://schema.org',
+    '@type': 'Demand',
+    itemOffered: product
+  },
   sameAs: 'https://github.com/oneOffJS',
   url: 'https://zuen.store'
 }
