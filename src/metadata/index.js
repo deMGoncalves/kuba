86205ce4113aa625932715d493c548a66a9051dd
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:09:38 -0300

    removendo os simbolos para inversaro de dependencia

--- src/metadata/index.js
+++ src/metadata/index.js
@@ -1,25 +1,3 @@
 import metadata from './metadata'
 
-import { __base__ } from './base'
-import { __canonical__ } from './canonical'
-import { __charset__ } from './charset'
-import { __description__ } from './description'
-import { __icon__ } from './icon'
-import { __manifest__ } from './manifest'
-import { __themeColor__ } from './themeColor'
-import { __title__ } from './title'
-import { __viewport__ } from './viewport'
-
 export default metadata
-
-export {
-  __base__,
-  __canonical__,
-  __charset__,
-  __description__,
-  __icon__,
-  __manifest__,
-  __themeColor__,
-  __title__,
-  __viewport__
-}
