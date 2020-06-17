be594cc7f1b61393d9974a092c5865a23e92196a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-17 09:43:17 -0300

    estilizacao basica do elementos de layout

--- src/pages/product/component.jsx
+++ src/pages/product/component.jsx
@@ -1,13 +1,23 @@
 import h from '@h'
 import * as s from '@pages/share'
+import style from './style.css'
 
 export default (page) =>
   <s.Main>
-    <s.Container>
-      <h1>{ page.title }</h1>
-      <s.Image alt={page.title}>
+    <s.Container className={style.product}>
+      <s.Image className={style.product__image} alt={page.title}>
         <source srcSet={page.image[0]} />
       </s.Image>
-      <data value='1329.05'>R$ 1.329,05</data>
+      <div className={style.product__div}>
+        <h1 className={style.product__title}>{ page.title }</h1>
+        <data className={style.product__price} value='1329.05'>R$ 1.329,05</data>
+        <p className={style.product__description}>{ page.description }</p>
+        <h2 className={style.product__color}>Cores</h2>
+        <ul className={style.colors}>
+          <li className={style.colors__white} />
+          <li className={style.colors__black} />
+          <li className={style.colors__purple} />
+        </ul>
+      </div>
     </s.Container>
   </s.Main>
