6a3114eb2a6d1e2dea55b26b3a52653c5e271e70
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-17 11:56:46 -0300

    ajuste para: Current page does not respond with a 200 when offline

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,9 @@
 import h from '@h'
 import router from '@router'
 
+router(/^\/$/, async () =>
+  document.body.append(<strong>Zuen Store</strong>))
+
 router(/^\/(samsung-galaxy-a30s-64gb-branco)$/, async () => {
   const { default: Product } = await import(/* webpackChunkName: "product" */ '@pages/product')
   document.body.append(<Product />)
