503b8af1e8a5f7dfcd879db699ceebbeb9ac7486
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-17 15:14:19 -0300

    inclusao de forma manual as especificacao do produto

--- src/pages/product/component.jsx
+++ src/pages/product/component.jsx
@@ -23,8 +23,8 @@ export default (page) =>
           <li className={style.rating__li}><s.Icon>star--full</s.Icon></li>
           <li className={style.rating__li}><s.Icon>star--full</s.Icon></li>
           <li className={style.rating__li}><s.Icon>star--full</s.Icon></li>
+          <li className={style.rating__li}><s.Icon>star--full</s.Icon></li>
           <li className={style.rating__li}><s.Icon>star--half</s.Icon></li>
-          <li className={style.rating__li}><s.Icon>star</s.Icon></li>
         </ul>
         <data className={style.product__price} value='1599.00'>de R$ 1.599,00</data>
         <data className={style.product__salePrice} value='1329.05'>R$ <strong>1.329,05</strong></data>
@@ -42,6 +42,81 @@ export default (page) =>
         <ul className={style.memory}>
           <li className={style.memory__64}>64 GB</li>
         </ul>
+        <details className={[style.details, style.product__details]}>
+          <summary className={style.details__summary}>Especificações <s.Icon>down</s.Icon></summary>
+          <dl className={style.details__dl}>
+            <dt className={style.details__dt}>E-mail (convencional)</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Flash</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>TV Digital</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Redes Sociais</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Rádio FM</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Wifi</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Cor</dt>
+            <dd className={style.details__dd}>Branco</dd>
+            <dt className={style.details__dt}>Marca</dt>
+            <dd className={style.details__dd}>Samsung</dd>
+            <dt className={style.details__dt}>Resolução Gravação de vídeo</dt>
+            <dd className={style.details__dd}>FHD</dd>
+            <dt className={style.details__dt}>GPS/AGPS</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Resolução</dt>
+            <dd className={style.details__dd}>6,4"</dd>
+            <dt className={style.details__dt}>Resolução Display (Pixels)</dt>
+            <dd className={style.details__dd}>720 x 1560</dd>
+            <dt className={style.details__dt}>Banda</dt>
+            <dd className={style.details__dd}>B1, B2, B3, B4, B5, B7, B8, B12, B13, B17, B20, B26, B28, B66</dd>
+            <dt className={style.details__dt}>EDGE</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Câmera Frontal</dt>
+            <dd className={style.details__dd}>16 MP</dd>
+            <dt className={style.details__dt}>Câmera Traseira</dt>
+            <dd className={style.details__dd}>25MP + 5MP + 8MP Ultra Wide 123°</dd>
+            <dt className={style.details__dt}>Agenda Telefônica (número de contatos)</dt>
+            <dd className={style.details__dd}>De acordo com a memória do aparelho</dd>
+            <dt className={style.details__dt}>Tecnologia</dt>
+            <dd className={style.details__dd}>4G</dd>
+            <dt className={style.details__dt}>Suporte a Cartão Micro SD</dt>
+            <dd className={style.details__dd}>Até 512 GB</dd>
+            <dt className={style.details__dt}>Memória RAM</dt>
+            <dd className={style.details__dd}>4 GB</dd>
+            <dt className={style.details__dt}>Processador</dt>
+            <dd className={style.details__dd}>OctaCore 1.8Ghz</dd>
+            <dt className={style.details__dt}>LTE (4G)</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Chip</dt>
+            <dd className={style.details__dd}>Nano</dd>
+            <dt className={style.details__dt}>GPRS</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Dual Chip</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>SMS</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Bluetooth</dt>
+            <dd className={style.details__dd}>Sim</dd>
+            <dt className={style.details__dt}>Medidas do Aparelho</dt>
+            <dd className={style.details__dd}>15,8 x 7,4 x 0,7</dd>
+            <dt className={style.details__dt}>Sistema Operacional</dt>
+            <dd className={style.details__dd}>Android 9.0</dd>
+            <dt className={style.details__dt}>Peso do aparelho (gr)</dt>
+            <dd className={style.details__dd}>169g</dd>
+            <dt className={style.details__dt}>EAN</dt>
+            <dd className={style.details__dd}>7892509112437, 7892509112147</dd>
+            <dt className={style.details__dt}>NCM</dt>
+            <dd className={style.details__dd}>8517.12.31</dd>
+            <dt className={style.details__dt}>Conteúdo da Embalagem</dt>
+            <dd className={style.details__dd}>01 Smartphone, 01 Carregador, 01 Cabo USB, 01 Fone de ouvido, 01 Extrator de Chip, 01 Manual do usuário</dd>
+            <dt className={style.details__dt}>Destaque</dt>
+            <dd className={style.details__dd}>Sale</dd>
+            <dt className={style.details__dt}>Tamanho</dt>
+            <dd className={style.details__dd}>Único</dd>
+          </dl>
+        </details>
       </div>
     </s.Container>
   </s.Main>
