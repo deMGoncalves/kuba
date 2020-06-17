e8f4e3960731c0bda435fc0270eadb402f75339a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:18:26 -0300

    usando o method append

--- src/opengraph/title.jsx
+++ src/opengraph/title.jsx
@@ -2,4 +2,4 @@ import h from '@h'
 import * as f from '@f'
 
 export default (page) =>
-  document.head.appendChild(<meta property='og:title' content={f.or(page.title, 'Zuen')} />)
+  document.head.append(<meta property='og:title' content={f.or(page.title, 'Zuen')} />)
