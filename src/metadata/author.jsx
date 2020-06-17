7ffdcc050d241629494d9b36f80fd647516dd986
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:05:25 -0300

    definindo o auto do site

--- src/metadata/author.jsx
+++ src/metadata/author.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __author__ = Symbol('author')
-
 export default (page) =>
-  document.head.appendChild(<meta name='author' content={f.or(page[__author__], 'JRMod')} />)
-
-export {
-  __author__
-}
+  document.head.appendChild(<meta name='author' content={f.or(page.author, 'Zuen')} />)
