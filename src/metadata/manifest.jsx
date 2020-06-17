8819edf3714981c31cd6502801904fb87bcd5cb6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:10:04 -0300

    definicao do manifest

--- src/metadata/manifest.jsx
+++ src/metadata/manifest.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __manifest__ = Symbol('manifest')
-
 export default (page) =>
-  document.head.appendChild(<link href={f.or(page[__manifest__], '/manifest.json')} rel='manifest' />)
-
-export {
-  __manifest__
-}
+  document.head.appendChild(<link href={f.or(page.manifest, '/manifest.json')} rel='manifest' />)
