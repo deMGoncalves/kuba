272ca937eed67d809e48c90f17ccad56b93506f3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:16:50 -0300

    definicao do locale

--- src/opengraph/locale.jsx
+++ src/opengraph/locale.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __locale__ = Symbol('locale')
-
 export default (page) =>
-  document.head.appendChild(<meta property='og:locale' content={f.or(page[__locale__], 'pr_BR')} />)
-
-export {
-  __locale__
-}
+  document.head.append(<meta property='og:locale' content={f.or(page.locale, 'pr_BR')} />)
