32e97783ffe8589fd0f385a1e718fc8df6b261de
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:09:04 -0300

    definicao do icon do site

--- src/metadata/charset.jsx
+++ src/metadata/charset.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __charset__ = Symbol('charset')
-
 export default (page) =>
-  document.head.appendChild(<meta charset={f.or(page[__charset__], 'utf-8')} />)
-
-export {
-  __charset__
-}
+  document.head.appendChild(<meta charset={f.or(page.charset, 'utf-8')} />)
--- src/metadata/icon.jsx
+++ src/metadata/icon.jsx
@@ -1,13 +1,6 @@
 import h from '@h'
 import * as f from '@f'
 
-const __icon__ = Symbol('icon')
-const defaultIcon = 'https://dummyimage.com/192x192/1a1a1a/1a1a1a.png'
-
 export default (page) =>
   ['icon', 'shortcut', 'apple-touch-icon']
-    .forEach(rel => document.head.appendChild(<link href={f.or(page[__icon__], defaultIcon)} rel={rel} type='image/png' />))
-
-export {
-  __icon__
-}
+    .forEach(rel => document.head.appendChild(<link href={f.or(page.icon, 'https://zuen.store/zuen.png')} rel={rel} type='image/png' />))
