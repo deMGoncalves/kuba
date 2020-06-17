cb2d620383164a75b22b8a30ec674e45b06e3148
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:19:40 -0300

    definicao do url

--- src/opengraph/url.jsx
+++ src/opengraph/url.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __url__ = Symbol('url')
-
 export default (page) =>
-  document.head.appendChild(<meta property='og:url' content={f.or(page[__url__], location.href)} />)
-
-export {
-  __url__
-}
+  document.head.append(<meta property='og:url' content={f.or(page.url, location.href)} />)
