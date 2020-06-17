e9819f3810cf464bdb81c37b7d0c594ba3e98dd2
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:07:04 -0300

    definicao da rota padrao

--- src/metadata/base.jsx
+++ src/metadata/base.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __base__ = Symbol('base')
-
 export default (page) =>
-  document.head.appendChild(<base href={f.or(page[__base__], '/')} />)
-
-export {
-  __base__
-}
+  document.head.appendChild(<base href={f.or(page.base, '/')} />)
