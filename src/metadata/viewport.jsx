b0762c0e6c5e8e28dc55c8d0a8307377641ea307
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:12:20 -0300

    definicao do view port

--- src/metadata/viewport.jsx
+++ src/metadata/viewport.jsx
@@ -1,12 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __viewport__ = Symbol('viewport')
-const defaultViewport = 'width=device-width, initial-scale=1, shrink-to-fit=no'
-
 export default (page) =>
-  document.head.appendChild(<meta name='viewport' content={f.or(page[__viewport__], defaultViewport)} />)
-
-export {
-  __viewport__
-}
+  document.head.appendChild(<meta name='viewport' content={f.or(page.viewport, 'width=device-width, initial-scale=1, shrink-to-fit=no')} />)
