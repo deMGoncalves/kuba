53d5743ec986755bb1b12c942a2f0924827e7766
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-16 22:07:37 -0300

    definicao do canonical

--- src/metadata/canonical.jsx
+++ src/metadata/canonical.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __canonical__ = Symbol('canonical')
-
 export default (page) =>
-  document.head.appendChild(<link href={f.or(page[__canonical__], location.href)} rel='canonical' />)
-
-export {
-  __canonical__
-}
+  document.head.appendChild(<link href={f.or(page.canonical, location.href)} rel='canonical' />)
