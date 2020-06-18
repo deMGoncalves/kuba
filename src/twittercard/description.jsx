ba44190b6855e15a58744ac254af53a21483818f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-18 16:31:13 -0300

    ajuste para o twitter buscar a mesma descricao

--- src/twittercard/description.jsx
+++ src/twittercard/description.jsx
@@ -1,12 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __description__ = Symbol('description')
-const defaultDescription = 'JRMod é uma empresa de modificação e aperfeiçoamento de pedais de efeitos do Brasil, considerada uma das melhores do mercado'
-
 export default (page) =>
-  document.head.appendChild(<meta name='twitter:description' content={f.or(page[__description__], defaultDescription)} />)
-
-export {
-  __description__
-}
+  document.head.appendChild(<meta name='twitter:description' content={f.or(page.description, '')} />)
