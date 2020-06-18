ff906565ea9daa59d4514a151d4fea4febe25320
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-18 16:34:50 -0300

    ajuste para montar o site para o twitter

--- src/twittercard/site.jsx
+++ src/twittercard/site.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __site__ = Symbol('site')
-
 export default (page) =>
-  document.head.appendChild(<meta name='twitter:site' content={f.or(page[__site__], '@juniorrossetti')} />)
-
-export {
-  __site__
-}
+  document.head.append(<meta name='twitter:site' content={f.or(page.site, '@zuen')} />)
