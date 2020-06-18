514b872286a86369a760a2cdc56ec5b706ca0270
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-18 16:35:43 -0300

    ajuste no summary para o twitter card

--- src/twittercard/summary.jsx
+++ src/twittercard/summary.jsx
@@ -1,11 +1,5 @@
 import h from '@h'
 import * as f from '@f'
 
-const __summary__ = Symbol('summary')
-
 export default (page) =>
-  document.head.appendChild(<meta name='twitter:card' content={f.or(page[__summary__], 'summary_large_image')} />)
-
-export {
-  __summary__
-}
+  document.head.append(<meta name='twitter:card' content={f.or(page.summary, 'summary_large_image')} />)
