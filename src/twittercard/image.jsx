87cecc2b0d1ad7e48d87ae9cb5b90d554d853165
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-18 16:33:27 -0300

    ajuste para montar as imagens para o twitter

--- src/twittercard/image.jsx
+++ src/twittercard/image.jsx
@@ -1,12 +1,7 @@
 import h from '@h'
 import * as f from '@f'
 
-const __image__ = Symbol('image')
-const defaultUrl = 'https://dummyimage.com/200x200/1a1a1a/1a1a1a.png'
-
 export default (page) =>
-  document.head.appendChild(<meta name='twitter:image' content={f.or(page[__image__], defaultUrl)} />)
-
-export {
-  __image__
-}
+  []
+    .concat(page.image)
+    .forEach(url => document.head.append(<meta name='twitter:image' content={f.or(url, '')} />))
