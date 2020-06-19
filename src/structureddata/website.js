0a2f87605c0a87e8ef5cdd36e7d968f372ea3cb7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-19 10:11:01 -0300

    indentificacao do website e url dinamico

--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -2,12 +2,12 @@ import organization from './organization'
 
 export default {
   '@context': 'http://schema.org',
-  '@id': `${location.href}#website`,
-  '@type': 'Website',
+  '@id': `${location.href}#webpage`,
+  '@type': 'Webpage',
   author: organization,
   image: 'https://zuen.store/zuen.png',
   isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
   name: 'Zuen',
   sameAs: 'https://github.com/oneOffJS',
-  url: 'https://zuen.store'
+  url: location.href
 }
