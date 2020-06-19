843fe6280585b627300b735fb610f9c3c0a0a371
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-19 10:15:16 -0300

    inclusao da descricao do webpage

--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -5,8 +5,8 @@ export default {
   '@id': `${location.href}#webpage`,
   '@type': 'Webpage',
   author: organization,
+  description: 'JavaScript é como uma folha em branco – A liberdade de programar do jeito que faça sentido para o negócio!',
   image: 'https://zuen.store/zuen.png',
-  isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
   name: 'Zuen',
   sameAs: 'https://github.com/oneOffJS',
   url: location.href
