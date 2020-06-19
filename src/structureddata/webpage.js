fc327dd6c26b4e3f48395101457b31d0ef6a702b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-19 11:03:57 -0300

    o conteudo esta invertido

--- src/structureddata/webpage.js
+++ src/structureddata/webpage.js
@@ -1,14 +1,30 @@
+import breadcrumb from './breadcrumb'
 import organization from './organization'
+import website from './website'
 
-export default {
+export default (page) => ({
   '@context': 'http://schema.org',
-  '@id': 'https://zuen.store#website',
-  '@type': 'Website',
+  '@id': `${location.href}#webpage`,
+  '@type': 'Webpage',
   author: organization,
-  description: 'JavaScript é como uma folha em branco – A liberdade de programar do jeito que faça sentido para o negócio!',
-  image: 'https://zuen.store/zuen.png',
-  name: 'Zuen',
-  publisher: organization,
-  sameAs: 'https://github.com/oneOffJS',
-  url: 'https://zuen.store'
-}
+  breadcrumb,
+  description: page.description,
+  image: {
+    '@type': 'ImageObject',
+    '@id': `${page.image[0]}#image`,
+    caption: page.title,
+    url: page.image[0]
+  },
+  inLanguage: 'pr-BR',
+  isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
+  isPartOf: website,
+  primaryImageOfPage: {
+    '@type': 'ImageObject',
+    '@id': `${page.image[0]}#primaryimage`,
+    caption: page.title,
+    url: page.image[0]
+  },
+  name: page.title,
+  sameAs: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
+  url: location.href
+})
--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -1,30 +1,14 @@
-import breadcrumb from './breadcrumb'
 import organization from './organization'
-import website from './website'
 
-export default (page) => ({
+export default {
   '@context': 'http://schema.org',
-  '@id': `${location.href}#webpage`,
-  '@type': 'Webpage',
+  '@id': 'https://zuen.store#website',
+  '@type': 'Website',
   author: organization,
-  breadcrumb,
-  description: page.description,
-  image: {
-    '@type': 'ImageObject',
-    '@id': `${page.image[0]}#image`,
-    caption: page.title,
-    url: page.image[0]
-  },
-  inLanguage: 'pr-BR',
-  isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
-  isPartOf: website,
-  primaryImageOfPage: {
-    '@type': 'ImageObject',
-    '@id': `${page.image[0]}#primaryimage`,
-    caption: page.title,
-    url: page.image[0]
-  },
-  name: page.title,
-  sameAs: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
-  url: location.href
-})
+  description: 'JavaScript é como uma folha em branco – A liberdade de programar do jeito que faça sentido para o negócio!',
+  image: 'https://zuen.store/zuen.png',
+  name: 'Zuen',
+  publisher: organization,
+  sameAs: 'https://github.com/oneOffJS',
+  url: 'https://zuen.store'
+}
