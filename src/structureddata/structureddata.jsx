8380c261cdb8d0386ffaa84cb5989a2fce82acad
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-19 10:59:00 -0300

    deixando apenas o produto como dado estruturado

--- src/structureddata/structureddata.jsx
+++ src/structureddata/structureddata.jsx
@@ -1,25 +1,9 @@
 import h from '@h'
-import breadcrumb from './breadcrumb'
-import organization from './organization'
 import product from './product'
-import website from './website'
 
 export default (Klass) =>
   function () {
     const page = new Klass(...arguments)
-
-    document.body.append(
-      <script type='application/ld+json'>
-        {
-          JSON.stringify([
-            breadcrumb(page),
-            organization,
-            product(page),
-            website
-          ])
-        }
-      </script>
-    )
-
+    document.body.append(<script type='application/ld+json'>{ JSON.stringify(product(page)) }</script>)
     return page
   }
