88575798a2793725d293b6d104659fc267710601
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-19 10:16:33 -0300

    inclusao da descricao do website

--- src/structureddata/webpage.js
+++ src/structureddata/webpage.js
@@ -5,6 +5,7 @@ export default {
   '@id': `${location.href}#website`,
   '@type': 'Website',
   author: organization,
+  description: 'JavaScript é como uma folha em branco – A liberdade de programar do jeito que faça sentido para o negócio!',
   image: 'https://zuen.store/zuen.png',
   name: 'Zuen',
   sameAs: 'https://github.com/oneOffJS',
