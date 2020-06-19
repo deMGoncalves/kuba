a5b88f017a42b5aae28da3b47936955f58b823cd
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-19 12:15:05 -0300

    teste para quanto nao tiver review no produto

--- src/structureddata/organization.js
+++ src/structureddata/organization.js
@@ -1,16 +1,10 @@
 import address from './address'
-import review from './review'
 
 export default {
   '@context': 'http://schema.org',
   '@id': 'https://zuen.store#organization',
   '@type': 'Organization',
   address,
-  aggregateRating: {
-    '@type': 'AggregateRating',
-    ratingValue: '4.6',
-    reviewCount: '2'
-  },
   image: {
     '@type': 'ImageObject',
     '@id': 'https://zuen.store/zuen.png#image',
@@ -25,7 +19,6 @@ export default {
     url: 'https://zuen.store/zuen.png'
   },
   name: 'Zuen',
-  review,
   sameAs: 'https://github.com/oneOffJS/zuen',
   url: 'https://zuen.store'
 }
