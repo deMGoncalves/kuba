fe5b160ea16447cfefd3e859ddaf152e54981421
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-19 10:19:34 -0300

    o webpage deve manter o site que se esta baseado

--- src/structureddata/website.js
+++ src/structureddata/website.js
@@ -7,6 +7,7 @@ export default {
   author: organization,
   description: 'JavaScript é como uma folha em branco – A liberdade de programar do jeito que faça sentido para o negócio!',
   image: 'https://zuen.store/zuen.png',
+  isBasedOn: 'https://www.samsung.com/br/smartphones/galaxy-a30s-a307/SM-A307GZWBZTO',
   name: 'Zuen',
   sameAs: 'https://github.com/oneOffJS',
   url: location.href
