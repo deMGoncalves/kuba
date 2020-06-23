174ea6971b4f34aefea438d311cab2ffe21dc4ef
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-23 11:00:34 -0300

    documentacao da funcao assign

--- src/f/assign.js
+++ src/f/assign.js
@@ -1,14 +1,25 @@
 import curry from './curry'
 
 /**
- * Extende um objeto com mais propriedades
+ * Extende a fonte de forma profunda mantendo a referência a ele.
+ * O objeto de origem pode um `Array`, `Object`, `Number`, `String`, `Boolean`, `Date` e `Function`.
  *
- * @name assign
- * @function
- * @access public
- * @param {*} target Objeto que sera extendido
- * @param {Object} descriptor Novas chaves e valores
- * @return {*} Objeto com as novas propriedades
+ * Observe que, se o objeto de origem tiver vários nós que compartilham uma referência,
+ * o objeto retornado terá a mesma estrutura, mas as referências serão
+ * apontado para o local dentro do valor clonado
+ *
+ * @func
+ * @memberOf f
+ * @since v0.1.0
+ * @category Object
+ * @sig {*} -> {*}
+ * @param {*} target O objeto que sera extendido
+ * @param {*} descriptor O objeto que sera clonado
+ * @return {*} Objeto alvo extendido profundamente clonado
+ * @example
+ *
+ *      const target = { a: 1 };
+ *      f.assign(target, { b: 2 }); //=> { a: 1, b: 2 }
  */
 const assign = (target, descriptor) =>
   Object.assign(target, descriptor)
