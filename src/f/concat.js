85be229ddd6986762c62d3822a1a68ae3d6a5d0b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-23 14:12:34 -0300

    documentacao da funcao concat com os exemplos

--- src/f/concat.js
+++ src/f/concat.js
@@ -1,16 +1,27 @@
 import curry from './curry'
 
 /**
- * Concatena novos valoes dentro do matrix alvo
+ * Retorna o resultado da concatenação das listas ou strings fornecidas
  *
- * @name concat
- * @function
- * @access public
- * @param {Array} target Matrix base
- * @param {*} values Novo valor a ser adicionado
- * @return {Array} Uma nova matrix com os novos valores
+ * Nota: `f.concat` espera que ambos os argumentos sejam do mesmo tipo,
+ * ao contrário do método nativo `Array.prototype.concat`. Vai jogar
+ * um erro se você concatenar uma matriz com um valor não-matriz
+ *
+ * @func
+ * @memberOf f
+ * @since v0.1.0
+ * @category List
+ * @sig [a] -> [a] -> [a]
+ * @sig String -> String -> String
+ * @param {Array} target O primeiro valor
+ * @param {Array} value O segundo valor
+ * @return {Array} O primeiro array com os valores do segundo array
+ * @example
+ *
+ *      f.concat([4, 5, 6], [1, 2, 3]); //=> [4, 5, 6, 1, 2, 3]
+ *      f.concat([], []); //=> []
  */
-const concat = (target, values) =>
-  target.concat(values)
+const concat = (target, value) =>
+  target.concat(value)
 
 export default curry(concat)
