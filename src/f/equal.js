ea729ad5a40b9b56d5cb2a79e647faea07921d06
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-24 18:54:30 -0300

    documentacao base para a funcao equal

--- src/f/equal.js
+++ src/f/equal.js
@@ -1,14 +1,29 @@
 import curry from './curry'
 
 /**
- * Compara se dois valores sao iguais, tanto tipo quanto valor
+ * Retorna `true` se seus argumentos forem equivalentes, `false` caso contrário.
+ * Alças estruturas de dados cíclicos.
  *
- * @name equal
- * @function
- * @access public
+ * Despacha simetricamente para os métodos `iguais` de ambos os argumentos, se
+ * presente.
+ *
+ * @func
+ * @memberOf f
+ * @since v0.15.0
+ * @category f.lation
+ * @sig a -> b -> Boolean
  * @param {*} x Primeiro valor
  * @param {*} y Segundo valor
- * @return {Boolean} Verdadeio se os valores forem iguais
+ * @return {Boolean} Retorna verdadeiro se se ambos forem iguais
+ * @example
+ *
+ *      f.equal(1, 1); //=> true
+ *      f.equal(1, '1'); //=> false
+ *      f.equal([1, 2, 3], [1, 2, 3]); //=> true
+ *
+ *      const a = {}; a.v = a;
+ *      const b = {}; b.v = b;
+ *      f.equal(a, b); //=> true
  */
 const equal = (x, y) =>
   x === y
