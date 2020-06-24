1661ac13c39e6e77713e72e700985c30765a187d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-24 20:00:36 -0300

    documentacao base para a funcao flatten e encapsulando a funcao no curry

--- src/f/flatten.js
+++ src/f/flatten.js
@@ -1,13 +1,24 @@
+import curry from './curry'
 import concat from './concat'
 
 /**
- * Retorna uma nova lista, retirando todas as sub-matrizes e colocando nas nova matriz em primeira profundidade
+ * Retorna uma nova lista, retirando todos os itens (e todas as suas sub-matrizes)
+ * e colocá-los em uma nova matriz, em profundidade
  *
- * @name h
- * @function
- * @access private
- * @param {Array} target Uma matriz com sub-matrizes
- * @return {Array} Uma nova matriz com seus sub-matrizes em primeiro na profundidade
+ * @func
+ * @memberOf f
+ * @since v0.1.0
+ * @category List
+ * @sig [a] -> [b]
+ * @param {Array} target A lista que sera achatado
+ * @return {Array} Uma lista achatada
+ * @see f.unnest
+ * @example
+ *
+ *      f.flatten([1, 2, [3, 4], 5, [6, [7, 8, [9, [10, 11], 12]]]]);
+ *      //=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
  */
-export default (target) =>
+const flatten = (target) =>
   target.reduce(concat, [])
+
+export default curry(flatten)
