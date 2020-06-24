4ef2ccc402ad9c0b052330ae0b07aa3dbc52b87e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-24 19:12:16 -0300

    documentacao base da funcao different

--- src/f/different.js
+++ src/f/different.js
@@ -2,14 +2,29 @@ import curry from './curry'
 import equal from './equal'
 
 /**
- * Testa se os dois valores fornecidos nao diferente
+ * Retorna `true` se seus argumentos não forem equivalentes, `false` caso contrário.
+ * Alças estruturas de dados cíclicos.
  *
- * @name different
- * @function
- * @access public
+ * Despacha simetricamente para os métodos `iguais` de ambos os argumentos, se
+ * presente.
+ *
+ * @func
+ * @memberOf f
+ * @since v0.15.0
+ * @category f.lation
+ * @sig a -> b -> Boolean
  * @param {*} x Primeiro valor
  * @param {*} y Segundo valor
- * @return {Boolean} Verdadeiro se os valores forem diferente
+ * @return {Boolean} Retorna verdadeiro se se ambos forem iguais
+ * @example
+ *
+ *      f.different(1, 1); //=> true
+ *      f.different(1, '1'); //=> false
+ *      f.different([1, 2, 3], [1, 2, 3]); //=> true
+ *
+ *      const a = {}; a.v = a;
+ *      const b = {}; b.v = b;
+ *      f.different(a, b); //=> true
  */
 const different = (x, y) =>
   !equal(x, y)
