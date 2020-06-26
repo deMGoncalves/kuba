37274ec8eab62cf38e879823b9ec51c5c524e8b6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-06-25 22:24:45 -0300

    documentacao base para a funcao foreach

--- src/f/forEach.js
+++ src/f/forEach.js
@@ -1,14 +1,38 @@
 import curry from './curry'
 
 /**
- * Executa uma funcao callback para cada elemento da matrix
+ * Iterate over an input `list`, calling a provided function `fn` for each
+ * element in the list.
  *
- * @name forEach
- * @function
- * @access public
- * @param {Array} target Array de valores que sera usando para repassar a funcao
- * @param {Function} callback Funcao callback que recebe o valor de um indice do array
- * @return {undefined} Nao retorna nada
+ * `fn` receives one argument: *(value)*.
+ *
+ * Note: `f.forEach` does not skip deleted or unassigned indices (sparse
+ * arrays), unlike the native `Array.prototype.forEach` method. For more
+ * details on this behavior, see:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/f.ference/Global_Objects/Array/forEach#Description
+ *
+ * Also note that, unlike `Array.prototype.forEach`, f.mda's `forEach` returns
+ * the original array. In some libraries this function is named `each`.
+ *
+ * Dispatches to the `forEach` method of the second argument, if present.
+ *
+ * @func
+ * @memberOf f
+ * @since v0.1.1
+ * @category List
+ * @sig (a -> *) -> [a] -> [a]
+ * @param {Function} fn The function to invoke. f.ceives one argument, `value`.
+ * @param {Array} list The list to iterate over.
+ * @return {Array} The original list.
+ * @see f.addIndex
+ * @example
+ *
+ *      const printXPlusFive = x => console.log(x + 5);
+ *      f.forEach(printXPlusFive, [1, 2, 3]); //=> [1, 2, 3]
+ *      // logs 6
+ *      // logs 7
+ *      // logs 8
+ * @symb f.forEach(f, [a, b, c]) = [a, b, c]
  */
 const forEach = (target, callback) =>
   target.forEach(callback)
