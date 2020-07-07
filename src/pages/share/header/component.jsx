cfa3c047bf3305b7112543d51885ba1b3aa91a7b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-07 14:05:11 -0300

    estrutura base do header

--- src/pages/share/header/component.jsx
+++ src/pages/share/header/component.jsx
@@ -1,5 +1,13 @@
 import h from '@h'
+import * as s from '@pages/share'
 import style from './style.css'
 
 export default () =>
-  <header className={style.header} />
+  <header className={style.header}>
+    <s.Container className={style.header__container}>
+      <a className={style.header__menu}>Menu</a>
+      <a className={style.header__apple}>Apple</a>
+      <nav className={style.header__nav} />
+      <a className={style.header__bag}>Bag</a>
+    </s.Container>
+  </header>
