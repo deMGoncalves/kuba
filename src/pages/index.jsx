a8634ec9eb95a34ac286befa780955dfe27022ad
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-07 13:22:28 -0300

    as functions component no jsx so funcionam se a primeira letra for caixa alta

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,6 @@ import h from '@h'
 import router from '@router'
 
 router(/^\/$/, async () => {
-  const { default: iPhone } = await import(/* webpackChunkName: "iphone" */ '@pages/iphone')
-  document.body.append(<iPhone />)
+  const { default: IPhone } = await import(/* webpackChunkName: "iphone" */ '@pages/iphone')
+  document.body.append(<IPhone />)
 })
