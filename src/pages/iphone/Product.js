25f04b70a05dfef22d20f6bea7874033d40a5c6c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-07 11:55:00 -0300

    modificando o nome e descricao da pagina

--- src/pages/iphone/Product.js
+++ src/pages/iphone/Product.js
@@ -2,26 +2,19 @@ import { paint } from '@h'
 import component from './component'
 import markut from '@markup'
 
-import imagemFrente from './samsung-galaxy-a30s-64gb-branco-frente.jpg'
-import imagemVerso from './samsung-galaxy-a30s-64gb-branco-verso.jpg'
-import imagemDiagonalDireito from './samsung-galaxy-a30s-64gb-branco-diagonal-direito.jpg'
-import imagemDiagonalEsquerdo from './samsung-galaxy-a30s-64gb-branco-diagonal-esquerdo.jpg'
-import imagemLateralDireito from './samsung-galaxy-a30s-64gb-branco-lateral-direito.jpg'
-import imagemLateralEsquerdo from './samsung-galaxy-a30s-64gb-branco-lateral-esquerdo.jpg'
-
 @paint(component)
 @markut
 class Product {
   get description () {
-    return 'Com seu processador octa-core, o Galaxy A30s oferece desempenho potente, memória RAM e armazenamento para melhorar sua experiência gamer móvel, e carregamento rápido para você aproveitar seu dia'
+    return 'Descubra mais sobre o iPhone, o aparelho pessoal mais poderoso do mundo. Confira os novos iPhone 11, iPhone 11 Pro e iPhone 11 Pro Max.'
   }
 
   get image () {
-    return [imagemFrente, imagemVerso, imagemDiagonalDireito, imagemDiagonalEsquerdo, imagemLateralDireito, imagemLateralEsquerdo]
+    return []
   }
 
   get title () {
-    return 'Samsung Galaxy A30s 64GB Branco'
+    return 'iPhone - Apple (BR)'
   }
 }
 
