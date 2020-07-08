39d24a7610c6ef61acb3ca29a6a2a3c591cb756e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-08 08:21:33 -0300

    inclusao da extensao do jpeg no loader de imagens

--- webpack.common.js
+++ webpack.common.js
@@ -44,7 +44,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
         use: [
           {
             loader: 'file-loader',
