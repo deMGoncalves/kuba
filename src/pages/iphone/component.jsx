46143f14cbd71e3ee12ad3b90e7c15339e8d29f4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-08 11:05:12 -0300

    inclusao do nav de produto

--- src/pages/iphone/component.jsx
+++ src/pages/iphone/component.jsx
@@ -1,7 +1,16 @@
 import h from '@h'
 import * as s from '@pages/share'
+import style from './style.css'
 
 export default () =>
   <s.Main>
     <s.Header />
+    <section className={style.iPhone}>
+      <nav className={style.iPhone__nav}>
+        <s.Container className={style.iPhone__container}>
+          <strong className={style.iPhone__name}>iPhone X🅁</strong>
+          <data className={style.iPhone__price} value={'4.299'}>A partir de R$ 4.299</data>
+        </s.Container>
+      </nav>
+    </section>
   </s.Main>
