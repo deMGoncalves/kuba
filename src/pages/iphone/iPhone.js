4c66b00db6c37acd334188612b7610a41d65bcd9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-08 08:10:48 -0300

    inclusao da imagem de capa da pagina de produto

--- src/pages/iphone/iPhone.js
+++ src/pages/iphone/iPhone.js
@@ -2,11 +2,17 @@ import { paint } from '@h'
 import component from './component'
 import markut from '@markup'
 
+import iPhoneXRSelect2019Family from './iphone-xr-select-2019-family.jpeg'
+
 @paint(component)
 @markut
 class iPhone {
   get description () {
     return 'Compre o iPhone XR em seis lindas cores, com Face ID avançado, chip A12 Bionic e tela Liquid Retina. Compre agora com frete grátis.'
+  }
+
+  get image () {
+    return [iPhoneXRSelect2019Family]
   }
 
   get title () {
