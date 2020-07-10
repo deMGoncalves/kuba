873e161ee902c4d7e1275118766cef20aab621c3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-10 16:31:20 -0300

    inclusao href para passar nas metricas de seo

--- src/pages/share/header/component.jsx
+++ src/pages/share/header/component.jsx
@@ -5,15 +5,15 @@ import style from './style.css'
 export default () =>
   <header className={style.header}>
     <s.Container className={style.header__container}>
-      <a className={style.header__menu}>Menu</a>
-      <a className={style.header__apple}>Apple</a>
+      <a className={style.header__menu} href='#'>Menu</a>
+      <a className={style.header__apple} href='#'>Apple</a>
       <nav className={style.header__nav}>
         {
           ['Mac', 'iPad', 'iPhone', 'Watch', 'TV', 'Music', 'Suporte'].map(text =>
             <a className={style.header__link} href='#'>{ text }</a>)
         }
-        <a className={style.header__search}>Search</a>
+        <a className={style.header__search} href='#'>Search</a>
       </nav>
-      <a className={style.header__bag}>Bag</a>
+      <a className={style.header__bag} href='#'>Bag</a>
     </s.Container>
   </header>
