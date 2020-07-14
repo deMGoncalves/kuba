89bbd75883386488f66e653639906623f22f19e2
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-14 09:39:52 -0300

    recebendo o produto no constructor

--- src/pages/product/nav/Nav.js
+++ src/pages/product/nav/Nav.js
@@ -1,9 +1,14 @@
 import { paint } from '@h'
 import component from './component'
 
+const oneOff = Symbol('oneOff')
+
 @paint(component)
 class Nav {
-
+  constructor (product) {
+    this[oneOff].product = product
+    return this
+  }
 }
 
 export default Nav
