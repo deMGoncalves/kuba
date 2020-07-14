620e6746258cc4e4055250e56c8ca5e8b946a589
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-14 10:02:10 -0300

    agora o component pega o preco do objeto nav, respeitando os niveis de encapsulamento

--- src/pages/product/nav/component.jsx
+++ src/pages/product/nav/component.jsx
@@ -6,6 +6,6 @@ export default (nav) =>
   <nav className={style.nav}>
     <s.Container className={style.nav__container}>
       <strong className={style.nav__name}>{ nav.name }</strong>
-      <data className={style.nav__price} value='4.299'>A partir de R$ 4.299</data>
+      <data className={style.nav__price} value={nav.price}>R$ { nav.price }</data>
     </s.Container>
   </nav>
