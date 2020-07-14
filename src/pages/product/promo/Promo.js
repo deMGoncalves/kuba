e557a48299985501ad5b99c3d761ef67ff4e6303
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-14 17:43:53 -0300

    recebendo o produto e exibindo o testo da promo

--- src/pages/product/promo/Promo.js
+++ src/pages/product/promo/Promo.js
@@ -1,9 +1,18 @@
 import { paint } from '@h'
 import component from './component'
 
+const oneOff = Symbol('oneOff')
+
 @paint(component)
 class Promo {
+  get text () {
+    return this[oneOff].product.promo
+  }
 
+  contructor (product) {
+    this[oneOff] = { product }
+    return this
+  }
 }
 
 export default Promo
