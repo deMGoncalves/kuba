ba3e93ba8dd2092c548373439c96b0e6116ff8b5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-14 09:07:24 -0300

    buscando os dados do produto

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 import h from '@h'
 import router from '@router'
 
-router(/^\/$/, async () => {
+router(/^\/shop\/buy\/([\w-]+)$/, async (productName) => {
   const { default: Product } = await import(/* webpackChunkName: "product" */ '@pages/product')
-  document.body.append(<Product />)
+  const data = await fetch(`/${productName}.json`).then(response => response.json())
+  document.body.append(<Product {...data} />)
 })
