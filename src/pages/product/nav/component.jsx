d0a1b43b83a017477adf260d5bb91900766216a0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-14 09:41:40 -0300

    no template o nome do produto sera dinamico, pegando da propria nav

--- src/pages/product/nav/component.jsx
+++ src/pages/product/nav/component.jsx
@@ -2,10 +2,10 @@ import h from '@h'
 import * as s from '@pages/share'
 import style from './style.css'
 
-export default () =>
+export default (nav) =>
   <nav className={style.nav}>
     <s.Container className={style.nav__container}>
-      <strong className={style.nav__name}>iPhone X🅁</strong>
+      <strong className={style.nav__name}>{ nav.name }</strong>
       <data className={style.nav__price} value='4.299'>A partir de R$ 4.299</data>
     </s.Container>
   </nav>
