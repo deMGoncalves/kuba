b797590be6ffe5bfd3d18674d2d90734e12d4f6b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-14 17:44:53 -0300

    usando o dado que vem do produto

--- src/pages/product/promo/component.jsx
+++ src/pages/product/promo/component.jsx
@@ -2,11 +2,9 @@ import h from '@h'
 import * as s from '@pages/share'
 import style from './style.css'
 
-export default () =>
+export default (promo) =>
   <div className={style.promo}>
     <s.Container className={style.promo__container}>
-      <p className={style.promo__p}>
-        Frete grátis para todos os iPhones. Em São Paulo, os pedidos qualificados têm envio gratuito no dia seguinte.
-      </p>
+      <p className={style.promo__p}>{ promo.text }</p>
     </s.Container>
   </div>
