b1b9141c482fbfd4dd38137ebabf3b1aea9f56ff
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-15 11:28:01 -0300

    o tipo do card é fixo

--- src/markup/twittercard/summary.jsx
+++ src/markup/twittercard/summary.jsx
@@ -1,5 +1,4 @@
 import h from '@h'
-import * as f from '@f'
 
-export default (page) =>
-  document.head.append(<meta name='twitter:card' content={f.or(page.summary, 'summary_large_image')} />)
+export default () =>
+  document.head.append(<meta name='twitter:card' content='summary_large_image' />)
