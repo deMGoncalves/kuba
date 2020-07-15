8a7f2abe5632cffcd29d12be5e52cf64d3d896ad
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-15 11:28:29 -0300

    o page title default é zuen

--- src/markup/twittercard/title.jsx
+++ src/markup/twittercard/title.jsx
@@ -2,4 +2,4 @@ import h from '@h'
 import * as f from '@f'
 
 export default (page) =>
-  document.head.append(<meta name='twitter:title' content={f.or(page.title, 'Zuen Store')} />)
+  document.head.append(<meta name='twitter:title' content={f.or(page.title, 'Zuen')} />)
