4cc356ec905e62871bd946ab9246730ce436892f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-15 10:25:43 -0300

    usandoo o forEach do f

--- src/markup/metadata/icon.jsx
+++ src/markup/metadata/icon.jsx
@@ -2,5 +2,7 @@ import h from '@h'
 import * as f from '@f'
 
 export default (page) =>
-  ['icon', 'shortcut', 'apple-touch-icon']
-    .forEach(rel => document.head.append(<link href={f.or(page.icon, 'https://zuen.store/zuen.png')} rel={rel} type='image/png' />))
+  f.forEach(
+    ['icon', 'shortcut', 'apple-touch-icon'],
+    rel => document.head.append(<link href={f.or(page.icon, 'https://zuen.store/zuen.png')} rel={rel} type='image/png' />)
+  )
