cdb173177a168470aa040f390ef8d23be60bc651
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-07-15 11:25:03 -0300

    a url do site é fixo

--- src/markup/opengraph/url.jsx
+++ src/markup/opengraph/url.jsx
@@ -1,5 +1,4 @@
 import h from '@h'
-import * as f from '@f'
 
-export default (page) =>
-  document.head.append(<meta property='og:url' content={f.or(page.url, location.href)} />)
+export default () =>
+  document.head.append(<meta property='og:url' content={location.href} />)
