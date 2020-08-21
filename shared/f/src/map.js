6a7c2b0ba34a6aaf06d215a61657a0534dd28239
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-21 14:32:15 -0300

    estou removendo as doc

--- shared/f/src/map.js
+++ shared/f/src/map.js
@@ -1,16 +1,6 @@
 import curry from './curry'
 import or from './or'
 
-/**
- * Executar uma funcao callback para cada elemento da matrix e devolve o retorno no lugar do elemento
- *
- * @name map
- * @function
- * @access public
- * @param {Array} target Matrix com o valoes alvos
- * @param {Function} callback Funcao callback que trata cada valor
- * @return {Array} Uma nova matrix com os retornos mapeado
- */
 const map = (target, callback) =>
   or(target['f/map'], target).map(callback)
 
