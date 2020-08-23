e4cef585e9f061b44dac7597edac99a814327e0d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-23 14:08:56 -0300

    testa a criacao de um elemento simples

--- shared/h/src/h/h.spec.js
+++ shared/h/src/h/h.spec.js
@@ -4,13 +4,10 @@ describe('h', function () {
   test('cria um elemento', function () {
     function Component () {
       return (
-        <div>Rex</div>
+        <div />
       )
     }
 
-    const el = document.createElement('div')
-    el.innerHTML = 'Rex'
-
-    expect(<Component />).toEqual(el)
+    expect(<Component />).toEqual(document.createElement('div'))
   })
 })
