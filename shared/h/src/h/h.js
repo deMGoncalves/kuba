199508a079498724bb0e5dfe4a7850d50d11513e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-23 11:48:53 -0300

    nao vou trabalhar com comentario, meu codigo e testes dever deixar claro como o component funcionar

--- shared/h/src/h/h.js
+++ shared/h/src/h/h.js
@@ -5,25 +5,10 @@ import executeComponent from '@rex/h/src/executeComponent'
 import isComponent from '@rex/h/src/isComponent'
 import isTagName from '@rex/h/src/isTagName'
 
-/**
- * Hyperscript, crie um HyperText com JavaScript no cliente usando JSX
- *
- * Esta sintaxe estranha de tags não é uma string, nem HTML
- * É chamada JSX e é uma extensão de sintaxe para JavaScript
- * Recomendo usar JSX com o h para descrever como a UI deveria parecer
- * JSX pode lembrar uma linguagem de template, mas que vem com todo o poder do JavaScript
- *
- * @name h
- * @function
- * @access public
- * @param {String|function} tagNameOrComponent Nome do tag HTML ou nome do Web Componente
- * @param {Object} descriptor Atributos, classes e eventos do Elemento ou Web componente
- * @param {Array} children Elementos filhos
- * @return {HTMLElement} Elemento com os atributos, classes, eventos e filhos que foram especificados
- */
 export default (tagNameOrComponent, descriptor, ...children) =>
   f.cond(
     [isTagName, createElement],
     [isComponent, executeComponent],
+    [(...args) => { console.log(...args); return false }, () => {}],
     [f.T, createComment]
   )(tagNameOrComponent, { ...descriptor }, f.flatten(children))
--- shared/h/src/h/h.spec.js
+++ shared/h/src/h/h.spec.js
@@ -1,7 +1,13 @@
-import h from '@rex/h'
+import h, { Fragment } from '@rex/h'
 
 describe('h', function () {
   test('cria um fragmento para retorna mais de um elemento root', function () {
+    const Faq = () =>
+      <>
+        <h1>Pergunta</h1>
+        <p>respost</p>
+      </>
 
+    expect(<Faq />).toEqual()
   })
 })
