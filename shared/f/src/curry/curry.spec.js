7c78a77de33dfbee8ea23f43305be78bcf038799
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-24 11:31:58 -0300

    executa o twoParameters quando a funcao alvo tiver dois parametros

--- shared/f/src/curry/curry.spec.js
+++ shared/f/src/curry/curry.spec.js
@@ -1,7 +1,9 @@
-import * as f from '@rex/f'
+import curry from './curry'
 import oneParameter from './oneParameter'
+import twoParameters from './twoParameters'
 
 jest.mock('./oneParameter')
+jest.mock('./twoParameters')
 
 describe('f.curry', function () {
   beforeEach(function () {
@@ -9,14 +11,30 @@ describe('f.curry', function () {
   })
 
   test('executa o oneParameter quando a funcao alvo tiver um parametro', function () {
-    oneParameter.mockImplementation((target) => (a) => target(a))
+    oneParameter.mockImplementation((target) => function (_a) { return target(...arguments) })
 
     const x = (a) => a
-    const y = f.curry(x)
+    const y = curry(x)
 
     expect(y('rex')).toBe('rex')
     expect(oneParameter).toHaveBeenCalled()
     expect(oneParameter).toHaveBeenCalledTimes(1)
     expect(oneParameter).toHaveBeenCalledWith(x)
+
+    expect(twoParameters).not.toHaveBeenCalled()
+  })
+
+  test('executa o twoParameters quando a funcao alvo tiver dois parametros', function () {
+    twoParameters.mockImplementation((target) => function (_a, _b) { return target(...arguments) })
+
+    const x = (a, b) => a + b
+    const y = curry(x)
+
+    expect(y('rex', '.js')).toBe('rex.js')
+    expect(twoParameters).toHaveBeenCalled()
+    expect(twoParameters).toHaveBeenCalledTimes(1)
+    expect(twoParameters).toHaveBeenCalledWith(x)
+
+    expect(oneParameter).not.toHaveBeenCalled()
   })
 })
