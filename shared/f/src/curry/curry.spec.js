834851042a48435acfe97e5647faf30783de4beb
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-24 10:54:30 -0300

    agora sim :D vamos testar apeans o que o curry realmente faz

--- shared/f/src/curry/curry.spec.js
+++ shared/f/src/curry/curry.spec.js
@@ -1,70 +1,22 @@
 import * as f from '@rex/f'
+import oneParameter from '@rex/f/src/curry/oneParameter'
+
+jest.mock('@rex/f/src/curry/oneParameter')
 
 describe('f.curry', function () {
-  test('execucao unica', function () {
-    const x = (a, b, c) => a * b / c // x(2, 6, 3) => 2 * 6 / 3
+  beforeEach(function () {
+    jest.clearAllMocks()
+  })
+
+  test('executa o oneParameter quando a funcao alvo tiver um parametro', function () {
+    oneParameter.mockImplementation((target) => (a) => target(a))
+
+    const x = (a) => a
     const y = f.curry(x)
 
-    expect(y(2, 6, 3)).toBe(4)
-  })
-
-  test('execucao multipla', function () {
-    const x = (a, b, c) => a * b / c // x(2, 6, 3) => 2 * 6 / 3
-    const y = f.curry(x)
-
-    expect(y(2)(6)(3)).toBe(4)
-    expect(y(2, 6)(3)).toBe(4)
-    expect(y(2)(6, 3)).toBe(4)
-  })
-
-  describe('f.__', function () {
-    test('execucao multipla', function () {
-      const x = (a, b, c) => a * b / c // x(2, 6, 3) => 2 * 6 / 3
-      const y = f.curry(x)
-
-      expect(y(2)(6, 3)).toBe(4)
-      expect(y(2, 6)(3)).toBe(4)
-
-      expect(y(f.__, 6)(2, 3)).toBe(4)
-      expect(y(f.__, 6, 3)(2)).toBe(4)
-      expect(y(2, f.__, 3)(6)).toBe(4)
-      expect(y(2, 6, f.__)(3)).toBe(4)
-
-      expect(y(f.__, f.__, 3)(2, 6)).toBe(4)
-      expect(y(2, f.__, f.__)(6, 3)).toBe(4)
-      expect(y(f.__, 6, f.__)(2, 3)).toBe(4)
-
-      expect(y()(2, 6, 3)).toBe(4)
-      expect(y(f.__, f.__, f.__)(2, 6, 3)).toBe(4)
-    })
-  })
-
-  describe('f.arity', function () {
-    test('excucao unica', function () {
-      const x = (a, b, c, d) => (a * b / c) + d // x(2, 6, 3, 1) => (2 * 6 / 3) + 1
-      const y = f.curry(f.arity(3, x))
-
-      expect(y(2, 6, 3, 1)).toBe(5)
-    })
-
-    test('execucao multipla', function () {
-      const x = (a, b, c, d) => (a * b / c) + d // x(2, 6, 3, 1) => (2 * 6 / 3) + 1
-      const y = f.curry(f.arity(3, x))
-
-      expect(y(2)(6, 3, 1)).toBe(5)
-      expect(y(2, 6)(3, 1)).toBe(5)
-
-      expect(y(f.__, 6)(2, 3, 1)).toBe(5)
-      expect(y(f.__, 6, 3)(2, 1)).toBe(5)
-      expect(y(2, f.__, 3)(6, 1)).toBe(5)
-      expect(y(2, 6, f.__)(3, 1)).toBe(5)
-
-      expect(y(f.__, f.__, 3)(2, 6, 1)).toBe(5)
-      expect(y(2, f.__, f.__)(6, 3, 1)).toBe(5)
-      expect(y(f.__, 6, f.__)(2, 3, 1)).toBe(5)
-
-      expect(y()(2, 6, 3, 1)).toBe(5)
-      expect(y(f.__, f.__, f.__)(2, 6, 3, 1)).toBe(5)
-    })
+    expect(y('rex')).toBe('rex')
+    expect(oneParameter).toHaveBeenCalled()
+    expect(oneParameter).toHaveBeenCalledTimes(1)
+    expect(oneParameter).toHaveBeenCalledWith(x)
   })
 })
