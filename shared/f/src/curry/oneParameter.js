0c526153b89da4f39273131a5f5992140508e11f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-25 10:26:46 -0300

    removendo comentarios

--- shared/f/src/curry/oneParameter.js
+++ shared/f/src/curry/oneParameter.js
@@ -1,13 +1,5 @@
 import { isGap } from '../gap'
 
-/**
- * Função de curry otimizado para um argumento.
- *
- * @private
- * @category Function
- * @param {Function} target A função para o curry.
- * @return {Function} A função curried.
- */
 export default (target) =>
   function f1 (a0) {
     if (arguments.length === 0 || isGap(a0)) {
