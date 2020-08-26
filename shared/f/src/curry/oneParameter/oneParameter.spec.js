722e490c38a3c32f13b4b751493258603421c06d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-26 08:18:12 -0300

    retorna uma funcao callback quando o parametro for um __

--- shared/f/src/curry/oneParameter/oneParameter.spec.js
+++ shared/f/src/curry/oneParameter/oneParameter.spec.js
@@ -1,8 +1,30 @@
 import oneParameter from './oneParameter'
+import __, { isGap } from '@rex/f/src/gap'
 
-test('retorna uma funcao callback quando nao passar nenhum parametro', function () {
-  const x = (a) => a
-  const y = oneParameter(x)
+jest.mock('@rex/f/src/gap')
 
-  expect(y()).toBeInstanceOf(Function)
+describe('f.curry.oneParameter', function () {
+  beforeEach(function () {
+    jest.clearAllMocks()
+  })
+
+  test('retorna uma funcao callback quando nao passar nenhum parametro', function () {
+    const x = (a) => a
+    const y = oneParameter(x)
+
+    expect(y()).toBeInstanceOf(Function)
+  })
+
+  test('retorna uma funcao callback quando o parametro for um __', function () {
+    isGap.mockReturnValue(true)
+
+    const x = (a) => a
+    const y = oneParameter(x)
+
+    expect(y(__)).toBeInstanceOf(Function)
+
+    expect(isGap).toHaveBeenCalled()
+    expect(isGap).toHaveBeenCalledTimes(1)
+    expect(isGap).toHaveBeenCalledWith(__)
+  })
 })
