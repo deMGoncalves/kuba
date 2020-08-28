3a125514d243c9b841d46fca5bb4d4333d0d785e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-28 08:24:19 -0300

    devolve uma funcao callback quando passar apenas um parametro e sendo um __

--- shared/f/src/curry/twoParameters/twoParameters.spec.js
+++ shared/f/src/curry/twoParameters/twoParameters.spec.js
@@ -1,6 +1,6 @@
 import oneParameter from '@rex/f/src/curry/oneParameter'
 import twoParameters from './twoParameters'
-import { isGap } from '@rex/f/src/gap'
+import __, { isGap } from '@rex/f/src/gap'
 
 jest.mock('@rex/f/src/curry/oneParameter')
 jest.mock('@rex/f/src/gap')
@@ -18,5 +18,20 @@ describe('f.curry.twoParameters', function () {
 
     expect(oneParameter).not.toHaveBeenCalled()
     expect(isGap).not.toHaveBeenCalled()
+  })
+
+  test('devolve uma funcao callback quando passar apenas um parametro e sendo um __', function () {
+    isGap.mockReturnValue(true)
+
+    const x = (a, b) => a + b
+    const y = twoParameters(x)
+
+    expect(y(__)).toBeInstanceOf(Function)
+
+    expect(isGap).toHaveBeenCalled()
+    expect(isGap).toHaveBeenCalledTimes(1)
+    expect(isGap).toHaveBeenCalledWith(__)
+
+    expect(oneParameter).not.toHaveBeenCalled()
   })
 })
