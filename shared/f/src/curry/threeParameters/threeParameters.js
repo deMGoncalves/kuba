11c1154eca7ef5fd3b6a1a9a50d4b550bdfca2c9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-08-31 15:14:57 -0300

    nao posso executar os targets com call

--- shared/f/src/curry/threeParameters/threeParameters.js
+++ shared/f/src/curry/threeParameters/threeParameters.js
@@ -10,30 +10,30 @@ export default (target) =>
       case 1:
         return isGap(a0)
           ? callback
-          : twoParameters((...args) => target.call(this, a0, ...args))
+          : twoParameters((...args) => target(a0, ...args))
       case 2:
         return isGap(a0) && isGap(a1)
           ? callback
           : isGap(a0)
-            ? twoParameters((_a0, ...args) => target.call(this, _a0, a1, ...args))
+            ? twoParameters((_a0, ...args) => target(_a0, a1, ...args))
             : isGap(a1)
-              ? twoParameters((...args) => target.call(this, a0, ...args))
-              : oneParameter((...args) => target.call(this, a0, a1, ...args))
+              ? twoParameters((...args) => target(a0, ...args))
+              : oneParameter((...args) => target(a0, a1, ...args))
       default:
         return isGap(a0) && isGap(a1) && isGap(a2)
           ? callback
           : isGap(a0) && isGap(a1)
-            ? twoParameters((_a0, _a1, ...args) => target.call(this, _a0, _a1, a2, ...args))
+            ? twoParameters((_a0, _a1, ...args) => target(_a0, _a1, a2, ...args))
             : isGap(a0) && isGap(a2)
-              ? twoParameters((_a0, ...args) => target.call(this, _a0, a1, ...args))
+              ? twoParameters((_a0, ...args) => target(_a0, a1, ...args))
               : isGap(a1) && isGap(a2)
-                ? twoParameters((...args) => target.call(this, a0, ...args))
+                ? twoParameters((...args) => target(a0, ...args))
                 : isGap(a0)
-                  ? oneParameter((_a0, ...args) => target.call(this, _a0, a1, a2, ...args))
+                  ? oneParameter((_a0, ...args) => target(_a0, a1, a2, ...args))
                   : isGap(a1)
-                    ? oneParameter((_a1, ...args) => target.call(this, a0, _a1, a2, ...args))
+                    ? oneParameter((_a1, ...args) => target(a0, _a1, a2, ...args))
                     : isGap(a2)
-                      ? oneParameter((...args) => target.call(this, a0, a1, ...args))
-                      : target.call(this, ...arguments)
+                      ? oneParameter((...args) => target(a0, a1, ...args))
+                      : target(...arguments)
     }
   }
