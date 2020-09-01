18f37de0d5b9974970191dffe35e6c2bbdb2844c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-01 09:52:50 -0300

    regorna uma funcao callback que sempre retorna o valor/referencia passado

--- shared/f/src/always/always.js
+++ shared/f/src/always/always.js
@@ -1,6 +1,8 @@
 import curry from '@rex/f/src/curry'
+import magic from '@rex/f/src/magic'
+import or from '@rex/f/src/or'
 
 const always = (value) =>
-  () => value
+  () => or(value[magic('f/always')], value)
 
 export default curry(always)
--- shared/f/src/always/always.spec.js
+++ shared/f/src/always/always.spec.js
@@ -1,10 +1,30 @@
-import * as f from '@rex/f'
+import always from './always'
+import magic from '@rex/f/src/magic'
+import or from '@rex/f/src/or'
+
+jest.mock('@rex/f/src/magic')
+jest.mock('@rex/f/src/or')
 
 describe('f.always', function () {
+  beforeEach(function () {
+    jest.clearAllMocks()
+  })
+
   test('retorna uma funcao callback que sempre retorna o valor/referencia passado', function () {
-    const x = f.always('rex')
+    magic.mockReturnValue('f/always')
+    or.mockImplementation((a, b) => a || b)
+
+    const x = always('rex')
 
     expect(x).toBeInstanceOf(Function)
     expect(x()).toBe('rex')
+
+    expect(magic).toHaveBeenCalled()
+    expect(magic).toHaveBeenCalledTimes(1)
+    expect(magic).toHaveBeenCalledWith('f/always')
+
+    expect(or).toHaveBeenCalled()
+    expect(or).toHaveBeenCalledTimes(1)
+    expect(or).toHaveBeenCalledWith(undefined, 'rex')
   })
 })
