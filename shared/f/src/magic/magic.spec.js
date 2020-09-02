12aefdf3bc975072d85f9e0b08a85a4aee8bb050
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-02 13:51:44 -0300

    nao cria um simbolo duas vezes, memoriza todas as keys criadas

--- shared/f/src/magic/magic.spec.js
+++ shared/f/src/magic/magic.spec.js
@@ -5,14 +5,14 @@ import symbol from './symbol'
 jest.mock('@rex/f/src/or')
 jest.mock('./symbol')
 
+const rex = Symbol('f/rex')
+
 describe('f.magic', function () {
   beforeEach(function () {
     jest.clearAllMocks()
   })
 
   test('retorna um simbolo para a key fornecido', function () {
-    const rex = Symbol('f/rex')
-
     or.mockReturnValue(rex)
     symbol.mockReturnValue(rex)
 
@@ -21,6 +21,21 @@ describe('f.magic', function () {
     expect(or).toHaveBeenCalled()
     expect(or).toHaveBeenCalledTimes(1)
     expect(or).toHaveBeenCalledWith(undefined, rex)
+
+    expect(symbol).toHaveBeenCalled()
+    expect(symbol).toHaveBeenCalledTimes(1)
+    expect(symbol).toHaveBeenCalledWith('f/rex')
+  })
+
+  test('nao cria um simbolo duas vezes, memoriza todas as keys criadas', function () {
+    or.mockReturnValue(rex)
+    symbol.mockReturnValue(rex)
+
+    expect(magic('f/rex')).toEqual(rex)
+
+    expect(or).toHaveBeenCalled()
+    expect(or).toHaveBeenCalledTimes(1)
+    expect(or).toHaveBeenCalledWith(rex, rex)
 
     expect(symbol).toHaveBeenCalled()
     expect(symbol).toHaveBeenCalledTimes(1)
