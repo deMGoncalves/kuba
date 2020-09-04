c7782276805260afed9f2a8bad44f81cbcfdc026
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-04 18:12:54 -0300

    nao preciso do comentario

--- shared/f/src/and.js
+++ shared/f/src/and.js
@@ -1,15 +1,5 @@
 import curry from './curry'
 
-/**
- * Operador logico and
- *
- * @name adn
- * @function
- * @access public
- * @param {*} x Primeiro valor
- * @param {*} y Segundo valor
- * @return {Boolean} Verdadeio se a condicao for verdadeiro
- */
 const and = (x, y) =>
   x && y
 
