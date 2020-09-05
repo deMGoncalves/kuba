85b4213676f785b50d717ba113de8080fef0c8a5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-05 10:17:10 -0300

    com a utilizacao do chain e do hook consigo remover codigo duplicado

--- apps/todomvc/src/markup/markup.js
+++ apps/todomvc/src/markup/markup.js
@@ -1,18 +1,7 @@
 import * as f from '@rex/f'
+import hook from '@rex/hook'
 import metadata from './metadata'
 import opengraph from './opengraph'
 import twittercard from './twittercard'
 
-export default (Klass) =>
-  function () {
-    const page = new Klass(...arguments)
-
-    setImmediate(() =>
-      f.forEach(
-        [metadata, opengraph, twittercard],
-        m => m(page)
-      )
-    )
-
-    return page
-  }
+export default hook(f.chain(metadata, opengraph, twittercard))
