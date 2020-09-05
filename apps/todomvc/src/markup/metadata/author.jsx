3f84adfba61c2a27a683f36fa44eb8fb1200b04d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-05 10:35:41 -0300

    usando o modulo render para nao deixar o meta tag author bloqueante

--- apps/todomvc/src/markup/metadata/author.jsx
+++ apps/todomvc/src/markup/metadata/author.jsx
@@ -1,5 +1,5 @@
-import h from '@rex/h'
+import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 
 export default (page) =>
-  document.head.append(<meta name='author' content={f.or(page.author, 'Cleber de Moraes Gonçalves')} />)
+  render(document.head, <meta name='author' content={f.or(page.author, 'Cleber de Moraes Gonçalves')} />)
