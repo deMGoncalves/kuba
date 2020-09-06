6a3ce29c74c164edd80f331fa9ab22ab1c76ea4d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-06 18:47:39 -0300

    usando o modulo render para nao deixar o meta tag base seja bloqueante

--- apps/todomvc/src/markup/metadata/base.jsx
+++ apps/todomvc/src/markup/metadata/base.jsx
@@ -1,5 +1,5 @@
-import h from '@rex/h'
+import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 
 export default (page) =>
-  document.head.append(<base href={f.or(page.base, '/')} />)
+  render(document.head, <base href={f.or(page.base, '/')} />)
