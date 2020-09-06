99a2d8c52fa9a871770e1c111e6d03b6964fceff
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-06 18:54:22 -0300

    usando o modulo render para nao deixar o meta tag icon seja bloqueante

--- apps/todomvc/src/markup/metadata/icon.jsx
+++ apps/todomvc/src/markup/metadata/icon.jsx
@@ -1,8 +1,8 @@
-import h from '@rex/h'
+import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 
 export default (page) =>
   f.forEach(
     ['icon', 'shortcut', 'apple-touch-icon'],
-    rel => document.head.append(<link href={f.or(page.icon, 'https://rex-todomvc.web.app/rex.png')} rel={rel} type='image/png' />)
+    rel => render(document.head, <link href={f.or(page.icon, '/rex.png')} rel={rel} type='image/png' />)
   )
