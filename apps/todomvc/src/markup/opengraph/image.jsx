55189e5ed47d27b5105ec59d060f5315c5ed44a1
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-07 12:01:05 -0300

    usando o modulo render para nao deixar o open graphic image nao bloquante

--- apps/todomvc/src/markup/opengraph/image.jsx
+++ apps/todomvc/src/markup/opengraph/image.jsx
@@ -1,5 +1,5 @@
-import h from '@rex/h'
+import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 
 export default (page) =>
-  document.head.append(document.head.append(<meta property='og:image' content={f.or(page.image, 'https://rex-todomvc.web.app/rex.png')} />))
+  render(document.head, <meta property='og:image' content={f.or(page.image, '/rex.png')} />)
