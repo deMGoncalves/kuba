288c99c51f9827af9746753ee257d5db2718ccc8
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-07 11:59:09 -0300

    usando o modulo render para nao deixar o open graphic description

--- apps/todomvc/src/markup/opengraph/description.jsx
+++ apps/todomvc/src/markup/opengraph/description.jsx
@@ -1,5 +1,5 @@
-import h from '@rex/h'
+import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 
 export default (page) =>
-  document.head.append(<meta property='og:description' content={f.or(page.description, 'Developers these days are spoiled with choice when it comes to selecting an MV* framework for structuring and organizing their JavaScript web apps')} />)
+  render(document.head, <meta property='og:description' content={f.or(page.description, 'Developers these days are spoiled with choice when it comes to selecting an MV* framework for structuring and organizing their JavaScript web apps')} />)
