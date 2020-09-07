b26ecbb35f8950e12eaa4f4583c588080be99171
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-07 12:16:00 -0300

    usando o modulo render para nao deixar o twitter card image nao seje bloqueante

--- apps/todomvc/src/markup/twittercard/image.jsx
+++ apps/todomvc/src/markup/twittercard/image.jsx
@@ -1,5 +1,5 @@
-import h from '@rex/h'
+import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 
 export default (page) =>
-  document.head.append(<meta name='twitter:image' content={f.or(page.image, 'https://rex-todomvc.web.app/rex.png')} />)
+  render(document.head, <meta name='twitter:image' content={f.or(page.image, 'https://rex-todomvc.web.app/rex.png')} />)
