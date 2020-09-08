7e2d1cc1194ccd65284a622182c562aa15ada5a6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-07 21:19:27 -0300

    executando a rota quando o dom estiver pronto

--- shared/router/src/router.js
+++ shared/router/src/router.js
@@ -1,4 +1,5 @@
 import * as f from '@rex/f'
 
 export default (path, listener) =>
-  f.test(path, location.pathname) && listener(...path.exec(location.pathname).slice(1))
+  window.addEventListener('DOMContentLoaded', () =>
+    (f.test(path, location.pathname) && listener(...path.exec(location.pathname).slice(1))))
