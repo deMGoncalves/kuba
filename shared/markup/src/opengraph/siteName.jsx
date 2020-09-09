52465ce3b993b9e9b33046d6f0e7169f8137b5bc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-09 10:17:23 -0300

    removendo console do codigo

--- shared/markup/src/opengraph/siteName.jsx
+++ shared/markup/src/opengraph/siteName.jsx
@@ -2,4 +2,4 @@ import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 
 export default (page, config) =>
-  render(document.head, <meta property='og:site_name' content={f.or(page.title, f.prop('name', config), console.log(config))} />)
+  render(document.head, <meta property='og:site_name' content={f.or(page.title, f.prop('name', config))} />)
