d7751fffd56cb963d2e483372bc4feb2907158d0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-10 17:31:09 -0300

    inclusao do link para features

--- apps/photosnap/src/pages/home/component.jsx
+++ apps/photosnap/src/pages/home/component.jsx
@@ -4,5 +4,7 @@ import style from './style.css'
 export default () =>
   <>
     <h1 className={style.home__h1}>Photosnap • Rex</h1>
-    <p>Home</p>
+    <nav>
+      <a href='/features'>features</a>
+    </nav>
   </>
