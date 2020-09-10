32a3db49dded45228d0a42552d231b932e8752ad
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-10 17:11:14 -0300

    nome da pagina

--- apps/photosnap/src/pages/home/component.jsx
+++ apps/photosnap/src/pages/home/component.jsx
@@ -4,5 +4,5 @@ import style from './style.css'
 export default () =>
   <>
     <h1 className={style.home__h1}>Photosnap • Rex</h1>
-    <p>Simples, pequeno e imperfeito - Uma visão além da programação</p>
+    <p>Home</p>
   </>
