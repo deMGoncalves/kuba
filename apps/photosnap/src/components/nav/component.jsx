75eeeea75d6044163e2abdddd93bc235643ee161
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-11 12:47:19 -0300

    construcoa do menu

--- apps/photosnap/src/components/nav/component.jsx
+++ apps/photosnap/src/components/nav/component.jsx
@@ -2,13 +2,13 @@ import h, { Fragment } from '@rex/h'
 import * as c from '@components'
 import style from './style.css'
 
-export default (nav) =>
+export default () =>
   <>
-    <button className={[style.nav__toggle, style[nav.status]]} onClick_prevent_stop={() => nav.toggle()}>Menu</button>
-    <div className={[style.nav, style[nav.status]]}>
+    <button className={style.nav__toggle} onClick_prevent_stop={(e) => e.target.classList.toggle(style.opened)}>Menu</button>
+    <nav className={style.nav}>
       <c.Link className={style.nav__link} href='/stories'>Stories</c.Link>
       <c.Link className={style.nav__link} href='/features'>Features</c.Link>
       <c.Link className={style.nav__link} href='/pricing'>Pricing</c.Link>
-    </div>
-    <c.Button className={[style.nav__button, style[nav.status]]} href='/'>Get in invite</c.Button>
+    </nav>
+    <c.Button className={style.nav__button} href='/'>Get in invite</c.Button>
   </>
