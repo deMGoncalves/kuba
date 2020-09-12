a71a314834d5035c274c67bd5953aed3ca24c4b9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-11 21:13:30 -0300

    removendo as imagens da solucao e recalculando o aspect ratio das imagens

--- apps/photosnap/src/pages/home/component.jsx
+++ apps/photosnap/src/pages/home/component.jsx
@@ -3,10 +3,6 @@ import * as c from '@components'
 import Story from './story'
 import style from './style.css'
 
-import createAndShareMobile from './create-and-share.mobile.jpg'
-import createAndShareTablet from './create-and-share.tablet.jpg'
-import createAndShareDesktop from './create-and-share.desktop.jpg'
-
 export default () =>
   <main className={style.home}>
     <c.Header className={style.home__header} />
@@ -15,8 +11,8 @@ export default () =>
       description='Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.'
       url='/'
     >
-      <source srcSet={createAndShareMobile} media='(max-width: 767px)' />
-      <source srcSet={createAndShareTablet} media='(min-width: 768px) and (max-width: 959px)' />
-      <source srcSet={createAndShareDesktop} media='(min-width: 960px)' />
+      <source srcSet='/assets/create-and-share.mobile.jpg' media='(max-width: 767px)' />
+      <source srcSet='/assets/create-and-share.tablet.jpg' media='(min-width: 768px) and (max-width: 959px)' />
+      <source srcSet='/assets/create-and-share.desktop.jpg' media='(min-width: 960px)' />
     </Story>
   </main>
