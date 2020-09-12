1f3e6955470c502650b38eac7a81ad115508de5d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-12 20:47:40 -0300

    pequeno ajuste para deixar as responsabilidades saparados

--- apps/photosnap/src/pages/home/banner/component.jsx
+++ apps/photosnap/src/pages/home/banner/component.jsx
@@ -1,11 +1,14 @@
 import h from '@rex/h'
+import * as f from '@rex/f'
 import * as c from '@components'
 import style from './style.css'
 
-export default (props, children) =>
+export default (props) =>
   <figure className={[style.banner, style[props.color], style[props.direction], props.className]}>
     <c.Picture className={style.banner__picture} alt={props.title}>
-      {children}
+      <source srcSet={f.prop('[0]', props.sources)} media='(max-width: 767px)' />
+      <source srcSet={f.prop('[1]', props.sources)} media='(min-width: 768px) and (max-width: 959px)' />
+      <source srcSet={f.prop('[2]', props.sources)} media='(min-width: 960px)' />
     </c.Picture>
     <figcaption className={style.banner__figcaption}>
       <h1 className={[style.banner__h1, style[props.color]]}>{props.title}</h1>
--- apps/photosnap/src/pages/home/component.jsx
+++ apps/photosnap/src/pages/home/component.jsx
@@ -1,40 +1,14 @@
 import h, { Fragment } from '@rex/h'
+import * as f from '@rex/f'
 import * as c from '@components'
 import Banner from './banner'
+import banners from './banners.json'
 import style from './style.css'
 
 export default () =>
   <main className={style.home}>
     <c.Header className={style.home__header} />
-    <Banner
-      color='inverse'
-      description='Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others'
-      direction='reverse'
-      title='Create and share your photo stories'
-      url='/'
-    >
-      <source srcSet='/assets/create-and-share.mobile.jpg' media='(max-width: 767px)' />
-      <source srcSet='/assets/create-and-share.tablet.jpg' media='(min-width: 768px) and (max-width: 959px)' />
-      <source srcSet='/assets/create-and-share.desktop.jpg' media='(min-width: 960px)' />
-    </Banner>
-    <Banner
-      description='We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone'
-      title='Beautiful stories every time'
-      url='/'
-    >
-      <source srcSet='/assets/beautiful-stories.mobile.jpg' media='(max-width: 767px)' />
-      <source srcSet='/assets/beautiful-stories.tablet.jpg' media='(min-width: 768px) and (max-width: 959px)' />
-      <source srcSet='/assets/beautiful-stories.desktop.jpg' media='(min-width: 960px)' />
-    </Banner>
-    <Banner
-      description='Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it'
-      direction='reverse'
-      title='Designed for everyone'
-      url='/'
-    >
-      <source srcSet='/assets/designed-for-everyone.mobile.jpg' media='(max-width: 767px)' />
-      <source srcSet='/assets/designed-for-everyone.tablet.jpg' media='(min-width: 768px) and (max-width: 959px)' />
-      <source srcSet='/assets/designed-for-everyone.desktop.jpg' media='(min-width: 960px)' />
-    </Banner>
-
+    <Banner { ...f.prop('[0]', banners) } color='inverse' />
+    <Banner { ...f.prop('[1]', banners) } />
+    <Banner { ...f.prop('[2]', banners) } direction='reverse' />
   </main>
