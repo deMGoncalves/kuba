dc48d418f88713049a21e3f524c3fc1b23669ad7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-12 23:11:42 -0300

    construcoa dos cards

--- apps/photosnap/src/pages/home/component.jsx
+++ apps/photosnap/src/pages/home/component.jsx
@@ -1,14 +1,12 @@
 import h, { Fragment } from '@rex/h'
-import * as f from '@rex/f'
 import * as c from '@components'
-import Banner from './banner'
-import banners from './banners.json'
+import banners from './banners'
+import cards from './cards'
 import style from './style.css'
 
 export default () =>
   <main className={style.home}>
     <c.Header className={style.home__header} />
-    <Banner { ...f.prop('[0]', banners) } color='inverse' />
-    <Banner { ...f.prop('[1]', banners) } />
-    <Banner { ...f.prop('[2]', banners) } direction='reverse' />
+    {banners}
+    {cards}
   </main>
