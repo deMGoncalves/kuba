475e7fe6cf7c1157764a2df610ec343857d3f591
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-14 13:57:40 -0300

    alinhamento das features da home

--- apps/photosnap/src/pages/home/features/component.jsx
+++ apps/photosnap/src/pages/home/features/component.jsx
@@ -1,5 +1,20 @@
 import h from '@rex/h'
+import * as f from '@rex/f'
+import * as c from '@components'
+import schema from './schema.json'
 import style from './style.css'
 
 export default () =>
-  <ul className={style.features} />
+  <ul className={style.features}>
+    {
+      f.map(schema, (dto, i) =>
+        <li className={style.features__li}>
+          <c.Picture className={[style.features__picture, style[`p${i}`]]}>
+            <source srcSet={dto.icon} />
+          </c.Picture>
+          <h3 className={style.features__h3}>{dto.title}</h3>
+          <p className={style.features__p}>{dto.description}</p>
+        </li>
+      )
+    }
+  </ul>
