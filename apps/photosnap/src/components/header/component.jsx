fb577e7c3ddc1283772fb45c789c1786a0106755
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-15 14:07:34 -0300

    movendo o logo para dentro do header

--- apps/photosnap/src/components/header/component.jsx
+++ apps/photosnap/src/components/header/component.jsx
@@ -1,12 +1,12 @@
 import h from '@rex/h'
-import * as c from '@components'
+import Logo from './logo'
 import Nav from './nav'
 import style from './style.css'
 
 export default () =>
   <header className={style.header}>
     <div className={style.header__container}>
-      <c.Logo className={style.header__logo} />
+      <Logo className={style.header__logo} />
       <Nav className={style.header__nav} />
     </div>
   </header>
