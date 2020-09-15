cc2cee6bd930c4136aed72abab65d1242e088133
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-15 18:01:41 -0300

    quando clicar no logo deve redirecionar para a home do site

--- apps/photosnap/src/components/footer/logo/component.jsx
+++ apps/photosnap/src/components/footer/logo/component.jsx
@@ -4,6 +4,6 @@ import photosnap from './photosnap.png'
 import style from './style.css'
 
 export default (props) =>
-  <c.Picture className={[style.logo, props.className]}>
+  <c.Picture className={[style.logo, props.className]} onClick={() => location.assign('/')}>
     <source srcSet={photosnap} />
   </c.Picture>
