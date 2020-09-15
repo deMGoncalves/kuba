6f8e5839a88de681d816447417df805c648b0f19
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-15 11:33:38 -0300

    inclusao do logo

--- apps/photosnap/src/components/footer/component.jsx
+++ apps/photosnap/src/components/footer/component.jsx
@@ -1,5 +1,11 @@
 import h from '@rex/h'
+import * as c from '@components'
+import photosnap from './photosnap.png'
 import style from './style.css'
 
 export default () =>
-  <footer className={style.footer} />
+  <footer className={style.footer}>
+    <c.Picture className={style.footer__picture}>
+      <source srcSet={photosnap} />
+    </c.Picture>
+  </footer>
