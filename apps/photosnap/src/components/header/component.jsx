9bb8bda7b8f0f95c67f92498fbc1044b898b0f7b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-15 13:41:57 -0300

    movendo o nav para dentro do header

--- apps/photosnap/src/components/header/component.jsx
+++ apps/photosnap/src/components/header/component.jsx
@@ -1,11 +1,12 @@
 import h from '@rex/h'
 import * as c from '@components'
+import Nav from './nav'
 import style from './style.css'
 
 export default () =>
   <header className={style.header}>
     <div className={style.header__container}>
       <c.Logo className={style.header__logo} />
-      <c.Nav className={style.header__nav} />
+      <Nav className={style.header__nav} />
     </div>
   </header>
