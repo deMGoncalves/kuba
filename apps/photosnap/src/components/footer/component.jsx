5da8893f9e6159c44103c2ae42da494b4c55a688
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-15 15:34:36 -0300

    inclusao da navegacao do footer

--- apps/photosnap/src/components/footer/component.jsx
+++ apps/photosnap/src/components/footer/component.jsx
@@ -1,5 +1,6 @@
 import h from '@rex/h'
 import Logo from './logo'
+import Nav from './nav'
 import Social from './social'
 import style from './style.css'
 
@@ -7,4 +8,5 @@ export default () =>
   <footer className={style.footer}>
     <Logo className={style.footer__logo} />
     <Social className={style.footer__social} />
+    <Nav className={style.footer__nav} />
   </footer>
