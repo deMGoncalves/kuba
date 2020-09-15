27430dd7a9cd4362289ca26192d94cd32479e8d9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-15 15:58:53 -0300

    inclusao do get an invite e copyright

--- apps/photosnap/src/components/footer/component.jsx
+++ apps/photosnap/src/components/footer/component.jsx
@@ -1,4 +1,5 @@
 import h from '@rex/h'
+import * as c from '@components'
 import Logo from './logo'
 import Nav from './nav'
 import Social from './social'
@@ -9,4 +10,6 @@ export default () =>
     <Logo className={style.footer__logo} />
     <Social className={style.footer__social} />
     <Nav className={style.footer__nav} />
+    <c.LinkButton className={style.footer__linkButton} color='inverse' href='/'> Get an invite</c.LinkButton>
+    <p className={style.footer__p}>Copyright {(new Date()).getFullYear()}. All Rights Reserved</p>
   </footer>
