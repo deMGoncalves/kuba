3c791913f0bc521777508e68731219f87c0a43c2
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-17 14:02:16 -0300

    separando as responsabilidades dos atributos do pixel

--- pixels/gtm/src/tag.jsx
+++ pixels/gtm/src/tag.jsx
@@ -1,15 +1,25 @@
 import h, { render } from '@rex/h'
 
+const event = Symbol('event')
+const src = Symbol('src')
+
 customElements.define('pixel-gtm', class extends HTMLElement {
-  connectedCallback () {
-    window.dataLayer = window.dataLayer || []
-    window.dataLayer.push({
+  get [event] () {
+    return {
       'gtm.start': new Date().getTime(),
       event: 'gtm.js'
-    })
+    }
+  }
 
-    render(this, <script src={`https://www.googletagmanager.com/gtm.js?id=${this.dataset.id}`} async />)
+  get [src] () {
+    return `https://www.googletagmanager.com/gtm.js?id=${this.dataset.id}`
+  }
 
+  connectedCallback () {
+    window.dataLayer = window.dataLayer || []
+    window.dataLayer.push(this[event])
+
+    render(this, <script src={this[src]} async />)
     return this
   }
 })
