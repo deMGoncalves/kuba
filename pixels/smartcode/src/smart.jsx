69ee0d5c68b18ca1da7cbdc451aa781f284a2b35
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-18 18:24:07 -0300

    reste do trigger do action para o gtm

--- pixels/smartcode/src/smart.jsx
+++ pixels/smartcode/src/smart.jsx
@@ -2,17 +2,15 @@ import h, { render } from '@rex/h'
 import * as f from '@rex/f'
 import schema from './schema.json'
 
-window.addEventListener('DOMContentLoaded', () =>
-  f.forEach(schema, (host) =>
-    fetch(`${host}/stats.json`)
-      .then(response => response.json())
-      .then(stats => stats.assetsByChunkName)
-      .then(assetsByChunkName =>
-        f.forEach(
-          f.entries(assetsByChunkName),
-          ([_, url]) =>
-            render(document.head, <script src={`${host}/${url}`} async />)
-        )
+f.forEach(schema, (host) =>
+  fetch(`${host}/stats.json`)
+    .then(response => response.json())
+    .then(stats => stats.assetsByChunkName)
+    .then(assetsByChunkName =>
+      f.forEach(
+        f.entries(assetsByChunkName),
+        ([_, url]) =>
+          render(document.head, <script src={`${host}/${url}`} async />)
       )
-  )
+    )
 )
