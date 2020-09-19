f32bf0d5d1126f81863720a3f4761e11026343d7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-19 11:50:22 -0300

    construcao do banner para feature

---
+++ apps/photosnap/src/pages/features/banner/index.js
@@ -0,0 +1,5 @@
+import h from '@rex/h'
+import Component from './component'
+import schema from './schema.json'
+
+export default <Component {...schema} />
--- apps/photosnap/src/pages/features/component.jsx
+++ apps/photosnap/src/pages/features/component.jsx
@@ -1,8 +1,11 @@
 import h, { Fragment } from '@rex/h'
 import * as c from '@components'
+import banner from './banner'
 import style from './style.css'
 
 export default () =>
   <main className={style.features}>
     <c.Header className={style.features__header} />
+    {banner}
+    <c.Footer className={style.features__footer} />
   </main>
