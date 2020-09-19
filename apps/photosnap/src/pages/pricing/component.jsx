c8ecfe2da24aa982bcfd8cdbe0999c3f0600db98
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-19 11:56:47 -0300

    contrucao do hero para pricing

--- apps/photosnap/src/pages/pricing/component.jsx
+++ apps/photosnap/src/pages/pricing/component.jsx
@@ -1,8 +1,11 @@
 import h, { Fragment } from '@rex/h'
 import * as c from '@components'
+import banner from './banner'
 import style from './style.css'
 
 export default () =>
   <main className={style.pricing}>
     <c.Header className={style.pricing__header} />
+    {banner}
+    <c.Footer className={style.pricing__footer} />
   </main>
