76ba758bf471ffdf98648a5dfbfe4a196336a1fc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-21 16:03:58 -0300

    vamos manter o padrao para os tipos de arquivos

--- shared/h/src/Fragment/fragment.jsx
+++
@@ -1,5 +0,0 @@
-import appendChildren from '@rex/h/src/appendChildren'
-import extendAttributes from './extendAttributes'
-
-export default (props, children) =>
-  appendChildren(children, extendAttributes(props, document.createDocumentFragment()))
