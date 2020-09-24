260007a2679ce812e96ee00582b96587d00e9509
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-24 07:33:45 -0300

    mudando o pixel do trackjs para um padrao simples de implementacao

--- pixels/smartcode/jsdom.setup.js
+++
@@ -1,8 +0,0 @@
-const { JSDOM } = require('jsdom')
-const { window } = new JSDOM('<!doctype html><html><head></head><body></body></html>')
-
-global.window = window
-global.document = window.document
-global.navigator = {
-  userAgent: 'node.js'
-}
--- pixels/smartcode/src/hj.js
+++
@@ -1,2 +0,0 @@
-window.hj = (...args) =>
-  (window.hj.q = window.hj.q || []).push(args)
