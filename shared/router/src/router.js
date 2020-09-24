6ee14ddc86d1a3108d55065a5dafb6a531f2afba
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-24 09:28:51 -0300

    removendo a execuca da tela do dom content loaded

--- shared/router/src/router.js
+++ shared/router/src/router.js
@@ -1,9 +1,7 @@
 import * as f from '@rex/f'
 import pathname from './pathname'
-import ready from './ready'
 
 const router = (path, listener) =>
-  ready(() =>
-    f.test(path, pathname) && listener(...f.slice(f.exec(path, pathname), 1)))
+  f.test(path, pathname) && listener(...f.slice(f.exec(path, pathname), 1))
 
 export default f.curry(router)
