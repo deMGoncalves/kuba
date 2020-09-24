3b495eea220e1542b4e27985b07e11e04ff325da
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-24 10:02:44 -0300

    vamos deixar o codigo o mais facil de se ler

--- apps/photosnap/src/pages/index.jsx
+++ apps/photosnap/src/pages/index.jsx
@@ -2,21 +2,21 @@ import h, { render } from '@rex/h'
 import router from '@rex/router'
 
 router(/^\/$/, async () => {
-  const { default: Home } = await import(/* webpackPrefetch: true, webpackPreload: -99999, webpackChunkName: "home" */ '@pages/home')
+  const { default: Home } = await import('@pages/home' /* webpackPrefetch: true, webpackPreload: -99999, webpackChunkName: "home" */)
   render(document.body, <Home />)
 })
 
 router(/^\/features$/, async () => {
-  const { default: Features } = await import(/* webpackPrefetch: true, webpackPreload: true, webpackChunkName: "features" */ '@pages/features')
+  const { default: Features } = await import('@pages/features' /* webpackPrefetch: true, webpackPreload: true, webpackChunkName: "features" */)
   render(document.body, <Features />)
 })
 
 router(/^\/pricing$/, async () => {
-  const { default: Pricing } = await import(/* webpackPrefetch: true, webpackPreload: true, webpackChunkName: "pricing" */ '@pages/pricing')
+  const { default: Pricing } = await import('@pages/pricing' /* webpackPrefetch: true, webpackPreload: true, webpackChunkName: "pricing" */)
   render(document.body, <Pricing />)
 })
 
 router(/^\/stories$/, async () => {
-  const { default: Stories } = await import(/* webpackPrefetch: true, webpackPreload: true, webpackChunkName: "stories" */ '@pages/stories')
+  const { default: Stories } = await import('@pages/stories' /* webpackPrefetch: true, webpackPreload: true, webpackChunkName: "stories" */)
   render(document.body, <Stories />)
 })
