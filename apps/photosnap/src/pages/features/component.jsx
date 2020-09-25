26d2d34003cfebfb18a3adb8e4c32fcc41a628fe
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-25 18:57:22 -0300

    usando os components do ui

--- apps/photosnap/src/pages/features/component.jsx
+++ apps/photosnap/src/pages/features/component.jsx
@@ -1,14 +1,16 @@
 import h, { Fragment } from '@rex/h'
-import * as c from '@components'
+import * as ui from '@rex/kit'
+import Header from '@rex/header'
+import Footer from '@rex/footer'
 import banner from './banner'
 import items from './items'
 import style from './style.css'
 
 export default () =>
   <main className={style.features}>
-    <c.Header className={style.features__header} />
+    <Header className={style.features__header} />
     {banner}
     {items}
-    <c.Tarja className={style.features__tarja} />
-    <c.Footer className={style.features__footer} />
+    <ui.Tarja className={style.features__tarja} />
+    <Footer className={style.features__footer} />
   </main>
--- apps/photosnap/src/pages/features/items/component.jsx
+++ apps/photosnap/src/pages/features/items/component.jsx
@@ -1,6 +1,6 @@
 import h from '@rex/h'
 import * as f from '@rex/f'
-import * as c from '@components'
+import * as ui from '@rex/kit'
 import schema from './schema.json'
 import style from './style.css'
 
@@ -9,9 +9,9 @@ export default () =>
     {
       f.map(schema, (dto, i) =>
         <li className={style.items__li}>
-          <c.Picture className={[style.items__picture, style[`p${i}`]]}>
+          <ui.Picture className={[style.items__picture, style[`p${i}`]]}>
             <source srcSet={dto.icon} />
-          </c.Picture>
+          </ui.Picture>
           <h2 className={style.items__h2}>{dto.title}</h2>
           <p className={style.items__p}>{dto.description}</p>
         </li>
