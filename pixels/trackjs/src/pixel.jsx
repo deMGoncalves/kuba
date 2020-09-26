0a8eb7eada6fbd9600dfac9ae0725ed11f14dab1
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-26 07:29:59 -0300

    vamos colocar os pixel dentro dos processo de request idle callback

--- pixels/trackjs/src/pixel.jsx
+++ pixels/trackjs/src/pixel.jsx
@@ -4,7 +4,7 @@ import trackJs from './track'
 
 trackJs(schema.token)
 
-window.addEventListener('DOMContentLoaded', () =>
+window.requestIdleCallback(() =>
   render(document.head, <script src='https://cdn.trackjs.com/releases/current/tracker.js' async />))
 
 export default trackJs
