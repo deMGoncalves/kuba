821e05737f71f77fe7b2a0f10cee9998aea47ff4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-26 21:17:11 -0300

    deixando os metadados mais segregado

--- ui/markup/src/metadata/author.jsx
+++ ui/markup/src/metadata/author.jsx
@@ -3,4 +3,4 @@ import * as f from '@rex/f'
 import schema from '@rex/markup/src/schema.json'
 
 export default (page) =>
-  render(document.head, <meta name='author' content={f.or(page.author, f.prop('author', schema))} />))
+  render(document.head, <meta name='author' content={f.or(page.author, f.prop('author', schema))} />)
