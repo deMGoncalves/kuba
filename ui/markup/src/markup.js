21f153d21ab7799d4f6f9ebc2e4db73922da24ce
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-28 07:45:36 -0300

    nao precisa mais do schema

--- ui/markup/src/markup.js
+++ ui/markup/src/markup.js
@@ -2,9 +2,8 @@ import * as f from '@rex/f'
 import hook from '@rex/hook'
 import metadata from './metadata'
 import opengraph from './opengraph'
-import schema from './schema.json'
 import twittercard from './twittercard'
 
 export default hook((page) =>
   window.requestIdleCallback(() =>
-    f.chain(metadata, opengraph, twittercard)(page, schema)))
+    f.chain(metadata, opengraph, twittercard)(page)))
