b1398f215079cb3875d8cfcfe6ed5e67bb1eae35
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-28 07:04:20 -0300

    deixando os metadados mais segregado

--- ui/markup/src/twittercard/site.jsx
+++ ui/markup/src/twittercard/site.jsx
@@ -1,5 +1,6 @@
 import h, { render } from '@rex/h'
 import * as f from '@rex/f'
+import schema from '@rex/markup/src/schema.json'
 
-export default (page, config) =>
-  render(document.head, <meta name='twitter:site' content={f.or(page.name, f.prop('name', config))} />)
+export default (page) =>
+  render(document.head, <meta name='twitter:site' content={f.or(page.name, f.prop('name', schema))} />)
