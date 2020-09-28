913338f5cd53b9233fe1a7c4f78db2f21482a9f0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-28 17:39:37 -0300

    separando e dividindo os processo do f.idle e manipulacoes de dom no h.frame

--- shared/h/src/paint/createHookReflow.js
+++ shared/h/src/paint/createHookReflow.js
@@ -1,35 +1,14 @@
 import * as f from '@rex/f'
 import reflow from '../reflow'
 
-/**
- * Cria o component, referencia o elemento no instancia da classe e
- * marca o elemento com o contexto para ser usando no processo de reflow
- *
- * @name createHookReflow
- * @function
- * @access private
- * @param {Object} target Contexto da classe que sera passao do component
- * @param {Function} component Web component
- * @param {Array} children Filhos que seram adicionas no web component
- * @return {HTMLElement} Elemento html gerado pelo web component
- */
 export default (target, component, children) => {
   f.assign(target, {
-
-    // Elemento html, com base neste elemento que sera
-    // feito o trabalho de flow
     __element__: component(target, children),
-
-    // Este metodo sera utilizado para os disparos
-    // de repintar os elemento no DOM
     __reflow__ () {
-      reflow(this.__element__, component(this, children))
+      f.idle(() => reflow(this.__element__, component(this, children)))
     }
   })
 
-  // Sera colocado no elemento html uma referencia do seu contexto,
-  // assim no processo de reflow nao sera analizado a arvore deste component
-  // por se tratar de responsabilidade da propria instancia refazer o reflow
   f.assign(target.__element__, {
     __target__: target
   })
--- shared/h/src/paint/hasElementInTarget.js
+++ shared/h/src/paint/hasElementInTarget.js
@@ -1,12 +1,3 @@
 import * as f from '@rex/f'
 
-/**
- * Verifica se um contexto ja tem um elemento criado
- *
- * @name hasElementInTarget
- * @function
- * @access private
- * @param {Object} target Contexto que sera encapsulado pelo component
- * @return {Boolean} Verdadeiro se tiver um elemento criado
- */
 export default f.has('__element__', f.__)
