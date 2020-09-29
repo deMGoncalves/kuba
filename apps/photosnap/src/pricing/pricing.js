48063385f3cd881d2bdc9dc1cc51b39bb5f8ca46
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-29 07:09:30 -0300

    definindo title e description para a pagina pricing

--- apps/photosnap/src/pricing/pricing.js
+++ apps/photosnap/src/pricing/pricing.js
@@ -5,7 +5,13 @@ import component from './component'
 @paint(component)
 @markup
 class Pricing {
+  get description () {
+    return 'Create a your stories, Photosnap is a platform for photographers and visual storytellers. It’s the simple way to create and share your photos'
+  }
 
+  get title () {
+    return 'Pricing • Create and share your photo stories'
+  }
 }
 
 export default Pricing
