123ed4bf8c67967f8d3fbc7a3d29fcc82689af21
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-09-29 07:10:39 -0300

    inclusao do title e description na pagina de features

--- apps/photosnap/src/features/features.js
+++ apps/photosnap/src/features/features.js
@@ -5,7 +5,13 @@ import component from './component'
 @paint(component)
 @markup
 class Features {
+  get description () {
+    return 'We make sure all of our features are designed to be loved by every aspiring and even professional photograpers who wanted to share their stories'
+  }
 
+  get title () {
+    return 'Features • Create and share your photo stories'
+  }
 }
 
 export default Features
