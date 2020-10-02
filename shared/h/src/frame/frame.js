6dab131a7d104fc56f721f511efce987c99fd841
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-02 06:09:16 -0300

    tenho que encapsular a chamada do request animation frame, pois ele passar o id do agendamento da execucao

--- shared/h/src/frame/frame.js
+++ shared/h/src/frame/frame.js
@@ -1,6 +1,6 @@
 import * as f from '@rex/f'
 
 const frame = (target, ...args) =>
-  window.requestAnimationFrame(target.bind(null, ...args))
+  window.requestAnimationFrame(() => target(...args))
 
 export default f.curry(f.arity(2, frame))
