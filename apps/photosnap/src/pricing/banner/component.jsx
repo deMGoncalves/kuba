49f3ccde7236b9a3501f3f454199beb95db036a3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-06 13:55:40 -0300

    agora o hero tem apenas dois slot

--- apps/photosnap/src/pricing/banner/component.jsx
+++ apps/photosnap/src/pricing/banner/component.jsx
@@ -10,6 +10,8 @@ export default (props) =>
       <source srcSet={f.prop('[1]', props.sources)} media='(min-width: 768px) and (max-width: 959px)' />
       <source srcSet={f.prop('[2]', props.sources)} media='(min-width: 960px)' />
     </Fragment>
-    <h1 slot='title' className={style.banner__h1}>{props.title}</h1>
-    <p slot='description' className={style.banner__p}>{props.description}</p>
+    <Fragment slot='caption'>
+      <h1 className={style.banner__h1}>{props.title}</h1>
+      <p className={style.banner__p}>{props.description}</p>
+    </Fragment>
   </ui.Hero>
