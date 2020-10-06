12b45813b1146bd7a90abeae273f4a5f53d88d27
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-06 13:58:17 -0300

    agora o hero tem apenas dois slot

--- apps/photosnap/src/stories/banner/component.jsx
+++ apps/photosnap/src/stories/banner/component.jsx
@@ -10,13 +10,13 @@ export default (props) =>
       <source srcSet={f.prop('[1]', props.sources)} media='(min-width: 768px) and (max-width: 959px)' />
       <source srcSet={f.prop('[2]', props.sources)} media='(min-width: 960px)' />
     </Fragment>
-    <Fragment slot='title'>
+    <Fragment slot='caption'>
       <hgroup>
         <h2 className={style.banner__h2}>{props.subtitle}</h2>
         <h1 className={style.banner__h1}>{props.title}</h1>
       </hgroup>
+      <p className={style.banner__p}><date>{props.date}</date> by {props.author}</p>
+      <p className={style.banner__p}>{props.description}</p>
+      <ui.LinkButton className={style.banner__linkButton} color='inverse' href={props.url}>Read story</ui.LinkButton>
     </Fragment>
-    <p slot='author' className={style.banner__p}><date>{ props.date }</date> by { props.author }</p>
-    <p slot='description' className={style.banner__p}>{props.description}</p>
-    <ui.LinkButton slot='link' className={style.banner__linkButton} color='inverse' href={props.url}>Read story</ui.LinkButton>
   </ui.Hero>
