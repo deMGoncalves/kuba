d00d4a09dc2d24ed85a2ad6cc5ec4bc49bcbadd2
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-06 13:51:03 -0300

    o modulo se chama hero e nao banner, mudando as referencias para hero

--- ui/kit/src/hero/component.jsx
+++ ui/kit/src/hero/component.jsx
@@ -3,11 +3,11 @@ import Picture from '@rex/kit/src/picture'
 import style from './style.css'
 
 export default (props, children) =>
-  <figure className={[style.banner, style[props.color], style[props.direction], props.className]}>
-    <Picture className={style.banner__picture} alt={props.alt}>
+  <figure className={[style.hero, style[props.color], style[props.direction], props.className]}>
+    <Picture className={style.hero__picture} alt={props.alt}>
       {children.source}
     </Picture>
-    <figcaption className={style.banner__figcaption}>
+    <figcaption className={style.hero__figcaption}>
       {children.caption}
     </figcaption>
   </figure>
