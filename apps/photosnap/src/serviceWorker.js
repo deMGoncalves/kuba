8c36dab80dcf4c5905a87cf54f8aa27bffb6372c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-07 16:45:18 -0300

    o service worker nao vai rodar quando o user agent for um bot

--- apps/photosnap/src/serviceWorker.js
+++ apps/photosnap/src/serviceWorker.js
@@ -1,9 +1,14 @@
-if (process.env.NODE_ENV === 'production') {
-  if ('serviceWorker' in navigator) {
-    navigator
-      .serviceWorker
-      .register('/sw.js')
-      .then(registration => console.log('SW registered: ', registration))
-      .catch(registrationError => console.log('SW registration failed: ', registrationError))
+import * as f from '@rex/f'
+import bot from '@rex/boot'
+
+if (f.not(bot)) {
+  if (process.env.NODE_ENV === 'production') {
+    if ('serviceWorker' in navigator) {
+      navigator
+        .serviceWorker
+        .register('/sw.js')
+        .then(registration => console.log('SW registered: ', registration))
+        .catch(registrationError => console.log('SW registration failed: ', registrationError))
+    }
   }
 }
