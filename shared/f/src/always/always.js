f405a8128d75d1768b5da74918afc0011754f9a0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-13 16:44:19 -0300

    inclusao da validacao do valor, caso ele seja vazio eu nao posso busca o magic mathod

--- shared/f/src/always/always.js
+++ shared/f/src/always/always.js
@@ -1,8 +1,11 @@
 import curry from '@rex/f/src/curry'
+import isEmpty from '@rex/f/src/isEmpty'
 import magic from '@rex/f/src/magic'
 import or from '@rex/f/src/or'
 
 const always = (value) =>
-  () => or(value[magic('f/always')], value)
+  () => isEmpty(value)
+    ? value
+    : or(value[magic('f/always')], value)
 
 export default curry(always)
--- shared/f/src/always/always.spec.js
+++ shared/f/src/always/always.spec.js
@@ -1,7 +1,9 @@
 import always from './always'
+import isEmpty from '@rex/f/src/isEmpty'
 import magic from '@rex/f/src/magic'
 import or from '@rex/f/src/or'
 
+jest.mock('@rex/f/src/isEmpty')
 jest.mock('@rex/f/src/magic')
 jest.mock('@rex/f/src/or')
 
@@ -11,6 +13,7 @@ describe('f.always', function () {
   })
 
   test('retorna uma funcao callback que sempre retorna o valor/referencia passado', function () {
+    isEmpty.mockReturnValue(false)
     magic.mockReturnValue('f/always')
     or.mockImplementation((a, b) => a || b)
 
@@ -18,6 +21,10 @@ describe('f.always', function () {
 
     expect(x).toBeInstanceOf(Function)
     expect(x()).toBe('rex')
+
+    expect(isEmpty).toHaveBeenCalled()
+    expect(isEmpty).toHaveBeenCalledTimes(1)
+    expect(isEmpty).toHaveBeenCalledWith('rex')
 
     expect(magic).toHaveBeenCalled()
     expect(magic).toHaveBeenCalledTimes(1)
@@ -29,6 +36,7 @@ describe('f.always', function () {
   })
 
   test('retorna uma funcao callback que sempre retorna o valor/referencia do magic getter', function () {
+    isEmpty.mockReturnValue(false)
     magic.mockReturnValue('f/always')
     or.mockImplementation((a, b) => a || b)
 
@@ -42,6 +50,10 @@ describe('f.always', function () {
 
     expect(y).toBeInstanceOf(Function)
     expect(y()).toBe('rex')
+
+    expect(isEmpty).toHaveBeenCalled()
+    expect(isEmpty).toHaveBeenCalledTimes(1)
+    expect(isEmpty).toHaveBeenCalledWith(x)
 
     expect(magic).toHaveBeenCalled()
     expect(magic).toHaveBeenCalledTimes(1)
