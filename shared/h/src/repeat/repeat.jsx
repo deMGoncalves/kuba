44435ad1a84ec843f4d3d12c4fa1c10e6191d175
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-16 19:30:41 -0300

    vou passar o indice do iterator para o component poder entender qme indice esta trabalhando

--- shared/h/src/repeat/repeat.jsx
+++ shared/h/src/repeat/repeat.jsx
@@ -1,7 +1,8 @@
+import h from '@rex/h/src/h'
 import * as f from '@rex/f'
 import Fragment from '@rex/h/src/fragment'
 
 export default (props) =>
   <Fragment slot={props.slot}>
-    {f.map(props.iterator, (attrs) => props.component(attrs))}
+    {f.map(props.iterator, (attrs, i) => props.component(attrs, i))}
   </Fragment>
