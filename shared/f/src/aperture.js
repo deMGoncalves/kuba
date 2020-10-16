47acfea8ce4f6ca70bbb6c61018793abf7a6e9cf
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-16 13:51:33 -0300

    reaproveitando funcoes do proprio f

--- shared/f/src/aperture.js
+++ shared/f/src/aperture.js
@@ -1,12 +1,14 @@
-import arity from './arity'
-import curry from './curry'
+import arity from '@rex/f/src/arity'
+import curry from '@rex/f/src/curry'
+import len from '@rex/f/src/len'
+import push from '@rex/f/src/push'
+import splice from '@rex/f/src/splice'
 
-const aperture = (n, target, result = []) => {
-  target = [...target]
+const aperture = (x, y, z = []) => {
+  y = [...y]
 
-  while (target.length) { result.push(target.splice(0, n)) }
-
-  return result
+  while (len(y)) { push(z, splice(y, 0, x)) }
+  return z
 }
 
 export default curry(arity(2, aperture))
