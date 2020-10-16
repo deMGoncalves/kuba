24706293a124535bef7f9a4bbe6192e87e88c72b
Evandro P. Santos <evandropsantos@gmail.com>
2020-10-15 22:18:46 -0300

    Estilo base cards

--- apps/photosnap/src/pricing/card/component.jsx
+++ apps/photosnap/src/pricing/card/component.jsx
@@ -1,11 +1,11 @@
 import h from '@rex/h'
 import style from './style.css'
 
-export default (props, child) =>
+export default (props, children) =>
   <article className={[style.card, style[props.type]]}>
     <header className={style.card__header}>
       <h4 className={style.card__title}>{props.title}</h4>
-      <p className={style.card__text}>{child}</p>
+      <p className={style.card__text}>{children}</p>
     </header>
     <p className={style.card__price}>
       <data className={style.card__value} value={props.price}>$ {props.price}</data> per month
--- apps/photosnap/src/pricing/component.jsx
+++ apps/photosnap/src/pricing/component.jsx
@@ -13,6 +13,34 @@ export default () =>
       <Card title='Basic' price='19.00'>
         Includes basic usage of our platform. Recommended for new and aspiring photographers.
       </Card>
+
+      <Card title='Pro' price='39.00' type='negative'>
+        More advanced features available. Recommended for photography veterans and professionals.
+      </Card>
+
+      <Card title='Business' price='99.00'>
+        Additional features available such as more detailed metrics. Recommended for business owners.
+      </Card>
+
+      {/*
+        <table className={style.pricing__table}>
+        <thead>
+          <tr>
+            <th colSpan='3'>The Features</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th colSpan='3'>Unlimited Story Posting</th>
+          </tr>
+          <tr>
+            <td>Basic</td>
+            <td>Pro</td>
+            <td>Business</td>
+          </tr>
+        </tbody>
+      </table>
+      */}
     </section>
 
     <ui.Tarja className={style.pricing__tarja} />
