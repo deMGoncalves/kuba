ab3ed6d079269e7d9048ad1546ce1e880013f283
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 10:07:00 -0300

    usando o component ui Story, reaproveitamento de codigo

--- apps/photosnap/src/home/cards/component.jsx
+++ apps/photosnap/src/home/cards/component.jsx
@@ -1,15 +1,13 @@
-import h from '@rex/h'
+import h, { Fragment } from '@rex/h'
 import * as ui from '@rex/kit'
 import style from './style.css'
 
 export default (props) =>
-  <figure className={[style.card, props.className]}>
-    <ui.Picture className={style.card__picture} alt={props.title}>
-      <ui.Sources {...props} />
-    </ui.Picture>
-    <figcaption className={style.card__figcaption}>
+  <ui.Story>
+    <ui.Sources slot='picture' {...props} />
+    <Fragment slot='caption'>
       <ui.Heading level='2' {...props}>{props.title}</ui.Heading>
       <ui.P {...props}>by {props.author}</ui.P>
-      <ui.Link className={style.card__link} icon='arrow' {...props}>Read story</ui.Link>
-    </figcaption>
-  </figure>
+      <ui.Link className={style.story__link} icon='arrow' {...props}>Read story</ui.Link>
+    </Fragment>
+  </ui.Story>
