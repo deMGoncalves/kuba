695e0acd70c6c82c527cff6122f222d2df23a0d2
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 14:12:01 -0300

    usando o modulo repeat, assim consigo remover a logica expllicita

--- apps/photosnap/src/stories/cards/component.jsx
+++ apps/photosnap/src/stories/cards/component.jsx
@@ -1,14 +1,6 @@
-import h, { Fragment } from '@rex/h'
-import * as ui from '@rex/kit'
-import style from './style.css'
+import h, { Repeat } from '@rex/h'
+import schema from './schema.json'
+import Story from './story'
 
-export default (props) =>
-  <ui.Story>
-    <ui.Sources slot='picture' {...props} />
-    <Fragment slot='caption'>
-      <data className={style.story__data} value={props.date}>{props.date}</data>
-      <ui.Heading level='2' {...props}>{props.title}</ui.Heading>
-      <ui.P {...props}>by {props.author}</ui.P>
-      <ui.Link className={style.story__link} {...props}>{props.link}</ui.Link>
-    </Fragment>
-  </ui.Story>
+export default () =>
+  <Repeat iterator={schema} component={Story} />
---
+++ apps/photosnap/src/stories/cards/story.jsx
@@ -0,0 +1,14 @@
+import h, { Fragment } from '@rex/h'
+import * as ui from '@rex/kit'
+import style from './style.css'
+
+export default (props) =>
+  <ui.Story>
+    <ui.Sources slot='picture' {...props} />
+    <Fragment slot='caption'>
+      <data className={style.story__data} value={props.date}>{props.date}</data>
+      <ui.Heading level='2' {...props}>{props.title}</ui.Heading>
+      <ui.P {...props}>by {props.author}</ui.P>
+      <ui.Link className={style.story__link} {...props}>{props.link}</ui.Link>
+    </Fragment>
+  </ui.Story>
