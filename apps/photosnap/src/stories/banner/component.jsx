b4095ab968eceb88662a193f6db25bce67285302
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 15:47:15 -0300

    usando o component link

--- apps/photosnap/src/stories/banner/component.jsx
+++ apps/photosnap/src/stories/banner/component.jsx
@@ -10,8 +10,10 @@ export default (props) =>
         <ui.Heading level='2' className={style.banner__h2} size='small' {...props}>{props.subtitle}</ui.Heading>
         <ui.Heading level='1' className={style.banner__h1} {...props}>{props.title}</ui.Heading>
       </hgroup>
-      <p className={style.banner__p}><date>{props.date}</date> by {props.author}</p>
-      <p className={style.banner__p}>{props.description}</p>
-      <ui.LinkButton className={style.banner__linkButton} color='inverse' href={props.url}>Read story</ui.LinkButton>
+      <ui.P className={style.banner__p} {...props}>
+        <date>{props.date}</date> by {props.author}
+      </ui.P>
+      <ui.P className={style.banner__p} {...props}>{props.description}</ui.P>
+      <ui.Link className={style.banner__linkButton} {...props}>{props.link}</ui.Link>
     </Fragment>
   </ui.Hero>
