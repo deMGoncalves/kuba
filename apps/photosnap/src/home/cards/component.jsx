af76fdd806167339f9adf3e5e2d145c9cf0a0000
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 08:08:46 -0300

    usando o ui component sources

--- apps/photosnap/src/home/cards/component.jsx
+++ apps/photosnap/src/home/cards/component.jsx
@@ -1,14 +1,11 @@
 import h from '@rex/h'
-import * as f from '@rex/f'
 import * as ui from '@rex/kit'
 import style from './style.css'
 
 export default (props) =>
   <figure className={[style.card, props.className]}>
     <ui.Picture className={style.card__picture} alt={props.title}>
-      <source srcSet={f.prop('[0]', props.sources)} media='(max-width: 767px)' />
-      <source srcSet={f.prop('[1]', props.sources)} media='(min-width: 768px) and (max-width: 959px)' />
-      <source srcSet={f.prop('[2]', props.sources)} media='(min-width: 960px)' />
+      <ui.Sources {...props} />
     </ui.Picture>
     <figcaption className={style.card__figcaption}>
       <ui.Heading level='2' {...props}>{props.title}</ui.Heading>
