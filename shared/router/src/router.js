46e6453902fb39cd448668be06ceb1072cb0ab82
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 13:38:19 -0300

    teste de performance para a rota, executando no processo de load

--- shared/router/src/router.js
+++ shared/router/src/router.js
@@ -2,6 +2,7 @@ import * as f from '@rex/f'
 import pathname from './pathname'
 
 const router = (path, listener) =>
-  f.test(path, pathname) && listener(...f.slice(f.exec(path, pathname), 1))
+  window.addEventListener('DOMContentLoaded', () =>
+    f.test(path, pathname) && listener(...f.slice(f.exec(path, pathname), 1)))
 
 export default f.curry(router)
