28e2c31dbf0cd26e18d38467fb949edfcaceceff
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 12:10:46 -0300

    reaproveitando os component story e sources

--- apps/photosnap/src/stories/cards/component.jsx
+++ apps/photosnap/src/stories/cards/component.jsx
@@ -1,19 +1,14 @@
-import h from '@rex/h'
-import * as f from '@rex/f'
+import h, { Fragment } from '@rex/h'
 import * as ui from '@rex/kit'
 import style from './style.css'
 
 export default (props) =>
-  <figure className={[style.card, props.className]}>
-    <ui.Picture className={style.card__picture} alt={props.title}>
-      <source srcSet={f.prop('[0]', props.sources)} media='(max-width: 767px)' />
-      <source srcSet={f.prop('[1]', props.sources)} media='(min-width: 768px) and (max-width: 959px)' />
-      <source srcSet={f.prop('[2]', props.sources)} media='(min-width: 960px)' />
-    </ui.Picture>
-    <figcaption className={style.card__figcaption}>
+  <ui.Story>
+    <ui.Sources slot='picture' {...props} />
+    <Fragment slot='caption'>
       <data className={style.card__data} value={props.date}>{props.date}</data>
       <h2 className={style.card__h2}>{props.title}</h2>
       <p className={style.card__p}>by {props.author}</p>
       <ui.LinkButton className={style.card__linkButton} color='inverse' href={props.url}>Read story</ui.LinkButton>
-    </figcaption>
-  </figure>
+    </Fragment>
+  </ui.Story>
