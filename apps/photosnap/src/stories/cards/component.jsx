d97f41804567b799f8308610007ff5c267ae15ff
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 13:31:31 -0300

    removendo os css que nao sao usados

--- apps/photosnap/src/stories/cards/component.jsx
+++ apps/photosnap/src/stories/cards/component.jsx
@@ -6,9 +6,9 @@ export default (props) =>
   <ui.Story>
     <ui.Sources slot='picture' {...props} />
     <Fragment slot='caption'>
-      <data className={style.card__data} value={props.date}>{props.date}</data>
+      <data className={style.story__data} value={props.date}>{props.date}</data>
       <ui.Heading level='2' {...props}>{props.title}</ui.Heading>
       <ui.P {...props}>by {props.author}</ui.P>
-      <ui.Link className={style.card__link} {...props}>{props.link}</ui.Link>
+      <ui.Link className={style.story__link} {...props}>{props.link}</ui.Link>
     </Fragment>
   </ui.Story>
