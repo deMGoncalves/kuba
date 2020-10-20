088601f5ab1921df3721d8a882c550c869ea2318
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-20 14:24:04 -0300

    separando as responsabilidades do stories

--- apps/photosnap/src/stories/cards/component.jsx
+++
@@ -1,6 +0,0 @@
-import h, { Repeat } from '@rex/h'
-import schema from './schema.json'
-import Story from './story'
-
-export default () =>
-  <Repeat iterator={schema} component={Story} />
--- apps/photosnap/src/stories/component.jsx
+++ apps/photosnap/src/stories/component.jsx
@@ -1,13 +1,14 @@
-import h, { Fragment } from '@rex/h'
+import h, { Repeat } from '@rex/h'
 import * as ui from '@rex/kit'
 import banner from './banner'
-import cards from './cards'
+import schema from './schema.json'
+import Story from './story'
 import style from './style.css'
 
 export default () =>
   <main className={style.stories}>
-    <ui.Header className={style.stories__header} />
+    <ui.Header />
     {banner}
-    {cards}
-    <ui.Footer className={style.stories__footer} />
+    <Repeat iterator={schema} component={Story} />
+    <ui.Footer />
   </main>
