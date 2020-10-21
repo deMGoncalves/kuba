85301751cd8f46e9197a17b4584155821a4284ce
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-21 08:50:31 -0300

    reparando as responsabilidades

--- apps/photosnap/src/home/features/component.jsx
+++ apps/photosnap/src/home/features/component.jsx
@@ -1,16 +1,7 @@
-import h, { Fragment, Repeat } from '@rex/h'
-import * as ui from '@rex/kit'
+import h, { Repeat } from '@rex/h'
+import Feature from './feature'
 import schema from './schema.json'
 import style from './style.css'
-
-const Feature = (props, i) =>
-  <ui.Feature className={style[`p${i}`]}>
-    <ui.Sources slot='picture' {...props} />
-    <Fragment slot='caption'>
-      <ui.Heading level='3' className={style.features__heading}>{props.title}</ui.Heading>
-      <ui.P className={style.features__p}>{props.description}</ui.P>
-    </Fragment>
-  </ui.Feature>
 
 export default () =>
   <ul className={style.features}>
---
+++ apps/photosnap/src/home/features/feature.jsx
@@ -0,0 +1,12 @@
+import h, { Fragment } from '@rex/h'
+import * as ui from '@rex/kit'
+import style from './style.css'
+
+export default (props, i) =>
+  <ui.Feature className={style[`p${i}`]}>
+    <ui.Sources slot='picture' {...props} />
+    <Fragment slot='caption'>
+      <ui.Heading level='3' className={style.features__heading}>{props.title}</ui.Heading>
+      <ui.P className={style.features__p}>{props.description}</ui.P>
+    </Fragment>
+  </ui.Feature>
