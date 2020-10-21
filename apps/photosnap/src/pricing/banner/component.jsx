a4b8cca57e4526bf6b2256c43354501388f67d8a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-21 07:39:50 -0300

    reaproveitando o component heading

--- apps/photosnap/src/pricing/banner/component.jsx
+++ apps/photosnap/src/pricing/banner/component.jsx
@@ -1,5 +1,4 @@
 import h, { Fragment } from '@rex/h'
-import * as f from '@rex/f'
 import * as ui from '@rex/kit'
 import style from './style.css'
 
@@ -7,7 +6,7 @@ export default (props) =>
   <ui.Hero color='inverse' direction='reverse'>
     <ui.Sources slot='figure' {...props} />
     <Fragment slot='caption'>
-      <h1 className={style.banner__h1}>{props.title}</h1>
+      <ui.Heading className={style.banner__heading} {...props}>{props.title}</ui.Heading>
       <p className={style.banner__p}>{props.description}</p>
     </Fragment>
   </ui.Hero>
