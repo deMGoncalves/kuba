e4c853a57be76bc75929cdf73344a3d27009d5f9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-21 08:47:35 -0300

    usando o component feature

--- apps/photosnap/src/home/features/component.jsx
+++ apps/photosnap/src/home/features/component.jsx
@@ -1,16 +1,16 @@
-import h, { Repeat } from '@rex/h'
+import h, { Fragment, Repeat } from '@rex/h'
 import * as ui from '@rex/kit'
 import schema from './schema.json'
 import style from './style.css'
 
 const Feature = (props, i) =>
-  <li className={style.features__li}>
-    <ui.Picture className={[style.features__picture, style[`p${i}`]]}>
-      <source srcSet={props.icon} />
-    </ui.Picture>
-    <ui.Heading level='3'>{props.title}</ui.Heading>
-    <p className={style.features__p}>{props.description}</p>
-  </li>
+  <ui.Feature className={style[`p${i}`]}>
+    <ui.Sources slot='picture' {...props} />
+    <Fragment slot='caption'>
+      <ui.Heading level='3' className={style.features__heading}>{props.title}</ui.Heading>
+      <ui.P className={style.features__p}>{props.description}</ui.P>
+    </Fragment>
+  </ui.Feature>
 
 export default () =>
   <ul className={style.features}>
