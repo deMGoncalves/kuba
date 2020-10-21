296a1b3737354890eda8a92c3e425ced6e113199
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-10-21 08:35:15 -0300

    usando o component feature

--- apps/photosnap/src/features/feature/component.jsx
+++ apps/photosnap/src/features/feature/component.jsx
@@ -1,12 +1,12 @@
-import h from '@rex/h'
+import h, { Fragment } from '@rex/h'
 import * as ui from '@rex/kit'
 import style from './style.css'
 
 export default (props, i) =>
-  <li className={style.feature}>
-    <ui.Picture className={[style.feature__picture, style[`p${i}`]]}>
-      <ui.Sources {...props} />
-    </ui.Picture>
-    <ui.Heading level='2' size='large' className={style.feature__heading}>{props.title}</ui.Heading>
-    <ui.P className={style.feature__p}>{props.description}</ui.P>
-  </li>
+  <ui.Feature className={style[`p${i}`]}>
+    <ui.Sources slot='picture' {...props} />
+    <Fragment slot='caption'>
+      <ui.Heading level='2' size='large' className={style.feature__heading}>{props.title}</ui.Heading>
+      <ui.P className={style.feature__p}>{props.description}</ui.P>
+    </Fragment>
+  </ui.Feature>
