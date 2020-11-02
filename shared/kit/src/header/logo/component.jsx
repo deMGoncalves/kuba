eee218cca20b662475e4570d083d9145816542ca
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-02 11:51:49 -0300

    removendo o header do kit

--- shared/kit/src/header/logo/component.jsx
+++
@@ -1,8 +0,0 @@
-import h from '@rex/h'
-import Picture from '@rex/kit/src/picture'
-import style from './style.css'
-
-export default (logo) =>
-  <Picture className={style.logo} onClick={() => logo.redirect()}>
-    <source srcSet={logo.src} />
-  </Picture>
