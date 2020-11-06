4d211ea3c69cf551ae050c0f3cc3b4ab9c388bc8
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-06 20:12:45 -0300

    inclusao de um descriptiopn em stories

--- buckets/rexsnap/src/home/home.js
+++ buckets/rexsnap/src/home/home.js
@@ -5,7 +5,13 @@ import component from './component'
 @paint(component)
 @markup
 class Home {
+  get description () {
+    return 'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others'
+  }
 
+  get title () {
+    return 'Rexsnap'
+  }
 }
 
 export default Home
--- buckets/rexsnap/src/stories/stories.js
+++ buckets/rexsnap/src/stories/stories.js
@@ -5,6 +5,10 @@ import component from './component'
 @paint(component)
 @markup
 class Stories {
+  get description () {
+    return 'Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others'
+  }
+
   get title () {
     return 'Stories • Create and share your photo stories'
   }
