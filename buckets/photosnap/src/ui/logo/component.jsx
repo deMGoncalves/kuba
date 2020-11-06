c4fa8ddc577962764c68239cca9c41a756520b36
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-06 14:09:21 -0300

    configuracao do thema

--- buckets/photosnap/src/ui/logo/component.jsx
+++ buckets/photosnap/src/ui/logo/component.jsx
@@ -3,8 +3,8 @@ import * as ui from '@ui'
 import style from './style.css'
 
 export default (logo) =>
-  <div className={style.logo}>
-    <ui.Picture className={style.logo__picture} onClick={() => logo.redirect()}>
+  <div className={style.logo} onClick={() => logo.redirect()}>
+    <ui.Picture className={style.logo__picture}>
       <source srcSet={logo.src} />
     </ui.Picture>
     <ui.Heading className={style.logo__heading} size='large' color={logo.color}>{logo.name}</ui.Heading>
