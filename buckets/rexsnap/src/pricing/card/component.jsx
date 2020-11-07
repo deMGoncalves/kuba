536b17a25272a248b51059eb8ab8da9bee2ff2a7
Evandro P. Santos <evandropsantos@gmail.com>
2020-11-07 15:18:20 -0300

    update card component

--- buckets/rexsnap/src/pricing/card/component.jsx
+++ buckets/rexsnap/src/pricing/card/component.jsx
@@ -1,19 +1,21 @@
-import h from '@rex/h'
+import h, { Show } from '@rex/h'
+import * as ui from '@ui'
 import style from './style.css'
 
-export default (props, children) =>
-  <div className={[style.card, style[props.type], props.className, 'rainbowBar']}>
+export default (card) =>
+  <div className={[style.card, style[card.type], card.className]}>
+    <Show if={card.type}>
+      <ui.Gradient className={style.card__gradient} />
+    </Show>
+
     <header className={style.card__header}>
-      <em className={style.card__title}>{props.title}</em>
-      <p className={style.card__text}>{children}</p>
+      <em className={style.card__name}>{card.name}</em>
+      <p className={style.card__description}>{card.description}</p>
     </header>
-    <p className={style.card__price}>
-      <data className={style.card__dataMonth} value={props.priceMonth}>
-        <em className={style.card__month}>$ {props.priceMonth}</em> per month
-      </data>
-      <data className={style.card__dataYear} value={props.priceYear}>
-        <em className={style.card__year}>$ {props.priceYear}</em> per year
-      </data>
-    </p>
-    <button className={style.card__button}>Pick Plan</button>
+
+    <data className={style.card__price} value={card.price}>
+      <em className={style.card__label}>{card.price}</em> per {card.plan}
+    </data>
+
+    <ui.Button className={style.card__button} type={card.type}>Pick Plan</ui.Button>
   </div>
