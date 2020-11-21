6d1834696c101713517706bd541b0518b2366e31
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 15:34:44 -0300

    os imports devem seguir ordem alfabetica

--- shared/h/src/extendAttributes/extendAttributes.js
+++ shared/h/src/extendAttributes/extendAttributes.js
@@ -1,7 +1,7 @@
 import * as f from '@rex/f'
 import resolveExtend from './resolveExtend'
 
-const extendAttributes = (props, element) =>
-  f.always(element)(f.forEach(f.toPairs(props), resolveExtend(element)))
+const extendAttributes = (props, node) =>
+  f.always(node)(f.forEach(f.toPairs(props), resolveExtend(node)))
 
 export default f.curry(extendAttributes)
