775ed913578e1f6fa5ddfe849088bc59085c6a83
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 18:03:01 -0300

    vou usar o mapeamento nos dunders, assim consigo evitar a repeticao

--- shared/h/src/paint/createHookReflow.js
+++ shared/h/src/paint/createHookReflow.js
@@ -1,16 +1,15 @@
 import * as f from '@rex/f'
+import * as d from '@rex/h/src/dunders'
 import reflow from '@rex/h/src/reflow'
-
-const element = f.magic('h/element')
 
 export default (instance, component, children) => {
   f.assign(instance, {
-    [element]: f.assign(component(instance, children), { [f.magic('h/target')]: instance }),
+    [d.element]: f.assign(component(instance, children), { [f.magic('h/target')]: instance }),
 
     [f.magic('h/reflow')]: f.idle(function () {
-      reflow(this[element], component(this, children))
+      reflow(this[d.element], component(this, children))
     })
   })
 
-  return instance[element]
+  return instance[d.element]
 }
