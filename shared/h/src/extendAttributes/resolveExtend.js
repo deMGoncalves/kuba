b60ec3b9a203c18811b7de6caf2e899231941ae5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 15:37:52 -0300

    trocando o identificador element para node, vamos manter a linguagem ubiqua

--- shared/h/src/extendAttributes/resolveExtend.js
+++ shared/h/src/extendAttributes/resolveExtend.js
@@ -13,14 +13,14 @@ import setAttribute from './setAttribute'
 import setClassName from './setClassName'
 import setHtmlFor from './setHtmlFor'
 
-export default (element) =>
+export default (node) =>
   (keyAndValue) =>
     f.cond(
-      [isEvent, addEventListenerPure(element)],
-      [isEventWithStop, addEventListenerWithStop(element)],
-      [isEventWithPrevent, addEventListenerWithPrevent(element)],
-      [isEventWithStopAndPrevent, addEventListenerWithStopAndPrevent(element)],
-      [isClassName, setClassName(element)],
-      [isHtmlFor, setHtmlFor(element)],
-      [f.T, setAttribute(element)]
+      [isEvent, addEventListenerPure(node)],
+      [isEventWithStop, addEventListenerWithStop(node)],
+      [isEventWithPrevent, addEventListenerWithPrevent(node)],
+      [isEventWithStopAndPrevent, addEventListenerWithStopAndPrevent(node)],
+      [isClassName, setClassName(node)],
+      [isHtmlFor, setHtmlFor(node)],
+      [f.T, setAttribute(node)]
     )(...keyAndValue)
