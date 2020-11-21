ac4484780b1be2766bd976568404fb44a1debf45
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 17:18:26 -0300

    nao preciso usar o jsx para fazer uma atividade bem simples

--- shared/h/src/hide/hide.jsx
+++ shared/h/src/hide/hide.jsx
@@ -2,4 +2,4 @@ import h from '@rex/h/src/h'
 import Fragment from '@rex/h/src/fragment'
 
 export default (props, children) =>
-  props.if ? <Fragment /> : children
+  props.if ? Fragment : children
