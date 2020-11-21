f89d102837413f9ec9986a61d559ddfdd2766bfb
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 16:34:09 -0300

    vamos manter os identificadores dos functors o mais legivel possivel

--- shared/h/src/extendAttributes/extendAttributes.js
+++ shared/h/src/extendAttributes/extendAttributes.js
@@ -1,7 +1,7 @@
 import * as f from '@rex/f'
-import resolveExtend from './resolveExtend'
+import mapAttribute from './mapAttribute'
 
 const extendAttributes = (props, node) =>
-  f.always(node)(f.forEach(f.toPairs(props), resolveExtend(node)))
+  f.always(node)(f.forEach(f.toPairs(props), mapAttribute(node)))
 
 export default f.curry(extendAttributes)
