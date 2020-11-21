5c789dad89ce34b66f95a9033244bbf22552c7d0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 15:42:14 -0300

    vamos manter o padrao das outras functors, de eventName para name

--- shared/h/src/extendAttributes/addEventListenerPure.js
+++ shared/h/src/extendAttributes/addEventListenerPure.js
@@ -2,5 +2,5 @@ import * as f from '@rex/f'
 import addEventListener from './addEventListener'
 
 export default (node) =>
-  (eventName, handler) =>
-    addEventListener(node, eventName, f.idle(handler))
+  (name, handler) =>
+    addEventListener(node, name, f.idle(handler))
