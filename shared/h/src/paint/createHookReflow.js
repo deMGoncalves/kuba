fab507e00667087504f7e5389f6184bd18c6de92
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 18:11:09 -0300

    usando o dunder target para evitar a repeticao de instrucao

--- shared/h/src/paint/createHookReflow.js
+++ shared/h/src/paint/createHookReflow.js
@@ -4,7 +4,7 @@ import reflow from '@rex/h/src/reflow'
 
 export default (instance, component, children) => {
   f.assign(instance, {
-    [d.element]: f.assign(component(instance, children), { [f.magic('h/target')]: instance }),
+    [d.element]: f.assign(component(instance, children), { [d.target]: instance }),
 
     [f.magic('h/reflow')]: f.idle(function () {
       reflow(this[d.element], component(this, children))
