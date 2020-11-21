309e58828ab3b3af56b9849c4da2e6f1cadfa628
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 15:09:24 -0300

    vou deixar o codigo mais legivel possivel, trocando o identificador element para node

--- shared/h/src/h/appendChildren.js
+++ shared/h/src/h/appendChildren.js
@@ -2,7 +2,7 @@ import * as f from '@rex/f'
 import mapComponent from './mapComponent'
 import mapTextNode from './mapTextNode'
 
-const appendChildren = (children, element) =>
-  f.always(element)(element.append(...mapTextNode(mapComponent(children))))
+const appendChildren = (children, node) =>
+  f.always(node)(node.append(...mapTextNode(mapComponent(children))))
 
 export default f.curry(appendChildren)
