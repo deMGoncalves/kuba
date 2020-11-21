af38f7292e4245ba24ebbc695c73f2a66929625c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 12:33:49 -0300

    vamos deixa a funcao o mais parecido com a linguagem ubiqua dos programadores

--- shared/h/src/h/h.js
+++ shared/h/src/h/h.js
@@ -4,8 +4,8 @@ import executeComponent from './executeComponent'
 import isComponent from './isComponent'
 import isTagName from './isTagName'
 
-export default (tagNameOrComponent, attributes, ...children) =>
+export default (tagNameOrComponent, props, ...children) =>
   f.cond(
     [isTagName, createElement],
     [isComponent, executeComponent]
-  )(tagNameOrComponent, { ...attributes }, f.flatten(children))
+  )(tagNameOrComponent, { ...props }, f.flatten(children))
