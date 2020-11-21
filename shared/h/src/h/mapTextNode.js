927f8d21e88e9a5158b2c3656bbff0a19a911bbf
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 15:06:04 -0300

    vamos trabalhar com linguagem ubiqua, quero deixar o codigo mais legivel

--- shared/h/src/h/mapTextNode.js
+++ shared/h/src/h/mapTextNode.js
@@ -1,6 +1,5 @@
 import * as f from '@rex/f'
 import isTextNode from './isTextNode'
 
-export default (children) =>
-  f.map(children, (child) =>
-    isTextNode(child) ? document.createTextNode(child) : child)
+export default f.map(f.__, (nodeOrText) =>
+  isTextNode(nodeOrText) ? document.createTextNode(nodeOrText) : nodeOrText)
