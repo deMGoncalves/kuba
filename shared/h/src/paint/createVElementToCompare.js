79e59482b3a45e4495455d346ead595fc059abcf
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-21 18:24:18 -0300

    usando o dunder target para evitar repeticao de intrucao

--- shared/h/src/paint/createVElementToCompare.js
+++ shared/h/src/paint/createVElementToCompare.js
@@ -1,4 +1,5 @@
 import * as f from '@rex/f'
+import * as d from '@rex/h/src/dunders'
 
 export default (instance, component, children) =>
-  f.assign(component(instance, children), { [f.magic('h/target')]: instance })
+  f.assign(component(instance, children), { [d.target]: instance })
