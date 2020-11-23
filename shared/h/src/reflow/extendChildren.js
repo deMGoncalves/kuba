60febee96f52868b3f2e16daaf562cbb1a999d57
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-23 10:11:37 -0300

    trocando o identificador element por node

--- shared/h/src/reflow/extendChildren.js
+++ shared/h/src/reflow/extendChildren.js
@@ -2,12 +2,12 @@ import * as f from '@rex/f'
 import greaterNumberOfChildren from './greaterNumberOfChildren'
 import reflow from './reflow'
 
-export default (element, vElement) =>
-  f.always(element)(
+export default (node, vNode) =>
+  f.always(node)(
     f.forEach(
       f.map(
-        f.repeat(null, greaterNumberOfChildren(element, vElement)),
-        (_, i) =>
-          [element.childNodes[i], vElement.childNodes[i], element]
+        f.repeat(null, greaterNumberOfChildren(node, vNode)),
+        (_, index) =>
+          [node.childNodes[index], vNode.childNodes[index], node]
       ),
       (args) => reflow(...args)))
