487a5cf54aa56267986dc62765d7a2313bff24b4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-23 10:16:25 -0300

    trocando o termo element para node

--- shared/h/src/reflow/getAttributes.js
+++ shared/h/src/reflow/getAttributes.js
@@ -1,8 +1,8 @@
 import * as f from '@rex/f'
 
-export default (vElement) =>
+export default (vNode) =>
   []
     .slice
-    .call(f.or(vElement.attributes, []))
+    .call(f.or(vNode.attributes, []))
     .reduce((reducer, { name, value }) =>
       ({ ...reducer, [name]: value }), {})
