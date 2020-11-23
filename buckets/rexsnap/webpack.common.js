e4b29f6ac6be0c7846f27ee791290be821534050
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-23 16:25:14 -0300

    separando o css do projeto

--- buckets/rexsnap/webpack.common.js
+++ buckets/rexsnap/webpack.common.js
@@ -1,6 +1,7 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
@@ -23,7 +24,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
             options: {
@@ -71,6 +72,17 @@ module.exports = {
         to: '.'
       }
     ]),
+    new MiniCssExtractPlugin({
+      attributes: {
+        as: 'style',
+        async: true,
+        onload: 'this.rel="stylesheet"',
+        rel: 'preload'
+      },
+      chunkFilename: '[name].[hash].css',
+      filename: '[name].[hash].css',
+      linkType: false
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'template/index.html')
     })
--- buckets/rexsnap/webpack.prod.js
+++ buckets/rexsnap/webpack.prod.js
@@ -1,4 +1,5 @@
 const common = require('./webpack.common.js')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const merge = require('webpack-merge')
 const path = require('path')
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
@@ -8,6 +9,7 @@ module.exports = merge.smart(common, {
   mode: 'production',
   optimization: {
     minimizer: [
+      new CssMinimizerPlugin(),
       new TerserJSPlugin({
         terserOptions: {
           keep_fnames: true,
