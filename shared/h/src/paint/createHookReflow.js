eb8cc8e3362ddba129d4e4aea53cdbc5333435ec
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-23 09:06:28 -0300

    usando o identificado klass ao inves de instance

--- shared/h/src/paint/createHookReflow.js
+++ shared/h/src/paint/createHookReflow.js
@@ -3,14 +3,14 @@ import * as d from '@rex/h/src/dunders'
 import reflow from '@rex/h/src/reflow'
 import createComponent from './createComponent.js'
 
-export default (instance, component, children) => {
-  f.assign(instance, {
-    [d.element]: createComponent(instance, component, children),
+export default (klass, component, children) => {
+  f.assign(klass, {
+    [d.element]: createComponent(klass, component, children),
 
     [d.reflow]: f.idle(function () {
       reflow(this[d.element], component(this, children))
     })
   })
 
-  return instance[d.element]
+  return klass[d.element]
 }
