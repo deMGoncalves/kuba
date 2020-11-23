eb94ba1b8a40aa11cfe2d557b2d8d295490a193f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-23 08:18:19 -0300

    vamos reaproveitar codigo

--- shared/h/src/paint/createHookReflow.js
+++ shared/h/src/paint/createHookReflow.js
@@ -1,10 +1,11 @@
 import * as f from '@rex/f'
 import * as d from '@rex/h/src/dunders'
 import reflow from '@rex/h/src/reflow'
+import createVElementToCompare from './createVElementToCompare.js'
 
 export default (instance, component, children) => {
   f.assign(instance, {
-    [d.element]: f.assign(component(instance, children), { [d.target]: instance }),
+    [d.element]: createVElementToCompare(instance, component, children),
 
     [d.reflow]: f.idle(function () {
       reflow(this[d.element], component(this, children))
