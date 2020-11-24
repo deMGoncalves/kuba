cadcde08de9514530e63fd9e98156110bae0613b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-24 11:55:16 -0300

    precisa fazer o flatten

--- shared/h/src/raw/raw.js
+++ shared/h/src/raw/raw.js
@@ -1,5 +1,6 @@
+import * as f from '@rex/f'
 import fragment from '@rex/h/src/fragment'
 import parser from './parser'
 
 export default (props, children) =>
-  fragment(props, parser(children))
+  fragment(props, f.flatten(parser(children)))
