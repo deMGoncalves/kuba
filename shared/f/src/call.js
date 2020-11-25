659d27f97358cd07acaa9d39b68eb3ff2e0b3193
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-25 15:16:37 -0300

    referencia errado

--- shared/f/src/call.js
+++ shared/f/src/call.js
@@ -1,7 +1,7 @@
 import arity from '@rex/f/src/arity'
 import curry from '@rex/f/src/curry'
 
-const call = (tagert, ...args) =>
+const call = (target, ...args) =>
   () => target(...args)
 
 export default curry(arity(2, call))
