bcd5239e20017b97145b2e090cea31b39a43918c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-26 20:43:56 -0300

    movendo o footer para o contexto de bloco

--- buckets/rexsnap/src/site/component.jsx
+++ buckets/rexsnap/src/site/component.jsx
@@ -1,10 +1,11 @@
 import h from '@rex/h'
 import * as ui from '@ui'
+import Footer from '@rex/footer'
 import style from './style.css'
 
 export default (site, children) =>
   <main className={[style.template, site.className]}>
     <ui.Header />
     {children}
-    <ui.Footer />
+    <Footer />
   </main>
