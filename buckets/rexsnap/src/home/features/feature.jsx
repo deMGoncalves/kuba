006c54f3175b3bcfbc9c4ce1ad762c59687aceec
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-11-27 10:32:21 -0300

    usando os bloco e tags

--- buckets/rexsnap/src/home/features/feature.jsx
+++ buckets/rexsnap/src/home/features/feature.jsx
@@ -1,12 +1,14 @@
 import h, { Fragment } from '@rex/h'
-import * as ui from '@ui'
+import Feature from '@rex/feature'
+import Sources from '@rex/sources'
+import text from '@rex/text'
 import style from './style.css'
 
 export default (props, i) =>
-  <ui.Feature className={style[`p${i}`]}>
-    <ui.Sources slot='picture' {...props} />
+  <Feature className={style[`p${i}`]}>
+    <Sources slot='picture' {...props} />
     <Fragment slot='caption'>
-      <ui.Heading level='3' className={style.features__heading}>{props.title}</ui.Heading>
-      <ui.P className={style.features__p}>{props.description}</ui.P>
+      <text.H3 className={style.features__heading}>{props.title}</text.H3>
+      <text.P className={style.features__p}>{props.description}</text.P>
     </Fragment>
-  </ui.Feature>
+  </Feature>
