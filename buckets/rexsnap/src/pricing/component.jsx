e1d2cdb7549ac8a630ddac83d59f8cb3aa190409
Evandro P. Santos <evandropsantos@gmail.com>
2020-12-01 08:38:22 -0300

    Erro no repaint

--- buckets/rexsnap/src/pricing/component.jsx
+++ buckets/rexsnap/src/pricing/component.jsx
@@ -12,7 +12,11 @@ export default (pricing) =>
   <Site>
     <Hero />
     <section className={style.pricing__content} data-pricing='month'>
-      <Switch className={style.pricing__switch} />
+      <header className={style.pricing__switch}>
+        <data value='Monthly' className={style.switch__monthly}>Monthly</data>
+        <Switch className={style.pricing__switch} onChange={(event) => pricing.checked(event)} />
+        <data value='Yearly' className={style.switch__yearly}>Yearly</data>
+      </header>
 
       <Repeat iterator={pricing.data} component={Card} />
 
--- buckets/rexsnap/src/pricing/switch/component.jsx
+++ buckets/rexsnap/src/pricing/switch/component.jsx
@@ -1,20 +1,16 @@
 import h from '@rex/h'
 import style from './style.css'
 
-const toggle = () => {
-  const markup = document.querySelector('[data-pricing]')
+// const toggle = () => {
+//   const markup = document.querySelector('[data-pricing]')
 
-  return (markup.dataset.pricing === 'month')
-    ? markup.setAttribute('data-pricing', 'year')
-    : markup.setAttribute('data-pricing', 'month')
-}
+//   return (markup.dataset.pricing === 'month')
+//     ? markup.setAttribute('data-pricing', 'year')
+//     : markup.setAttribute('data-pricing', 'month')
+// }
 
-export default (props) =>
-  <header className={[style.switch, props.className]}>
-    <data value='Monthly' className={style.switch__monthly}>Monthly</data>
-    <label className={style.switch__label}>
-      <input className={style.switch__check} type='checkbox' onChange={() => toggle()} />
-      <span className={style.switch__slider} />
-    </label>
-    <data value='Yearly' className={style.switch__yearly}>Yearly</data>
-  </header>
+export default ({ onChange }) =>
+  <label className={style.switch__label}>
+    <input className={style.switch__check} type='checkbox' onChange={onChange} />
+    <span className={style.switch__slider} />
+  </label>
