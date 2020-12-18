5fb000d455efa1681b94efe29fd176dbf83d5573
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-12-18 10:20:49 -0300

    organizando a estrutura do pacote h

---
+++ shared/h/src/index.js
@@ -0,0 +1,7 @@
+export { default } from './h'
+export { default as Fragment } from './fragment'
+export { default as Hide } from './hide'
+export { default as render } from './render'
+export { default as Raw } from './raw'
+export { default as Repeat } from './repeat'
+export { default as Show } from './show'
