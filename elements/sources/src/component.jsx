66ba397081c3c9d15e8f30605fc5fcdf8d5a1a7d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-12-18 08:55:07 -0300

    organizando o projeto sources

--- elements/sources/src/component.jsx
+++ elements/sources/src/component.jsx
@@ -1,9 +1,9 @@
-import h, { Fragment } from '@rex/sandbox'
+import h, { Fragment } from '@rex/htmlparser'
 import * as f from '@rex/f'
 
-export default ({ slot, thumbnails: [mobile, tablet, desktop] }) =>
-  <Fragment slot={slot}>
+export default ({ thumbnails: [mobile, tablet, desktop] }) =>
+  <>
     <source srcSet={f.or(mobile, tablet, desktop)} media='(max-width: 767px)' />
     <source srcSet={f.or(tablet, mobile, desktop)} media='(min-width: 768px) and (max-width: 959px)' />
     <source srcSet={f.or(desktop, tablet, mobile)} media='(min-width: 960px)' />
-  </Fragment>
+  </>
