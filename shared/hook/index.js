e85306d2310b31c4bfcb8ec1f314d4bec0ea5340
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-12-18 10:23:15 -0300

    organizando a estrutura do pacote hook

--- shared/hook/index.js
+++
@@ -1,9 +0,0 @@
-import after from './src/after'
-import before from './src/before'
-import call from './src/call'
-
-export default call
-export {
-  after,
-  before
-}
---
+++ shared/hook/src/index.js
@@ -0,0 +1,9 @@
+import after from './after'
+import before from './before'
+import call from './call'
+
+export default call
+export {
+  after,
+  before
+}
