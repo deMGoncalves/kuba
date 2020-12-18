201def8fdf22156516801e66623595d08d5f5748
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-12-18 10:47:53 -0300

    organizando a estrutura do pacote router

--- shared/router/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './src/router'
-export { default as urlFor } from './src/urlfor'
---
+++ shared/router/src/index.js
@@ -0,0 +1,2 @@
+export { default } from './router'
+export { default as urlFor } from './urlfor'
