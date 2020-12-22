d1822603163df509b9183118e8848c9f41d322c2
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-12-22 11:51:54 -0300

    implementando o controle de elementos hide

--- shared/htmlparser/src/index.js
+++ shared/htmlparser/src/index.js
@@ -1,5 +1,6 @@
 export { default } from './h'
 export { default as Fragment } from './fragment'
+export { default as Hide } from './hide'
 export { default as paint } from './paint'
 export { default as render } from './render'
 export { default as repaint } from './repaint'
