81943d0082be23675d0834fdcc8bded65fc7f50c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2020-12-22 15:04:26 -0300

    deixando a estrutura do text o mais simples possivel

--- elements/text/src/text.js
+++ elements/text/src/text.js
@@ -4,5 +4,5 @@ import style from './style.css'
 export default new Proxy({}, {
   get: (_, tagName) =>
     (props, children) =>
-      h(tagName, { ...props, className: [style.text, style[props.color], style[props.size], props.className] }, ...children)
+      h(tagName, { ...props, className: [style.text, props.className] }, ...children)
 })
