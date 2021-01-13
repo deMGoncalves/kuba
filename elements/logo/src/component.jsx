b9d9fad4cd3c9955c2f849025ad7b249b797bf94
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-13 19:17:49 -0300

    nao vou usar o texto no logo, apenas o icone

--- elements/logo/src/component.jsx
+++ elements/logo/src/component.jsx
@@ -1,12 +1,10 @@
 import h from '@rex/htmlparser'
-import text from '@rex/text'
 import Picture from '@rex/picture'
 import style from './style.css'
 
 export default (logo) =>
-  <div className={style.logo} onClick={() => logo.redirect()}>
+  <div className={[style.logo, logo.className]} onClick={() => logo.redirect()}>
     <Picture className={style.logo__picture}>
       <source srcSet={logo.thumbnail} />
     </Picture>
-    <text.H1 className={style.logo__heading} color={logo.color} size='large'>{logo.name}</text.H1>
   </div>
