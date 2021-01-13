703cc3f00a75515271867785bfd904d3dd0813ed
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-13 19:18:36 -0300

    diagramacao do header

--- blocks/header/src/component.jsx
+++ blocks/header/src/component.jsx
@@ -1,28 +1,18 @@
 import h from '@rex/htmlparser'
 import Container from '@rex/container'
-// import Button from '@rex/button'
-// import Link from '@rex/link'
 import Logo from '@rex/logo'
 import style from './style.css'
 
 export default (header) =>
   <header className={style.header}>
     <Container className={style.header__container}>
-      <div className={style.header__partner}>nega</div>
-      <div className={style.header__menu}>Menu</div>
+      <div className={style.header__partner}>partner</div>
+      <div className={style.header__menu}>menu</div>
       <Logo className={style.header__logo} />
-      <div className={style.header__search}>Search</div>
-      <div className={style.header__services}>Services</div>
-      <div className={style.header__user}>User</div>
-      <div className={style.header__bag}>Bag</div>
-      <div className={style.header__region}>Region</div>
-
-      {/* <button className={[style.header__toggle, style[header.status]]} onClick_prevent_stop={() => header.toggle()}>Menu</button>
-      <nav className={style.header__nav}>
-        <Link className={style.header__link} href='./stories'>Stories</Link>
-        <Link className={style.header__link} href='./features'>Features</Link>
-        <Link className={style.header__link} href='./pricing'>Pricing</Link>
-      </nav>
-      <Button className={style.header__button} href='/'>Get in invite</Button> */}
+      <div className={style.header__search}>search</div>
+      <div className={style.header__services}>services</div>
+      <div className={style.header__user}>user</div>
+      <div className={style.header__bag}>bag</div>
+      <div className={style.header__region}>region</div>
     </Container>
   </header>
