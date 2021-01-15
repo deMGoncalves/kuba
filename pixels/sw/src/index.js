ecc0d2c430ab5ddaf9096417d4304ae6920db172
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-15 13:54:41 -0300

    registrando e usando o pacote @rex/env para registrar o service worker

--- pixels/sw/src/index.js
+++ pixels/sw/src/index.js
@@ -1,9 +1,10 @@
 import * as f from '@rex/f'
 import agent from '@rex/agent'
+import env from '@rex/env'
 import register from './register'
 
 f.and(
   agent.isUser,
-  f.equal(process.env.NODE_ENV, 'production'),
+  env.isProd,
   f.has('serviceWorker', navigator)
 ) && register()
