0656d33f4e2822f1433736f466e90dbbb6f95e11
Evandro P. Santos <evandropsantos@gmail.com>
2021-01-19 15:14:35 -0300

    versao desktop drops

--- elements/drops/src/component.jsx
+++ elements/drops/src/component.jsx
@@ -1,8 +1,10 @@
 import h from '@rex/htmlparser'
 import style from './style.css'
 
-export default ({ icon, subtitle, title }) =>
-  <section className={[style.drops, style[`drops__${icon}`]]}>
-    <span className={style.drops__subtitle}>{subtitle}</span>
-    <p className={style.drops__title}>{title}</p>
+export default ({ name, subtitle, title }) =>
+  <section className={[style.drops, style[`drops__${name}`]]}>
+    <p className={style.drops__title}>
+      <span className={style.drops__subtitle}>{subtitle}</span>
+      {title}
+    </p>
   </section>
