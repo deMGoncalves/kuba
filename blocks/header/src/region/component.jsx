60d0c05120637926553fc3a06101e21913484d3c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-20 07:57:44 -0300

    vamos usar a cor info para indormacoes e labels

--- blocks/header/src/region/component.jsx
+++ blocks/header/src/region/component.jsx
@@ -5,6 +5,6 @@ import style from './style.css'
 
 export default (props) =>
   <Drop className={[style.region, props.className]}>
-    <text.Span className={style.region__subtitle} slot='subtitle' small>ative condições e</text.Span>
+    <text.Span className={style.region__subtitle} slot='subtitle' small info>ative condições e</text.Span>
     <text.P className={style.region__title} slot='title' small>ofertas da sua região</text.P>
   </Drop>
