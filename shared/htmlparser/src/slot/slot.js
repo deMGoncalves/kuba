8f463a5c764248a6383fed818d37dd6d40363547
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-20 07:49:27 -0300

    a logica do slot estava errado, tinha que fazer a busca dentro dos attributes

--- shared/htmlparser/src/slot/slot.js
+++ shared/htmlparser/src/slot/slot.js
@@ -3,5 +3,5 @@ import * as f from '@rex/f'
 export default (children) =>
   new Proxy(children, {
     get: (_, key) =>
-      f.or(children[key], f.find(children, f.compose(f.equal(key), f.prop('slot'))))
+      f.or(children[key], f.find(children, f.compose(f.equal(key), f.prop('attributes.slot'))))
   })
