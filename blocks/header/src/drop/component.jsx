8615dd69f3e252076e94cff32b77b9a7a780f621
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-20 07:41:10 -0300

    construcao da logica para o drop

--- blocks/header/src/drop/component.jsx
+++ blocks/header/src/drop/component.jsx
@@ -1,8 +1,10 @@
 import h from '@rex/htmlparser'
+import icon from '@rex/icon'
 import style from './style.css'
 
 export default (props, children) =>
   <section className={[style.drops, props.className]}>
     {children.subtitle}
     {children.title}
+    <icon.ArrowLeft className={style.drops__arrowLeft} slot='icon' />
   </section>
