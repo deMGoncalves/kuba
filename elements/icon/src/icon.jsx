ed870e8f34ad38964dd88353a6d0721b260d9d7a
Evandro P. Santos <evandropsantos@gmail.com>
2021-01-21 08:37:58 -0300

    passando as propriedades para o componente icone, adicionando novo icone

--- elements/icon/src/icon.jsx
+++ elements/icon/src/icon.jsx
@@ -6,7 +6,7 @@ import style from './style.css'
 export default new Proxy({}, {
   get: (_, icon) =>
     (props) =>
-      <Picture className={[style.icon, props.className]}>
+      <Picture {...props} className={[style.icon, props.className]}>
         <source srcSet={mapper[icon]} />
       </Picture>
 
--- elements/icon/src/mapper.js
+++ elements/icon/src/mapper.js
@@ -1,1 +1,2 @@
 export { default as ArrowLeft } from './assets/arrow-left.svg'
+export { default as Location } from './assets/location.svg'
