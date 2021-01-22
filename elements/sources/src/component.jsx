9a17f8334a643e91d2011eb14278e2df3b10f1a7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-22 07:00:30 -0300

    a escala de medias mudaram, nao temos mais o 960 e vamos usar o 1024

--- elements/sources/src/component.jsx
+++ elements/sources/src/component.jsx
@@ -4,6 +4,6 @@ import * as f from '@rex/f'
 export default ({ thumbnails: [mobile, tablet, desktop] }) =>
   <>
     <source srcSet={f.or(mobile, tablet, desktop)} media='(max-width: 767px)' />
-    <source srcSet={f.or(tablet, mobile, desktop)} media='(min-width: 768px) and (max-width: 959px)' />
-    <source srcSet={f.or(desktop, tablet, mobile)} media='(min-width: 960px)' />
+    <source srcSet={f.or(tablet, mobile, desktop)} media='(min-width: 768px) and (max-width: 1023px)' />
+    <source srcSet={f.or(desktop, tablet, mobile)} media='(min-width: 1024px)' />
   </>
