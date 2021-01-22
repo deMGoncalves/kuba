da99613c8fd224a739da8b2aaa94a3b028858814
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-22 07:03:55 -0300

    temos que extender o props para o component

--- elements/container/src/component.jsx
+++ elements/container/src/component.jsx
@@ -2,4 +2,4 @@ import h from '@rex/htmlparser'
 import style from './style.css'
 
 export default (props, children) =>
-  <div className={[style.container, props.className]}>{children}</div>
+  <div {...props} className={[style.container, props.className]}>{children}</div>
