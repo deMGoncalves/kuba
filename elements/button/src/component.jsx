cec33c9d5ff89b34a6d4929edc3ca051d53383a5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-01-22 07:05:00 -0300

    temos que extender o props para o component

--- elements/button/src/component.jsx
+++ elements/button/src/component.jsx
@@ -3,4 +3,4 @@ import * as f from '@rex/f'
 import style from './style.css'
 
 export default (props, children) =>
-  <a className={[style.button, props.className]} href={f.or(props.href, '#')}>{children}</a>
+  <a {...props} className={[style.button, props.className]} href={f.or(props.href, '#')}>{children}</a>
