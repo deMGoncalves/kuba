0056955e9b2b01e7d61aad7fcf9deef18cacd138
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-22 15:36:08 -0300

    style(lint): removendo dependencia que nao eh usado

--- buckets/app/src/notFound/notFound.js
+++ buckets/app/src/notFound/notFound.js
@@ -1,7 +1,5 @@
 import { paint } from '@kuba/h'
 import jsonld from '@kuba/jsonld'
-import { urlFor } from '@kuba/router'
-import Breadcrumb from '@kuba/breadcrumb'
 import markup from '@kuba/markup'
 import component from './component'
 import data from './data'
