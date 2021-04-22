3e5cd38c989c46bd275cd8cf4c332276c723147a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-22 16:09:04 -0300

    chore: padronizando o codigo para o padrao

--- shared/f/src/slug.js
+++ shared/f/src/slug.js
@@ -3,5 +3,5 @@ export default (value) =>
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
     .replace(/([^\w]+|\s+)/g, '-')
-    .replace(/\-\-+/g, '-')
+    .replace(/-+/g, '-')
     .replace(/(^-+|-+$)/, '')
