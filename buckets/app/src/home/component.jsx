e8b3fd5c78836155ab78dc37a4f009a23d7457fa
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-26 12:52:54 -0300

    feat(home): separando a descricao do projeto

--- buckets/app/src/home/component.jsx
+++ buckets/app/src/home/component.jsx
@@ -1,8 +1,7 @@
 import h from '@kuba/h'
 import Site from '@kuba/site'
-import Story from '@kuba/story'
-import text from '@kuba/text'
 import Cite from './cite'
+import Description from './description'
 import Summary from './summary'
 import style from './style.css'
 
@@ -10,9 +9,5 @@ export default () =>
   <Site className={style.home}>
     <Cite className={style.home__cite} />
     <Summary className={style.home__summary} />
-    <Story className={style.home__story}>
-      <text.P className={style.home__p} master medium>Ninguém precisa ter muitas habilidades para desenvolver um software que funcione, fazer direito é outra história. Desenvolver um software de maneira correta é um desafio que deixo lançado. Requer maturidade e conhecimentos que a maioria dos programadores ainda não tem.</text.P>
-      <br />
-      <text.P className={style.home__p} master medium>Quando focamos no domínio, minimizamos a quantidade de programadores para desenvolver e sustentar um software. As mudanças se tornam cirúrgicas, simples e fáceis. Deixamos de ser eficiente e nos tornamos eficazes. As funcionalidades e flexibilidades são maximizadas.</text.P>
-    </Story>
+    <Description className={style.home__description} />
   </Site>
