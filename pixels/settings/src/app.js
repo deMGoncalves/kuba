024fa6599c3d3440fcc12bda041a923c4d3d51bc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-26 08:35:31 -0300

    chore: voltando o nome do app

--- pixels/settings/src/app.js
+++ pixels/settings/src/app.js
@@ -2,6 +2,7 @@ export default {
   author: 'Cleber de Moraes Gonçalves <cleber.engineer@gmail.com>',
   description: 'Arquiteturar um software é como uma folha em branco, a liberdade de programar do jeito que faça sentido para o negócio',
   language: 'pt-BR',
+  name: 'Kuba',
   themeColor: '#fafafa',
   title: 'Kuba',
   url: `${location.protocol}//${location.host}`
