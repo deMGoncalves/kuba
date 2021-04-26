5db2954cdab007ef78cbec35ad0002daf30ddb80
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-26 14:21:30 -0300

    feat(footer): inclusao do icone do github

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -10,6 +10,7 @@ export default (props) =>
       <text.I className={style.footer__i} master small>Simples, pequeno e imperfeito. Uma visão além da programação.</text.I>
       <nav className={style.footer__nav}>
         <icon.Facebook className={style.footer__icon} large />
+        <icon.Github className={style.footer__icon} large />
         <icon.Instagram className={style.footer__icon} large />
         <icon.Linkedin className={style.footer__icon} large />
         <icon.Twitter className={style.footer__icon} large />
