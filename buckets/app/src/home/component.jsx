b4e3f46b0269d5ff26c39fac24758d0da4cf2518
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-26 11:22:12 -0300

    refactor(home): removendo lixo visual

--- buckets/app/src/home/component.jsx
+++ buckets/app/src/home/component.jsx
@@ -10,7 +10,6 @@ export default () =>
   <Site className={style.home}>
     <Story className={style.home__story}>
       <text.Blockquote className={style.home__blockquote} master largest>
-        <text.Sup className={style.home__sup} master darker largest>“</text.Sup>
         <text.Strong master largest>Kuba</text.Strong> é como uma folha em branco, a liberdade de programar do jeito que faça sentido para o produto
         <br />
         <text.Cite className={style.home__cite} master darker small>- deMGoncalves</text.Cite>
