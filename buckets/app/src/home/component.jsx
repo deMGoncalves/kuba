0932d6cdd0c125c6467e4957c49b6a1a1b385604
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-26 12:25:20 -0300

    feat(home): descricao do projeto

--- buckets/app/src/home/component.jsx
+++ buckets/app/src/home/component.jsx
@@ -39,4 +39,9 @@ export default () =>
         </Box>
       </div>
     </Story>
+    <Story className={style.home__story}>
+      <text.P className={style.home__p} master medium>Ninguém precisa ter muitas habilidades para desenvolver um software que funcione, fazer direito é outra história. Desenvolver um software de maneira correta é um desafio que deixo lançado. Requer maturidade e conhecimentos que a maioria dos programadores ainda não tem.</text.P>
+      <br />
+      <text.P className={style.home__p} master medium>Quando focamos no domínio, minimizamos a quantidade de programadores para desenvolver e sustentar um software. As mudanças se tornam cirúrgicas, simples e fáceis. Deixamos de ser eficiente e nos tornamos eficazes. As funcionalidades e flexibilidades são maximizadas.</text.P>
+    </Story>
   </Site>
