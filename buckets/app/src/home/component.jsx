9a6aebb542d649fac49b5ecf9a39167ec0c637df
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-27 12:54:40 -0300

    style: ajustando para ficar igual a linguagem ubiqua

--- buckets/app/src/home/component.jsx
+++ buckets/app/src/home/component.jsx
@@ -1,7 +1,7 @@
 import h, { Fragment } from '@kuba/h'
 import Header from '@kuba/header'
 import Site from '@kuba/site'
-import Cite from './cite'
+import Definition from './definition'
 import Description from './description'
 import Summary from './summary'
 import style from './style.css'
@@ -10,7 +10,7 @@ export default () =>
   <Site className={style.home}>
     <Header className={style.home__header} slot='header' />
     <Fragment slot='main'>
-      <Cite className={style.home__cite} />
+      <Definition className={style.home__definition} />
       <Summary className={style.home__summary} />
       <Description className={style.home__description} />
     </Fragment>
--- buckets/app/src/home/definition/component.jsx
+++ buckets/app/src/home/definition/component.jsx
@@ -4,10 +4,10 @@ import text from '@kuba/text'
 import style from './style.css'
 
 export default (props) =>
-  <Story className={[style.cite__story, props.className]}>
-    <text.Blockquote className={style.cite__blockquote} master largest>
+  <Story className={[style.definition__story, props.className]}>
+    <text.Blockquote className={style.definition__blockquote} master largest>
       <text.Strong master largest>Kuba</text.Strong> é como uma folha em branco! A liberdade de programar do jeito que faça sentido para o produto
       <br />
-      <text.Cite className={style.cite__author} master darker small>- deMGoncalves</text.Cite>
+      <text.Cite className={style.definition__cite} master darker small>- deMGoncalves</text.Cite>
     </text.Blockquote>
   </Story>
