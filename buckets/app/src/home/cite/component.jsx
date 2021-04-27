05bd483b7097793390a2b32cba59fe5aab25983c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-27 08:56:11 -0300

    refactor(cite): padronizando os descritivo do kuba

--- buckets/app/src/home/cite/component.jsx
+++ buckets/app/src/home/cite/component.jsx
@@ -6,7 +6,7 @@ import style from './style.css'
 export default (props) =>
   <Story className={[style.cite__story, props.className]}>
     <text.Blockquote className={style.cite__blockquote} master largest>
-      <text.Strong master largest>Kuba</text.Strong> é como uma folha em branco, a liberdade de programar do jeito que faça sentido para o produto
+      <text.Strong master largest>Kuba</text.Strong> é como uma folha em branco! A liberdade de programar do jeito que faça sentido para o produto
       <br />
       <text.Cite className={style.cite__author} master darker small>- deMGoncalves</text.Cite>
     </text.Blockquote>
