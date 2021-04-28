98abb9ef5f2ae3c32edc9bd899a5134549d4f16f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-28 12:53:21 -0300

    refactor: nao precisa ser uma navegacao

--- blocks/footer/src/social/component.jsx
+++ blocks/footer/src/social/component.jsx
@@ -3,11 +3,11 @@ import icon from '@kuba/icon'
 import style from './style.css'
 
 export default (props) =>
-  <nav className={[style.social, props.className]}>
+  <div className={[style.social, props.className]}>
     <icon.Facebook className={style.social__icon} large />
     <icon.Github className={style.social__icon} large />
     <icon.Instagram className={style.social__icon} large />
     <icon.Linkedin className={style.social__icon} large />
     <icon.Twitter className={style.social__icon} large />
     <icon.Youtube className={style.social__icon} large />
-  </nav>
+  </div>
