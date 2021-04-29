f194518669a8202cad4883ef8a51f57e57e48566
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-29 10:59:11 -0300

    feat(picture): quando nao definido um alt vou usar o nome do kuba como fallback

--- elements/picture/src/component.jsx
+++ elements/picture/src/component.jsx
@@ -1,8 +1,9 @@
 import h from '@kuba/h'
+import * as f from '@kuba/f'
 import style from './style.css'
 
 export default (props, children) =>
   <picture {...props} className={[style.picture, props.className]}>
     {children}
-    <img className={style.picture__img} loading='lazy' alt={props.alt} />
+    <img className={style.picture__img} loading='lazy' alt={f.or(props.alt, __settings.app.name)} />
   </picture>
