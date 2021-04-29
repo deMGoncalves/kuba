1855b1a42b2c7a8db92785795180977dbbc0ac2b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-29 10:56:15 -0300

    fix: nao posso de deixar de incluir um alt nos icones

--- blocks/footer/src/social/component.jsx
+++ blocks/footer/src/social/component.jsx
@@ -4,10 +4,10 @@ import style from './style.css'
 
 export default (props) =>
   <div className={[style.social, props.className]}>
-    <icon.Facebook className={style.social__icon} large />
-    <icon.Github className={style.social__icon} large />
-    <icon.Instagram className={style.social__icon} large />
-    <icon.Linkedin className={style.social__icon} large />
-    <icon.Twitter className={style.social__icon} large />
-    <icon.Youtube className={style.social__icon} large />
+    <icon.Facebook className={style.social__icon} alt='facebook' large />
+    <icon.Github className={style.social__icon} alt='github' large />
+    <icon.Instagram className={style.social__icon} alt='instagram' large />
+    <icon.Linkedin className={style.social__icon} alt='linkedin' large />
+    <icon.Twitter className={style.social__icon} alt='twitter' large />
+    <icon.Youtube className={style.social__icon} alt='youtube' large />
   </div>
