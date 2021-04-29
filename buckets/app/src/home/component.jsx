dc3de007fce946717f8800eebc7212c75f709227
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-29 12:35:49 -0300

    refactor: usando o summary para construir o sumario

--- buckets/app/src/home/component.jsx
+++ buckets/app/src/home/component.jsx
@@ -1,16 +1,17 @@
 import h, { Fragment } from '@kuba/h'
 import Site from '@kuba/site'
+import Summary from '@kuba/summary'
 import Zone from '@kuba/zone'
 import Definition from './definition'
 import Description from './description'
-import Summary from './summary'
+import schema from './schema.json'
 import style from './style.css'
 
 export default () =>
   <Site className={style.home}>
     <Fragment slot='main'>
       <Definition className={style.home__definition} />
-      <Summary className={style.home__summary} />
+      <Summary className={style.home__summary} iterator={schema} />
       <Zone className={style.home__zone}>
         <Description className={style.home__description} />
       </Zone>
