ee368a57fcfbad8e2afe6baf2d2d41613125d281
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-29 10:53:49 -0300

    refactor: padronizando a contrucoa de tags dinamicas

--- elements/link/src/index.js
+++ elements/link/src/index.js
@@ -5,5 +5,5 @@ import style from './style.css'
 export default new Proxy({}, {
   get: (_, type) =>
     (props, children) =>
-      <a { ...props } className={[style.link, style[f.toLower(type)], props.className]}>{children}</a>
+      h('a', { ...props, className: [style.link, style[f.toLower(type)], props.className] }, ...children)
 })
