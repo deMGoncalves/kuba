334c45d45b5edfdc13e27d80ef142ebc5924c6a9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-04-30 07:29:15 -0300

    style(lint): normalizacao do codigo

--- buckets/app/src/mindset/mindset.js
+++ buckets/app/src/mindset/mindset.js
@@ -17,10 +17,6 @@ class Mindset {
     return schema.summary
   }
 
-  get summary () {
-    return schema.summary
-  }
-
   get title () {
     return schema.title
   }
--- buckets/app/src/routers.jsx
+++ buckets/app/src/routers.jsx
@@ -1,5 +1,5 @@
 import h from '@kuba/h'
-import router, { urlFor } from '@kuba/router'
+import router from '@kuba/router'
 import render from './render'
 
 router(/^\/$/, async function home () {
