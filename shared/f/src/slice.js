d69f8d0d3e095d740cf00910868fd35bd162a685
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-05-15 18:06:21 -0300

    feat(f): fatia uma colection

--- shared/f/src/slice.js
+++ shared/f/src/slice.js
@@ -1,7 +1,7 @@
 import arity from './arity'
 import curry from './curry'
 
-const slice = (target, start, end) =>
-  target.slice(start, end)
+const slice = (target, kuba, end) =>
+  target.slice(kuba, end)
 
 export default curry(arity(2, slice))
