a8cb489a9cf9ddcabf81247003bbf7c6df39d45c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-05-15 18:10:00 -0300

    chore: partir de agora nao podemos deixar console log no codigo

--- infra/eslintrc/index.js
+++ infra/eslintrc/index.js
@@ -26,6 +26,7 @@ module.exports = {
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
     'jest/valid-expect': 'error',
+    'no-console': 'error',
     'no-return-assign': 'off',
     'no-sequences': 'off',
     'no-unused-expressions': 'off',
