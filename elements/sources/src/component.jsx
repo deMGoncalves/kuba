edadbd409ca1758fd1d7fa34c0ed11af4ca0ac36
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-05-21 08:35:21 -0300

    feat(sources): component para renderizar diferentes tipos de sources

---
+++ elements/sources/src/component.jsx
@@ -0,0 +1,9 @@
+import h, { Fragment } from '@kuba/h'
+import * as f from '@kuba/f'
+
+export default ({ thumbnails: [mobile, tablet, desktop] }) =>
+  <>
+    <source srcSet={f.or(mobile, tablet, desktop)} media='(max-width: 768px)' />
+    <source srcSet={f.or(tablet, mobile, desktop)} media='(min-width: 769px) and (max-width: 960px)' />
+    <source srcSet={f.or(desktop, tablet, mobile)} media='(min-width: 961px)' />
+  </>
