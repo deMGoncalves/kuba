2de16e5bdc45e9f0529cb78a7ec068ead08179d7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-05-21 07:59:54 -0300

    chore: removendo bloco que nao sera usandos

--- blocks/footer/src/nav/component.jsx
+++ blocks/footer/src/nav/component.jsx
@@ -6,8 +6,4 @@ import style from './style.css'
 export default (props) =>
   <nav className={[style.nav, props.className]}>
     <link.Master className={style.nav__link} href={urlFor('home')} darker small>Home</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('mindset')} darker small>Mindset</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('patterns')} darker small>Patterns</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('structure')} darker small>Structure</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('components')} darker small>Components</link.Master>
   </nav>
--- blocks/summary/src/component.jsx
+++
@@ -1,9 +0,0 @@
-import h, { Repeat } from '@kuba/h'
-import * as f from '@kuba/f'
-import Chapter from './chapter'
-import style from './style.css'
-
-export default (props) =>
-  <div className={[style.summary, props.className]}>
-    <Repeat iterator={f.or(props.iterator, [])} component={Chapter} />
-  </div>
