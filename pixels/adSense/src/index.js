2725538049c48d1cb47c42f2909d85651ea1b388
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-02 12:54:13 -0300

    chore: padronizacao do ad sense

--- pixels/adSense/src/index.js
+++ pixels/adSense/src/index.js
@@ -2,8 +2,8 @@ import h, { render } from '@kuba/h'
 import * as f from '@kuba/f'
 import agent from '@kuba/agent'
 import env from '@kuba/env'
-import settings from './settings'
+import schema from './schema.json'
 
 f.and(agent.isUser, env.isProd) && (
-  render(document.head, <script data-ad-client={settings.adClient} src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' async />)
+  render(document.head, <script data-ad-client={schema.adClient} src={schema.src} async />)
 )
