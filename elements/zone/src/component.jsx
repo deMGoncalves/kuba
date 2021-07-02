4e992f31009c5f0dee88fcc480b243de2deabd21
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-02 12:18:19 -0300

    chore: agora o zone fica nas actions

--- elements/zone/src/component.jsx
+++
@@ -1,8 +0,0 @@
-import h, { Show } from '@kuba/h'
-
-export default (zone, children) =>
-  <div className={zone.className} slot={zone.slot}>
-    <Show if={zone.onScreen}>
-      {children}
-    </Show>
-  </div>
--- elements/zone/src/scroll.js
+++
@@ -1,8 +0,0 @@
-import { dom } from '@kuba/h'
-import * as f from '@kuba/f'
-import lazyLoad from '@kuba/lazyload'
-
-const sroll = (zone) =>
-  lazyLoad(dom.query(zone[f.magic('element')].id), () => zone.render())
-
-export default f.idle(sroll)
