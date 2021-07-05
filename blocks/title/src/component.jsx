289cf3d7def8ca5ae6364bf0753e703a6c40ce9c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-05 14:08:40 -0300

    feat(title): bloco de renderizacao dos titulos das paginas

---
+++ blocks/title/src/component.jsx
@@ -0,0 +1,11 @@
+import h from '@kuba/h'
+import Container from '@kuba/container'
+import text from '@kuba/text'
+import style from './style.css'
+
+export default (props, children) =>
+  <section {...props} className={[style.title, props.className]}>
+    <Container className={style.title__container}>
+      <text.H1 className={style.title__h1} master darker>{children}</text.H1>
+    </Container>
+  </section>
