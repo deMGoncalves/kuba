8266467653273b105fee8994a9dc2d975044cdb6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-06 14:34:39 -0300

    refactor: removendo as strings magicas do template

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -4,6 +4,7 @@ import Logo from '@kuba/logo'
 import text from '@kuba/text'
 import Nav from './nav'
 import Social from './social'
+import schema from './schema.json'
 import style from './style.css'
 
 export default (props) =>
@@ -11,8 +12,8 @@ export default (props) =>
     <Container className={style.footer__container}>
       <div className={style.footer__div}>
         <Logo className={style.footer__logo} />
-        <text.Strong className={style.footer__strong} master darker small>Sua casa moderna</text.Strong>
-        <text.P className={style.footer__i} master small>Design e sofisticaçao em peças únicas feitas a mão. Sua casa nunca mais será a mesma. Dê um toque de sofisticação em sua casa com nossas cadeiras, sofás e acessórios</text.P>
+        <text.Strong className={style.footer__strong} master darker>{schema.title}</text.Strong>
+        <text.P className={style.footer__i} master small>{schema.description}</text.P>
         <Nav className={style.footer__nav} />
       </div>
       <Social className={style.footer__social} />
