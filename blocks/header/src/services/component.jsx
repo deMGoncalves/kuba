cce212a1645656d71d8e4292d9fa9a147f469d21
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-06 14:17:58 -0300

    refactor: trocando os links dos servicos

--- blocks/header/src/services/component.jsx
+++ blocks/header/src/services/component.jsx
@@ -2,6 +2,7 @@ import h, { Fragment } from '@kuba/h'
 import Action from '@kuba/action'
 import icon from '@kuba/icon'
 import link from '@kuba/link'
+import { urlFor } from '@kuba/router'
 import text from '@kuba/text'
 import style from './style.css'
 
@@ -13,7 +14,13 @@ export default (props) =>
     </text.P>
     <text.Strong className={style.services__strong} slot='header' master darker>Serviços e atendimento</text.Strong>
     <Fragment slot='main'>
-      <link.Master className={style.services__link} href='mailto:suporte@usefashion.com' master darker small>suporte@usefashion.com</link.Master>
-      <link.Master className={style.services__link} href='tel:11-4858-9425' master darker small>(11) 4858-9425</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>central de atendimento</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>compre pelo Whatsapp</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>cartão Kuba 4004-0000</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>acompanhar meu pedido</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>solicitar devolução de pedido</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>encontrar lojas</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>Kuba&VC</link.Master>
+      <link.Master className={style.services__link} href={urlFor('home')} master darker small>venda no Kuba</link.Master>
     </Fragment>
   </Action>
