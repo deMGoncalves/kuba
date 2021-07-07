ec103c5ae5907645bc94562517ffabda630955bc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-07 11:09:39 -0300

    feat: inclusao do region

--- blocks/header/src/component.jsx
+++ blocks/header/src/component.jsx
@@ -7,6 +7,7 @@ import Darlings from './darlings'
 import Menu from './menu'
 import Nav from './nav'
 import Profile from './profile'
+import Region from './region'
 import Services from './services'
 import style from './style.css'
 
@@ -14,14 +15,15 @@ export default (props) =>
   <header {...props} className={[style.header, props.className]}>
     <Container className={style.header__container}>
       <div className={style.header__div}>
+        <Menu className={style.header__menu} />
         <Logo className={style.header__logo} />
         <Search className={style.header__search} />
       </div>
       <div className={style.header__div}>
+        <Region className={style.header__region} />
         <Services className={style.header__services} />
         <Profile className={style.header__profile} />
         <Bag className={style.header__bag} />
-        <Menu className={style.header__menu} />
       </div>
     </Container>
     <Container className={[style.header__container, style.desktop]}>
---
+++ blocks/header/src/region/component.jsx
@@ -0,0 +1,15 @@
+import h, { Fragment } from '@kuba/h'
+import Action from '@kuba/action'
+import icon from '@kuba/icon'
+import text from '@kuba/text'
+import style from './style.css'
+
+export default (props) =>
+  <Action className={[style.region, props.className]}>
+    <icon.MapMarker className={style.region__icon} alt='ative ofertas da sua região' slot='icon' larger />
+    <text.P className={style.region__p} slot='label' master ligth small>
+      ative ofertas<br /><strong>da sua região</strong>
+    </text.P>
+    <text.Strong className={style.region__strong} slot='header' master darker>ative ofertas da sua região</text.Strong>
+    <Fragment slot='main' />
+  </Action>
