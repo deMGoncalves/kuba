c303916c6bd89b60cbde3dcbb8697ad621fd86a4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-07 10:02:29 -0300

    refactor: padronizacao dos textos e links

--- blocks/footer/src/nav/component.jsx
+++ blocks/footer/src/nav/component.jsx
@@ -5,9 +5,9 @@ import style from './style.css'
 
 export default (props) =>
   <nav className={[style.nav, props.className]}>
-    <link.Master className={style.nav__link} href={urlFor('home')} darker small>Home</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('cadeiras')} darker small>Cadeiras</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('sofas')} darker small>Sofás</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('acessorios')} darker small>Acessórios</link.Master>
-    <link.Master className={style.nav__link} href={urlFor('home')} darker small>Blog</link.Master>
+    <link.Master className={style.nav__link} href={urlFor('home')} darker small>home</link.Master>
+    <link.Master className={style.nav__link} href={urlFor('cadeiras')} darker small>cadeiras</link.Master>
+    <link.Master className={style.nav__link} href={urlFor('sofas')} darker small>sofás</link.Master>
+    <link.Master className={style.nav__link} href={urlFor('acessorios')} darker small>acessórios</link.Master>
+    <link.Master className={style.nav__link} href={urlFor('home')} darker small>blog</link.Master>
   </nav>
--- blocks/footer/src/social/component.jsx
+++ blocks/footer/src/social/component.jsx
@@ -1,12 +1,13 @@
 import h from '@kuba/h'
 import icon from '@kuba/icon'
+import { urlFor } from '@kuba/router'
 import style from './style.css'
 
 export default (props) =>
   <div className={[style.social, props.className]}>
-    <icon.Instagram className={style.social__icon} onClick={() => location.assign('//www.instagram.com/usefashion/?hl=pt-br')} alt='instagram' large />
-    <icon.Facebook className={style.social__icon} onClick={() => location.assign('//www.facebook.com/PortalUseFashion/?ref=bookmarks')} alt='facebook' large />
-    <icon.Linkedin className={style.social__icon} onClick={() => location.assign('//www.linkedin.com/company/usefashion/')} alt='linkedin' large />
-    <icon.Youtube className={style.social__icon} onClick={() => location.assign('//www.youtube.com/channel/UC7vIxxBw-iRSQ0nQBsvf-wg')} alt='youtube' large />
-    <icon.Pinterest className={style.social__icon} onClick={() => location.assign('//br.pinterest.com/usefashion/')} alt='pinterest' large />
+    <icon.Instagram className={style.social__icon} onClick={() => location.assign(urlFor('home'))} alt='instagram' large />
+    <icon.Facebook className={style.social__icon} onClick={() => location.assign(urlFor('home'))} alt='facebook' large />
+    <icon.Linkedin className={style.social__icon} onClick={() => location.assign(urlFor('home'))} alt='linkedin' large />
+    <icon.Youtube className={style.social__icon} onClick={() => location.assign(urlFor('home'))} alt='youtube' large />
+    <icon.Pinterest className={style.social__icon} onClick={() => location.assign(urlFor('home'))} alt='pinterest' large />
   </div>
