63f993e8b7c44c034bae0b70fe5189233962cf39
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-08 11:15:51 -0300

    feat: inclusao do inspiration na pagina dentro de uma zona

--- buckets/app/src/home/component.jsx
+++ buckets/app/src/home/component.jsx
@@ -2,7 +2,9 @@ import h, { Fragment } from '@kuba/h'
 import Exploration from '@kuba/exploration'
 import Header from '@kuba/header'
 import Hero from '@kuba/hero'
+import Inspiration from '@kuba/inspiration'
 import Site from '@kuba/site'
+import Zone from '@kuba/zone'
 import style from './style.css'
 
 export default () =>
@@ -11,5 +13,8 @@ export default () =>
     <Fragment slot='main'>
       <Hero className={style.home__hero} />
       <Exploration className={style.home__exploration} />
+      <Zone>
+        <Inspiration className={style.home__inspiration} />
+      </Zone>
     </Fragment>
   </Site>
