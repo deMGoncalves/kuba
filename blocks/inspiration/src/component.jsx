8af80dcb52bcef1603b46aa47277635ee43d4896
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-08 13:11:48 -0300

    refactor: configuracao do banner para as inspiracoes

--- blocks/inspiration/src/component.jsx
+++ blocks/inspiration/src/component.jsx
@@ -4,31 +4,31 @@ import Picture from '@kuba/picture'
 import Sources from '@kuba/sources'
 import Story from '@kuba/story'
 import text from '@kuba/text'
-import schema from './schema.json'
+import schema from './schema'
 import style from './style.css'
 
 export default (props) =>
   <Story {...props} className={[style.inspiration, props.className]}>
-    <text.H2 className={style.inspiration__h2} master darker>{schema.title}</text.H2>
+    <text.H2 className={style.inspiration__h2} master darker large>{schema.title}</text.H2>
     <div className={style.inspiration__main}>
       <Picture className={style.inspiration__picture}>
         <Sources thumbnails={schema.thumbnails} />
       </Picture>
       <Box className={style.inspiracao__box}>
         <Sources thumbnails={schema.estrategia.thumbnails} slot='sources' />
-        <text.H3 className={style.inspiration__h3} slot='caption' master darker>{schema.estrategia.title}</text.H3>
+        <text.Strong className={style.inspiration__strong} slot='caption' master dark small>{schema.estrategia.title}</text.Strong>
       </Box>
       <Box className={style.inspiracao__box}>
         <Sources thumbnails={schema.inspiracao.thumbnails} slot='sources' />
-        <text.H3 className={style.inspiration__h3} slot='caption' master darker>{schema.inspiracao.title}</text.H3>
+        <text.Strong className={style.inspiration__strong} slot='caption' master dark small>{schema.inspiracao.title}</text.Strong>
       </Box>
       <Box className={style.inspiracao__box}>
         <Sources thumbnails={schema.produto.thumbnails} slot='sources' />
-        <text.H3 className={style.inspiration__h3} slot='caption' master darker>{schema.produto.title}</text.H3>
+        <text.Strong className={style.inspiration__strong} slot='caption' master dark small>{schema.produto.title}</text.Strong>
       </Box>
       <Box className={style.inspiracao__box}>
         <Sources thumbnails={schema.venda.thumbnails} slot='sources' />
-        <text.H3 className={style.inspiration__h3} slot='caption' master darker>{schema.venda.title}</text.H3>
+        <text.Strong className={style.inspiration__strong} slot='caption' master dark small>{schema.venda.title}</text.Strong>
       </Box>
     </div>
   </Story>
---
+++ blocks/inspiration/src/schema.js
@@ -0,0 +1,38 @@
+import large from './inspiration_591x645.jpg'
+import small from './inspiration_736x400.jpg'
+
+export default {
+  title: 'destaques da semana',
+  thumbnails: [
+    small,
+    large
+  ],
+  estrategia: {
+    thumbnails: [
+      'https://via.placeholder.com/360x360',
+      'https://via.placeholder.com/285x285'
+    ],
+    title: 'Estratégia'
+  },
+  inspiracao: {
+    thumbnails: [
+      'https://via.placeholder.com/360x360',
+      'https://via.placeholder.com/285x285'
+    ],
+    title: 'Inspiração'
+  },
+  produto: {
+    thumbnails: [
+      'https://via.placeholder.com/360x360',
+      'https://via.placeholder.com/285x285'
+    ],
+    title: 'Produto'
+  },
+  venda: {
+    thumbnails: [
+      'https://via.placeholder.com/360x360',
+      'https://via.placeholder.com/285x285'
+    ],
+    title: 'Venda'
+  }
+}
