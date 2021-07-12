41b77b13389139cac37619b4c53541827619bd66
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-12 12:14:38 -0300

    chore: nomeando o bundle inicial

--- buckets/app/webpack.common.js
+++ buckets/app/webpack.common.js
@@ -7,7 +7,9 @@ const path = require('path')
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: {
+    app: './index.js'
+  },
   module: {
     rules: [
       {
