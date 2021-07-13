2f0ee693d0f39e3e1df089ab5ccccbde1662b478
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-13 13:31:31 -0300

    refactor: o identificador estava escrito errado

--- actions/zone/src/scroll.js
+++ actions/zone/src/scroll.js
@@ -1,7 +1,7 @@
 import * as f from '@kuba/f'
 import lazyLoad from '@kuba/lazyload'
 
-const sroll = (zone) =>
+const scroll = (zone) =>
   lazyLoad(zone[f.magic('tag')].element, () => zone.render())
 
-export default f.idle(sroll)
+export default f.idle(scroll)
