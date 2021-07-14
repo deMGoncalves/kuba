2be0c98de7d968199a0389b132e2fb99cf15c851
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-14 12:23:47 -0300

    chore: a font sera carregado somente quando for um usuario

--- infra/reset/src/index.js
+++ infra/reset/src/index.js
@@ -1,1 +1,10 @@
+import h, { render } from '@kuba/h'
+import agent from '@kuba/agent'
 import './style.css'
+
+agent.isUser && (
+  render(
+    document.head,
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap' />
+  )
+)
