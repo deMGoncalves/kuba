342f7f6cd3bc5779b00573ba9f9ae5dfec77ae1b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-14 12:07:37 -0300

    feat(viewport): inclusao do view port no markup

--- pixels/markup/src/index.js
+++ pixels/markup/src/index.js
@@ -1,1 +1,15 @@
-export { default } from './markup'
+import * as f from '@kuba/f'
+import hook from '@kuba/hook'
+import alternate from './alternate'
+import author from './author'
+import base from './base'
+import canonical from './canonical'
+import charset from './charset'
+import description from './description'
+import icon from './icon'
+import robots from './robots'
+import themeColor from './themeColor'
+import title from './title'
+import viewport from './viewport'
+
+export default hook(f.chain(alternate, author, base, canonical, charset, description, icon, robots, themeColor, title, viewport))
---
+++ pixels/markup/src/viewport.jsx
@@ -0,0 +1,5 @@
+import h from '@kuba/h'
+import render from './render'
+
+export default () =>
+  render(<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />)
