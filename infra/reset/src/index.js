bd4df4a0cd23695d89eeab2f995ddcc13f212658
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-14 12:52:30 -0300

    chore: movendo a logica de import de fonts para outro contexto

--- infra/reset/src/index.js
+++ infra/reset/src/index.js
@@ -1,10 +1,2 @@
-import h, { render } from '@kuba/h'
-import agent from '@kuba/agent'
+import './renderFonts'
 import './style.css'
-
-agent.isUser && (
-  render(
-    document.head,
-    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap' />
-  )
-)
---
+++ infra/reset/src/renderFonts.js
@@ -0,0 +1,13 @@
+import h, { Fragment, render } from '@kuba/h'
+import agent from '@kuba/agent'
+
+agent.isUser && (
+  render(
+    document.head,
+    <>
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+      <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap' />
+    </>
+  )
+)
