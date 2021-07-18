3c13d4a67a98369f823993386fdaf459cce9eba1
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-18 13:41:29 -0300

    refactor: usando texto genericos

--- blocks/hero/src/schema.js
+++ blocks/hero/src/schema.js
@@ -8,6 +8,6 @@ export default {
     medium,
     larger
   ],
-  "title": "sua casa moderna",
-  "description": "Design e sofisticaçao em peças únicas feitas a mão. Sua casa nunca mais será a mesma. Dê um toque de sofisticação em sua casa com nossas cadeiras, sofás e acessórios"
+  "title": "Nunc eu dignissim leo. Donec.",
+  "description": "Nulla id placerat sem. Vestibulum congue suscipit erat, eu tempor augue venenatis ut. Vestibulum magna ex, pulvinar eget quam et, rutrum sodales nisl. Curabitur finibus tristique."
 }
