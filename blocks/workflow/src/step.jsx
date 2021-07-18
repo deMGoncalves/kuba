6559a5af12bfc421c20a30a1241c2f29f552bf57
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-18 16:26:42 -0300

    refactor: trocando os textos por conteudos fakes

---
+++ blocks/workflow/src/step.jsx
@@ -0,0 +1,10 @@
+import h from '@kuba/h'
+import * as f from '@kuba/f'
+import text from '@kuba/text'
+import style from './style.css'
+
+export default (props, i) =>
+  <li className={style.workflow__li} data-index={f.inc(i)}>
+    <text.Strong className={style.workflow__strong} master darker>{props.title}</text.Strong>
+    <text.P className={style.workflow__p} master small>{props.description}</text.P>
+  </li>
