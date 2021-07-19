750bad0c22fafdf2d94c9423b91936d3066944de
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-19 09:44:29 -0300

    refactor: nao usamos mais o paint

--- actions/download/src/index.js
+++ actions/download/src/index.js
@@ -1,8 +1,7 @@
 import h from '@kuba/h'
-import * as f from '@kuba/f'
 
 export default (url) =>
   fetch(url)
     .then(response => response.blob())
     .then(blob => URL.createObjectURL(blob))
-    .then(href => h('a', { href, download: url })[f.magic('paint')]().click())
+    .then(href => h('a', { href, download: url }).paint().click())
