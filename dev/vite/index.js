b09417512e233ea15b0fd865c2c4132b5983e816
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-19 10:00:14 -0300

    chore: apenas o padrao da documentacao

--- dev/vite/index.js
+++ dev/vite/index.js
@@ -3,13 +3,6 @@
 module.exports = {
   esbuild: {
     jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      }
-    }
+    jsxFragment: 'Fragment'
   }
 }
