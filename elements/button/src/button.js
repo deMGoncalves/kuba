282689240bda88dfc0001edda23d02891d710c69
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-19 12:32:33 -0300

    refactor: usando o styled para montar o button

--- elements/button/src/button.js
+++
@@ -1,8 +0,0 @@
-import h from '@kuba/h'
-import style from './style.css'
-
-export default new Proxy({}, {
-  get: (_, type) =>
-    (props, children) =>
-      h('button', { ...props, [type]: '', className: [style.button, props.className] }, ...children)
-})
---
+++ elements/button/src/component.jsx
@@ -0,0 +1,22 @@
+import styled, { color } from '@kuba/styled'
+
+export default new Proxy({}, {
+  get: (_, type) =>
+    styled.button`
+      align-items: center;
+      background-color: var(${() => color({ [type]: '', dark: '' })});
+      border: none;
+      border-radius: 18px;
+      color: var(--color-master-lightest);
+      cursor: pointer;
+      display: inline-flex;
+      font-size: var(--size-small) !important;
+      font-weight: 500;
+      height: var(--size-largest);
+      justify-content: center;
+      letter-spacing: 0.618px;
+      line-height: 1;
+      padding: var(--size-smaller) var(--size-largest);
+      text-decoration: none;
+    `
+})
