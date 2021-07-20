341b9a6caf9c2bcc732f3510174f531abf3123a6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-20 13:46:31 -0300

    feat: inclusao da flag no model

--- models/site/src/component.jsx
+++ models/site/src/component.jsx
@@ -1,10 +1,12 @@
 import h, { Fragment } from '@kuba/h'
+import Flag from '@kuba/flag'
 import Lazy from '@kuba/lazy'
 import Zone from '@kuba/zone'
 import style from './style.css'
 
 export default (_site, children) =>
   <>
+    <Flag className={style.site__flag} />
     {children.header}
     <main className={style.site__main}>
       {children.main}
