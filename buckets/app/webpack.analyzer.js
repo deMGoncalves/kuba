4821a92a09e923ecdb3d0d187e26e5da10c47472
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-21 23:37:13 -0300

    refactor(webpack): removendo as dependencia do webpack e redirecionando para o workspace dev

--- buckets/app/webpack.analyzer.js
+++ buckets/app/webpack.analyzer.js
@@ -1,11 +1,3 @@
 'use strict'
 
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-const prod = require('./webpack.prod.js')
-const { merge } = require('webpack-merge')
-
-module.exports = merge(prod, {
-  plugins: [
-    new BundleAnalyzerPlugin()
-  ]
-})
+module.exports = require('@kuba/webpack').analyzer(__dirname)
--- buckets/app/webpack.dev.js
+++ buckets/app/webpack.dev.js
@@ -1,35 +1,3 @@
 'use strict'
 
-const common = require('./webpack.common.js')
-const DashboardPlugin = require('webpack-dashboard/plugin')
-const { merge } = require('webpack-merge')
-const path = require('path')
-const portFinderSync = require('portfinder-sync')
-const port = 5000
-
-module.exports = merge(common, {
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, '.temp'),
-    historyApiFallback: true,
-    hot: true,
-    index: './.temp/index.html',
-    port: portFinderSync.getPort(port),
-    proxy: {
-      '/api/*': {
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api/': '/'
-        },
-        target: 'http://localhost:5001/api'
-      }
-    }
-  },
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, '.temp')
-  },
-  plugins: [
-    new DashboardPlugin()
-  ]
-})
+module.exports = require('@kuba/webpack').dev(__dirname)
