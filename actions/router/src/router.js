5ade37a1a0a3aa5437ba760e8bec3c2f6ea0715d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-22 11:16:35 -0300

    fix: para urls com hifen nao funcionava

--- actions/router/src/router.js
+++ actions/router/src/router.js
@@ -5,8 +5,8 @@ import urls from './urls'
 
 export default function (path, listener) {
   const { name } = listener
-  const params = new RegExp(f.replace(path, /(:)\w+/g, '$1([a-z0-9]+)'), 'i')
-  const router = new RegExp(`^${f.replace(path, /:\w+/g, '([a-z0-9]+)')}$`, 'i')
+  const params = new RegExp(f.replace(path, /(:)\w+/g, '$1([a-z0-9-]+)'), 'i')
+  const router = new RegExp(`^${f.replace(path, /:\w+/g, '([a-z0-9-]+)')}$`, 'i')
 
   f.assign(urls, {
     [name]: { listener, params, path, router }
