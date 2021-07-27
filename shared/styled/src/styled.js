8df7382c2366bb1406fa16ca53f2ce967cdcefd0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-27 13:45:11 -0300

    refactor: trocando o nome para interpolate, assim deixo o codigo legivel

--- shared/styled/src/styled.js
+++ shared/styled/src/styled.js
@@ -1,9 +1,9 @@
 import h from '@kuba/h'
-import styleBuild from './styleBuild'
+import interpolate from './interpolate'
 
 export default new Proxy({}, {
   get: (_, tagName) =>
     (strings, ...funcs) =>
       (props, children) =>
-        h(tagName, { ...props, style: styleBuild(props, strings, funcs) }, ...children)
+        h(tagName, { ...props, style: interpolate(props, strings, funcs) }, ...children)
 })
