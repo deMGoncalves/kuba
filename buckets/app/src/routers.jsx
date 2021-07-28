39f316de12b8b4c35c6959bf2dc15c5c8b80e9d8
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-28 15:36:38 -0300

    refactor: deixando as rotas simples

--- buckets/app/src/routers.jsx
+++ buckets/app/src/routers.jsx
@@ -7,27 +7,8 @@ import NotFound from '@kuba/notfound'
 import Search from '@kuba/search'
 import render from './render'
 
-router('/', async function home () {
-  // const { default: Home } = await import('@kuba/home' /* webpackChunkName: "home" */)
-  render(<Home />)
-})
-
-router('/departament', async function departament () {
-  // const { default: Departament } = await import('@kuba/departament' /* webpackChunkName: "departament" */)
-  render(<Departament />)
-})
-
-router('/departament/category', async function category () {
-  // const { default: Category } = await import('@kuba/category' /* webpackChunkName: "category" */)
-  render(<Category />)
-})
-
-router('/search', async function search () {
-  // const { default: Search } = await import('@kuba/search' /* webpackChunkName: "search" */)
-  render(<Search />)
-})
-
-router('', async function notFound () {
-  // const { default: NotFound } = await import('@kuba/notfound' /* webpackChunkName: "notfound" */)
-  render(<NotFound />)
-})
+router('/', () => render(<Home />))
+router('/departament', () => render(<Departament />))
+router('/departament/category', () => render(<Category />))
+router('/search', () => render(<Search />))
+router('', () => render(<NotFound />))
