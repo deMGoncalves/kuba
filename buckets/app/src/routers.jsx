015db9b5add7ba47af86df4de061f2044887f239
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-28 14:38:51 -0300

    chore: testando todas as estrutura injetado no html

--- buckets/app/src/routers.jsx
+++ buckets/app/src/routers.jsx
@@ -1,28 +1,33 @@
 import h from '@kuba/h'
 import router from '@kuba/router'
+import Category from '@kuba/category'
+import Departament from '@kuba/departament'
+import Home from '@kuba/home'
+import NotFound from '@kuba/notfound'
+import Search from '@kuba/search'
 import render from './render'
 
 router('/', async function home () {
-  const { default: Home } = await import('@kuba/home' /* webpackChunkName: "home" */)
+  // const { default: Home } = await import('@kuba/home' /* webpackChunkName: "home" */)
   render(<Home />)
 })
 
 router('/departament', async function departament () {
-  const { default: Departament } = await import('@kuba/departament' /* webpackChunkName: "departament" */)
+  // const { default: Departament } = await import('@kuba/departament' /* webpackChunkName: "departament" */)
   render(<Departament />)
 })
 
 router('/departament/category', async function category () {
-  const { default: Category } = await import('@kuba/category' /* webpackChunkName: "category" */)
+  // const { default: Category } = await import('@kuba/category' /* webpackChunkName: "category" */)
   render(<Category />)
 })
 
 router('/search', async function search () {
-  const { default: Search } = await import('@kuba/search' /* webpackChunkName: "search" */)
+  // const { default: Search } = await import('@kuba/search' /* webpackChunkName: "search" */)
   render(<Search />)
 })
 
 router('', async function notFound () {
-  const { default: NotFound } = await import('@kuba/notfound' /* webpackChunkName: "notfound" */)
+  // const { default: NotFound } = await import('@kuba/notfound' /* webpackChunkName: "notfound" */)
   render(<NotFound />)
 })
