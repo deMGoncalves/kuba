2f02e974f837bd9a29f71f3d1ac6ce2fe39821e2
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-28 15:48:57 -0300

    refactor: vou deixar as paginas separadas

--- buckets/app/src/routers.jsx
+++ buckets/app/src/routers.jsx
@@ -1,14 +1,28 @@
 import h from '@kuba/h'
 import router from '@kuba/router'
-import Category from '@kuba/category'
-import Departament from '@kuba/departament'
-import Home from '@kuba/home'
-import NotFound from '@kuba/notfound'
-import Search from '@kuba/search'
 import render from './render'
 
-router('/', function home () { render(<Home />) })
-router('/departament', function departament () { render(<Departament />) })
-router('/departament/category', function category () { render(<Category />) })
-router('/search', function search () { render(<Search />) })
-router('', function notfound () { render(<NotFound />) })
+router('/', async function home () {
+  const { default: Home } = await import('@kuba/home' /* webpackChunkName: "home" */)
+  render(<Home />)
+})
+
+router('/departament', async function departament () {
+  const { default: Departament } = await import('@kuba/departament' /* webpackChunkName: "departament" */)
+  render(<Departament />)
+})
+
+router('/departament/category', async function category () {
+  const { default: Category } = await import('@kuba/category' /* webpackChunkName: "category" */)
+  render(<Category />)
+})
+
+router('/search', async function search () {
+  const { default: Search } = await import('@kuba/search' /* webpackChunkName: "search" */)
+  render(<Search />)
+})
+
+router('', async function notFound () {
+  const { default: NotFound } = await import('@kuba/notfound' /* webpackChunkName: "notfound" */)
+  render(<NotFound />)
+})
