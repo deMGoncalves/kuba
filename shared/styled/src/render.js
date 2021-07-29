2207343bcc9c973ee5e3eab4fa8c8126bfc2f1be
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-07-29 08:27:26 -0300

    fix: implementacao errado, nao estava usando o render

--- shared/styled/src/render.js
+++ shared/styled/src/render.js
@@ -1,7 +1,4 @@
 import h, { render } from '@kuba/h'
-import * as f from '@kuba/f'
 
-const render = (textContent) =>
-  document.head.append(h('style', {}, textContent).paint())
-
-export default f.curry(f.frame(render))
+export default (textContent) =>
+  render(document.head, h('style', {}, textContent))
