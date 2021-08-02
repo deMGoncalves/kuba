80f2e03a747a22dd9eee814817e5287ee7d1c457
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-02 17:28:15 -0300

    refactor: inclusao das actions no sandbox

--- actions/sandbox/src/connect.js
+++ actions/sandbox/src/connect.js
@@ -1,4 +1,14 @@
+import { render } from '@kuba/h'
+import * as f from '@kuba/f'
+
 export default function (sandbox) {
   const worker = sandbox.connect()
-  worker.addEventListener('message', console.log)
+  worker.addEventListener('message', ({ data: { action, payload }}) => {
+    console.log(action, payload)
+  })
+
+  worker.postMessage({
+    action: 'render',
+    payload: sandbox.props
+  })
 }
