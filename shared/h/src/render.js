8ff846839277e8246f4b1ffaa056621207f31005
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-03 08:53:50 -0300

    refactor: agora o render recebe mais de uma node

--- shared/h/src/render.js
+++ shared/h/src/render.js
@@ -1,7 +1,7 @@
 import '@kuba/htmlparser'
 import * as f from '@kuba/f'
 
-const render = (element, node) =>
-  element.append(node.paint())
+const render = (element, ...nodes) =>
+  element.append(...f.map(nodes, node => node.paint()))
 
 export default f.curry(f.arity(2, f.frame(render)))
