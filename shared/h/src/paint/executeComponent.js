a7efdc5e31c8060b20675bb648259cd2c7926b57
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-05 09:28:01 -0300

    refactor: inclusao do processo de reflow no debounce

--- shared/h/src/paint/executeComponent.js
+++ shared/h/src/paint/executeComponent.js
@@ -3,10 +3,12 @@ import * as f from '@kuba/f'
 export default function (componentRef, entity, children) {
   const tag = componentRef(entity, children)
 
+  const reflow = f.debounce(f.frame(() =>
+    entity[f.magic('tag')].reflow(componentRef(entity, children))), 0)
+
   f.assign(entity, {
     [f.magic('tag')]: tag,
-    [f.magic('reflow')]: f.frame(() =>
-      entity[f.magic('tag')].reflow(componentRef(entity, children)))
+    [f.magic('reflow')]: reflow
   })
 
   return tag
