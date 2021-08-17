ac9682dee2fd8ed84073aef4997e81c9ad2c04b5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-17 16:16:49 -0300

    refactor: voltando tag para o model

--- models/site/src/component.jsx
+++ models/site/src/component.jsx
@@ -1,4 +1,5 @@
 import h, { Fragment } from '@kuba/h'
+import Flag from '@kuba/flag'
 import Lazy from '@kuba/lazy'
 import Zone from '@kuba/zone'
 import style from './style'
@@ -12,4 +13,5 @@ export default (_site, children) =>
     <Zone className={style.site__zone}>
       <Lazy className={style.site__footer} require={() => import('@kuba/footer' /* webpackChunkName: "footer" */)} />
     </Zone>
+    <Flag className={style.site__flag} />
   </>
