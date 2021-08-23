80987ccc2b42a0a2e85d8953aed87631471c9a87
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-23 16:15:37 -0300

    fix: foi um erro trabalhar com bricklayer

--- shared/bricklayer/src/reducer.js
+++
@@ -1,8 +0,0 @@
-import reflow from './reflow'
-
-export default (wc, referenceId) =>
-  (value) => (
-    wc.state[referenceId] = value,
-    wc.useEffect(),
-    reflow(wc)
-  )
--- shared/bricklayer/src/render.js
+++
@@ -1,6 +0,0 @@
-import { render } from '@kuba/h'
-
-export default (wc) => (
-  wc.setElement(wc.component(wc.attrs, wc.hooks)),
-  render(wc, wc.element)
-)
