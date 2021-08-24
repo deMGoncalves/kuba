8359a06140d0fa4fd2ee8b710ad322d53a4ed462
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-24 14:23:27 -0300

    refactor: removendo o repeat do h

--- shared/h/src/index.js
+++ shared/h/src/index.js
@@ -4,7 +4,6 @@ export { default as filters } from './filters'
 export { default as paint } from './paint'
 export { default as render } from './render'
 export { default as repaint } from './repaint'
-export { default as Repeat } from './repeat'
 
 export { Fragment } from './tag'
 export { didMount, didUnmount, didUpdate, willMount, willUnmount, willUpdate } from './hooks'
