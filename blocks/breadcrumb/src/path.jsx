fe05f469557435cedbd42ee1f56202079b0b097b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-24 14:02:32 -0300

    refactor: trocando pela diretiva show

--- blocks/breadcrumb/src/path.jsx
+++ blocks/breadcrumb/src/path.jsx
@@ -1,11 +1,12 @@
-import h, { Fragment, Show } from '@kuba/h'
+import h, { Fragment } from '@kuba/h'
 import icon from '@kuba/icon'
 import link from '@kuba/link'
+import Show from '@kuba/Show'
 import style from './style'
 
 export default (path, i) =>
   <>
-    <Show if={i}>
+    <Show when={i}>
       <icon.AngleRight className={style.breadcrumb__icon} />
     </Show>
     <link.Master className={style.breadcrumb__link} href={path.url} small>{path.title}</link.Master>
