4a80f00419a0a83ec6836cd060b73daf89997bd7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-27 11:33:34 -0300

    feat(api): inclusao do mock para hero

---
+++ buckets/app/api/blocks/hero.js
@@ -0,0 +1,13 @@
+export default function (_request, response) {
+  response.json({
+    cta: 'Sed cursus',
+    description: 'Nulla id placerat sem. Vestibulum congue suscipit erat, eu tempor augue venenatis ut. Vestibulum magna ex, pulvinar eget quam et, rutrum sodales nisl. Curabitur finibus tristique.',
+    url: 'https://www.kuba.engineer/departament',
+    thumbnails: [
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_736x400.jpg',
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_928x400.jpg',
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_1198x400.jpg'
+    ],
+    title: 'Nunc eu dignissim leo. Donec.'
+  })
+}
