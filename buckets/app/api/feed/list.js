63aeaa43a5e7710064d1f00bc8a0731e2249dde0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-27 09:31:00 -0300

    refactor: removendo apis que nao sera usado

--- buckets/app/api/feed/list.js
+++
@@ -1,9 +0,0 @@
-import supabase from '@kuba/supabase'
-
-export default async function (_request, response) {
-  const { data, error } = await supabase
-    .from('feed')
-    .select('description, image (data, extension)')
-
-  response.json({ data, error })
-}
