7fbd382ffd4df0d8b451128031f019f83c0a9bd7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-27 09:31:56 -0300

    perf: nao posso usar o content visibility no story

--- elements/story/src/style.js
+++ elements/story/src/style.js
@@ -2,7 +2,6 @@ import styled from '@kuba/styled'
 
 export default styled.style`
   .story {
-    content-visibility: auto;
     display: inline-flex;
     margin: var(--spacing-lg) auto;
     width: 100%;
