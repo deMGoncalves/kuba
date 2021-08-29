0976d86d60fa8519e354cfca71ef67e8c80903e4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-29 19:10:51 -0300

    feat(button): inclusao do skeleton

--- elements/button/src/index.js
+++ elements/button/src/index.js
@@ -1,1 +1,11 @@
-export { default } from './button'
+import skeleton from '@kuba/skeleton'
+import has from './has'
+import button from './button'
+
+export default new Proxy({}, {
+  get: (_, type) =>
+    (props, children) =>
+      has(children)
+        ? button[type](props, children)
+        : skeleton.button(props)
+})
