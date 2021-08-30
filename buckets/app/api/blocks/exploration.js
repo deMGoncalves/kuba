af193cbfb1ac78bab6791026edebed4f232749eb
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-30 13:56:25 -0300

    feat(api): mock para o bloco shelf

--- buckets/app/api/blocks/exploration.js
+++ buckets/app/api/blocks/exploration.js
@@ -4,22 +4,22 @@ export default function (_request, response) {
     departaments: [
       {
         title: 'Mauris arcu dolor',
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/acessory.jpg',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/acessory_287x287.jpg',
         url: '/departament'
       },
       {
         title: 'Cras ac commodo',
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/chair.jpg',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/chair_287x287.jpg',
         url: '/departament'
       },
       {
         title: 'Nullam a pulvinar',
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/sofa.jpg',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/sofa_287x287.jpg',
         url: '/departament'
       },
       {
         title: 'Interdum et',
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/stool.jpg',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/exploration/stool_287x287.jpg',
         url: '/departament'
       }
     ]
---
+++ buckets/app/api/blocks/shelf.js
@@ -0,0 +1,102 @@
+export default function (_request, response) {
+  response.json({
+    products: [
+      {
+        name: 'Donec molestie malesuada aliquam',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Mauris tempus, lectus sed molestie',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Donec consequat urna in ante',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Nullam ac metus ante. Vestibulum',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Ut eu semper ex, vitae',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'In hac habitasse platea dictumst',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Vivamus elementum eros vel massa',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Integer vulputate, ligula id feugiat',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Donec vel ipsum blandit, bibendum',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Praesent ante tellus, pretium quis',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Etiam vitae porta nunc, in',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Duis sit amet nibh et',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Donec non massa aliquam erat',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'In hac habitasse platea dictumst',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Vivamus elementum eros vel massa',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Donec consequat urna in ante',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Donec molestie malesuada aliquam',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Ut eu semper ex, vitae',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Mauris tempus, lectus sed molestie',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Donec consequat urna in ante',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Nullam ac metus ante. Vestibulum',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Ut eu semper ex, vitae',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'Donec non massa aliquam erat',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      },
+      {
+        name: 'In hac habitasse platea dictumst',
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/product/product_287x287.png'
+      }
+    ]
+  })
+}
