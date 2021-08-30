722727686ef47a789e3d0db76cb37c1cf23edc6a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-30 14:51:39 -0300

    perf(header): usando o media com o schedule para exeucar o fetch

--- blocks/header/src/darlings/getData.js
+++ blocks/header/src/darlings/getData.js
@@ -1,8 +1,13 @@
+import * as f from '@kuba/f'
+import media from '@kuba/media'
 import schedule from '@kuba/schedule'
 
 const getData = (darlings) =>
-  fetch(`${__settings.api.host}/blocks/header`)
-    .then(response => response.json())
-    .then(data => darlings.change(data))
+  media('(min-width: 961px)')
+    .in(f.once(() =>
+      fetch(`${__settings.api.host}/blocks/header`)
+        .then(response => response.json())
+        .then(data => darlings.change(data))
+    ))
 
 export default schedule(getData)
--- blocks/header/src/menu/nav/getData.js
+++ blocks/header/src/menu/nav/getData.js
@@ -1,8 +1,13 @@
+import * as f from '@kuba/f'
+import media from '@kuba/media'
 import schedule from '@kuba/schedule'
 
 const getData = (nav) =>
-  fetch(`${__settings.api.host}/blocks/header`)
-    .then(response => response.json())
-    .then(data => nav.change(data))
+  media('(max-width: 960px)')
+    .in(f.once(() =>
+      fetch(`${__settings.api.host}/blocks/header`)
+        .then(response => response.json())
+        .then(data => nav.change(data))
+    ))
 
 export default schedule(getData)
