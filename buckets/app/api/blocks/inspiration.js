eeb501e44782b81c25f26452b3dbfc07c90b2a3e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-29 21:24:12 -0300

    feat(api): construcao da api para o bloco de inspiracao

---
+++ buckets/app/api/blocks/inspiration.js
@@ -0,0 +1,43 @@
+export default function (_request, response) {
+  response.json({
+    title: 'Integer vestibulum finibus mollis',
+    thumbnails: [
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/inspiration_736x400.jpg',
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/inspiration_591x645.jpg'
+    ],
+    departaments: [
+      {
+        thumbnails: [
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/chair_360x360.jpg',
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/chair_285x285.jpg'
+        ],
+        title: 'Nam dignissim',
+        url: '/departament'
+      },
+      {
+        thumbnails: [
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/hadle_360x360.jpg',
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/hadle_285x285.jpg'
+        ],
+        title: 'Proin dictum',
+        url: '/departament'
+      },
+      {
+        thumbnails: [
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/sofa_360x360.jpg',
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/sofa_285x285.jpg'
+        ],
+        title: 'Fusce leo',
+        url: '/departament'
+      },
+      {
+        thumbnails: [
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/clock_360x360.jpg',
+          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/clock_285x285.jpg'
+        ],
+        title: 'Nunc aliquam',
+        url: '/departament'
+      }
+    ]
+  })
+}
