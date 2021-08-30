b2e49c5609c1e7f7db3e61ccf60109cc85a59cb7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-30 10:01:56 -0300

    feat(skeleton): inclusao do link

---
+++ elements/skeleton/src/link.js
@@ -0,0 +1,9 @@
+import * as f from '@kuba/f'
+import { size } from '@kuba/polished'
+import styled from '@kuba/styled'
+
+export default styled.span`
+  background-color: var(--color-master-light);
+  height: ${size};
+  width: ${() => f.random(34, 110)}px;
+`
--- elements/skeleton/src/mapper.js
+++ elements/skeleton/src/mapper.js
@@ -1,9 +1,11 @@
 import button from './button'
+import link from './link'
 import picture from './picture'
 import text from './text'
 
 export default {
   button,
+  link,
   picture,
   text
 }
