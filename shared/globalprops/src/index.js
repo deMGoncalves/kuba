6267ea67176199058d967b475009eac236e2f53f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-30 18:14:35 -0300

    feat(globalprops): agora nao precisamos mais repassar os classnames e slot

---
+++ shared/globalprops/src/index.js
@@ -0,0 +1,25 @@
+import * as f from '@kuba/f'
+
+export default (Entity) =>
+  new Proxy(
+    function (props) {
+      const instance = (this instanceof Entity)
+        ? new Entity(...arguments)
+        : Entity(...arguments)
+
+      return new Proxy(instance, {
+        get: (target, key) =>
+          f.has(key, target)
+            ? f.is(Function, target[key])
+              ? target[key].bind(target)
+              : target[key]
+            : props[key],
+
+        set: (target, key, value) => f.T(target[key] = value)
+      })
+    },
+    {
+      get: (_, key) => Entity[key],
+      set: (_, key, value) => f.T(Entity[key] = value)
+    }
+  )
