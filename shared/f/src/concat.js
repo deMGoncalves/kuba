b0eb886700111ce8e958f957e3154af2794be356
wilberchair <wilberchair@gmail.com>
2021-08-30 12:22:52 -0300

    refactor(concat): atualizacao do concat

--- shared/f/src/concat.js
+++ shared/f/src/concat.js
@@ -1,6 +1,8 @@
+import arity from './arity'
 import curry from './curry'
+import reduce from './reduce'
 
-const concat = (x, y) =>
-  x.concat(y)
+const concat = (x, ...y) =>
+  reduce(y, (a, b) => a.concat(b), x)
 
-export default curry(concat)
+export default curry(arity(2, concat))
