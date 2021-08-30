a96c07e263e2978b6d3db0ec12722d57ff8c78d9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-30 12:47:45 -0300

    fix(f): o slice deve ter apenas 2 parametros

--- shared/f/src/slice.js
+++ shared/f/src/slice.js
@@ -1,6 +1,7 @@
+import arity from './arity'
 import curry from './curry'
 
 const slice = (array, start, end) =>
   array.slice(start, end)
 
-export default curry(slice)
+export default curry(arity(2, slice))
