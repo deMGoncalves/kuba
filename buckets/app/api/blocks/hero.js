8e34c2f4626ccaafef0262155aca1565fe01dfbe
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-31 16:06:07 -0300

    feat(api): inclusao do mock do banner

--- buckets/app/api/blocks/hero.js
+++ buckets/app/api/blocks/hero.js
@@ -2,12 +2,12 @@ export default function (_request, response) {
   response.json({
     cta: 'Sed cursus',
     description: 'Nulla id placerat sem. Vestibulum congue suscipit erat, eu tempor augue venenatis ut. Vestibulum magna ex, pulvinar eget quam et, rutrum sodales nisl. Curabitur finibus tristique.',
-    url: '/departament',
     thumbnails: [
       'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_736x400.jpg',
       'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_928x400.jpg',
       'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_1198x400.jpg'
     ],
-    title: 'Nunc eu dignissim leo. Donec.'
+    title: 'Nunc eu dignissim leo. Donec.',
+    url: '/departament'
   })
 }
