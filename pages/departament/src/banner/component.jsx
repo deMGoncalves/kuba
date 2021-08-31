f6fed1aeececab942c10b8e4b640c97f855b712b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-08-31 16:11:11 -0300

    refactor: agora o banner busca os dados sozinho

--- pages/departament/src/banner/component.jsx
+++ pages/departament/src/banner/component.jsx
@@ -1,7 +1,6 @@
 import h from '@kuba/h'
 import Banner from '@kuba/banner'
-import schema from './schema'
 import style from './style'
 
 export default (props) =>
-  <Banner {...schema} className={[style.banner, props.className]} />
+  <Banner className={[style.banner, props.className]} />
