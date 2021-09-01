523f0a6a98f76503bfc85e8040cf0f199ca5f3c9
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-01 17:04:57 -0300

    refactor(breadcrumb): nao preciso tratar atributos globais como o classname

--- blocks/breadcrumb/src/breadcrumb.js
+++ blocks/breadcrumb/src/breadcrumb.js
@@ -1,24 +1,20 @@
 import { paint } from '@kuba/h'
 import jsonld from '@kuba/jsonld'
+import props from '@kuba/props'
 import component from './component'
 import data from './data'
 
 @paint(component)
 @jsonld(data)
+@props
 class Breadcrumb {
-  #className
   #paths
-
-  get className () {
-    return this.#className
-  }
 
   get paths () {
     return this.#paths ??= []
   }
 
   constructor (props) {
-    this.#className = props.className
     this.#paths = props.paths
   }
 }
