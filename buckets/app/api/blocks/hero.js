6dd08d75fdd29398c7e83be59c36aedb10088533
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-01 18:14:33 -0300

    refactor: trocando as imagens do hero

--- buckets/app/api/blocks/hero.js
+++ buckets/app/api/blocks/hero.js
@@ -3,9 +3,9 @@ export default function (_request, response) {
     cta: 'Sed cursus',
     description: 'Nulla id placerat sem. Vestibulum congue suscipit erat, eu tempor augue venenatis ut. Vestibulum magna ex, pulvinar eget quam et, rutrum sodales nisl. Curabitur finibus tristique.',
     thumbnails: [
-      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_736x400.jpeg',
-      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_928x400.jpeg',
-      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_1198x400.jpeg'
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_736x400.jpg',
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_928x400.jpg',
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/hero/hero_1198x400.jpg'
     ],
     title: 'Nunc eu dignissim leo. Donec.',
     url: '/departament'
