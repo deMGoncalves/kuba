263cb4ec0021197be0b0c7be8d166df682bad96e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-01 16:05:23 -0300

    refactor(api): troca da imagem principal do inspiration

--- buckets/app/api/blocks/inspiration.js
+++ buckets/app/api/blocks/inspiration.js
@@ -2,8 +2,8 @@ export default function (_request, response) {
   response.json({
     title: 'Integer vestibulum finibus mollis',
     thumbnails: [
-      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/inspiration_736x400.jpg',
-      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/inspiration_591x645.jpg'
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/inspiration_736x400.jpeg',
+      'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/inspiration/inspiration_591x645.jpeg'
     ],
     departaments: [
       {
