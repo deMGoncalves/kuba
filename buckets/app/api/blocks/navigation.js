24c6c04bedc8280ae10a0e09c750704075b0c760
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-01 16:44:33 -0300

    feat(api): mock para o navigation

---
+++ buckets/app/api/blocks/navigation.js
@@ -0,0 +1,47 @@
+export default function (_request, response) {
+  response.json({
+    title: 'Vivamus nulla erat, interdum',
+    categories: [
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'Integer ultrices',
+        url: '/departament/category'
+      },
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'Praesent faucibus',
+        url: '/departament/category'
+      },
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'Cras id',
+        url: '/departament/category'
+      },
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'Vestibulum nibh',
+        url: '/departament/category'
+      },
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'Integer id',
+        url: '/departament/category'
+      },
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'Aenean vitae',
+        url: '/departament/category'
+      },
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'In non',
+        url: '/departament/category'
+      },
+      {
+        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/kuba/navigation/navigation_135x135.jpeg',
+        title: 'Sed pharetra',
+        url: '/departament/category'
+      }
+    ]
+  })
+}
