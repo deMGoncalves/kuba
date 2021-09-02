ef95eb5ab0f502b714d127834d443bad8bd84f8a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-02 10:59:52 -0300

    refactor(zone): usando o ciclo de vida para montar o zone

--- directives/zone/src/zone.js
+++ directives/zone/src/zone.js
@@ -1,36 +1,31 @@
-import { paint, repaint } from '@kuba/h'
+import { didMount, paint, repaint } from '@kuba/h'
 import * as f from '@kuba/f'
-import schedule from '@kuba/schedule'
+import onScreen from '@kuba/onscreen'
+import props from '@kuba/props'
 import component from './component'
-import scroll from './scroll'
 
 @paint(component)
-@schedule(scroll)
+@props
 class Zone {
-  #className
   #onView
-  #slot
 
-  get className () {
-    return this.#className
+  get element () {
+    return this[f.magic('tag')].element
   }
 
   get onView () {
     return this.#onView ??= f.F()
   }
 
-  get slot () {
-    return this.#slot
-  }
-
-  constructor (props) {
-    this.#className = props.className
-    this.#slot = props.slot
-  }
-
   @repaint
   render () {
     this.#onView = f.T()
+    return this
+  }
+
+  @didMount
+  scroll () {
+    onScreen(this.element, () => this.render())
     return this
   }
 }
