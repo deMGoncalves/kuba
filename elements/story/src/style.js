8ff4b68abb4ccad1cca8f68ceaf44fb3fd49659f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-02 09:15:32 -0300

    style(story): diminuindo o espacamento nos dispositivos mobile

--- elements/story/src/style.js
+++ elements/story/src/style.js
@@ -3,7 +3,13 @@ import styled from '@kuba/styled'
 export default styled.style`
   .story {
     display: inline-flex;
-    margin: var(--spacing-lg) auto;
+    margin: var(--spacing-sm) auto;
     width: 100%;
+  }
+
+  @media (min-width: 961px) {
+    .story {
+      margin: var(--spacing-lg) auto;
+    }
   }
 `
