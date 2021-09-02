b57ba518fef85291422ca08374a966414e0a45bb
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-02 00:41:44 -0300

    refactor: inclusao dos medias para os dispositivos moveis

--- blocks/exploration/src/departament.jsx
+++ blocks/exploration/src/departament.jsx
@@ -1,13 +1,14 @@
 import h from '@kuba/h'
 import Box from '@kuba/box'
 import Show from '@kuba/show'
+import Sources from '@kuba/sources'
 import text from '@kuba/text'
 import style from './style'
 
 export default (departament) =>
   <Box className={style.exploration__box} alt={departament.title} onClick={() => location.assign(departament.url)}>
-    <Show when={departament.thumbnail}>
-      <source srcSet={departament.thumbnail} slot='sources' />
+    <Show when={departament.thumbnails}>
+      <Sources thumbnails={departament.thumbnails} slot='sources' />
     </Show>
     <text.Strong className={style.exploration__strong} slot='caption' master dark xxxs>{departament.title}</text.Strong>
   </Box>
