b34d2df3c54fbb17ae5de3ecf6aa983d1df5b2b4
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-03 11:48:37 -0300

    perf(search): movendo o shelf para o carregamento lazy

--- pages/search/src/component.jsx
+++ pages/search/src/component.jsx
@@ -1,6 +1,6 @@
 import h, { Fragment } from '@kuba/h'
 import Filter from '@kuba/filter'
-import Shelf from '@kuba/shelf'
+import Lazy from '@kuba/lazy'
 import Site from '@kuba/site'
 import Title from '@kuba/title'
 import Breadcrumb from './breadcrumb'
@@ -12,6 +12,8 @@ export default (search) =>
       <Title className={style.search__title}>{search.q}</Title>
       <Breadcrumb className={style.search__breadcrumb} />
       <Filter className={style.search__filter} />
-      <Shelf className={style.search__shelf} />
+    </Fragment>
+    <Fragment slot='warm'>
+      <Lazy require={() => import('@kuba/shelf' /* webpackChunkName: "shelf" */)} />
     </Fragment>
   </Site>
