1cd74aaa900718f1249b6f05e628e8a67e4f5c42
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-03 13:10:18 -0300

    chore(web): inclusao dos pixels do gtm e analytics

--- buckets/web/src/index.js
+++ buckets/web/src/index.js
@@ -1,11 +1,20 @@
 import '@kuba/settings'
+
 import '@kuba/tokens'
 import '@kuba/reset'
+
 import '@kuba/boma'
+import '@kuba/ga'
+import '@kuba/gtag'
+import '@kuba/gtm'
+
 import '@kuba/formdata'
 import '@kuba/prevent'
 import '@kuba/stop'
 import '@kuba/value'
+
 import '@kuba/scrolllock'
+
 import '@kuba/sw'
+
 import './routers'
