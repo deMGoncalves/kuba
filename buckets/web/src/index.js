c9edc605300c81b770e0e0f1bf85b8b4a852d6cc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-03 13:39:28 -0300

    refactor(web): configuracao da ordem de execucao

--- buckets/web/src/index.js
+++ buckets/web/src/index.js
@@ -3,19 +3,18 @@ import '@kuba/settings'
 import '@kuba/tokens'
 import '@kuba/reset'
 
-import '@kuba/boma'
-import '@kuba/ga'
-import '@kuba/gtag'
-import '@kuba/gtm'
-import '@kuba/hotjar'
+import '@kuba/scrolllock'
 
 import '@kuba/formdata'
 import '@kuba/prevent'
 import '@kuba/stop'
 import '@kuba/value'
 
-import '@kuba/scrolllock'
-
-import '@kuba/sw'
-
 import './routers'
+
+import '@kuba/boma'
+import '@kuba/ga'
+import '@kuba/gtag'
+import '@kuba/gtm'
+import '@kuba/hotjar'
+import '@kuba/sw'
