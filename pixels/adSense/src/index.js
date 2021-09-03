122b56849f1af44a00d8c687aff0d6e9d8c40ad8
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-03 13:30:40 -0300

    refactor(adsense): configuracao do id

--- pixels/adSense/src/index.js
+++ pixels/adSense/src/index.js
@@ -1,9 +1,1 @@
-import h, { render } from '@kuba/h'
-import * as f from '@kuba/f'
-import agent from '@kuba/agent'
-import env from '@kuba/env'
-import schema from './schema.json'
-
-f.and(agent.isUser, env.isProd) && (
-  render(document.head, <script data-ad-client={schema.adClient} src={schema.src} async />)
-)
+export { default } from './pixel'
