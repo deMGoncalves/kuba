47b8cacae03a794b442150c284b7d9710e18d3a6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-08 12:21:11 -0300

    refactor(web): removendo duplicacoes de codigo nos mocks

--- buckets/web/api/blocks/inspiration.js
+++ buckets/web/api/blocks/inspiration.js
@@ -1,41 +1,31 @@
 export default function (_request, response) {
+  const thumbnails = [
+    'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
+    'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
+    'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
+  ]
+
   response.json({
     cta: 'Aliquam fringilla',
     description: 'Curabitur ornare ligula ex, non efficitur orci placerat in. Sed faucibus non orci et fringilla. Nulla tincidunt mattis mi. Vivamus in nulla eu est dignissim laoreet.',
     departaments: [
       {
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         title: 'Nam dignissim',
         url: '/departament'
       },
       {
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         title: 'Proin dictum',
         url: '/departament'
       },
       {
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         title: 'Fusce leo',
         url: '/departament'
       },
       {
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         title: 'Nunc aliquam',
         url: '/departament'
       }
--- buckets/web/api/blocks/navigation.js
+++ buckets/web/api/blocks/navigation.js
@@ -1,44 +1,46 @@
 export default function (_request, response) {
+  const thumbnail = 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg'
+
   response.json({
     title: 'Vivamus nulla erat, interdum',
     categories: [
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'Integer ultrices',
         url: '/departament/category'
       },
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'Praesent faucibus',
         url: '/departament/category'
       },
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'Cras id',
         url: '/departament/category'
       },
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'Vestibulum nibh',
         url: '/departament/category'
       },
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'Integer id',
         url: '/departament/category'
       },
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'Aenean vitae',
         url: '/departament/category'
       },
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'In non',
         url: '/departament/category'
       },
       {
-        thumbnail: 'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_135x135.svg',
+        thumbnail,
         title: 'Sed pharetra',
         url: '/departament/category'
       }
