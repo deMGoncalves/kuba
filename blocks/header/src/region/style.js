1671e2251a4699c2609d9913b4c8afeb440f4667
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-08 16:40:49 -0300

    chore: rollback

--- blocks/header/src/region/style.js
+++ blocks/header/src/region/style.js
@@ -1,16 +1,6 @@
 import styled from '@kuba/styled'
 
 export default styled.style`
-  .region {
-    display: none;
-  }
-
-  @media (min-width: 769px) {
-    .region {
-      display: flex;
-    }
-  }
-
   .region__p {
     display: none;
     text-align: left;
