19e9479a4b141778075da0a1df030a3865922e06
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-08 12:05:40 -0300

    refactor(api): removendo duplicacao de codigo

--- buckets/web/api/blocks/exploration.js
+++ buckets/web/api/blocks/exploration.js
@@ -1,41 +1,31 @@
 export default function (_request, response) {
+  const thumbnails = [
+    'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
+    'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
+    'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
+  ]
+
   response.json({
     title: 'In efficitur elementum odio',
     departaments: [
       {
         title: 'Mauris arcu dolor',
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         url: '/departament'
       },
       {
         title: 'Cras ac commodo',
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         url: '/departament'
       },
       {
         title: 'Nullam a pulvinar',
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         url: '/departament'
       },
       {
         title: 'Interdum et',
-        thumbnails: [
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_360x360.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_220x220.svg',
-          'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_287x287.svg'
-        ],
+        thumbnails,
         url: '/departament'
       }
     ]
