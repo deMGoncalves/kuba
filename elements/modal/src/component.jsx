73504ee334fa87579add9e99e1de326926a71dae
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-13 13:42:38 -0300

    refactor(modal): nao precisamo do fragment

--- elements/modal/src/component.jsx
+++ elements/modal/src/component.jsx
@@ -1,11 +1,9 @@
-import h, { Fragment } from '@kuba/h'
+import h from '@kuba/h'
 import style from './style'
 
 export default (props, children) =>
-  <>
-    <div className={style.modal__overlayer} opened={props.opened} onClick={props.onClose}>
-      <aside className={[style.modal, props.className]} onClick:stop={() => null}>
-        {children}
-      </aside>
-    </div>
-  </>
+  <div className={style.modal__overlayer} opened={props.opened} onClick={props.onClose}>
+    <aside className={[style.modal, props.className]} onClick:stop={() => null}>
+      {children}
+    </aside>
+  </div>
