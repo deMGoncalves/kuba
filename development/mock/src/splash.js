3a07856f62d4da09502c1959d34db16776354eb6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-09-16 08:56:26 -0300

    refactor(mock): trocando os textos do mock

--- development/mock/src/splash.js
+++ development/mock/src/splash.js
@@ -1,13 +1,13 @@
 export default function () {
   return Promise.resolve({
-    cta: 'Sed cursus',
-    description: 'Nulla id placerat sem. Vestibulum congue suscipit erat, eu tempor augue venenatis ut. Vestibulum magna ex, pulvinar eget quam et, rutrum sodales nisl. Curabitur finibus tristique.',
+    cta: 'Fusce vel lacus',
+    description: 'Etiam nec est aliquet, convallis ante nec, tincidunt purus. Ut vehicula dolor ut sagittis sagittis.',
     thumbnails: [
       'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_736x400.svg',
       'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_928x400.svg',
       'https://wbfsnickkamdoyzndmec.supabase.in/storage/v1/object/public/fakeimg/placeholder_1198x400.svg'
     ],
-    title: 'Nunc eu dignissim leo. Donec.',
+    title: 'Pellentesque finibus cursus.',
     url: '/departament'
   })
 }
