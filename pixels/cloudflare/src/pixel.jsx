12b236f99d3ccdddc47c43c5f6166ec437c86922
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-02 19:54:58 -0300

    chore(cloudflare): nao vamos mais usar o cloudflare

--- pixels/cloudflare/src/pixel.jsx
+++
@@ -1,9 +0,0 @@
-import h, { render } from '@kuba/h'
-import * as f from '@kuba/f'
-import agent from '@kuba/agent'
-import env from '@kuba/env'
-import schema from './schema.json'
-
-f.and(agent.isUser, env.isProd) && (
-  render(document.head, <script defer src={schema.src} data-cf-beacon={schema.beacon} async />)
-)
