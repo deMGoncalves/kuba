b783864e4440238bd1af0c9579ad0fc3cf3898cc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-04 15:26:38 -0300

    refactor(exploration): troca do box pelo figure

--- blocks/exploration/src/departament.jsx
+++ blocks/exploration/src/departament.jsx
@@ -1,14 +1,14 @@
 import h from '@kuba/h'
-import Box from '@kuba/box'
+import Figure from '@kuba/figure'
 import Show from '@kuba/show'
 import Sources from '@kuba/sources'
 import text from '@kuba/text'
 import style from './style'
 
 export default (departament) =>
-  <Box className={style.exploration__box} alt={departament.title} onClick={() => location.assign(departament.url)}>
+  <Figure className={style.exploration__box} alt={departament.title} onClick={() => location.assign(departament.url)}>
     <Show when={departament.thumbnails}>
       <Sources thumbnails={departament.thumbnails} slot='sources' />
     </Show>
     <text.Strong className={style.exploration__strong} slot='caption' master dark xxxs>{departament.title}</text.Strong>
-  </Box>
+  </Figure>
