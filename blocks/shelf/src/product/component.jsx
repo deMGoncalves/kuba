9d98b64f311c88c0fabd1f5c18830eee8117a491
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-04 15:28:32 -0300

    refactor(shelf): troca do box pelo figure

--- blocks/shelf/src/product/component.jsx
+++ blocks/shelf/src/product/component.jsx
@@ -1,13 +1,13 @@
 import h from '@kuba/h'
-import Box from '@kuba/box'
+import Figure from '@kuba/figure'
 import Show from '@kuba/show'
 import text from '@kuba/text'
 import style from './style'
 
 export default (product) =>
-  <Box className={style.shelf__product}>
+  <Figure className={style.shelf__product}>
     <Show when={product.image}>
       <source srcSet={product.image} slot='sources' />
     </Show>
     <text.Strong className={style.shelf__strong} slot='caption' master darker xxxs regular>{product.name}</text.Strong>
-  </Box>
+  </Figure>
