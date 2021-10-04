c152a89c2bc7a9cc9945d21c4200e657dd64b0d7
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-04 14:49:59 -0300

    feat(f): inclusao do contrato de magic method para o is empty

--- shared/f/src/isEmpty.js
+++ shared/f/src/isEmpty.js
@@ -1,9 +1,13 @@
 import curry from './curry'
 import isNil from './isNil'
+import magic from './magic'
 import not from './not'
 import or from './or'
 
 const isEmpty = (value) =>
-  or(isNil(value), not(Object.keys(value).length))
+  or(
+    isNil(value),
+    value[magic('isEmpty')]?.() ?? not(Object.keys(value).length)
+  )
 
 export default curry(isEmpty)
