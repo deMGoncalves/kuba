e26feb2df8cb0dff3b58ded05cc325ec235857d6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-06 10:10:53 -0300

    refactor(f): inclusao de condicoes falsy no is empty

--- shared/f/src/isEmpty.js
+++ shared/f/src/isEmpty.js
@@ -1,13 +1,21 @@
+import cond from './cond'
 import curry from './curry'
+import F from './F'
+import is from './is'
 import isNil from './isNil'
+import len from './len'
 import magic from './magic'
 import not from './not'
-import or from './or'
+import T from './T'
 
-const isEmpty = (value) =>
-  or(
-    isNil(value),
-    value[magic('isEmpty')]?.() ?? not(Object.keys(value).length)
-  )
+const evalute = (value) =>
+  value[magic('isEmpty')]?.() ?? not(len(value))
+
+const isEmpty = cond(
+  [isNil, T],
+  [is(Boolean), F],
+  [is(Number), F],
+  [T, evalute]
+)
 
 export default curry(isEmpty)
