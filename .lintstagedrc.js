a98fd912078b72425177694947a26c26e8a70e39
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-12 16:52:33 -0300

    refactor(root): usando o lintstaged do proprio kuba

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,28 +1,5 @@
 'use strict'
 
-const fs = require('fs')
-const root = require('./package.json')
+const lintstaged = require('@kuba/lintstaged')
 
-const workspaces = root 
-  .workspaces
-  .packages
-  .map(w => w.slice(0, -2))
-
-const packages = workspaces
-  .map(w =>
-    fs.readdirSync(w, { withFileTypes: true })
-      .filter(d => d.isDirectory())
-      .map(d => d.name)
-  )
-
-const listener = (workspace, pkg) =>
-  `${workspace}/${pkg}/**/*`
-
-const command = (pkg) =>
-  `yarn workspace @${root.name}/${pkg} lint-staged --allow-empty`
-
-const mapper = workspaces
-  .reduce((object, workspace, index) =>
-    (packages[index].forEach(pkg => object[listener(workspace, pkg)] = command(pkg)), object), {})
-
-module.exports = mapper
+module.exports = lintstaged(__dirname)
