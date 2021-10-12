5ba9aad4e85a5d9e614bbfbb67326585395708dc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-12 13:16:46 -0300

    refactor(header): usando o mock

--- blocks/header/src/menu/nav/component.jsx
+++ blocks/header/src/menu/nav/component.jsx
@@ -1,5 +1,7 @@
 import h from '@kuba/h'
+import link from '@kuba/link'
 import Repeat from '@kuba/repeat'
+import { urlFor } from '@kuba/router'
 import Link from './link'
 import style from './style'
 
@@ -7,6 +9,9 @@ export default (nav) =>
   <nav slot={nav.slot}>
     <span className={style.nav__span}>Aliquam a metus erat</span>
     <ol className={style.nav__ol}>
+      <li className={style.nav__li}>
+        <link.Master className={style.nav__link} href={urlFor('home')}>Home</link.Master>
+      </li>
       <Repeat iterator={nav.nav} component={Link} />
     </ol>
     <span className={style.nav__span}>Proin vehicula sapien sed</span>
--- blocks/header/src/menu/nav/link.jsx
+++ blocks/header/src/menu/nav/link.jsx
@@ -1,8 +1,9 @@
 import h from '@kuba/h'
 import link from '@kuba/link'
+import { urlFor } from '@kuba/router'
 import style from './style'
 
 export default (props) =>
   <li className={style.nav__li}>
-    <link.Master className={style.nav__link} href={props.url}>{props.text}</link.Master>
+    <link.Master className={style.nav__link} href={urlFor('departament', { departament: props.url })}>{props.text}</link.Master>
   </li>
