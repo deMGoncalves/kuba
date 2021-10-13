f03b3713836fc9a600e7c217975397134e494349
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-13 08:50:25 -0300

    chore(category): nao vou mais usar a pagina de categoria

--- pages/category/src/breadcrumb/component.jsx
+++
@@ -1,7 +0,0 @@
-import h from '@kuba/h'
-import Breadcrumb from '@kuba/breadcrumb'
-import schema from './schema'
-import style from './style'
-
-export default (props) =>
-  <Breadcrumb {...schema} className={[style.breadcrumb, props.className]} />
