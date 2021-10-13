b942ce7508e817030b92463ee3f88ca31f03317e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-13 11:04:00 -0300

    refactor(home): inclusao do report

--- pages/home/src/component.jsx
+++ pages/home/src/component.jsx
@@ -2,6 +2,7 @@ import h, { Fragment } from '@kuba/h'
 import Exploration from '@kuba/exploration'
 import Hero from '@kuba/hero'
 import Lazy from '@kuba/lazy'
+import Report from '@kuba/report'
 import Sandbox from '@kuba/sandbox'
 import Site from '@kuba/site'
 import style from './style'
@@ -11,6 +12,7 @@ export default () =>
     <Fragment slot='hot'>
       <Hero className={style.home__hero} />
       <Sandbox require={() => new Worker(new URL('@kuba/hero', import.meta.url /* WebpackChunckName: 'hero' */))} />
+      <Report className={style.home__report} />
       <Exploration className={style.home__exploration} />
     </Fragment>
     <Fragment slot='warm'>
