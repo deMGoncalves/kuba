e668ac72934c4b51833bd3f278061be186c266a6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-13 09:25:13 -0300

    refactor(mock): alteracao dos textos do mock

--- development/mock/src/index.js
+++ development/mock/src/index.js
@@ -1,6 +1,5 @@
 export { default as banner } from './banner'
 export { default as exploration } from './exploration'
-export { default as header } from './header'
 export { default as hero } from './hero'
 export { default as inspiration } from './inspiration'
 export { default as navigation } from './navigation'
