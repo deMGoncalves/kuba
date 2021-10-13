be7cffb4cd69c85b3fa1329a6b36e1b369cae3ba
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-13 10:56:54 -0300

    feat(mock): dados para o report

---
+++ development/mock/src/report.js
@@ -0,0 +1,29 @@
+import thumbnail from './assets/placeholder_55x55.svg'
+
+export default function () {
+  return Promise.resolve({
+    title: 'In efficitur elementum odio',
+    shields: [
+      {
+        title: 'Standard',
+        description: 'Único padrão de codificação',
+        thumbnail
+      },
+      {
+        title: 'DeepSource',
+        description: 'Mantenha seu código limpo',
+        thumbnail
+      },
+      {
+        title: 'Code Climate',
+        description: 'Melhoria continua do seu código',
+        thumbnail
+      },
+      {
+        title: 'Codacy',
+        description: 'Qualidade do código alto',
+        thumbnail
+      }
+    ]
+  })
+}
