d1ca76af9d707d2beb6631cd460184a2a20a0f2f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-15 12:22:10 -0300

    style(cover): alinhando o espacamento estre os blocos

--- blocks/cover/src/style.js
+++ blocks/cover/src/style.js
@@ -3,10 +3,13 @@ import styled from '@kuba/styled'
 export default styled.style`
   .cover {
     background-color: var(--color-primary);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
     padding: var(--spacing_inset-sm);
   }
 
   .cover__blockquote {
-    width: 80%;
+    width: 89%;
   }
 `
