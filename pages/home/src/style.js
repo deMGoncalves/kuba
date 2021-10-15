0f24e54850bca0b80e839471ac108780b48cb811
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-15 11:03:51 -0300

    style(home): determinando quando o splash deve aparecer

--- pages/home/src/style.js
+++ pages/home/src/style.js
@@ -1,3 +1,13 @@
 import styled from '@kuba/styled'
 
-export default styled.style``
+export default styled.style`
+  .home__splash {
+    display: none;
+  }
+
+  @media (min-width: 769px) {
+    .home__splash {
+      display: flex;
+    }
+  }
+`
