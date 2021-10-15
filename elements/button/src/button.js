5108212ec502e3314436086a16967d4edca71fa0
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-15 13:38:06 -0300

    refactor(button): padronizando o button como o link

--- elements/button/src/button.js
+++ elements/button/src/button.js
@@ -1,27 +1,25 @@
 import * as f from '@kuba/f'
+import { color, blend } from '@kuba/polished'
 import styled from '@kuba/styled'
 
-const button = new Proxy({}, {
-  get: (_, type) =>
-    styled.button`
-      align-items: center;
-      background-color: var(--color-${() => f.toLower(type)}-dark);
-      border: none;
-      border-radius: var(--border-radius-pill);
-      color: var(--color-master-lightest);
-      cursor: pointer;
-      display: inline-flex;
-      font-family: var(--font-family-base);
-      font-size: var(--font-size-xxxs);
-      font-weight: var(--font-weight-medium);
-      justify-content: center;
-      letter-spacing: 0.618px;
-      line-height: var(--line-height-sm);
-      padding: var(--spacing_inset-nano) var(--spacing_inset-sm);
-      text-decoration: none;
-      user-select: none;
-    `
-})
+const button = styled.button`
+  align-items: center;
+  background-color: ${color};
+  border: none;
+  border-radius: var(--border-radius-pill);
+  color: ${blend};
+  cursor: pointer;
+  display: inline-flex;
+  font-family: var(--font-family-base);
+  font-size: var(--font-size-xxxs);
+  font-weight: var(--font-weight-medium);
+  justify-content: center;
+  letter-spacing: 0.618px;
+  line-height: var(--line-height-sm);
+  padding: var(--spacing_inset-nano) var(--spacing_inset-sm);
+  text-decoration: none;
+  user-select: none;
+`
 
 f.assign(button, {
   is: f.T
