28b8e3f4b7ba39bf27b64e416ff1d9c18c9aae7a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-18 09:01:50 -0300

    feat(app): vamos usar o bucket app para ser a entry point do todo mvc

---
+++ buckets/app/src/index.js
@@ -0,0 +1,19 @@
+import '@kuba/tokens'
+import '@kuba/reset'
+
+import '@kuba/scrolllock'
+
+import '@kuba/formdata'
+import '@kuba/istruthy'
+import '@kuba/prevent'
+import '@kuba/stop'
+import '@kuba/value'
+
+import './routers'
+
+import '@kuba/boma'
+import '@kuba/ga'
+import '@kuba/gtag'
+import '@kuba/gtm'
+import '@kuba/hotjar'
+import '@kuba/sw'
