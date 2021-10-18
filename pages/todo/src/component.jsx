90f77b0b75ac4fdc2544faaa3ef9164fdbc9dcc3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-18 13:51:15 -0300

    refactor(todo): estrutura do app

--- pages/todo/src/component.jsx
+++ pages/todo/src/component.jsx
@@ -1,21 +1,8 @@
 import h, { Fragment } from '@kuba/h'
-import Cover from '@kuba/cover'
-import Sandbox from '@kuba/sandbox'
 import Site from '@kuba/site'
-import Splash from '@kuba/splash'
 import style from './style'
 
 export default () =>
   <Site className={style.home}>
-    <Fragment slot='hot'>
-      <Splash className={style.home__splash} />
-      <Cover className={style.home__cover} />
-
-      {/**
-       * TODO: Teste do projeto Sandbox, cada bloco de
-       * layout deve ser executado em seu Web Worker
-       */}
-      <Sandbox require={() => new Worker(new URL('@kuba/cover', import.meta.url /* WebpackChunckName: 'cover' */))} />
-
-    </Fragment>
+    <Fragment slot='hot' />
   </Site>
