1263cb3b623a2cf33031d9abaabe3633683dc473
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-18 09:02:26 -0300

    chore(todomvc): sera usado o app no lugar

--- buckets/todomvc/src/routers.jsx
+++
@@ -1,8 +0,0 @@
-import h from '@kuba/h'
-import router from '@kuba/router'
-import render from './render'
-
-router('/', async function home () {
-  const { default: Home } = await import('@kuba/home' /* webpackChunkName: "home" */)
-  render(<Home />)
-})
