63ae394fba6e2bd2eb0e5e346c69d9fb87c07eb5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-20 16:07:33 -0300

    chore: neste momento vou deixar as coisas simples, vou focar na doc

--- blocks/splash/src/component.jsx
+++ blocks/splash/src/component.jsx
@@ -1,5 +1,5 @@
 import h from '@kuba/h'
 import style from './style'
 
-export default () =>
-  <section className={style.splash} />
+export default (props) =>
+  <section className={[style.splash, props.className]} />
