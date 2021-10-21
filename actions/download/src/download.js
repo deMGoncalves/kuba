d477f974424bcefb593fee3cef0be1f9e5d5a031
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-21 11:11:48 -0300

    refactor(download): separando as etapas do click

--- actions/download/src/download.js
+++ actions/download/src/download.js
@@ -5,4 +5,6 @@ export default (url, name) =>
   fetch(url)
     .then(response => response.blob())
     .then(blob => URL.createObjectURL(blob))
-    .then(href => h('a', { href, download: f.or(name, href) }).paint().click())
+    .then(href => h('a', { href, download: f.or(name, href) }))
+    .then(tag => tag.paint())
+    .then(element => element.click())
