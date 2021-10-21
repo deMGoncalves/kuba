ad113a6ffdeedaaeb630d18b61e4f78afa181d4d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-21 11:18:39 -0300

    refactor(download): quando o name nao for definido a url sera usado para definir o nome do arquivo

--- actions/download/src/download.js
+++ actions/download/src/download.js
@@ -1,10 +1,11 @@
 import h from '@kuba/h'
 import * as f from '@kuba/f'
+import evalutate from './evalutate'
 
 export default (url, name) =>
   fetch(url)
     .then(response => response.blob())
     .then(blob => URL.createObjectURL(blob))
-    .then(href => h('a', { href, download: f.or(name, href) }))
+    .then(href => h('a', { href, download: f.or(name, evalutate(url)) }))
     .then(tag => tag.paint())
     .then(element => element.click())
