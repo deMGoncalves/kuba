1bfc2b6805fe77fb77049d7816e87cc7d57f9bfc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-10-22 15:21:41 -0300

    fix(scripts): o comando de server um pacote o script se chama start

--- development/scripts/src/serve.js
+++ development/scripts/src/serve.js
@@ -3,7 +3,7 @@
 const shell = require('shelljs')
 
 module.exports = function (name) {
-  if (shell.exec(`yarn workspace @kuba/${name} serve`).code) {
+  if (shell.exec(`yarn workspace @kuba/${name} start`).code) {
     shell.echo('Error: yarn serve failed')
     shell.exit(1)
   }
