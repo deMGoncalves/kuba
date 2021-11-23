2df1045254866bdb51313f98fe03754ee141562e
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-23 14:48:30 -0300

    refactor(button): vamos trabalhar apenas com o elemento button

--- elements/button/src/index.js
+++ elements/button/src/index.js
@@ -1,11 +1,12 @@
 import skeleton from '@kuba/skeleton'
+import button from './button'
+import merge from './merge'
 import has from './has'
-import mapper from './mapper'
 
 export default new Proxy({}, {
   get: (_, type) =>
     (props, children) =>
       has(children)
-        ? mapper(type)(props, children)
+        ? button(merge(props, type), children)
         : skeleton.button(props)
 })
--- elements/button/src/mapper.js
+++
@@ -1,8 +0,0 @@
-import * as f from '@kuba/f'
-import button from './button'
-import ghost from './ghost'
-
-export default f.cond(
-  [ghost.is, f.always(ghost)],
-  [button.is, f.always(button)]
-)
