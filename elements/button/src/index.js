b59e1876b857e09a116d3f6cffd24d00b70959b5
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-23 12:44:40 -0300

    refactor(button): interceptando o tipo do botao

--- elements/button/src/index.js
+++ elements/button/src/index.js
@@ -2,7 +2,10 @@ import skeleton from '@kuba/skeleton'
 import has from './has'
 import mapper from './mapper'
 
-export default (props, children) =>
-  has(children)
-    ? mapper(props, children)
-    : skeleton.button(props)
+export default new Proxy({}, {
+  get: (_, type) =>
+    (props, children) =>
+      has(children)
+        ? mapper(type)(props, children)
+        : skeleton.button(props)
+})
