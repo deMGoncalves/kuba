cbdf274631911eda42beea6cf78d5aa64c6d5e66
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-23 15:33:47 -0300

    chore(controls): nao vou mais trabalhar com controls

--- elements/control/src/component.jsx
+++
@@ -1,9 +0,0 @@
-import h from '@kuba/h'
-import text from '@kuba/text'
-import style from './style'
-
-export default (control) =>
-  <div className={[style.control, control.className]}>
-    <text.Label className={style.control__label} for={control.id} master darker xxxs>{control.label}</text.Label>
-    <input className={style.control__input} id={control.id} name={control.name} type={control.type} />
-  </div>
