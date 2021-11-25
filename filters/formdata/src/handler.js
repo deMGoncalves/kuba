c3fa05c082d2d08857b16e866796c9c1612e2d09
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-25 17:14:11 -0300

    refactor(formdata): extendendo o comportamento para os inputs com mascara

--- filters/formdata/src/handler.js
+++ filters/formdata/src/handler.js
@@ -3,5 +3,12 @@ import * as f from '@kuba/f'
 export default (listener) =>
   (e) => (
     e.preventDefault(),
-    listener(f.pairs([...new FormData(e.target)]))
+    listener(
+      f
+        .from([...e.target])
+        .pipe(f.filter(f.__, f.prop('name')))
+        .pipe(f.map(f.__, ({ name, value, unmaskedValue }) => [name, f.or(unmaskedValue, value)]))
+        .pipe(f.pairs)
+        .done()
+    )
   )
