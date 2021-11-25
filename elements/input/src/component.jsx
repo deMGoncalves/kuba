2cd8a9e62bdafcb9877915a18ebeb873add534d6
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-25 12:36:10 -0300

    refactor(input): nao passar attributos que nao senha sido definido

--- elements/input/src/component.jsx
+++ elements/input/src/component.jsx
@@ -4,6 +4,6 @@ import style from './style'
 
 export default (props, children) =>
   <div className={style.input__div}>
-    <text.Label className={style.input__text} oncolor={props.onColor} master dark xxxs medium>{children.label}</text.Label>
-    <input className={style.input} oncolor={props.onColor} name={props.name} type={props.type} />
+    <text.Label className={style.input__text} oncolor:isTruthy={props.onColor} master dark xxxs medium>{children.label}</text.Label>
+    <input className={style.input} oncolor:isTruthy={props.onColor} name={props.name} type={props.type} />
   </div>
