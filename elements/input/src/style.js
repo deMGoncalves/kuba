9085434cb8c3b12d8a00cc6285741d0d802aeff3
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-25 11:45:38 -0300

    feat(input): elemento input

---
+++ elements/input/src/style.js
@@ -0,0 +1,28 @@
+import styled from '@kuba/styled'
+
+export default styled.style`
+  .input__div {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-nano);
+  }
+
+  .input__text[oncolor] {
+    color: var(--color-master-lighter) !important;
+  }
+
+  .input {
+    -webkit-appearance: none;
+    appearance: none;
+    border: var(--border-width-thin) solid var(--color-master-dark);
+    border-radius: var(--border-radius-sm);
+    color: var(--color-master);
+    padding: var(--spacing_inset-nano);
+  }
+
+  .input[oncolor] {
+    background-color: transparent;
+    border-color: var(--color-master-lightest);
+    color: var(--color-master-lighter);
+  }
+`
