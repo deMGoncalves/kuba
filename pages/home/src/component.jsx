9e217ee37aa776758fff549dfb509ea6ecaf28b0
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-25 17:18:44 -0300

    chore: tela de estudo

--- pages/home/src/component.jsx
+++ pages/home/src/component.jsx
@@ -1,16 +1,21 @@
 import h, { Fragment } from '@kuba/h'
 import button from '@kuba/button'
 import Container from '@kuba/container'
-import icon from '@kuba/icon'
+import Form from '@kuba/form'
 import input from '@kuba/input'
 import Story from '@kuba/story'
 
 export default () =>
   <Container>
     <Story>
-      <input.Money>
-        <Fragment slot='label'>Name</Fragment>
-      </input.Money>
-      <button.Primary dark>Cancel <icon.Close sm onColor /></button.Primary>
+      <Form onSubmit={console.log}>
+        <input.Email name='email'>
+          <Fragment slot='label'>Email</Fragment>
+        </input.Email>
+        <input.Password name='password'>
+          <Fragment slot='label'>Password</Fragment>
+        </input.Password>
+        <button.Primary dark medium>Log in</button.Primary>
+      </Form>
     </Story>
   </Container>
