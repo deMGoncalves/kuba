66e8604123090ebe572abb2818e812bed177870f
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-25 12:46:26 -0300

    feat(input): vinculado o input com a mascara

--- elements/input/src/component.jsx
+++ elements/input/src/component.jsx
@@ -5,5 +5,5 @@ import style from './style'
 export default (props, children) =>
   <div className={style.input__div}>
     <text.Label className={style.input__text} oncolor:isTruthy={props.onColor} master dark xxxs medium>{children.label}</text.Label>
-    <input className={style.input} oncolor:isTruthy={props.onColor} name={props.name} type={props.type} />
+    <input className={style.input} oncolor:isTruthy={props.onColor} name={props.name} type={props.type} is='input-mask' />
   </div>
