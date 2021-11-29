46269c91e97bc94b913558e972663f423cbe1e09
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 16:05:41 -0300

    feat(site): inclusao do header

--- models/site/src/component.jsx
+++ models/site/src/component.jsx
@@ -1,9 +1,11 @@
 import h, { Fragment } from '@kuba/h'
+import Header from '@kuba/header'
 import Zone from '@kuba/zone'
 import style from './style'
 
 export default (site, children) =>
   <>
+    <Header />
     <main className={[style.site__main, site.className]}>
       {children.hot}
       <Zone>
