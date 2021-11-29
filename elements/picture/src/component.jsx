c5e5dcea10a34daf20e94f695ac7969421a34672
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 17:14:08 -0300

    refactor(picture): preciso repassar os atributos para extender o picture

--- elements/picture/src/component.jsx
+++ elements/picture/src/component.jsx
@@ -4,7 +4,7 @@ import * as settings from '@kuba/settings'
 import style from './style'
 
 export default (props, children) =>
-  <picture className={[style.picture, props.className]}>
+  <picture {...props} className={[style.picture, props.className]}>
     {children}
     <img className={style.picture__img} loading='lazy' alt={f.or(props.alt, settings.app.name)} />
   </picture>
