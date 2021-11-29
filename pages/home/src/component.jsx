f3bad510dd58167e0471ada5072b054d423399d9
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 14:44:14 -0300

    refactor(app): testando o logo do kuba

--- pages/home/src/component.jsx
+++ pages/home/src/component.jsx
@@ -1,21 +1,11 @@
 import h, { Fragment } from '@kuba/h'
-import button from '@kuba/button'
 import Container from '@kuba/container'
-import Form from '@kuba/form'
-import input from '@kuba/input'
-import Modal from '@kuba/modal'
+import Logo from '@kuba/logo'
+import Story from '@kuba/story'
 
 export default () =>
   <Container>
-    <Modal opened={true}>
-      <Form onSubmit={console.log}>
-        <input.Email name='email'>
-          <Fragment slot='label'>Email</Fragment>
-        </input.Email>
-        <input.Password name='password'>
-          <Fragment slot='label'>Password</Fragment>
-        </input.Password>
-        <button.Primary dark medium>Log in</button.Primary>
-      </Form>
-    </Modal>
+    <Story>
+      <Logo />
+    </Story>
   </Container>
