a129654059a57a43410da3cfd4036731f2f16773
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 15:18:22 -0300

    chore(settings): alteracao da descricao do nome kuba

--- infra/settings/src/app.js
+++ infra/settings/src/app.js
@@ -1,10 +1,10 @@
 export default {
   author: 'deMGoncalves',
-  description: 'Como uma folha em branco, a liberdade de programar do jeito que faça sentido para o produto',
+  description: 'Kuba é como uma folha em branco, a liberdade de programar do jeito que faça sentido para o produto',
   language: 'pt-BR',
   name: 'kuba',
   release: '1.0.0',
-  themeColor: '#6d5cae',
+  themeColor: '#fafafa',
   title: 'kuba',
   url: `${location.protocol}//${location.host}`
 }
