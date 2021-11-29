d641b8e07634687ff837c4e44df323b8a895d4ed
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 14:35:47 -0300

    refactor(logo): repassando os attributos de manipulacao do tamano e sobre cor

--- elements/logo/src/component.jsx
+++ elements/logo/src/component.jsx
@@ -3,6 +3,6 @@ import Picture from '@kuba/picture'
 import style from './style'
 
 export default (logo) =>
-  <Picture className={[style.logo, logo.className]} alt={logo.name} onClick={() => logo.redirect()}>
+  <Picture className={[style.logo, logo.className]} onClick={() => logo.redirect()} small:isTruthy={logo.small} large:isTruthy={logo.large} oncolor:isTruthy={logo.onColor} alt={logo.name}>
     <source srcSet={logo.url} />
   </Picture>
