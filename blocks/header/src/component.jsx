43cb06d9f1336cbff5c3502d2a98d9f865e2b33a
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 16:00:07 -0300

    feat(header): inclusao do logo no header

--- blocks/header/src/component.jsx
+++ blocks/header/src/component.jsx
@@ -1,5 +1,11 @@
 import h from '@kuba/h'
+import Container from '@kuba/container'
+import Logo from '@kuba/logo'
 import style from './style'
 
-export default (header) =>
-  <div className={style.header}>{header.name}</div>
+export default (props) =>
+  <header className={[style.header, props.className]}>
+    <Container className={style.header__container}>
+      <Logo />
+    </Container>
+  </header>
--- blocks/header/src/style.js
+++ blocks/header/src/style.js
@@ -1,3 +1,13 @@
 import styled from '@kuba/styled'
 
-export default styled.style``
+export default styled.style`
+  .header {
+    border-bottom: var(--border-width-hairline) solid var(--color-master-light);
+  }
+
+  .header__container {
+    align-items: center;
+    display: flex;
+    height: 88px;
+  }
+`
