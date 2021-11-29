72006a9d1cb81875243f7281fa603f747977b0e1
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 10:14:07 -0300

    refactor(modal): tratando o evendo de click

--- elements/modal/src/component.jsx
+++ elements/modal/src/component.jsx
@@ -2,7 +2,7 @@ import h from '@kuba/h'
 import style from './style'
 
 export default (props, children) =>
-  <div className={style.modal__overlayer} opened:isTruthy={props.opened} onClick={props.onClose}>
+  <div className={style.modal__overlayer} opened:isTruthy={props.opened} onClick:stop={props.onClose}>
     <aside className={[style.modal, props.className]} onClick:stop={() => null}>
       {children}
     </aside>
