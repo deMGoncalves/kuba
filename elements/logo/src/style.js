d2a2dca88d739a56a768a5f427cde5918e067a51
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 14:43:48 -0300

    refactor(logo): regras para controle do tamanho do logo

--- elements/logo/src/style.js
+++ elements/logo/src/style.js
@@ -3,8 +3,17 @@ import styled from '@kuba/styled'
 export default styled.style`
   .logo {
     cursor: pointer;
-    min-width: 42px;
-    padding-top: 42px;
-    width: 42px;
+    padding-top: 28px;
+    width: 75px;
+  }
+
+  .logo[small] {
+    padding-top: 16px;
+    width: 43px;
+  }
+
+  .logo[large] {
+    padding-top: 40px;
+    width: 107px;
   }
 `
