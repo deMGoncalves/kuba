7a96d9752877a439cfe07ed76a1fed2e966ca2ac
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 17:12:41 -0300

    style(logo): estilizacao do novo logo

--- elements/logo/src/style.js
+++ elements/logo/src/style.js
@@ -4,16 +4,16 @@ export default styled.style`
   .logo {
     cursor: pointer;
     padding-top: 28px;
-    width: 75px;
+    width: 77px;
   }
 
   .logo[small] {
     padding-top: 16px;
-    width: 43px;
+    width: 44px;
   }
 
   .logo[large] {
     padding-top: 40px;
-    width: 107px;
+    width: 110px;
   }
 `
