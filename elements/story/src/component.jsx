0e69b3e6cf85461f14765c50ff139b54390c7960
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 15:26:51 -0300

    refactor(story): nao deve ter o container

--- elements/story/src/component.jsx
+++ elements/story/src/component.jsx
@@ -1,10 +1,7 @@
 import h from '@kuba/h'
-import Container from '@kuba/container'
 import style from './style'
 
 export default (props, children) =>
   <section {...props} className={[style.story, props.className]}>
-    <Container className={style.container}>
-      {children}
-    </Container>
+    {children}
   </section>
