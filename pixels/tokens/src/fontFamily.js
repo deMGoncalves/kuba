60ab1324ace724bd5a640cd25e4665a14956408b
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 16:23:28 -0300

    style(tokens): troca da font de display

--- pixels/tokens/src/fontFamily.js
+++ pixels/tokens/src/fontFamily.js
@@ -2,7 +2,7 @@ import styled from '@kuba/styled'
 
 export default styled.style`
   :root {
-    --font-family-highlight: 'Spectral', serif;
+    --font-family-highlight: 'Squada One', cursive;
     --font-family-base: 'Roboto', sans-serif;
   }
 `
--- pixels/tokens/src/opacity.js
+++ pixels/tokens/src/opacity.js
@@ -2,10 +2,10 @@ import styled from '@kuba/styled'
 
 export default styled.style`
   :root {
-    --opacity-level-semiopaque: .72;
-    --opacity-level-intense: .64;
-    --opacity-level-medium: .32;
-    --opacity-level-light: .16;
-    --opacity-level-semitransparent: .08;
+    --opacity-level-semiopaque: 0.72;
+    --opacity-level-intense: 0.64;
+    --opacity-level-medium: 0.32;
+    --opacity-level-light: 0.16;
+    --opacity-level-semitransparent: 0.08;
   }
 `
