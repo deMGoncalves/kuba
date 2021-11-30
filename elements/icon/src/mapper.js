acb516e70dabcb320aec7d2dc2427b6f65c01370
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-30 00:49:27 -0300

    feat(icon): icone de menu

--- elements/icon/src/mapper.js
+++ elements/icon/src/mapper.js
@@ -1,2 +1,4 @@
 export { default as Close } from './assets/close.svg'
-export { default as CloseOnColor } from './assets/close-onColor.svg'
+export { default as CloseOnColor } from './assets/close_onColor.svg'
+export { default as Menu } from './assets/menu.svg'
+export { default as MenuOnColor } from './assets/menu_onColor.svg'
