681b2e83787119d5d84aa2f27ed5ed4abd27e6b3
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 23:41:30 -0300

    refactor(logo): usando apenas uma imagem

--- elements/logo/src/style.js
+++ elements/logo/src/style.js
@@ -3,17 +3,14 @@ import styled from '@kuba/styled'
 export default styled.style`
   .logo {
     cursor: pointer;
-    padding-top: 28px;
-    width: 77px;
+    height: 28px;
   }
 
   .logo[small] {
-    padding-top: 16px;
-    width: 44px;
+    height: 16px;
   }
 
   .logo[large] {
-    padding-top: 40px;
-    width: 110px;
+    height: 40px;
   }
 `
