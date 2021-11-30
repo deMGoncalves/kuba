c8cf8e89e1094be4e45ae387e8c66375841bdeb3
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-29 23:03:11 -0300

    feat(container): agora posso definir qual tag deve ser o container

--- elements/container/src/container.js
+++ elements/container/src/container.js
@@ -1,9 +1,12 @@
 import styled from '@kuba/styled'
 
-export default styled.div`
-  margin: 0 auto;
-  max-width: 1230px;
-  padding: 0 var(--spacing_inset-xs);
-  position: relative;
-  width: 100%;
-`
+export default new Proxy({}, {
+  get: (_, tag) =>
+    styled[tag]`
+      margin: 0 auto;
+      max-width: 1230px;
+      padding: 0 var(--spacing_inset-xs);
+      position: relative;
+      width: 100%;
+    `
+})
