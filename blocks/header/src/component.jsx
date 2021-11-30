81f6130a2708c811d611369dd105dd55261865d6
deMGoncalves <cleber.engineer@hotmail.com>
2021-11-30 20:41:00 -0300

    feat(header): alinhamento e estilo de comportamento da navegacao

--- blocks/header/src/component.jsx
+++ blocks/header/src/component.jsx
@@ -1,7 +1,8 @@
 import h from '@kuba/h'
+import { urlFor } from '@kuba/router'
 import button from '@kuba/button'
 import container from '@kuba/container'
-import icon from '@kuba/icon'
+import link from '@kuba/link'
 import Logo from '@kuba/logo'
 import style from './style'
 
@@ -9,8 +10,13 @@ export default (props) =>
   <header className={[style.header, props.className]}>
     <container.Div className={style.header__container}>
       <Logo />
-      <button.Icon>
-        <icon.Menu />
-      </button.Icon>
+      <nav className={style.header__nav}>
+        <link.Master href={urlFor('about')} darker medium xxs>sobre</link.Master>
+        <link.Master href={urlFor('introduction')} darker medium xxs>introducao</link.Master>
+        <link.Master href={urlFor('designSystem')} darker medium xxs>design system</link.Master>
+        <link.Master href={urlFor('components')} darker medium xxs>componentes</link.Master>
+        <link.Master href={urlFor('arquiteture')} darker medium xxs>arquitetura</link.Master>
+      </nav>
+      <button.icon.Menu className={style.header__button} />
     </container.Div>
   </header>
--- blocks/header/src/style.js
+++ blocks/header/src/style.js
@@ -8,7 +8,46 @@ export default styled.style`
   .header__container {
     align-items: center;
     display: flex;
+    gap: var(--spacing_inset-md);
     height: 88px;
     justify-content: space-between;
+  }
+
+  .header__nav {
+    align-items: center;
+    background-color: var(--color-master-lightest);
+    display: none;
+    flex-direction: column;
+    gap: var(--spacing_inset-md);
+    height: calc(100vh - 89px);
+    left: 0;
+    padding: var(--spacing_inset-lg) var(--spacing_inset-xs);
+    position: absolute;
+    top: 89px;
+    width: 100%;
+  }
+
+  .header__nav[opened] {
+    display: flex;
+  }
+
+  @media (min-width: 648px) {
+    .header__nav {
+      background-color: transparent;
+      display: flex;
+      flex-direction: row;
+      height: auto;
+      left: auto;
+      padding: 0;
+      position: relative;
+      top: auto;
+      width: auto;
+    }
+  }
+
+  @media (min-width: 648px) {
+    .header__button {
+      display: none !important;
+    }
   }
 `
