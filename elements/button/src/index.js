c9bb3034df7e171841201f0aa20ccc1ec8392bcb
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-01 09:53:42 -0300

    feat(button): inclusao do botao do tipo naked

--- elements/button/src/index.js
+++ elements/button/src/index.js
@@ -1,11 +1,13 @@
 import * as f from '@kuba/f'
 import button from './button'
 import icon from './icon'
+import naked from './naked'
 
 export default new Proxy({}, {
   get: (_, type) =>
     f.cond(
       [icon.is, icon],
+      [naked.is, naked],
       [button.is, button]
     )(type)
 })
