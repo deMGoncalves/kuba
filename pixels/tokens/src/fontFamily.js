f1c93ada7b2df59e5dc87d9f25acbd6777b24f0a
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-01 15:39:50 -0300

    feat(tokens): inclusao da font mono spacado

--- pixels/tokens/src/fontFamily.js
+++ pixels/tokens/src/fontFamily.js
@@ -4,5 +4,6 @@ export default styled.style`
   :root {
     --font-family-highlight: 'Squada One', cursive;
     --font-family-base: 'Roboto', sans-serif;
+    --font-family-mono: 'Roboto Mono', sans-serif;
   }
 `
