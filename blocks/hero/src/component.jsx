06b0f83bf687022a01a4fb2f314639e2d0414c7e
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-01 13:20:02 -0300

    refactor(hero): estilizacao do layout

--- blocks/hero/src/component.jsx
+++ blocks/hero/src/component.jsx
@@ -1,11 +1,24 @@
-import h from '@kuba/h'
+import h, { Fragment } from '@kuba/h'
 import container from '@kuba/container'
+import icon from '@kuba/icon'
 import story from '@kuba/story'
+import text from '@kuba/text'
 import style from './style'
 
 export default (hero) =>
-  <story.Section className={style.hero} slot={hero.slot}>
-    <container.Div>
-      {hero.name}
-    </container.Div>
-  </story.Section>
+  <Fragment slot={hero.slot}>
+    <story.Section className={style.hero__story}>
+      <container.HGroup>
+        <text.H1 master darker lg medium>como uma folha em branco</text.H1>
+        <text.H2 master dark xs>a liberdade de programar do jeito que faca sentido para o produto</text.H2>
+      </container.HGroup>
+      <container.Div>
+        <div className={style.hero__div}>
+          <text.P master dark xxxs>https://github.com/deMGoncalves/kuba.git</text.P>
+          <button className={style.hero__button}>
+            <icon.Clone />
+          </button>
+        </div>
+      </container.Div>
+    </story.Section>
+  </Fragment>
--- blocks/hero/src/style.js
+++ blocks/hero/src/style.js
@@ -1,3 +1,31 @@
 import styled from '@kuba/styled'
 
-export default styled.style``
+export default styled.style`
+  .hero__story {
+    display: inline-flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-lg);
+    margin: var(--spacing-giant) 0;
+  }
+
+  .hero__div {
+    align-items: center;
+    border: var(--border-width-hairline) solid var(--color-master-darker);
+    border-radius: var(--border-radius-sm);
+    display: flex;
+    gap: var(--spacing_inset-xs);
+    height: 48px;
+    justify-content: space-between;
+    padding-left: var(--spacing_inset-xs);
+    width: 338px;
+  }
+
+  .hero__button {
+    background-color: transparent;
+    border: none;
+    border-left: var(--border-width-hairline) solid var(--color-master-darker);
+    cursor: pointer;
+    height: 46px;
+    width: 48px;
+  }
+`
