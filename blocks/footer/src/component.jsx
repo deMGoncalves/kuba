95be851dae8f21b9583a7fd3a9278fa7a3f1be91
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-02 15:14:28 -0300

    refactor(footer): separando o texto de descricao em um schema

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -2,12 +2,13 @@ import h from '@kuba/h'
 import container from '@kuba/container'
 import Logo from '@kuba/logo'
 import text from '@kuba/text'
+import schema from './schema.json'
 import style from './style'
 
 export default (footer) =>
   <footer className={style.footer}>
     <container.Div className={style.footer__container}>
       <Logo onClick={() => footer.redirectTo('home')} symbol />
-      <text.P className={style.footer__text} master dark xxxs>Simples, pequeno e imperfeito. Uma visao alem da programacao.</text.P>
+      <text.P className={style.footer__text} master dark xxxs>{schema.description}</text.P>
     </container.Div>
   </footer>
