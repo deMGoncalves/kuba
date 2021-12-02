f1d88bbb7b1251fe3933f5d83e149ac9b8559d51
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-02 10:40:09 -0300

    fix(header): erro no calculo de altura

--- blocks/header/src/style.js
+++ blocks/header/src/style.js
@@ -3,13 +3,14 @@ import styled from '@kuba/styled'
 export default styled.style`
   .header {
     border-bottom: var(--border-width-hairline) solid var(--color-master-light);
+    height: 88px;
   }
 
   .header__container {
     align-items: center;
     display: flex;
     gap: var(--spacing_inset-md);
-    height: 88px;
+    height: 100%;
     justify-content: space-between;
   }
 
