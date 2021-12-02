a3201cabbb62b24afb1b90cc3efb42c4fd17a622
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-02 11:04:43 -0300

    feat(footer): inclusao da descricao do kuba

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -1,11 +1,13 @@
 import h from '@kuba/h'
 import container from '@kuba/container'
 import Logo from '@kuba/logo'
+import text from '@kuba/text'
 import style from './style'
 
 export default () =>
   <footer className={style.footer}>
     <container.Div className={style.footer__container}>
       <Logo symbol />
+      <text.P master xxxs>Simples, pequeno e imperfeito. Uma visao alem da programacao</text.P>
     </container.Div>
   </footer>
