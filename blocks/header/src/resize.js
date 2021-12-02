00b86235b14474dd6b366d349706b6183bc70f18
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-02 14:23:17 -0300

    fix(header): o resize close so pode ser disparado quando o menu estiver aberto

--- blocks/header/src/resize.js
+++ blocks/header/src/resize.js
@@ -2,6 +2,7 @@ import media from '@kuba/media'
 import middleware from '@kuba/middleware'
 
 const resize = (header) =>
-  media('(min-width: 648px)').in(() => header.close())
+  media('(min-width: 648px)')
+    .in(() => header.opened && header.close())
 
 export default middleware(resize)
