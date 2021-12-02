c6bfc5d5b0d52de22a76b5e0cf6196a714f69279
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-02 11:50:46 -0300

    refactor(footer): correcao do texto do bordao

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -8,6 +8,6 @@ export default () =>
   <footer className={style.footer}>
     <container.Div className={style.footer__container}>
       <Logo symbol />
-      <text.P className={style.footer__text} master dark xxxs>Simples, pequeno e imperfeito. Uma visao alem da programacao</text.P>
+      <text.P className={style.footer__text} master dark xxxs>Simples, pequeno e imperfeito. Uma visao alem da programacao.</text.P>
     </container.Div>
   </footer>
