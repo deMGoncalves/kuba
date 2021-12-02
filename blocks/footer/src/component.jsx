be29db893261a05873287a4ae8eb53e2c1bb0f54
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-02 10:39:00 -0300

    feat(footer): inclusao do simbolo

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -1,10 +1,11 @@
 import h from '@kuba/h'
 import container from '@kuba/container'
+import Logo from '@kuba/logo'
 import style from './style'
 
-export default (footer) =>
+export default () =>
   <footer className={style.footer}>
     <container.Div className={style.footer__container}>
-      {footer.name}
+      <Logo symbol />
     </container.Div>
   </footer>
