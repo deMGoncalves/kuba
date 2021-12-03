cfcbddf59c8edb833bf2c5514299f4f38d5d76fa
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-03 11:39:35 -0300

    feat(about): primeiro paragrafo

--- pages/about/src/component.jsx
+++ pages/about/src/component.jsx
@@ -1,5 +1,32 @@
-import h from '@kuba/h'
+import h, { Fragment } from '@kuba/h'
+import container from '@kuba/container'
+import text from '@kuba/text'
+import story from '@kuba/story'
 import Site from '@kuba/site'
+import style from './style'
 
 export default () =>
-  <Site />
+  <Site>
+    <Fragment slot='hot'>
+      <story.Article className={style.about__article}>
+        <container.HGroup className={style.about__hgroup}>
+          <text.H1 master darker xl highlight medium>o kuba</text.H1>
+          <text.H2 master xxxs>simples, pequeno e imperfeito, uma visao alem da programacao</text.H2>
+        </container.HGroup>
+        <container.Section className={style.about__section}>
+          <text.P master dark xs>
+            ninguem precisa ter muitas habilidades para desenvolver um produto que funcione, fazer direito eh outra história. desenvolver um produto de maneira correta eh um desafio que deixo lancado. requer maturidade e conhecimentos que a maioria dos programadores ainda nao tem.
+          </text.P>
+          <text.P master dark xs>
+            quando focamos no dominio, minimizamos a quantidade de programadores para desenvolver e sustentar um produto.
+          </text.P>
+          <text.P master dark xs>
+            as mudancas se tornam cirurgicas, simples e faceis. deixamos de ser eficiente e nos tornamos eficazes.
+          </text.P>
+          <text.P master dark xs>
+            as funcionalidades e flexibilidades sao maximizadas.
+          </text.P>
+        </container.Section>
+      </story.Article>
+    </Fragment>
+  </Site>
--- pages/about/src/style.js
+++ pages/about/src/style.js
@@ -1,3 +1,24 @@
 import styled from '@kuba/styled'
 
-export default styled.style``
+export default styled.style`
+  .about__article {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-lg);
+  }
+
+  .about__hgroup h1,
+  .about__hgroup h2 {
+    line-height: var(--line-height-md) !important;
+  }
+
+  .about__section {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-xs);
+  }
+
+  .about__section p {
+    line-height: var(--line-height-md) !important;
+  }
+`
