d7d1d78058f56f5c2db35c50eb086f3ff7c1865e
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-03 11:08:43 -0300

    feat(app): configuracao da rota para about

--- buckets/app/src/routers.jsx
+++ buckets/app/src/routers.jsx
@@ -6,3 +6,8 @@ router('/', async function home () {
   const { default: Home } = await import('@kuba/home' /* webpackChunkName: "home" */)
   render(<Home />)
 })
+
+router('/about', async function about () {
+  const { default: About } = await import('@kuba/about' /* webpackChunkName: "about" */)
+  render(<About />)
+})
