aa183a597d60a58ccb37deca8bc2920c132c7d15
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-06 09:34:53 -0300

    refactor(about): estruturando o texto

--- pages/about/src/component.jsx
+++ pages/about/src/component.jsx
@@ -11,20 +11,14 @@ export default () =>
       <story.Article className={style.about__article}>
         <container.Section className={style.about__section}>
           <hgroup className={style.about__hgroup}>
-            <text.H1 master darker xxl highlight medium>o kuba</text.H1>
+            <text.H1 master darker xxl bold>o kuba</text.H1>
             <text.H2 master xxxs>simples, pequeno e imperfeito, uma visao alem da programacao</text.H2>
           </hgroup>
           <text.P master dark xs>
             ninguem precisa ter muitas habilidades para desenvolver um produto que funcione, fazer direito eh outra história. desenvolver um produto de maneira correta eh um desafio que deixo lancado. requer maturidade e conhecimentos que a maioria dos programadores ainda nao tem.
           </text.P>
           <text.P master dark xs>
-            quando focamos no dominio, minimizamos a quantidade de programadores para desenvolver e sustentar um produto.
-          </text.P>
-          <text.P master dark xs>
-            as mudancas se tornam cirurgicas, simples e faceis. deixamos de ser eficiente e nos tornamos eficazes.
-          </text.P>
-          <text.P master dark xs>
-            as funcionalidades e flexibilidades sao maximizadas.
+            quando focamos no dominio, minimizamos a quantidade de programadores para desenvolver e sustentar um produto. as mudancas se tornam cirurgicas, simples e faceis. deixamos de ser eficiente e nos tornamos eficazes. as funcionalidades e flexibilidades sao maximizadas.
           </text.P>
           <text.P master dark xs>
             criei este projeto de referencia como a unica fonte da verdade para todos os processos e crencas. como qualquer ferramenta ou metodologia, ela so funcionara corretamente quando usada da maneira certa. e o "certo" so pode ser determinado por meio de um processo de polimento e pratica constantes.
@@ -32,7 +26,7 @@ export default () =>
         </container.Section>
         <container.Section className={style.about__section}>
           <hgroup className={style.about__hgroup}>
-            <text.H2 master darker lg highlight medium>objetivo</text.H2>
+            <text.H2 master darker lg bold>objetivo</text.H2>
           </hgroup>
           <text.P master dark xs>
             o kuba nao eh uma vontade recente, o plano de ter uma arquitetura criado a partir das minhas experiencias e metodologias eh um sonho que comecou em marco de 2016. uni essa vontade com o momento profissional que estou vivendo para unir o util ao agradavel. por isso, este projeto acumula alguns objetivos bastante transparentes.
@@ -40,7 +34,7 @@ export default () =>
         </container.Section>
         <container.Section className={style.about__section}>
           <hgroup className={style.about__hgroup}>
-            <text.H3 master darker sm highlight medium>apoio ao aprendizado</text.H3>
+            <text.H3 master darker sm bold>apoio ao aprendizado</text.H3>
           </hgroup>
           <text.P master dark xs>
             o primeiro e, talvez maior, papel deste projeto eh servir como apoio aos meus estudos, dos meus colegas de trabalho e de qualquer pessoa interessada, tornando nossos conceitos mais acessiveis e fáceis de compreender atraves de um exemplo pratico.
@@ -48,7 +42,7 @@ export default () =>
         </container.Section>
         <container.Section className={style.about__section}>
           <hgroup className={style.about__hgroup}>
-            <text.H4 master darker sm highlight medium>piloto para versão em rust</text.H4>
+            <text.H4 master darker sm bold>piloto para versão em rust</text.H4>
           </hgroup>
           <text.P master dark xs>
             estou trabalhando para a versao deste projeto totalmente escrito em rust (hoje eh 100% em javascript), a ideia eh que esta arquitetura seja um grande facilitador para o desenvolvedor. e claro, naturalmente ter um boilerplate aberto ao publico tambem ajuda a divulgar minhas metodologia de codificacao.
