3d2c86a3a79973a47c192fcd6ca3e439e1015220
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-08 07:34:50 -0300

    refactor(lang): nao preciso adaptar o settings

--- infra/lang/src/getValue.js
+++ infra/lang/src/getValue.js
@@ -1,6 +1,6 @@
 import * as f from '@kuba/f'
+import * as settings from '@kuba/settings'
 import cookie from '@kuba/cookie'
-import settings from './settings'
 
 export default () =>
-  f.or(cookie.getItem('lang'), settings.fallback)
+  f.or(cookie.getItem('lang'), settings.lang.fallback)
