9707c5677cfdc315db7d8027e143c72439376f38
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-09 13:22:29 -0300

    refactor(avatar): controle do tamanho do avatar

--- elements/avatar/src/icon.js
+++ elements/avatar/src/icon.js
@@ -7,6 +7,18 @@ import merge from './merge'
 const style = styled.style`
   .avatar {
     border-radius: var(--border-radius-circular);
+    height: 40px;
+    width: 40px;
+  }
+
+  .avatar[small] {
+    height: 24px;
+    width: 24px;
+  }
+
+  .avatar[large] {
+    height: 64px;
+    width: 64px;
   }
 `
 
