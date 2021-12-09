bde1741b1bf6797400b4effec1f2d5bacdb26431
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-09 13:15:25 -0300

    refactor(icon): precisamos redefinir o alt padrao para os icones

--- elements/icon/src/icon.jsx
+++ elements/icon/src/icon.jsx
@@ -1,4 +1,6 @@
 import h from '@kuba/h'
+import * as f from '@kuba/f'
+import * as settings from '@kuba/settings'
 import * as mapper from './mapper'
 import merge from './merge'
 import style from './style'
@@ -6,5 +8,5 @@ import style from './style'
 export default new Proxy({}, {
   get: (_, name) =>
     (props) =>
-      <img className={[style.icon, props.className]} src={mapper[merge(props, name)]} loading='lazy' alt={props.alt} small:isTruthy={props.small} large:isTruthy={props.large} />
+      <img className={[style.icon, props.className]} src={mapper[merge(props, name)]} loading='lazy' alt={f.or(props.alt, settings.app.name)} small:isTruthy={props.small} large:isTruthy={props.large} />
 })
