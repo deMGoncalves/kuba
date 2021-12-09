49f589dce4817d396c20cc997e292ccce4107e7b
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-09 14:39:31 -0300

    feat(app): inclusao do sidebar no layout

--- models/app/src/component.jsx
+++ models/app/src/component.jsx
@@ -1,10 +1,13 @@
 import h, { Fragment } from '@kuba/h'
 import Header from '@kuba/header'
+import Sidebar from '@kuba/sidebar'
 import style from './style'
 
 export default (app, children) =>
   <>
-    <aside className={style.app__aside} />
+    <aside className={style.app__aside}>
+      <Sidebar />
+    </aside>
     <main className={style.app__main}>
       <Header fluid />
       {children}
