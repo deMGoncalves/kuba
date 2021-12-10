311aeddc6e84f9c3dc0514da6a904374948e0034
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-10 12:17:41 -0300

    feat(sidebar): inclusao do container para delimitar o corpo do sidebar

--- blocks/sidebar/src/component.jsx
+++ blocks/sidebar/src/component.jsx
@@ -1,4 +1,5 @@
 import h, { Fragment } from '@kuba/h'
+import container from '@kuba/container'
 import Header from '@kuba/header'
 import style from './style'
 
@@ -6,6 +7,7 @@ export default (sidebar) =>
   <>
     <aside className={style.sidebar} onMouseenter={() => sidebar.open()} onMouseleave={() => sidebar.close()} opened:isTruthy={sidebar.opened}>
       <Header className={style.sidebar__header} fixed />
+      <container.Main />
     </aside>
     <div className={style.sidebar__div} onClick={() => sidebar.close()} opened:isTruthy={sidebar.opened} />
   </>
