9260dc9fdf309f6e5f24634c49aadf3fc9c9a7bf
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-10 10:35:22 -0300

    refactor(logo): nao preciso usar o isTruthy

--- elements/logo/src/component.jsx
+++ elements/logo/src/component.jsx
@@ -2,4 +2,4 @@ import h from '@kuba/h'
 import style from './style'
 
 export default (logo) =>
-  <img className={[style.logo, logo.className]} src={logo.url} loading='lazy' alt={logo.name} onClick={logo.onClick} small:isTruthy={logo.small} large:isTruthy={logo.large} />
+  <img className={[style.logo, logo.className]} src={logo.url} loading='lazy' alt={logo.name} onClick={logo.onClick} small={logo.small} large={logo.large} />
