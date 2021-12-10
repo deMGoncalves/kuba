7a3e97e0b7d9bbc563404d14741c459ff978f38b
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-10 11:13:50 -0300

    refactor(sidebar): os eventos de scroll so deve ocorre no mobile

--- blocks/sidebar/src/scroll.js
+++ blocks/sidebar/src/scroll.js
@@ -1,15 +1,23 @@
+import * as f from '@kuba/f'
 import echo from '@kuba/echo'
+import media from '@kuba/media'
 import { after } from '@kuba/middleware'
 
+var mobile = f.T()
+
 const lock = after(function (output) {
-  echo.emit('overlayer:open')
+  mobile && echo.emit('overlayer:open')
   return output
 })
 
 const unlock = after(function (output) {
-  echo.emit('overlayer:close')
+  mobile && echo.emit('overlayer:close')
   return output
 })
+
+media('(min-width: 769px)')
+  .in(() => mobile = f.F())
+  .out(() => mobile = f.T())
 
 export default {
   lock,
