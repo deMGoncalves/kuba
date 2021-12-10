2673143a96310ffb278ad6eef9b40a3d90f924ec
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-10 10:49:17 -0300

    style(sidebar): elevando para o nivel 2, para respeitar o header

--- blocks/sidebar/src/style.js
+++ blocks/sidebar/src/style.js
@@ -12,7 +12,7 @@ export default styled.style`
       opacity 618ms cubic-bezier(0.77, 0, 0.175, 1),
       transform 618ms cubic-bezier(0.77, 0, 0.175, 1);
     width: 298px;
-    z-index: 1;
+    z-index: 2;
   }
 
   .sidebar[opened] {
@@ -46,6 +46,6 @@ export default styled.style`
     position: fixed;
     right: 0;
     width: calc(100% - 298px);
-    z-index: 1;
+    z-index: 2;
   }
 `
