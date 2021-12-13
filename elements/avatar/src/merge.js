232412012efc5b122971dbb3ebafe10d8cc52ee0
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-13 12:25:44 -0300

    fix(avatar): definicao do valor e nao do boolean

--- elements/avatar/src/merge.js
+++ elements/avatar/src/merge.js
@@ -1,2 +1,2 @@
 export default (props, style) =>
-  ({ ...props, className: [style.avatar, style[props.small], style[props.large], props.className] })
+  ({ ...props, className: [style.avatar, style[props.small && 'small'], style[props.large && 'large'], props.className] })
