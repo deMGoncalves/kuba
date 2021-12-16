34374d6f20276ff41952d5d4dd0d4fbe6ab9c87a
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-16 12:46:04 -0300

    refactor(header): configuracao da navegacao

--- blocks/header/src/nav/component.jsx
+++ blocks/header/src/nav/component.jsx
@@ -9,11 +9,11 @@ export default (nav) =>
   <>
     <nav className={style.nav} opened:isTruthy={nav.opened}>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('about')} darker medium>about</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.edirectTo('values')} darker medium>values</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.edirectTo('design')} darker medium>design</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.edirectTo('arquiteture')} darker medium>architecture</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.edirectTo('components')} darker medium>components</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.edirectTo('patterns')} darker medium>patterns</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('values')} darker medium>values</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('design')} darker medium>design</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('arquiteture')} darker medium>architecture</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('components')} darker medium>components</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('patterns')} darker medium>patterns</link.Master>
     </nav>
     <Hide when={nav.opened}>
       <button.icon.Bars className={style.nav__button} onClick={() => nav.open()} />
