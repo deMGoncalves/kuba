f12b55d47de97933fc0303a7c6c98be4ac9c7aa8
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-21 09:09:32 -0300

    feat(h): com esta class vou usalo para mapear os children que forem entity

---
+++ standard/h/src/entity.js
@@ -0,0 +1,13 @@
+import * as f from '@kuba/f'
+
+class Entity {
+  static is (target) {
+    return f.has(f.magic('tag'), target)
+  }
+
+  static transform (entity) {
+    return entity[f.magic('tag')]
+  }
+}
+
+export default Entity
