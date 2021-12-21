40188a1b5f60036cbd147eab71606e6556e117ef
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-21 10:42:18 -0300

    refactor(h): mapeando as entity nos children

--- standard/h/src/tag/children/mapper.js
+++ standard/h/src/tag/children/mapper.js
@@ -1,6 +1,9 @@
 import * as f from '@kuba/f'
-import { Text } from '@kuba/h'
+import { Entity, Text } from '@kuba/h'
 
 export default (children) =>
-  f.map(children, (child) =>
-    Text.is(child) ? Text.create(child) : child)
+  f.map(children, f.cond(
+    [Entity.is, Entity.transform],
+    [Text.is, Text.create],
+    [f.T, (c) => c]
+  ))
