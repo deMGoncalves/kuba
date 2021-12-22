2f8d18281a9db16fd577e3b6138e0771ff571978
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-22 09:35:46 -0300

    refactor(h): mantendo o padrao que usado no mapper do children

--- standard/h/src/render.js
+++ standard/h/src/render.js
@@ -1,6 +1,6 @@
 import * as f from '@kuba/f'
 
 const render = (element, ...children) =>
-  element.append(...f.map(children, child => child.paint()))
+  element.append(...f.map(children, c => c.paint()))
 
 export default f.curry(f.arity(2, f.frame(render)))
