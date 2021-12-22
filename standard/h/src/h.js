ec6b77e9bacb0ed7e8cf638aedb9b4a0c106fcad
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-22 09:35:03 -0300

    refactor(h): padronizacao da taxonomia do h

--- standard/h/src/h.js
+++ standard/h/src/h.js
@@ -2,9 +2,9 @@ import * as f from '@kuba/f'
 import Component from './component'
 import Tag, { Custom } from './tag'
 
-export default (tagNameOrComponent, props, ...children) =>
+export default (target, props, ...children) =>
   f.cond(
     [Tag.is, Tag.create],
     [Component.is, Component.execute],
     [Custom.is, Custom.create]
-  )(tagNameOrComponent, { ...props }, f.flatten(children))
+  )(target, { ...props }, f.flatten(children))
