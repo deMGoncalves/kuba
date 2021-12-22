0877a35cb9f3ae2f7545c35c2c7f2d2296421d55
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-22 09:39:06 -0300

    refactor(h): nao precisamos fazer a verificacao se o dunder method reflow exite

--- standard/h/src/repaint.js
+++ standard/h/src/repaint.js
@@ -2,7 +2,7 @@ import * as f from '@kuba/f'
 import { after } from '@kuba/middleware'
 
 function repaint (output) {
-  this?.[f.magic('reflow')]?.()
+  this[f.magic('reflow')]()
   return output
 }
 
