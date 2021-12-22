204c9069982ed33cb8c0770ed2a8cb1ee80c9084
deMGoncalves <cleber.engineer@hotmail.com>
2021-12-22 15:19:12 -0300

    refactor(header): troca do link para o logo

--- blocks/header/src/logo/component.jsx
+++ blocks/header/src/logo/component.jsx
@@ -2,4 +2,4 @@ import h from '@kuba/h'
 import Logo from '@kuba/logo'
 
 export default (logo) =>
-  <Logo onClick={() => logo.redirectTo('home')} />
+  <Logo onClick={() => logo.redirectTo('skateboard')} />
