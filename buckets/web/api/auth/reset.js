12d97083309049eac7e97d315f8a8914b7cd5b37
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-12-23 13:31:31 -0300

    feat(web): criacao das primeiras api para auth

---
+++ buckets/web/api/auth/reset.js
@@ -0,0 +1,9 @@
+import supabase from '@kuba/supabase'
+
+export default async function (request, response) {
+  const { authorization } = request.headers
+  const { password } = JSON.parse(request.body)
+  const { data, error } = await supabase.auth.api.updateUser(authorization, { password })
+
+  response.json({ data, error })
+}
---
+++ buckets/web/api/auth/user.js
@@ -0,0 +1,8 @@
+import supabase from '@kuba/supabase'
+
+export default async function (request, response) {
+  const { authorization } = request.headers
+  const { data, error } = await supabase.auth.api.getUser(authorization)
+
+  response.json({ data, error })
+}
