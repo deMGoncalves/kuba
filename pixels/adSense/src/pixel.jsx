670baf676485c960a725126b8174b8bc97091da1
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-12-23 21:27:05 -0300

    refactor(adsense): usando o settings para configurar o adsense

--- pixels/adSense/src/pixel.jsx
+++ pixels/adSense/src/pixel.jsx
@@ -2,8 +2,11 @@ import h, { render } from '@kuba/h'
 import * as f from '@kuba/f'
 import agent from '@kuba/agent'
 import env from '@kuba/env'
-import schema from './schema.json'
+import { adSense } from '@kuba/settings'
 
 f.and(agent.isUser, env.isProd) && (
-  render(document.head, <script data-ad-client={schema.adClient} src={schema.src} async />)
+  render(
+    document.head,
+    <script data-ad-client={adSense.adClient} src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' async />
+  )
 )
