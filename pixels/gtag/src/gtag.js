b40b447d84db971104d23290687a1d1386eeeb3a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-12-23 21:00:09 -0300

    refactor(gtag): nao vamos mais trabalhar com o gtag

--- pixels/gtag/src/gtag.js
+++
@@ -1,6 +0,0 @@
-import dataLayer from './dataLayer'
-
-window.gtag = (...args) =>
-  dataLayer.push(args)
-
-export default window.gtag
--- pixels/gtag/src/index.js
+++
@@ -1,4 +0,0 @@
-import './pixel'
-
-export { default as gtag } from './gtag'
-export { default as dataLayer } from './dataLayer'
