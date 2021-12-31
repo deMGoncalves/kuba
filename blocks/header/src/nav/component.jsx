ec1aa3e48b296931c5e967dbf86fef9f92e31d63
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-12-30 23:53:34 -0300

    feat(header): inclusao de todos os items para montar um skate

--- blocks/header/src/nav/component.jsx
+++ blocks/header/src/nav/component.jsx
@@ -10,11 +10,13 @@ export default (nav) =>
     <nav className={style.nav} opened:isTruthy={nav.opened}>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('shape')} darker medium xxs>shape</link.Master>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('lixa')} darker medium xxs>lixa</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('parafusos')} darker medium xxs>parafusos</link.Master>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('truck')} darker medium xxs>truck</link.Master>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('cushionSet')} darker medium xxs>cushion set</link.Master>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('pads')} darker medium xxs>pads</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('roda')} darker medium xxs>roda</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('rolamento')} darker medium xxs>rolamento</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('rodas')} darker medium xxs>roda</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('rolamentos')} darker medium xxs>rolamentos</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('chave')} darker medium xxs>chave</link.Master>
     </nav>
     <Hide when={nav.opened}>
       <button.icon.Bars className={style.nav__button} onClick={() => nav.open()} />
