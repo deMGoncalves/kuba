c01fcebfb9c4c5b4c3ca927177d3231529efc6a3
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-12-31 02:07:14 -0300

    chore(api): montegem do select para shelf dos shapes

--- buckets/web/api/shape/shelf.js
+++ buckets/web/api/shape/shelf.js
@@ -3,7 +3,22 @@ import supabase from '@kuba/supabase'
 export default async function (_request, response) {
   const { data, error } = await supabase
     .from('shape')
-    .select('*')
+    .select(`
+      id,
+      tipo (valor),
+      marca (nome, descricao, site, origem (valor)),
+      tamanho (valor),
+      material (valor),
+      wheelbase (valor) as wheelbase,
+      montagem (valor),
+      flag (valor),
+      lixa,
+      nose,
+      tail,
+      concave,
+      modelo,
+      laminas
+    `)
 
   response.json({ data, error })
 }
