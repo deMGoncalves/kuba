5bda40adcb5182d3ef83b47a4e2d7915ef76753c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-12-30 23:19:30 -0300

    feat(header): incluso de dois novos link para montagem do skate

--- blocks/header/src/nav/component.jsx
+++ blocks/header/src/nav/component.jsx
@@ -11,8 +11,10 @@ export default (nav) =>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('shape')} darker medium xxs>shape</link.Master>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('lixa')} darker medium xxs>lixa</link.Master>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('truck')} darker medium xxs>truck</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('rolamento')} darker medium xxs>rolamento</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('cushionSet')} darker medium xxs>cushion set</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('pads')} darker medium xxs>pads</link.Master>
       <link.Master className={style.nav__link} onClick={() => nav.redirectTo('roda')} darker medium xxs>roda</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('rolamento')} darker medium xxs>rolamento</link.Master>
     </nav>
     <Hide when={nav.opened}>
       <button.icon.Bars className={style.nav__button} onClick={() => nav.open()} />
