9ba48fead93a07fb2422587c28f626ce2318430b
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2021-12-31 01:19:59 -0300

    style(web): estilizacao da escala das imagens para exibir os shapes

--- buckets/web/src/shape/shelf/shape/component.jsx
+++ buckets/web/src/shape/shelf/shape/component.jsx
@@ -1,9 +1,10 @@
 import h, { Fragment } from '@kuba/h'
 import Figure from '@kuba/figure'
 import text from '@kuba/text'
+import style from './style'
 
 export default (shape) =>
-  <Figure>
+  <Figure className={style.shape}>
     <Fragment slot='caption'>
       <text.Strong master darker xxs medium>{shape.modelo}</text.Strong>
     </Fragment>
--- buckets/web/src/shape/shelf/shape/style.js
+++ buckets/web/src/shape/shelf/shape/style.js
@@ -1,3 +1,7 @@
 import styled from '@kuba/styled'
 
-export default styled.style``
+export default styled.style`
+  .shape picture {
+    padding-top: calc(4 / 3 * 100%) !important;
+  }
+`
