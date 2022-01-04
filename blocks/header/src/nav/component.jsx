acad711b33d8259514663a19b9e8507d0518f987
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-04 09:49:10 -0300

    refactor(header): so vou trabalhar com shapes e marcas por hora

--- blocks/header/src/nav/component.jsx
+++ blocks/header/src/nav/component.jsx
@@ -8,15 +8,8 @@ import style from './style'
 export default (nav) =>
   <>
     <nav className={style.nav} opened:isTruthy={nav.opened}>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('shape')} darker medium xxs>shape</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('lixa')} darker medium xxs>lixa</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('parafusos')} darker medium xxs>parafusos</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('truck')} darker medium xxs>truck</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('cushionSet')} darker medium xxs>cushion set</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('pads')} darker medium xxs>pads</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('rodas')} darker medium xxs>rodas</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('rolamentos')} darker medium xxs>rolamentos</link.Master>
-      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('chave')} darker medium xxs>chave</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('shape')} darker medium xxs>shapes</link.Master>
+      <link.Master className={style.nav__link} onClick={() => nav.redirectTo('marca')} darker medium xxs>marcas</link.Master>
     </nav>
     <Hide when={nav.opened}>
       <button.icon.Bars className={style.nav__button} onClick={() => nav.open()} />
