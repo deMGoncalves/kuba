4ae0cc0c96f016f7061090987a5ae26127c2d20a
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-04 15:51:28 -0300

    chore(marcas): alinhando o conteudo do shelf

--- buckets/web/src/marca/shelf/component.jsx
+++ buckets/web/src/marca/shelf/component.jsx
@@ -1,5 +1,11 @@
 import h from '@kuba/h'
+import container from '@kuba/container'
+import story from '@kuba/story'
 import style from './style'
 
 export default (shelf) =>
-  <div className={style.shelf}>{shelf.name}</div>
+  <story.Section>
+    <container.Div>
+      <div className={style.shelf}>{shelf.name}</div>
+    </container.Div>
+  </story.Section>
