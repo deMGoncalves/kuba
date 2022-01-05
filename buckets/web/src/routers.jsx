ffdfd3c112a783e63db7d60e88094183658ffc1a
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-05 11:24:30 -0300

    chore(router): configuracoes das rotas

--- buckets/web/src/routers.jsx
+++ buckets/web/src/routers.jsx
@@ -2,12 +2,22 @@ import h from '@kuba/h'
 import router from '@kuba/router'
 import render from './render'
 
-router('/', async function shape () {
-  const { default: Shape } = await import('./shape' /* webpackChunkName: "shape" */)
-  render(<Shape />)
+router('/', async function shapes () {
+  const { default: Shapes } = await import('./shape' /* webpackChunkName: "shapes" */)
+  render(<Shapes />)
 })
 
-router('/marcas', async function marca () {
+router('/marcas', async function marcas () {
+  const { default: Marcas } = await import('./marca' /* webpackChunkName: "marcas" */)
+  render(<Marcas />)
+})
+
+router('/:marca', async function marca () {
   const { default: Marca } = await import('./marca' /* webpackChunkName: "marca" */)
   render(<Marca />)
+})
+
+router('/:marca/:shape', async function shape () {
+  const { default: Shape } = await import('./shape' /* webpackChunkName: "shape" */)
+  render(<Shape />)
 })
