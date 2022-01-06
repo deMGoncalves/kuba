7928f6111eab9149a72eb77fadd5e1b1fdf8ebbd
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-06 16:09:18 -0300

    feat(api): busca de shape pelo slug

---
+++ buckets/web/api/shape/slug.js
@@ -0,0 +1,20 @@
+import supabase from '@kuba/supabase'
+
+export default async function (request, response) {
+  const { slug } = JSON.parse(request.body)
+  const { data, error } = await supabase
+    .from('shape')
+    .select(`
+      *,
+      tipo (*),
+      marca (*, origem (*)),
+      tamanho (*),
+      material (*),
+      wheelbase (*),
+      montagem (*),
+      flag (*)
+    `)
+    .eq('slug', slug)
+
+  response.json({ data, error })
+}
