87270d5cc6b21c621fae3ae796a58a5a92c52ac1
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-07 15:52:44 -0300

    chore(jsonld): removendo a validaca do dado estruturado

--- pixels/jsonld/src/render.jsx
+++ pixels/jsonld/src/render.jsx
@@ -1,23 +1,19 @@
 import h, { render } from '@kuba/h'
 import * as f from '@kuba/f'
-import agent from '@kuba/agent'
-import env from '@kuba/env'
 import graph from './graph'
 
 const jsonld = () =>
-  f.and(agent.isBot, env.isProd) && (
-    render(
-      document.body,
-      <script type='application/ld+json'>
-        {
-          JSON.stringify({
-            '@context': 'https://schema.org',
-            '@graph': f.filter(graph, f.compose(f.not, f.isEmpty)),
-            '@type': 'Schema'
-          })
-        }
-      </script>
-    )
+  render(
+    document.body,
+    <script type='application/ld+json'>
+      {
+        JSON.stringify({
+          '@context': 'https://schema.org',
+          '@graph': f.filter(graph, f.compose(f.not, f.isEmpty)),
+          '@type': 'Schema'
+        })
+      }
+    </script>
   )
 
 export default f.debounce(jsonld)
