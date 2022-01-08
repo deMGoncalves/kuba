68bcc48ea259fb406ecd0054b0788b783fab2e80
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-08 14:34:43 -0300

    chore(logo): para o lazy funcionar preciso defini height e width

--- elements/logo/src/component.jsx
+++ elements/logo/src/component.jsx
@@ -1,5 +1,17 @@
 import h from '@kuba/h'
 import style from './style'
+import height from './height'
+import width from './width'
 
 export default (logo) =>
-  <img className={[style.logo, logo.className]} src={logo.url} loading='lazy' alt={logo.name} onClick={logo.onClick} small={logo.small} large={logo.large} />
+  <img
+    alt={logo.name}
+    className={[style.logo, logo.className]}
+    height={height(logo)}
+    large={logo.large}
+    loading='lazy'
+    onClick={logo.onClick}
+    small={logo.small}
+    src={logo.url}
+    width={width(logo)}
+  />
---
+++ elements/logo/src/width.js
@@ -0,0 +1,7 @@
+import * as f from '@kuba/f'
+
+export default f.cond(
+  [f.has('small'), f.always('43.85px')],
+  [f.has('large'), f.always('109.633px')],
+  [f.T, f.always('76.75px')]
+)
