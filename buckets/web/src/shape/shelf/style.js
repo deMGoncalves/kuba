039cf116b8e823156498b5ae17252aa283b6a640
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-09 16:44:33 -0300

    chore(shape): tornando a página responsiva

--- buckets/web/src/shape/shelf/style.js
+++ buckets/web/src/shape/shelf/style.js
@@ -3,6 +3,13 @@ import styled from '@kuba/styled'
 export default styled.style`
   .shelf {
     display: flex;
+    flex-direction: column;
     gap: var(--spacing_inset-lg);
+  }
+
+  @media (min-width: 769px) {
+    .shelf {
+      flex-direction: row;
+    }
   }
 `
--- buckets/web/src/shape/shelf/thumbnail/style.js
+++ buckets/web/src/shape/shelf/thumbnail/style.js
@@ -1,6 +1,11 @@
 import styled from '@kuba/styled'
 
 export default styled.style`
+  .shelf__picture {
+    display: flex;
+    justify-content: center;
+  }
+
   .shelf__picture img {
     height: 383px;
     width: 287px;
