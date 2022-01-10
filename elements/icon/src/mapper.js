255c800e96d52591030f36cde70e2d6143b15ec9
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-10 16:08:29 -0300

    chore(icon): icone para fazer comparacao de shapes

--- elements/icon/src/mapper.js
+++ elements/icon/src/mapper.js
@@ -1,3 +1,3 @@
 export { default as Close } from './assets/close.svg'
-export { default as CloseOnColor } from './assets/close_onColor.svg'
 export { default as Bars } from './assets/bars.svg'
+export { default as Compare } from './assets/compare.svg'
