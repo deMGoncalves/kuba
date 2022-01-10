5e8fb02ed32c3ed1fb3012ad97e5ca502b9d99d5
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-10 13:06:41 -0300

    chore(container): hoje temos muito pouco conteudo para ter uma layout largo

--- elements/container/src/container.js
+++ elements/container/src/container.js
@@ -4,7 +4,7 @@ export default new Proxy({}, {
   get: (_, tag) =>
     styled[tag]`
       margin: 0 auto;
-      max-width: ${(props) => props.fluid ? 'auto' : '1230px'};
+      max-width: ${(props) => props.fluid ? 'auto' : '987px'};
       padding: 0 var(--spacing_inset-xs);
       position: relative;
       width: 100%;
