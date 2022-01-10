6a971539f8844750c35d486897cd52fb5d334d48
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-10 00:01:42 -0300

    chore(shape): centralizando imagem no mobile

--- buckets/web/src/shape/thumbnail/style.js
+++ buckets/web/src/shape/thumbnail/style.js
@@ -1,6 +1,17 @@
 import styled from '@kuba/styled'
 
 export default styled.style`
+  .thumbnail {
+    display: flex;
+    justify-content: center;
+  }
+
+  @media (min-width: 769px) {
+    .thumbnail {
+      justify-content: flex-start;
+    }
+  }
+
   .thumbnail img {
     height: 383px;
     width: 287px;
