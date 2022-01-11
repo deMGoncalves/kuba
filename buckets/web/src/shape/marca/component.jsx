d1034f1725334cfb4d243bb773e516dd0eb049f2
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-11 13:29:09 -0300

    style(shape): padronizando o tamanho do shape

--- buckets/web/src/shape/marca/component.jsx
+++ buckets/web/src/shape/marca/component.jsx
@@ -2,4 +2,4 @@ import h from '@kuba/h'
 import text from '@kuba/text'
 
 export default (marca) =>
-  <text.Strong master darker xxs medium>{marca.nome}</text.Strong>
+  <text.Strong master darker xxxs medium>{marca.nome}</text.Strong>
