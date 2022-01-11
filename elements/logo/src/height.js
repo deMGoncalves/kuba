da44811d6f4d1877e159f0e35d68938b2520976a
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-11 11:29:05 -0300

    chore(logo): para o symbol eu nao preciso incluir o height e width

--- elements/logo/src/height.js
+++ elements/logo/src/height.js
@@ -1,6 +1,7 @@
 import * as f from '@kuba/f'
 
 export default f.cond(
+  [f.has('symbol'), f.always(undefined)],
   [f.has('small'), f.always('18px')],
   [f.has('large'), f.always('40px')],
   [f.T, f.always('28px')]
