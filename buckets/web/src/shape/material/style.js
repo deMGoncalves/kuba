cea695f0bc2ffad93b7c22862f1f2a7dd9635cfe
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-11 18:20:26 -0300

    chore(material): reescrevendo uma tag para os materiais

--- buckets/web/src/shape/material/style.js
+++ buckets/web/src/shape/material/style.js
@@ -5,4 +5,22 @@ export default styled.style`
     display: flex;
     gap: var(--spacing_inset-xh);
   }
+
+  .material__tag {
+    align-items: center;
+    background-color: var(--color-master-darker);
+    border: none;
+    border-radius: var(--border-radius-pill);
+    color: var(--color-master-lightest);
+    cursor: pointer;
+    display: inline-flex;
+    font-family: var(--font-family-base);
+    font-size: var(--font-size-xxxs);
+    font-weight: var(--font-weight-medium);
+    height: 26px;
+    justify-content: center;
+    letter-spacing: 0.618px;
+    padding: 0 var(--spacing_inset-sm);
+    user-select: none;
+  }
 `
