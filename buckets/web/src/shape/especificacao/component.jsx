cd1a98e3efe806c071553f72e5ecef43ca5a5d65
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-11 16:15:37 -0300

    chore(especificacao): cada bloco de historio sera uma section

--- buckets/web/src/shape/especificacao/component.jsx
+++ buckets/web/src/shape/especificacao/component.jsx
@@ -2,11 +2,11 @@ import h from '@kuba/h'
 import style from './style'
 
 export default (especificacao) =>
-  <div className={style.especificacao}>
+  <section className={style.especificacao}>
     {especificacao.tamanho}
     {especificacao.wheelbase}
     {especificacao.nose}
     {especificacao.tail}
     {especificacao.concave}
     {especificacao.lixa}
-  </div>
+  </section>
