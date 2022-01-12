894c16f293eabaefc743175aa55f2b5b3c5311b7
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-12 10:37:58 -0300

    chore(comparebar): contrucao do header para o compare bar

--- blocks/comparebar/src/component.jsx
+++ blocks/comparebar/src/component.jsx
@@ -1,6 +1,12 @@
 import h from '@kuba/h'
+import button from '@kuba/button'
+import Header from '@kuba/header'
 import Side from '@kuba/side'
 import style from './style'
 
 export default (comparebar) =>
-  <Side className={style.comparebar} onClose={() => comparebar.close()} opened={comparebar.opened} />
+  <Side className={style.comparebar} onClose={() => comparebar.close()} opened={comparebar.opened}>
+    <Header>
+      <button.icon.Close onClick={() => comparebar.close()} />
+    </Header>
+  </Side>
