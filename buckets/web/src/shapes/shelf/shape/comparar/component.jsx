33ea97f4e0d171403f34168d2713c478d33695eb
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-12 09:09:51 -0300

    chore(shapes): nao vou colocar o botao de comparar no shelf

--- buckets/web/src/shapes/shelf/shape/comparar/component.jsx
+++
@@ -1,6 +0,0 @@
-import h from '@kuba/h'
-import button from '@kuba/button'
-import style from './style'
-
-export default () =>
-  <button.icon.Compare className={style.comparar} />
--- buckets/web/src/shapes/shelf/shape/comparar/style.js
+++
@@ -1,9 +0,0 @@
-import styled from '@kuba/styled'
-
-export default styled.style`
-  .comparar {
-    left: var(--spacing-nano);
-    position: absolute;
-    top: var(--spacing-nano);
-  }
-`
