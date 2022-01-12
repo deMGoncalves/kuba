e78ab29cf4d33bf191d2f897235afbc1242d4edb
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-12 15:43:23 -0300

    style(modelo): aumentando uma escala o tamanho da font

--- buckets/web/src/shape/modelo/component.jsx
+++ buckets/web/src/shape/modelo/component.jsx
@@ -3,4 +3,4 @@ import text from '@kuba/text'
 import style from './style'
 
 export default (modelo) =>
-  <text.H1 className={style.modelo} master darker md medium>{modelo.valor}</text.H1>
+  <text.H1 className={style.modelo} master darker lg medium>{modelo.valor}</text.H1>
