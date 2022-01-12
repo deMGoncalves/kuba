9f5edfe9252ca73a43e82ae4e4707f846d4d2fb9
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-12 18:52:56 -0300

    chore(marca): adicionando medias query ao grid

--- buckets/web/src/marca/shapes/style.js
+++ buckets/web/src/marca/shapes/style.js
@@ -3,7 +3,20 @@ import styled from '@kuba/styled'
 export default styled.style`
   .shapes__grid {
     display: grid;
-    gap: var(--spacing_inset-xs);
-    grid-template-columns: repeat(3, 1fr);
+    gap: var(--spacing_inset-md);
+    grid-template-columns: 1fr;
+  }
+
+  @media (min-width: 769px) {
+    .shapes__grid {
+      gap: var(--spacing_inset-md) var(--spacing_inset-xs);
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+
+  @media (min-width: 961px) {
+    .shapes__grid {
+      grid-template-columns: repeat(3, 1fr);
+    }
   }
 `
