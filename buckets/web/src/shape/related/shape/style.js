d8541d7c812bc99cffa3ded97fea2fefddd320bb
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-12 17:23:56 -0300

    chore(shape): bloco dos realcionados

---
+++ buckets/web/src/shape/related/shape/style.js
@@ -0,0 +1,12 @@
+import styled from '@kuba/styled'
+
+export default styled.style`
+  .shape {
+    cursor: pointer;
+    position: relative;
+  }
+
+  .shape picture {
+    padding-top: calc(4 / 3 * 100%) !important;
+  }
+`
