aa053f1f3b8893c8037f573941fdf630c2496d66
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-12 16:34:30 -0300

    chore(api): selecao dos shapes parecidos com o selecionado

---
+++ buckets/web/api/shape/related.js
@@ -0,0 +1,24 @@
+import supabase from '@kuba/supabase'
+
+export default async function (request, response) {
+  const { material, slug, tamanho } = JSON.parse(request.body)
+  const { data, error } = await supabase
+    .from('shape')
+    .select(`
+      *,
+      tipo (*),
+      marca (*, origem (*)),
+      tamanho (*),
+      material (*),
+      wheelbase (*),
+      montagem (*),
+      flag (*)
+    `)
+    .neq('slug', slug)
+    .eq('tamanho', tamanho)
+    .in('material.id', material)
+    .limit(4)
+
+  response.setHeader('Cache-Control', 'public, max-age=86400')
+  response.json({ data, error })
+}
