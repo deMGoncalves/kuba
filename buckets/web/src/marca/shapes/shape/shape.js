aa762ed2ec90584559cd2c12d0a4bc5d6b01a11d
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-12 21:22:19 -0300

    chore(marca): redirecionando para página do shape

--- buckets/web/src/marca/shapes/shape/shape.js
+++ buckets/web/src/marca/shapes/shape/shape.js
@@ -1,28 +1,48 @@
 import { paint } from '@kuba/h'
+import { redirectTo } from '@kuba/router'
 import component from './component'
 
 @paint(component)
 class Shape {
   #thumbnail
   #modelo
+  #marca
+  #slug
+
+  get marca () {
+    return this.#marca ??= ''
+  }
 
   get modelo () {
     return this.#modelo ??= ''
+  }
+
+  get slug () {
+    return this.#slug ??= ''
   }
 
   get thumbnail () {
     return this.#thumbnail ??= ''
   }
 
-  constructor (modelo, thumbnail) {
+  constructor (modelo, thumbnail, marca, slug) {
     this.#modelo = modelo
     this.#thumbnail = thumbnail
+    this.#marca = marca
+    this.#slug = slug
+  }
+
+  redirect () {
+    redirectTo('shape', { marca: this.marca, shape: this.slug })
+    return this
   }
 
   static create (descriptor) {
     return new Shape(
       descriptor?.modelo,
-      descriptor?.thumbnail
+      descriptor?.thumbnail,
+      descriptor?.marca?.slug,
+      descriptor?.slug
     )
   }
 
