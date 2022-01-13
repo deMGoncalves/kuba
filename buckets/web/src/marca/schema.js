92df2c1e7a39096eaca352e9e14fe86a74e12258
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-13 14:00:16 -0300

    fix(marca): faltava um caminho do breadcrumb

--- buckets/web/src/marca/schema.js
+++ buckets/web/src/marca/schema.js
@@ -4,6 +4,7 @@ export default () => (
   {
     breadcrumb: [
       { page: 'shapes', title: 'Home' },
+      { page: 'marcas', title: 'Marcas' },
       { page: 'marca', params: { marca: global.slug }, title: global.nome }
     ]
   }
