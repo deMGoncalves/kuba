0c9853cdfa194a91abb0448184b23bd597ee7ae0
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-13 15:49:37 -0300

    chore(shelfs): padronizando a calha das linhas dos shelfs

--- buckets/web/src/shapes/shelf/style.js
+++ buckets/web/src/shapes/shelf/style.js
@@ -4,7 +4,7 @@ export default styled.style`
   .shelf__container {
     display: grid;
     grid-column-gap: var(--spacing_inset-xs);
-    grid-row-gap: var(--spacing_inset-md);
+    grid-row-gap: var(--spacing_inset-lg);
     grid-template-columns: repeat(2, 1fr);
   }
 
