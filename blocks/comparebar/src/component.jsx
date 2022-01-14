6135442731b82aeae436ce49167b94ce30412916
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-14 15:15:02 -0300

    chore(comparebar): estilizando o bloco do compare

--- blocks/comparebar/src/component.jsx
+++ blocks/comparebar/src/component.jsx
@@ -1,4 +1,5 @@
 import h from '@kuba/h'
+import container from '@kuba/container'
 import button from '@kuba/button'
 import Header from '@kuba/header'
 import Side from '@kuba/side'
@@ -6,9 +7,14 @@ import Shelf from './shelf'
 import style from './style'
 
 export default (comparebar) =>
-  <Side className={style.comparebar} onClose={() => comparebar.close()} opened={comparebar.opened}>
+  <Side onClose={() => comparebar.close()} opened={comparebar.opened}>
     <Header fixed>
       <button.icon.ArrowLeft onClick={() => comparebar.close()} />
     </Header>
     <Shelf />
+    <footer className={style.comparebar__footer}>
+      <container.Div className={style.comparebar__div}>
+        <button.naked.Master className={style.comparebar__button} darker medium>Comparar</button.naked.Master>
+      </container.Div>
+    </footer>
   </Side>
--- blocks/comparebar/src/style.js
+++ blocks/comparebar/src/style.js
@@ -1,3 +1,21 @@
 import styled from '@kuba/styled'
 
-export default styled.style``
+export default styled.style`
+  .comparebar__footer {
+    background-color: var(--color-master-lightest);
+    border-top: var(--border-width-hairline) solid var(--color-master-light);
+    bottom: 0;
+    height: 88px;
+    position: sticky;
+  }
+
+  .comparebar__div {
+    align-items: center;
+    display: flex;
+    height: 100%;
+  }
+
+  .comparebar__button {
+    width: 100%;
+  }
+`
