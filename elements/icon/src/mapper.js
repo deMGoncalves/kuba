b4af4205c3b2b5a2f1556e22c60da85be194d75a
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-14 15:43:02 -0300

    chore(icon): inclusao do icone de close onColor

--- elements/icon/src/mapper.js
+++ elements/icon/src/mapper.js
@@ -1,4 +1,5 @@
 export { default as ArrowLeft } from './assets/arrow-left.svg'
 export { default as Bars } from './assets/bars.svg'
 export { default as Close } from './assets/close.svg'
+export { default as CloseOnColor } from './assets/close_onColor.svg'
 export { default as Compare } from './assets/compare.png'
