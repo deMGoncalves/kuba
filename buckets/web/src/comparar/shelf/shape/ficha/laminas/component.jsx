df14f7c4bfe5016e7777f73df4e8ee4e7a0f3957
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-15 12:16:07 -0300

    chore: ajustes em laminas

--- buckets/web/src/comparar/shelf/shape/ficha/laminas/component.jsx
+++ buckets/web/src/comparar/shelf/shape/ficha/laminas/component.jsx
@@ -2,10 +2,10 @@ import h from '@kuba/h'
 import text from '@kuba/text'
 import style from './style'
 
-export default (laminas) =>
+export default () =>
   <div className={style.laminas}>
     <text.Label className={style.laminas__text}>
       <text.Strong regular xs>Laminas:</text.Strong>
-      <text.Span medium xs>{laminas.valor}</text.Span>
+      <text.Span medium xs>...</text.Span>
     </text.Label>
   </div>
