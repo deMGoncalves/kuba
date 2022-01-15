49d1d70cb5f5975044d648a2b9555343f7452889
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-14 23:21:24 -0300

    chore(comparar): configuracao base do markup para a pagina ser responsiva

--- buckets/web/src/comparar/comparar.js
+++ buckets/web/src/comparar/comparar.js
@@ -1,8 +1,18 @@
 import { paint } from '@kuba/h'
+import markup from '@kuba/markup'
+import * as settings from '@kuba/settings'
 import component from './component'
 
 @paint(component)
+@markup
 class Comparar {
+  get description () {
+    return settings.app.description
+  }
+
+  get title () {
+    return 'Comparar'
+  }
 }
 
 export default Comparar
