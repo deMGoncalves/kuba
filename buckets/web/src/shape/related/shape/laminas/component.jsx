b11e7aadace8777c9fe348b5fe5d21a05f3fbeab
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-16 00:56:18 -0300

    style(related): estilizacao para apenas tres cards

--- buckets/web/src/shape/related/shape/laminas/component.jsx
+++ buckets/web/src/shape/related/shape/laminas/component.jsx
@@ -3,4 +3,4 @@ import text from '@kuba/text'
 import style from './style'
 
 export default (laminas) =>
-  <text.H2 className={style.laminas__p} master xxxs>{laminas.valor}</text.H2>
+  <text.H2 className={style.laminas__h2} master xxxs>{laminas.valor}</text.H2>
--- buckets/web/src/shape/related/shape/style.js
+++ buckets/web/src/shape/related/shape/style.js
@@ -2,6 +2,8 @@ import styled from '@kuba/styled'
 
 export default styled.style`
   .shape {
+    background-color: #fff;
+    border-radius: var(--border-radius-sm);
     cursor: pointer;
     min-width: 75%;
     position: relative;
@@ -10,19 +12,33 @@ export default styled.style`
 
   @media (min-width: 426px) {
     .shape {
-      min-width: calc(40% - 12px);
-      width: calc(40% - 12px);
+      min-width: calc(40% - 10.66px);
+      width: calc(40% - 10.66px);
     }
   }
 
   @media (min-width: 769px) {
     .shape {
-      min-width: calc(25% - 12px);
-      width: calc(25% - 12px);
+      min-width: calc(33.33% - 10.66px);
+      width: calc(33.33% - 10.66px);
     }
   }
 
-  .shape picture {
+  .shape__header {
+    align-items: center;
+    display: flex;
+    height: 35px;
+    padding: 0 var(--spacing_inset-xs);
+  }
+
+  .shape__picture {
     padding-top: calc(4 / 3 * 100%) !important;
+  }
+
+  .shape__main {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-xs);
+    padding: var(--spacing_inset-xs);
   }
 `
