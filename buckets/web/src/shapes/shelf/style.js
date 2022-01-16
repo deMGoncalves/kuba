8d868d0a79bc269a6551d172f2e2f03211924abb
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-16 00:11:22 -0300

    style(shpaes): vou trabalhar apenas com tres colunas

--- buckets/web/src/shapes/shelf/style.js
+++ buckets/web/src/shapes/shelf/style.js
@@ -8,16 +8,9 @@ export default styled.style`
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (min-width: 426px) {
-    .shelf__container {
-      grid-template-columns: repeat(3, 1fr);
-    }
-  }
-
   @media (min-width: 769px) {
     .shelf__container {
-      grid-row-gap: var(--spacing_inset-lg);
-      grid-template-columns: repeat(4, 1fr);
+      grid-template-columns: repeat(3, 1fr);
     }
   }
 `
