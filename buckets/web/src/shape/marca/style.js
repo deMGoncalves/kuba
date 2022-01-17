b7d8dcef5123f9c4700f80187d27f9ef20183547
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-16 21:37:17 -0300

    style(shape): o marca nao poder ter o layout de uma flag

--- buckets/web/src/shape/marca/style.js
+++ buckets/web/src/shape/marca/style.js
@@ -1,9 +1,3 @@
 import styled from '@kuba/styled'
 
-export default styled.style`
-  .marca {
-    background-color: var(--color-master-darker);
-    border-radius: var(--border-radius-pill);
-    padding: var(--spacing_inset-quarck) var(--spacing_inset-xs);
-  }
-`
+export default styled.style``
