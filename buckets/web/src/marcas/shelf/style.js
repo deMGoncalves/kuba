faccad51ccee7b0d9ff4f89a95a544c7d1ae886d
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-18 13:45:02 -0300

    style(marcas): vou trabalhar com apaenas 4 colunas em marcas

--- buckets/web/src/marcas/shelf/style.js
+++ buckets/web/src/marcas/shelf/style.js
@@ -7,23 +7,17 @@ export default styled.style`
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (min-width: 426px) {
+  @media (min-width: 769px) {
     .shelf__container {
+      grid-row-gap: var(--spacing_inset-lg);
       grid-template-columns: repeat(3, 1fr);
     }
   }
 
-  @media (min-width: 769px) {
+  @media (min-width: 961px) {
     .shelf__container {
       grid-row-gap: var(--spacing_inset-lg);
       grid-template-columns: repeat(4, 1fr);
-    }
-  }
-
-  @media (min-width: 960px) {
-    .shelf__container {
-      grid-row-gap: var(--spacing_inset-lg);
-      grid-template-columns: repeat(5, 1fr);
     }
   }
 `
