36737bd58da7a9691e5bf5dc3f821ba94640d281
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-18 14:19:32 -0300

    style(link): alinhando o texto ao centro e calha entre os conteudos

--- elements/link/src/link.js
+++ elements/link/src/link.js
@@ -2,11 +2,14 @@ import { color, family, size, weight } from '@kuba/polished'
 import styled from '@kuba/styled'
 
 export default styled.a`
+  align-items: center;
   color: ${color};
   cursor: pointer;
+  display: inline-flex;
   font-family: ${family};
   font-size: ${size};
   font-weight: ${weight};
+  gap: var(--spacing_inset-xs);
   letter-spacing: 0.618px;
   line-height: var(--line-height-default);
   user-select: none;
