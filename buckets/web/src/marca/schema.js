edd2033b4f60eb51d6132f7451bb8bb7e0703826
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-18 15:00:14 -0300

    chore(marca): pagina de marca nao precisa passar por marcas

--- buckets/web/src/marca/schema.js
+++ buckets/web/src/marca/schema.js
@@ -4,7 +4,6 @@ export default () => (
   {
     breadcrumb: [
       { page: 'shapes', title: 'Home' },
-      { page: 'marcas', title: 'Marcas' },
       { page: 'marca', params: { marca: global.slug }, title: global.nome }
     ]
   }
