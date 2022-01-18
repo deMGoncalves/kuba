926d08d2b2c79fadf08ba2c4c8c2185ebb0d7ff0
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-18 13:40:51 -0300

    chore(marcas): inclusao de um titulo na pagina de marcas

--- buckets/web/src/marcas/component.jsx
+++ buckets/web/src/marcas/component.jsx
@@ -3,11 +3,13 @@ import Breadcrumb from '@kuba/breadcrumb'
 import Site from '@kuba/site'
 import Shelf from './shelf'
 import schema from './schema.json'
+import Title from './title'
 
 export default () =>
   <Site>
     <Fragment slot='hot'>
       <Breadcrumb paths={schema.breadcrumb} />
+      <Title />
       <Shelf />
     </Fragment>
   </Site>
---
+++ buckets/web/src/marcas/title/component.jsx
@@ -0,0 +1,8 @@
+import h from '@kuba/h'
+import container from '@kuba/container'
+import text from '@kuba/text'
+
+export default () =>
+  <container.Section>
+    <text.H1 master darker lg bold>Marcas</text.H1>
+  </container.Section>
