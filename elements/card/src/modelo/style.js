93a091739a4b4e1e27d30b618db6bca9eead4daa
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-18 23:39:55 -0300

    style(card): modelo do card nao pode ser sublinhado

--- elements/card/src/modelo/style.js
+++ elements/card/src/modelo/style.js
@@ -3,6 +3,7 @@ import styled from '@kuba/styled'
 export default styled.style`
   .modelo {
     line-height: var(--line-height-lg) !important;
+    text-decoration: none;
     white-space: break-spaces;
   }
 `
