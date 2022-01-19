2256ba4723c98c0149c2022a5a8a6bfb46d82e35
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-19 18:40:40 -0300

    fix(path): erro na montagem da url para o dado estruturado

--- blocks/breadcrumb/src/path/data.js
+++ blocks/breadcrumb/src/path/data.js
@@ -1,12 +1,10 @@
 import * as f from '@kuba/f'
-import * as settings from '@kuba/settings'
-import { urlFor } from '@kuba/router'
 
 export default (path) =>
   ({
     '@id': '#item',
     '@type': 'ListItem',
-    item: f.add(settings.app.url, urlFor(path.page)),
+    item: path.href,
     name: path.title,
     position: f.inc(path.index)
   })
