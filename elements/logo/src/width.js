025df4a0c2137178af7ca7ce54933d1db873077a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-19 01:05:13 -0300

    perf(logo): configuracao das dimenssoes do logo e simbolo

--- elements/logo/src/width.js
+++ elements/logo/src/width.js
@@ -1,8 +1,8 @@
 import * as f from '@kuba/f'
 
 export default f.cond(
-  [f.has('symbol'), f.always(undefined)],
-  [f.has('small'), f.always('43.85px')],
-  [f.has('large'), f.always('109.633px')],
-  [f.T, f.always('76.75px')]
+  [f.has('symbol'), f.always('94')],
+  [f.has('small'), f.always('43.85')],
+  [f.has('large'), f.always('109.633')],
+  [f.T, f.always('76.75')]
 )
