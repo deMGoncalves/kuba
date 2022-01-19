c4fb0ed4cc3f3322932e14f5f183bada2b172294
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-18 22:10:52 -0300

    chore(shapes): adicionando mensagem ao campo de related quando não tiver shapes para ser exibido

--- buckets/web/src/shape/related/component.jsx
+++ buckets/web/src/shape/related/component.jsx
@@ -1,14 +1,20 @@
 import h from '@kuba/h'
+import * as f from '@kuba/f'
 import container from '@kuba/container'
+import Hide from '@kuba/hide'
 import Metro from '@kuba/metro'
 import story from '@kuba/story'
 import text from '@kuba/text'
+import NoRelated from './noRelated'
 import style from './style'
 
 export default (related) =>
   <story.Section>
     <container.Div className={style.related__container}>
-      <text.H2 master darker md medium>Separamos alguns shapes</text.H2>
+      <text.H2 master darker md medium>Shapes que talvez você goste...</text.H2>
+      <Hide when={f.len(related.shapes)}>
+        <NoRelated />
+      </Hide>
       <Metro className={style.related__metro}>
         {related.shapes}
       </Metro>
