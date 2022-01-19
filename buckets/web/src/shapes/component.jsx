94a7047f8b6b561c46d445726c1db7a9dc7747dd
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-18 23:21:17 -0300

    chore(shpaes): na home nao podemos ter uma title

--- buckets/web/src/shapes/component.jsx
+++ buckets/web/src/shapes/component.jsx
@@ -1,14 +1,11 @@
 import h, { Fragment } from '@kuba/h'
-import Breadcrumb from '@kuba/breadcrumb'
 import Site from '@kuba/site'
 import Shelf from './shelf'
-import schema from './schema.json'
 import Title from './title'
 
 export default () =>
   <Site>
     <Fragment slot='hot'>
-      <Breadcrumb paths={schema.breadcrumb} />
       <Title />
       <Shelf />
     </Fragment>
--- buckets/web/src/shapes/title/component.jsx
+++ buckets/web/src/shapes/title/component.jsx
@@ -1,8 +1,6 @@
 import h from '@kuba/h'
-import container from '@kuba/container'
 import text from '@kuba/text'
+import style from './style'
 
 export default () =>
-  <container.Section>
-    <text.H1 master darker lg bold>Shapes</text.H1>
-  </container.Section>
+  <text.H1 className={style.title} master darker lg bold>Shapes</text.H1>
