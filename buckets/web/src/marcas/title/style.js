4d6a0878e8fc6bf8cd31e30f1ae7fc57939b0090
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-20 00:36:11 -0300

    chore(title): os titulos devem ser amigaveis

--- buckets/web/src/marcas/title/style.js
+++ buckets/web/src/marcas/title/style.js
@@ -1,3 +1,14 @@
 import styled from '@kuba/styled'
 
-export default styled.style``
+export default styled.style`
+  .title__div {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-quarck);
+  }
+
+  .title__span,
+  .title__h1 {
+    line-height: var(--line-height-md);
+  }
+`
--- buckets/web/src/shapes/title/component.jsx
+++ buckets/web/src/shapes/title/component.jsx
@@ -8,6 +8,6 @@ export default () =>
   <story.Section>
     <container.Div className={style.title__div}>
       <text.Span className={style.title__span} master xxxs darker medium>Seja bem vindo!</text.Span>
-      <text.H1 className={style.title__h1} master darker md bold>Escolha o melhor shape para seu role</text.H1>
+      <text.H1 className={style.title__h1} master darker md bold>Escolha o melhor shape para seu setup</text.H1>
     </container.Div>
   </story.Section>
