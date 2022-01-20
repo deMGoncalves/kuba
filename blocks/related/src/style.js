dc4815734014fedb4e5e717016f62380dc349e34
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-20 14:03:38 -0300

    style(related): agora iremos indicar 4 shapes

--- blocks/related/src/style.js
+++ blocks/related/src/style.js
@@ -15,22 +15,29 @@ export default styled.style`
     background-color: #fff;
     border-radius: var(--border-radius-sm);
     cursor: pointer;
-    min-width: calc(66.66% - 10.66px);
+    min-width: calc(66.66% - 12px);
     position: relative;
-    width: calc(66.66% - 10.66px);
+    width: calc(66.66% - 12px);
   }
 
   @media (min-width: 426px) {
     .related__metro section {
-      min-width: calc(40% - 10.66px);
-      width: calc(40% - 10.66px);
+      min-width: calc(40% - 12px);
+      width: calc(40% - 12px);
     }
   }
 
   @media (min-width: 769px) {
     .related__metro section {
-      min-width: calc(33.33% - 10.66px);
-      width: calc(33.33% - 10.66px);
+      min-width: calc(33.33% - 12px);
+      width: calc(33.33% - 12px);
+    }
+  }
+
+  @media (min-width: 961px) {
+    .related__metro section {
+      min-width: calc(25% - 12px);
+      width: calc(25% - 12px);
     }
   }
 `
