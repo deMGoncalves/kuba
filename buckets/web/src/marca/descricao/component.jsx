3f016fbc6cca783b5dc798ba5335d1af64195306
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-20 21:54:51 -0300

    chore(marca): movendo a descricao para o final da pagina

--- buckets/web/src/marca/descricao/component.jsx
+++ buckets/web/src/marca/descricao/component.jsx
@@ -4,6 +4,7 @@ import text from '@kuba/text'
 import style from './style'
 
 export default (descricao) =>
-  <container.Section>
-    <text.P className={style.descricao} master xxxs>{descricao.valor}</text.P>
+  <container.Section className={style.descricao}>
+    <text.H2 className={style.descricao__h2} master darker sm bold>Sobre a marca</text.H2>
+    <text.P className={style.descricao__p} master xxxs>{descricao.valor}</text.P>
   </container.Section>
--- buckets/web/src/marca/descricao/style.js
+++ buckets/web/src/marca/descricao/style.js
@@ -2,6 +2,16 @@ import styled from '@kuba/styled'
 
 export default styled.style`
   .descricao {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-lg);
+  }
+
+  .descricao__h2 {
+    line-height: var(--line-height-md) !important;
+  }
+
+  .descricao__p {
     line-height: var(--line-height-lg) !important;
   }
 `
