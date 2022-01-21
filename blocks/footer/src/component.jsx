266be7064ac06607693efce234ac491a97cfc514
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-20 22:02:38 -0300

    chore(footer): usando o description do settings

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -2,13 +2,12 @@ import h from '@kuba/h'
 import container from '@kuba/container'
 import Logo from '@kuba/logo'
 import text from '@kuba/text'
-import schema from './schema.json'
 import style from './style'
 
 export default (footer) =>
   <footer className={style.footer}>
     <container.Div className={style.footer__container}>
       <Logo onClick={() => footer.redirectTo('shapes')} symbol />
-      <text.P className={style.footer__text} master dark xxxs>{schema.description}</text.P>
+      <text.P className={style.footer__text} master dark xxxs>{footer.description}</text.P>
     </container.Div>
   </footer>
--- blocks/footer/src/footer.js
+++ blocks/footer/src/footer.js
@@ -1,10 +1,15 @@
 import { paint } from '@kuba/h'
 import { urlFor } from '@kuba/router'
+import * as settings from '@kuba/settings'
 import component from './component'
 import event from './event'
 
 @paint(component)
 class Footer {
+  get description () {
+    return settings.app.description
+  }
+
   @event.redirect
   redirectTo (page) {
     location.assign(urlFor(page))
