07cd450fca1053ce39f8be201cf694533801cb95
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-20 22:28:12 -0300

    chore(skeleton): deve representar exatamento o elemento html

--- elements/skeleton/src/button.js
+++ elements/skeleton/src/button.js
@@ -1,9 +1,10 @@
 import * as f from '@kuba/f'
 import styled from '@kuba/styled'
 
-export default styled.span`
+export default styled.button`
   background-color: var(--color-master-light);
+  border: none;
   border-radius: var(--border-radius-sm);
-  height: 48px;
+  height: 42px;
   width: ${() => f.random(89, 178)}px;
 `
--- elements/skeleton/src/link.js
+++ elements/skeleton/src/link.js
@@ -2,7 +2,7 @@ import * as f from '@kuba/f'
 import { size } from '@kuba/polished'
 import styled from '@kuba/styled'
 
-export default styled.span`
+export default styled.a`
   background-color: var(--color-master-light);
   border-radius: var(--border-radius-pill);
   height: ${size};
