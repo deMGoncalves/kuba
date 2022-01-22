963943cd4edf2d88f03899aa589513d0cadd2977
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-22 18:08:19 -0300

    feat(view): modelo para exigicao da comparacao de shapes

---
+++ models/view/src/component.jsx
@@ -0,0 +1,13 @@
+import h from '@kuba/h'
+import Footer from '@kuba/footer'
+import Header from './header'
+import style from './style'
+
+export default (site, children) =>
+  <>
+    <Header />
+    <main className={[style.site__main, site.className]}>
+      {children.main}
+    </main>
+    <Footer />
+  </>
