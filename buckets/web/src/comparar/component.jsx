e8215f7af185018bb6544a471a5abb88c53ea19d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-21 23:58:05 -0300

    fix(comparar): nao usaremos o breadcrumb na tela de comparacao

--- buckets/web/src/comparar/component.jsx
+++ buckets/web/src/comparar/component.jsx
@@ -1,13 +1,10 @@
 import h, { Fragment } from '@kuba/h'
-import Breadcrumb from '@kuba/breadcrumb'
 import Site from '@kuba/site'
 import Shelf from './shelf'
-import schema from './schema.json'
 
 export default () =>
   <Site>
     <Fragment slot='main'>
       <Shelf />
     </Fragment>
-    <Breadcrumb paths={schema.breadcrumb} />
   </Site>
