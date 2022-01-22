41a0cb0f9534e07d850d3d0101993c148e31a0bd
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-21 23:59:42 -0300

    chore(breadcrumb): removendo o metodo que nao usamos

--- blocks/breadcrumb/src/breadcrumb.js
+++ blocks/breadcrumb/src/breadcrumb.js
@@ -1,9 +1,7 @@
 import { paint } from '@kuba/h'
-import * as f from '@kuba/f'
 import jsonld from '@kuba/jsonld'
 import component from './component'
 import data from './data'
-import Path from './path'
 
 @paint(component)
 @jsonld(data)
@@ -12,14 +10,6 @@ class Breadcrumb {
 
   get paths () {
     return this.#paths ??= []
-  }
-
-  constructor (paths) {
-    this.#paths = paths
-  }
-
-  static create (paths) {
-    return new Breadcrumb(f.map(paths, Path.create))
   }
 }
 
