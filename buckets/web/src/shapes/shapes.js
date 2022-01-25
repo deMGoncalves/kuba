8c03d18568110a1ce34fed2f3876829429592705
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-25 17:42:29 -0300

    chore(shapes): configuracao dos title e description do markupt e jsonld

--- buckets/web/src/shapes/shapes.js
+++ buckets/web/src/shapes/shapes.js
@@ -2,6 +2,7 @@ import { didMount, paint } from '@kuba/h'
 import * as f from '@kuba/f'
 import { setGlobal } from '@kuba/global'
 import jsonld from '@kuba/jsonld'
+import { setDescription, setTitle } from '@kuba/markup'
 import component from './component'
 import data from './data'
 import getShapes from './getShapes'
@@ -9,10 +10,24 @@ import getShapes from './getShapes'
 @paint(component)
 @jsonld(data)
 class Shapes {
+  get description () {
+    return 'Seja bem vindo! Escolha o melhor shape para o seu setup'
+  }
+
+  get title () {
+    return 'Kuba'
+  }
+
   @didMount
   async mount () {
     const { data: shapes, error } = await getShapes()
-    f.not(error) && setGlobal({ shapes })
+
+    f.not(error) && (
+      setTitle(this.title),
+      setDescription(this.description),
+      setGlobal({ shapes })
+    )
+
     return this
   }
 }
