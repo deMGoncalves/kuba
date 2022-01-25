324df57c476eaafd2ca33bf2e92ebe5ad81d4f74
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-25 17:36:50 -0300

    refactor(marcas): inclusao do title e description para a pagina de marcas

--- buckets/web/src/marcas/marcas.js
+++ buckets/web/src/marcas/marcas.js
@@ -2,7 +2,7 @@ import { didMount, paint } from '@kuba/h'
 import * as f from '@kuba/f'
 import { setGlobal } from '@kuba/global'
 import jsonld from '@kuba/jsonld'
-import { setTitle } from '@kuba/markup'
+import { setDescription, setTitle } from '@kuba/markup'
 import component from './component'
 import data from './data'
 import getMarcas from './getMarcas'
@@ -10,12 +10,21 @@ import getMarcas from './getMarcas'
 @paint(component)
 @jsonld(data)
 class Marcas {
+  get description () {
+    return 'Está com dúvidas? Descubra novas marcas no kuba'
+  }
+
+  get title () {
+    return 'Marcas'
+  }
+
   @didMount
   async mount () {
     const { data: marcas, error } = await getMarcas()
 
     f.not(error) && (
-      setTitle('Marcas'),
+      setTitle(this.title),
+      setDescription(this.description),
       setGlobal({ marcas })
     )
 
--- buckets/web/src/marcas/title/component.jsx
+++ buckets/web/src/marcas/title/component.jsx
@@ -5,6 +5,6 @@ import style from './style'
 
 export default () =>
   <container.Section className={style.title}>
-    <text.Span className={style.title__span} master xxxs darker medium>Esta com duvida?</text.Span>
+    <text.Span className={style.title__span} master xxxs darker medium>Está com dúvidas?</text.Span>
     <text.H1 className={style.title__h1} master darker md bold>Descubra novas marcas no kuba</text.H1>
   </container.Section>
