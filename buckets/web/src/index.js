30f0381ea8a55fb69cf8a441c521f988f58b0d68
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-25 01:17:29 -0300

    refactor(web): removendo o que nao estamos usando

--- buckets/web/src/index.js
+++ buckets/web/src/index.js
@@ -3,15 +3,9 @@ import '@kuba/reset'
 
 import '@kuba/scrolllock'
 
-import '@kuba/mask'
-
-import '@kuba/formdata'
 import '@kuba/istruthy'
 import '@kuba/prevent'
 import '@kuba/stop'
-import '@kuba/value'
-
-import './routers'
 
 import '@kuba/adsense'
 import '@kuba/boma'
@@ -19,3 +13,5 @@ import '@kuba/gtm'
 import '@kuba/hotjar'
 import '@kuba/markup'
 import '@kuba/sw'
+
+import './routers'
