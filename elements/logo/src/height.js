6ff6580e86e02d93c6935788014a6f3629cacd67
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-26 12:13:19 -0300

    chore(logo): inclusao da altura e largura do simbolo

--- elements/logo/src/height.js
+++ elements/logo/src/height.js
@@ -1,7 +1,7 @@
 import * as f from '@kuba/f'
 
 export default f.cond(
-  [f.has('symbol'), f.always(undefined)],
+  [f.has('symbol'), f.always('28')],
   [f.has('small'), f.always('18')],
   [f.has('large'), f.always('40')],
   [f.T, f.always('28')]
