70fac35f3f9a099ff46fc7a231cc9a4a91d78ea7
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-26 16:28:49 -0300

    style(relacionado): inclusao do bloco no story

--- buckets/web/src/shape/relacionado/component.jsx
+++ buckets/web/src/shape/relacionado/component.jsx
@@ -1,11 +1,14 @@
 import h from '@kuba/h'
 import container from '@kuba/container'
 import Metro from '@kuba/metro'
+import story from '@kuba/story'
 import style from './style'
 
 export default (relacionado) =>
-  <container.Section className={style.relacionado} empty:isTruthy={relacionado.empty}>
-    <Metro className={style.relacionado__metro}>
-      {relacionado.shapes}
-    </Metro>
-  </container.Section>
+  <story.Section className={style.relacionado}>
+    <container.Div empty:isTruthy={relacionado.empty}>
+      <Metro className={style.relacionado__metro}>
+        {relacionado.shapes}
+      </Metro>
+    </container.Div>
+  </story.Section>
