a5a2758c73c5577e9e0d3863dc204d0a012dda34
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-26 00:00:40 -0300

    chore(shape): inclusao do dado estrutura base para o produto

--- buckets/web/src/shape/data.js
+++ buckets/web/src/shape/data.js
@@ -2,16 +2,31 @@ import * as settings from '@kuba/settings'
 
 export default (schema) =>
   ({
-    '@id': '#webpage',
-    '@type': 'WebPage',
-    breadcrumb: {
-      '@id': '#breadcrumb'
+    '@id': `#${schema.id}`,
+    '@type': 'Product',
+    additionalProperty: {
+      '@id': '#propertyvalue'
+    },
+    brand: {
+      '@id': '#brand'
     },
     description: schema.description,
-    inLanguage: settings.app.language,
-    isPartOf: {
-      '@id': '#website'
+    image: schema.image,
+    mainEntityOfPage: {
+      '@id': '#webpage',
+      '@type': 'WebPage',
+      breadcrumb: {
+        '@id': '#breadcrumb'
+      },
+      description: schema.description,
+      inLanguage: settings.app.language,
+      isPartOf: {
+        '@id': '#website'
+      },
+      name: schema.name,
+      url: settings.app.url
     },
+    material: schema.material,
     name: schema.name,
-    url: settings.app.url
+    size: schema.size
   })
--- buckets/web/src/shape/schema.js
+++ buckets/web/src/shape/schema.js
@@ -1,28 +1,62 @@
+import * as f from '@kuba/f'
 import jsonld from '@kuba/jsonld'
 import data from './data'
 
 @jsonld(data)
 class Schema {
   #description
+  #id
+  #image
+  #material
   #name
+  #size
 
   get description () {
     return this.#description
+  }
+
+  get id () {
+    return this.#id
+  }
+
+  get image () {
+    return this.#image
+  }
+
+  get material () {
+    return f
+      .from(this.#material)
+      .pipe(f.or(f.__, []))
+      .pipe(f.map(f.__, f.prop('valor')))
+      .pipe(f.join(f.__, ', '))
+      .done()
   }
 
   get name () {
     return this.#name
   }
 
-  constructor (description, name) {
+  get size () {
+    return this.#size
+  }
+
+  constructor (description, id, image, material, name, size) {
     this.#description = description
+    this.#id = id
+    this.#image = image
+    this.#material = material
     this.#name = name
+    this.#size = size
   }
 
   static create (shape) {
     return new Schema(
       shape.descricao,
-      shape.modelo
+      shape.slug,
+      shape.thumbnail,
+      shape.material,
+      shape.modelo,
+      shape.tamanho
     )
   }
 }
