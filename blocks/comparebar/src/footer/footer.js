493f879d907d98f20edd85bbbba10e554c506aaf
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-26 11:35:39 -0300

    fix(comparebar): nao podemos ter links sem href

--- blocks/comparebar/src/footer/footer.js
+++ blocks/comparebar/src/footer/footer.js
@@ -1,13 +1,17 @@
 import { paint } from '@kuba/h'
 import * as f from '@kuba/f'
 import Compare from '@kuba/compare'
-import { redirectTo } from '@kuba/router'
+import { redirectTo, urlFor } from '@kuba/router'
 import actions from './actions'
 import component from './component'
 
 @paint(component)
 @actions
 class Footer {
+  get href () {
+    return urlFor('comparar')
+  }
+
   get valid () {
     return f
       .from(Compare.shelf)
