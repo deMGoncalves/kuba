4812b36050aa4f5d9725da4c40d4c2b446824313
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-25 23:31:32 -0300

    chore(marca): inclusao dos dados da marca

--- buckets/web/src/marca/schema.js
+++ buckets/web/src/marca/schema.js
@@ -4,24 +4,38 @@ import data from './data'
 @jsonld(data)
 class Schema {
   #description
+  #id
+  #logo
   #name
 
   get description () {
     return this.#description
   }
 
+  get id () {
+    return this.#id
+  }
+
+  get logo () {
+    return this.#logo
+  }
+
   get name () {
     return this.#name
   }
 
-  constructor (description, name) {
+  constructor (description, id, logo, name) {
     this.#description = description
+    this.#id = id
+    this.#logo = logo
     this.#name = name
   }
 
   static create (marca) {
     return new Schema(
       marca.descricao,
+      marca.slug,
+      marca.logo,
       marca.nome
     )
   }
