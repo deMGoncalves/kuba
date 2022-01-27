6993996cdb7958408e5fc80aa4d095df4d60aba8
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-27 12:53:14 -0300

    chore(picture): repassando o evento de click

--- elements/picture/src/component.jsx
+++ elements/picture/src/component.jsx
@@ -4,7 +4,7 @@ import * as settings from '@kuba/settings'
 import style from './style'
 
 export default (props, children) =>
-  <picture className={[style.picture, props.className]}>
+  <picture className={[style.picture, props.className]} onClick={props.onClick}>
     {children}
     <img className={style.picture__img} loading='lazy' alt={f.or(props.alt, settings.app.name)} height={props.height} width={props.width} />
   </picture>
