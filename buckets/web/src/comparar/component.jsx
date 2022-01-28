bf2162cbb97f0bbec27b233eceee8ae9379994f6
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-27 21:34:43 -0300

    refactor(comparar): neste layout vou fragmentar cada tupla da caomparacao

--- buckets/web/src/comparar/component.jsx
+++ buckets/web/src/comparar/component.jsx
@@ -1,12 +1,8 @@
 import h, { Fragment } from '@kuba/h'
 import View from '@kuba/view'
-import Shelf from './shelf'
-import Title from './title'
 
 export default () =>
   <View>
     <Fragment slot='main'>
-      <Title />
-      <Shelf />
     </Fragment>
   </View>
