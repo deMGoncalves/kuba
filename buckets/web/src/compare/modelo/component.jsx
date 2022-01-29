e53c29a11e6248b7d13325151bf4ab4d5671d556
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-29 13:34:33 -0300

    chore(comapre): diminuindo escala dos modelos

--- buckets/web/src/compare/modelo/component.jsx
+++ buckets/web/src/compare/modelo/component.jsx
@@ -8,14 +8,14 @@ export default (modelo) =>
   <>
     <setion className={style.modelo}>
       <Marca master />
-      <text.H1 className={style.modelo__h1} master darker sm bold>{modelo.master}</text.H1>
+      <text.H1 className={style.modelo__h1} master darker xxs bold>{modelo.master}</text.H1>
       <div className={style.modelo__div}>
         <Origem master />
       </div>
     </setion>
     <setion className={style.modelo}>
       <Marca />
-      <text.H1 className={style.modelo__h1} master darker sm bold>{modelo.slave}</text.H1>
+      <text.H1 className={style.modelo__h1} master darker xxs bold>{modelo.slave}</text.H1>
       <div className={style.modelo__div}>
         <Origem />
       </div>
