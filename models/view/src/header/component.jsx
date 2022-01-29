e9679550a33f6c6ea8a95342216ef960b57e93b8
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-29 12:34:50 -0300

    chore(view): nao vou usar o modelo view

--- models/view/src/header/component.jsx
+++
@@ -1,8 +0,0 @@
-import h from '@kuba/h'
-import button from '@kuba/button'
-import Header from '@kuba/header'
-
-export default (header) =>
-  <Header>
-    <button.icon.ArrowLeft onClick:stop={() => header.back()} />
-  </Header>
