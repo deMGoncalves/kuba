a205711abde4e7fe1ccbe45bb2848295c51a37b1
Leehaney Nazare <leehaney.soares@gmail.com>
2022-01-29 16:02:07 -0300

    chore(marca.site): tratando a url na page das marcas

--- buckets/web/src/marca/nome/site/component.jsx
+++ buckets/web/src/marca/nome/site/component.jsx
@@ -1,5 +1,10 @@
 import h from '@kuba/h'
+import icon from '@kuba/icon'
 import link from '@kuba/link'
+import style from './style'
 
 export default (site) =>
-  <link.Complete uid='site' href={site.valor} target='_blank' dark xxxs medium>{site.valor}</link.Complete>
+  <link.Complete className={style.site__link} uid='site' href={site.valor} target='_blank' dark xxxs medium>
+    Site oficial
+    <icon.ArrowLongRight className={style.site__icon}/>
+  </link.Complete>
