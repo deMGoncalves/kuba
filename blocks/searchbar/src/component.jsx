26c19565f1bb62fed76f52cb3d63d5efe4e162f1
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-30 01:45:45 -0300

    chore(searchbar): o input eh obrigatorio

--- blocks/searchbar/src/component.jsx
+++ blocks/searchbar/src/component.jsx
@@ -8,7 +8,7 @@ export default (searchbar) =>
     <section className={style.searchbar} onClick:stop={() => null} opened:isTruthy={searchbar.opened}>
       <container.Div className={style.searchbar__div}>
         <form className={style.searchbar__form} action={searchbar.action} method='GET'>
-          <input className={style.searchbar__input} placeholder={searchbar.placeholder} name='q' type='search' autocomplete='off' />
+          <input className={style.searchbar__input} placeholder={searchbar.placeholder} name='q' type='search' autocomplete='off' required />
         </form>
         <button.icon.Close onClick:prevent={() => searchbar.close()} />
       </container.Div>
