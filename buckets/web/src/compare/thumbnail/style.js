d2e2fcb7f62cea705ebe5f81634bc4e1e06a3122
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-01-30 18:17:21 -0300

    style(compare): diminuindo o botao de comparar

--- buckets/web/src/compare/thumbnail/style.js
+++ buckets/web/src/compare/thumbnail/style.js
@@ -3,7 +3,6 @@ import styled from '@kuba/styled'
 export default styled.style`
   .thumbnail {
     border-radius: var(--border-radius-sm);
-    cursor: pointer;
     padding-top: calc(5 / 4 * 100%) !important;
   }
 `
--- buckets/web/src/compare/thumbnail/vs/style.js
+++ buckets/web/src/compare/thumbnail/vs/style.js
@@ -6,30 +6,30 @@ export default styled.style`
     background-color: var(--color-master-darker);
     border-radius: var(--border-radius-circular);
     display: flex;
-    height: 89px;
+    height: 68px;
     justify-content: center;
     position: absolute;
-    right: -64.5px;
-    top: calc(50% - 44.5px);
-    width: 89px;
+    right: -54px;
+    top: calc(50% - 34px);
+    width: 68px;
     z-index: 1;
   }
 
   @media (min-width: 769px) {
     .vs {
-      height: 110px;
-      right: -75px;
-      top: calc(50% - 55px);
-      width: 110px;
+      height: 89px;
+      right: -64.5px;
+      top: calc(50% - 44.5px);
+      width: 89px;
     }
   }
 
   @media (min-width: 961px) {
     .vs {
-      height: 144px;
-      right: -92px;
-      top: calc(50% - 72px);
-      width: 144px;
+      height: 110px;
+      right: -75px;
+      top: calc(50% - 55px);
+      width: 110px;
     }
   }
 `
