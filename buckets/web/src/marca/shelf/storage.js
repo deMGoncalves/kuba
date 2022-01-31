16d93037e6a5b3276a610b6f48a6d6671366bb65
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-31 15:39:17 -0300

    refactor(shelf): usando o http para buscar os dados

--- buckets/web/src/marca/shelf/storage.js
+++ buckets/web/src/marca/shelf/storage.js
@@ -1,4 +1,5 @@
 import * as f from '@kuba/f'
+import http from '@kuba/http'
 import middleware from '@kuba/middleware'
 import { params } from '@kuba/router'
 import * as settings from '@kuba/settings'
@@ -6,13 +7,8 @@ import * as settings from '@kuba/settings'
 const onChange = f.dunder.onChange
 
 const storage = middleware((target) =>
-  fetch(
-    `${settings.api.url}/shape/marca`,
-    {
-      body: JSON.stringify({ slug: params.marca }),
-      method: 'POST'
-    }
-  )
+  http
+    .post(`${settings.api.url}/shape/marca`, { slug: params.marca })
     .then(response => response.json())
     .then(({ data, error }) => (
       f.not(error) && target[onChange](data)
