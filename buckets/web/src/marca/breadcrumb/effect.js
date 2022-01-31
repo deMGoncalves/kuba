c8a8006f3025e0231019724fc9ace1d712e4e5b0
deMGoncalves <cleber.engineer@hotmail.com>
2022-01-31 12:49:08 -0300

    refactor(breadcrumb): aplicando o evento de contrato para o uso do effect

--- buckets/web/src/marca/breadcrumb/effect.js
+++ buckets/web/src/marca/breadcrumb/effect.js
@@ -1,11 +1,18 @@
+import * as f from '@kuba/f'
 import global, { useEffect } from '@kuba/global'
 import middleware from '@kuba/middleware'
 
-const effect = (breadcrumb) => (
-  useEffect(() => breadcrumb.changePaths([
+const onChange = f.dunder.onChange
+
+const effect = (target) => (
+  useEffect(() => target[onChange]([
     { page: 'shapes', title: 'Home' },
     { page: 'marca', params: { marca: global.marca.slug }, title: global.marca.nome }
   ]))
 )
 
 export default middleware(effect)
+
+export {
+  onChange
+}
