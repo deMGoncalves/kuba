dbf870fea4a9968e77b996a7bc8909e69a2af2d1
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-02 17:01:10 -0300

    refactor(woodlight): simplificando o codigo para entender o processo de atualizacao

--- buckets/bot/cypress/integration/wood_light.js
+++ buckets/bot/cypress/integration/wood_light.js
@@ -2,32 +2,32 @@ import { iterator, supabase } from '@kuba/bot'
 
 describe('Wood Light', () => {
   it('Update price', async () => {
-    const { data } = await supabase
+    const { data, error } = await supabase
       .from('shape')
       .select('*, marca!inner(*)')
       .eq('marca.slug', 'wood-light')
 
+    assert.isNull(error, 'OK: Supabase select')
+
     const shapes = iterator(data)
 
     const update = ({ done, shape }) => {
-      if (!done) {
-        assert.isFalse(done, shape.modelo)
-        cy.visit(shape.site)
-          .then(() => {
-            cy.get('.preco-produto')
-              .invoke('attr', 'content')
-              .then(async (content) => {
-                const { error } = await supabase
-                  .from('shape')
-                  .update({ preco: parseFloat(content) })
-                  .match({ id: shape.id })
+      if (done) return
 
-                assert.isNull(error, 'Update price')
-                cy.wait(20000)
-                update(shapes.next())
-              })
-          })
-      }
+      cy.visit(shape.site)
+      cy.get('.preco-produto')
+        .invoke('attr', 'content')
+        .then(async (content) => {
+          const { error } = await supabase
+            .from('shape')
+            .update({ preco: parseFloat(content) })
+            .match({ id: shape.id })
+
+          assert.isNull(error, 'OK: Supabase update')
+
+          cy.wait(20000)
+          update(shapes.next())
+        })
     }
 
     update(shapes.next())
