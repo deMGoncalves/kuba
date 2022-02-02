8e58973d93d67c3445ad6c39439a3095209d35f4
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-02 12:25:10 -0300

    chore(eslintrc): configuracao do cypress

--- development/eslintrc/index.js
+++ development/eslintrc/index.js
@@ -6,11 +6,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true
+    'jest/globals': true,
+    'cypress/globals': true
   },
   extends: [
     'standard',
-    'plugin:jest/recommended'
+    'plugin:jest/recommended',
+    'plugin:cypress/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -19,9 +21,17 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
-    'jest'
+    'jest',
+    'cypress'
   ],
   rules: {
+    'cypress/assertion-before-screenshot': 'warn',
+    'cypress/no-assigning-return-values': 'error',
+    'cypress/no-async-tests': 'off',
+    'cypress/no-force': 'warn',
+    'cypress/no-pause': 'error',
+    'cypress/no-unnecessary-waiting': 'off',
+    'jext/expect-expect': 'off',
     'jest/no-disabled-tests': 'warn',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
