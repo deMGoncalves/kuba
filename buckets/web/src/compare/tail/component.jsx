c2b00e7777600dc9b8c4deafe7b9c9b05b085a7d
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-04 09:05:22 -0300

    fix(tail): erro no valor do label

--- buckets/web/src/compare/tail/component.jsx
+++ buckets/web/src/compare/tail/component.jsx
@@ -6,7 +6,7 @@ export default (tail) =>
   <section className={style.tail}>
     <text.Data className={style.tail__data} master darker sm bold>{tail.master}</text.Data>
     <dl className={style.tail__dl}>
-      <text.Dt className={style.tail__text} master darker xxs medium>Nose</text.Dt>
+      <text.Dt className={style.tail__text} master darker xxs medium>Tail</text.Dt>
       <text.Dd className={style.tail__text} master xxxs>Parte traseira inclinada do shape</text.Dd>
     </dl>
     <text.Data className={style.tail__data} master darker sm bold>{tail.slave}</text.Data>
