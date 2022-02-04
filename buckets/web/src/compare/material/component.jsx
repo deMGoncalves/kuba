64afb0afeaaf764606e50c0f69731ef10e2375be
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-04 09:14:58 -0300

    refactor(material): neste component nao termos o titulo

--- buckets/web/src/compare/material/component.jsx
+++ buckets/web/src/compare/material/component.jsx
@@ -4,9 +4,6 @@ import style from './style'
 
 export default (material) =>
   <>
-    <hgroup className={style.material__hgroup}>
-      <text.H2 className={style.material__h2} master darker xs bold>Material</text.H2>
-    </hgroup>
-    <text.P className={[style.material__p, style.master]} master xxxs>{material.master}</text.P>
-    <text.P className={style.material__p} master xxxs>{material.slave}</text.P>
+    <text.P className={style.material} master xxxs>{material.master}</text.P>
+    <text.P className={[style.material, style.slave]} master xxxs>{material.slave}</text.P>
   </>
--- buckets/web/src/compare/material/style.js
+++ buckets/web/src/compare/material/style.js
@@ -1,22 +1,11 @@
 import styled from '@kuba/styled'
 
 export default styled.style`
-  .material__hgroup {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing_inset-quarck);
-    grid-column: 1 / 3;
-  }
-
-  .material__h2 {
-    line-height: var(--line-height-md) !important;
-  }
-
-  .material__p {
+  .material {
     line-height: var(--line-height-lg) !important;
   }
 
-  .material__p.master {
+  .material.slave {
     text-align: right;
   }
 `
