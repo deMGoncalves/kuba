9b3663bce0f6e40f9fed786a943fee08367be05a
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-04 11:12:38 -0300

    style(thumbnail): as imagens tem uma borada

--- buckets/web/src/compare/thumbnail/style.js
+++ buckets/web/src/compare/thumbnail/style.js
@@ -2,6 +2,7 @@ import styled from '@kuba/styled'
 
 export default styled.style`
   .thumbnail {
+    border: var(--border-width-hairline) solid var(--color-master-light);
     border-radius: var(--border-radius-sm);
     padding-top: calc(5 / 4 * 100%) !important;
   }
