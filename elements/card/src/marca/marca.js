69b53ae2ae9cf948f1c247664ab8dacba8c7a6f5
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-02-06 09:55:58 -0300

    style(card): padronizando os card dos shelf e comparebar

--- elements/card/src/marca/marca.js
+++ elements/card/src/marca/marca.js
@@ -4,26 +4,17 @@ import component from './component'
 @paint(component)
 class Marca {
   #nome
-  #slug
 
   get nome () {
     return this.#nome ??= ''
   }
 
-  get slug () {
-    return this.#slug ??= ''
-  }
-
-  constructor (nome, slug) {
+  constructor (nome) {
     this.#nome = nome
-    this.#slug = slug
   }
 
   static create (shape) {
-    return new Marca(
-      shape.marca?.nome,
-      shape.marca?.slug
-    )
+    return new Marca(shape.marca?.nome)
   }
 
   static stub () {
--- elements/card/src/tamanho/component.jsx
+++ elements/card/src/tamanho/component.jsx
@@ -6,9 +6,9 @@ import style from './style'
 
 export default (tamanho) =>
   <div className={style.tamanho}>
-    <text.Data className={style.tamanho__data} master darker xs medium>{tamanho.valor}</text.Data>
+    <text.Data className={style.tamanho__data} master darker xs bold>{tamanho.valor}</text.Data>
     <icon.ArrowLongRight small />
-    <text.Data className={style.tamanho__data} master darker xs medium>
+    <text.Data className={style.tamanho__data} master darker xs bold>
       {tamanho.wheelbase}
       <Show when={tamanho.wheelbase}>
         <text.Stub master darker xxxs> wb</text.Stub>
