478005b3500b904d9bfea73dddb75488d6c7d9f8
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-10 21:18:58 -0300

    feat(concave): disparando o dado estruturado

--- buckets/web/src/shape/especificacao/concave/concave.js
+++ buckets/web/src/shape/especificacao/concave/concave.js
@@ -2,6 +2,7 @@ import { paint, repaint } from '@kuba/h'
 import * as f from '@kuba/f'
 import component from './component'
 import effect from './effect'
+import setData from './setData'
 
 @paint(component)
 @effect
@@ -21,6 +22,7 @@ class Concave {
   [effect.onChange] (valor) {
     this.#pristine = f.F()
     this.#valor = valor
+    f.isTruthy(valor) && setData(valor)
     return this
   }
 }
