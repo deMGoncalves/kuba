0dc060c11e863f6ef679501efd0ec8d95c9e5523
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-13 22:04:43 -0300

    style(thumbnail): nao precisamos de uma borda

--- buckets/web/src/shape/hero/thumbnail/style.js
+++ buckets/web/src/shape/hero/thumbnail/style.js
@@ -2,7 +2,6 @@ import styled from '@kuba/styled'
 
 export default styled.style`
   .thumbnail {
-    border: var(--border-width-hairline) solid var(--color-master-light);
     border-radius: var(--border-radius-sm);
     cursor: pointer;
     padding-top: calc(5 / 4 * 100%) !important;
