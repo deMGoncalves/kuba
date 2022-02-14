2b485823ba0a62deaeb9d65575d49c4fc82a1583
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-14 11:44:40 -0300

    refactor(gtm): so pode ser instalado quando usuario aceitar politica de privacidade

--- pixels/gtm/src/pixel.js
+++ pixels/gtm/src/pixel.js
@@ -3,9 +3,10 @@ import * as f from '@kuba/f'
 import agent from '@kuba/agent'
 import env from '@kuba/env'
 import { gtm } from '@kuba/settings'
+import accepted from './accepted'
 import dataLayer from './dataLayer'
 
-f.and(agent.isUser, env.isProd) && (
+f.and(agent.isUser, env.isProd, accepted) && (
   render(
     document.head,
     <script src={`https://www.googletagmanager.com/gtm.js?id=${gtm.id}`} async />
