a578874b4d00e2990ce8e1a4723c7874e26b5cb6
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-14 12:55:30 -0300

    chore(footer): inclusao do bota de politica de privacidade

--- blocks/footer/src/component.jsx
+++ blocks/footer/src/component.jsx
@@ -1,5 +1,6 @@
 import h from '@kuba/h'
 import container from '@kuba/container'
+import { Cookie } from '@kuba/cookiebar'
 import Logo from '@kuba/logo'
 import text from '@kuba/text'
 import style from './style'
@@ -9,5 +10,6 @@ export default (footer) =>
     <container.Div className={style.footer__container}>
       <Logo onClick={() => footer.redirectTo('shapes')} symbol />
       <text.P className={style.footer__text} master dark xxxs>{footer.description}</text.P>
+      <Cookie />
     </container.Div>
   </footer>
