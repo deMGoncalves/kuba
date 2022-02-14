7e66e2d4694a5ae03b20d137d5f26aaf0a25f60e
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-14 11:43:02 -0300

    chore(adsense): so podemos instalar o adsense se a politica de privacidade for aceito

--- pixels/adSense/src/pixel.jsx
+++ pixels/adSense/src/pixel.jsx
@@ -3,8 +3,9 @@ import * as f from '@kuba/f'
 import agent from '@kuba/agent'
 import env from '@kuba/env'
 import { adSense } from '@kuba/settings'
+import accepted from './accepted'
 
-f.and(agent.isUser, env.isProd) && (
+f.and(agent.isUser, env.isProd, accepted) && (
   render(
     document.head,
     <script data-ad-client={adSense.adClient} src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' async />
