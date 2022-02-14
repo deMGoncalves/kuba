c1b2717fae05f385501e165857eb05ce2fead5d3
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-14 12:29:18 -0300

    refactor(footer): alinhando os elementos para receber o link de politica de privacidade

--- blocks/footer/src/style.js
+++ blocks/footer/src/style.js
@@ -3,14 +3,22 @@ import styled from '@kuba/styled'
 export default styled.style`
   .footer {
     border-top: var(--border-width-hairline) solid var(--color-master-light);
-    height: 89px;
   }
 
   .footer__container {
     align-items: center;
     display: flex;
+    flex-direction: column;
     gap: var(--spacing_inset-xs);
     height: 100%;
+    justify-content: center;
+    padding: var(--spacing_inset-lg) var(--spacing_inset-xs) !important;
+  }
+
+  @media (min-width: 426px) {
+    .footer__container {
+      align-items: flex-start;
+    }
   }
 
   .footer__text {
