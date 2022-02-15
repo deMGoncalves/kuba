5317af4280cd3643ad3b26a80c9bf93b844a2d83
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-15 00:32:47 -0300

    feat(web): inclusao do bloco filtro no ambiente dev

--- buckets/web/src/shapes/component.jsx
+++ buckets/web/src/shapes/component.jsx
@@ -1,4 +1,6 @@
 import h, { Fragment } from '@kuba/h'
+import ambient from '@kuba/ambient'
+import Filter from '@kuba/filter'
 import Site from '@kuba/site'
 import More from './more'
 import Shelf from './shelf'
@@ -8,6 +10,9 @@ export default () =>
   <Site>
     <Fragment slot='main'>
       <Title />
+      <ambient.Dev>
+        <Filter />
+      </ambient.Dev>
       <Shelf />
       <More />
     </Fragment>
