faf97dd572de4ba35e81a1385e78379d69a7605b
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-15 00:09:42 -0300

    refactor(tamanho): padronizacao do tamanho do texto

--- buckets/web/src/compare/tamanho/component.jsx
+++ buckets/web/src/compare/tamanho/component.jsx
@@ -4,10 +4,10 @@ import style from './style'
 
 export default (tamanho) =>
   <section className={style.tamanho}>
-    <text.Data className={style.tamanho__data} master darker sm bold>{tamanho.master}</text.Data>
+    <text.Data className={style.tamanho__data} master darker xs bold>{tamanho.master}</text.Data>
     <dl className={style.tamanho__dl}>
       <text.Dt className={style.tamanho__text} master darker xxs medium>Tamanho</text.Dt>
       <text.Dd className={style.tamanho__text} master xxxs>Medida do comprimento de desenvolvido do shape</text.Dd>
     </dl>
-    <text.Data className={style.tamanho__data} master darker sm bold>{tamanho.slave}</text.Data>
+    <text.Data className={style.tamanho__data} master darker xs bold>{tamanho.slave}</text.Data>
   </section>
