807e00493f44168149578cab27d538cde066a1e7
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-15 00:11:21 -0300

    refactor(wheelbase): padronizacao do tamanho da font

--- buckets/web/src/compare/wheelbase/component.jsx
+++ buckets/web/src/compare/wheelbase/component.jsx
@@ -4,10 +4,10 @@ import style from './style'
 
 export default (wheelbase) =>
   <section className={style.wheelbase}>
-    <text.Data className={style.wheelbase__data} master darker sm bold>{wheelbase.master}</text.Data>
+    <text.Data className={style.wheelbase__data} master darker xs bold>{wheelbase.master}</text.Data>
     <dl className={style.wheelbase__dl}>
       <text.Dt className={style.wheelbase__text} master darker xxs medium>Wheelbase</text.Dt>
       <text.Dd className={style.wheelbase__text} master xxxs>Menor distância entre os trucks do shape</text.Dd>
     </dl>
-    <text.Data className={style.wheelbase__data} master darker sm bold>{wheelbase.slave}</text.Data>
+    <text.Data className={style.wheelbase__data} master darker xs bold>{wheelbase.slave}</text.Data>
   </section>
