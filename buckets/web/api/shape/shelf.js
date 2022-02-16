f2dc7644684b960b95d07d10676a3e0c81501f82
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-16 17:09:51 -0300

    chore(api): inclusao do filtro por regiao nos shapes

--- buckets/web/api/shape/shelf.js
+++ buckets/web/api/shape/shelf.js
@@ -1,13 +1,13 @@
 import supabase from '@kuba/supabase'
 
 export default async function (request, response) {
-  const { page, size = 24 } = JSON.parse(request.body)
-  const { data, error } = await supabase
+  const { origem = [], page, size = 24 } = JSON.parse(request.body)
+  let query = supabase
     .from('shape')
     .select(`
       *,
       tipo (*),
-      marca (*, origem (*)),
+      marca!inner(*, origem!inner(*)),
       tamanho (*),
       material (*),
       wheelbase (*),
@@ -18,6 +18,10 @@ export default async function (request, response) {
       page * size - size,
       page * size - 1
     )
+
+  if (origem.length) { query = query.in('marca.origem.valor', origem) }
+
+  const { data, error } = await query
 
   response.setHeader('Cache-Control', 'public, max-age=86400')
   response.json({ data, error })
