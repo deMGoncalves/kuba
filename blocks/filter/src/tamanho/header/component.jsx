d3636e57c1bfc07b91a56334a34fbc90cca14b89
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-02-16 23:08:59 -0300

    refactor(filter): ajuste do texto

--- blocks/filter/src/tamanho/header/component.jsx
+++ blocks/filter/src/tamanho/header/component.jsx
@@ -5,5 +5,5 @@ import style from './style'
 export default () =>
   <header className={style.header}>
     <text.Strong className={style.header__strong} master darker xxxs medium>Tamanho</text.Strong>
-    <text.P className={style.header__p} master darker xs bold>Qual tamanho do shape você prefere?</text.P>
+    <text.P className={style.header__p} master darker xs bold>Qual tamanho de shape você prefere?</text.P>
   </header>
