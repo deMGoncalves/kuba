8d1e2d456b5eb0611d4de7dcfdd3fa1bd0a18b62
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-18 09:55:11 -0300

    feat(api): inclusao da opcao de filtro nose no shelf

--- buckets/web/api/shape/shelf.js
+++ buckets/web/api/shape/shelf.js
@@ -1,7 +1,7 @@
 import supabase from '@kuba/supabase'
 
 export default async function (request, response) {
-  const { material, origem, page, size = 24, tamanho } = JSON.parse(request.body)
+  const { material, nose, origem, page, size = 24, tamanho } = JSON.parse(request.body)
   let query = supabase
     .from('shape')
     .select(`
@@ -20,6 +20,7 @@ export default async function (request, response) {
     )
 
   if (material?.length) { query = query.in('material.valor', material) }
+  if (nose) { query = query.eq('nose', true) }
   if (origem?.length) { query = query.in('marca.origem.valor', origem) }
   if (tamanho?.length) { query = query.in('tamanho.valor', tamanho) }
 
