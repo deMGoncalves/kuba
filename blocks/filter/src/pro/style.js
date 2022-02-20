237d4a501cbc24398e2708fbf92aad0c5b9bdb30
Cleber / <cleber.engineer@hotmail.com>
2022-02-20 12:48:31 -0300

    Create style.js

---
+++ blocks/filter/src/pro/style.js
@@ -0,0 +1,13 @@
+import styled from '@kuba/styled'
+
+export default styled.style`
+  .pro {
+    background-color: var(--color-master-lighter) !important;
+    border-color: var(--color-master-lighter) !important;
+  }
+  .pro[selected] {
+    background-color: var(--color-master-darker) !important;
+    border-color: var(--color-master-darker) !important;
+    color: var(--color-master-lightest) !important;
+  }
+`
