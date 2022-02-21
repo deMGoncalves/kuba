5387df5a68b81623e0f47058e2b17abf84255d53
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-21 12:16:13 -0300

    refactor(montagem): troca do texto explicativo da montagem

--- blocks/filter/src/montagem/header/component.jsx
+++ blocks/filter/src/montagem/header/component.jsx
@@ -5,5 +5,5 @@ import style from './style'
 export default () =>
   <header className={style.header}>
     <text.Strong className={style.header__strong} master darker xxxs medium>Flex</text.Strong>
-    <text.P className={style.header__p} master darker xs bold>Qual nível de flexibilidade você prefere?</text.P>
+    <text.P className={style.header__p} master darker xs bold>Escolha a maneira como o truck é anexado ao shape</text.P>
   </header>
