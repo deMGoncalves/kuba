32fc5af54e9c81ac0c36b96c4f9e5d419741a56c
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-20 21:05:22 -0300

    feat(api):  inclusao da opcao de filtro para cut outs

--- buckets/web/api/shape/shelf.js
+++ buckets/web/api/shape/shelf.js
@@ -1,7 +1,7 @@
 import supabase from '@kuba/supabase'
 
 export default async function (request, response) {
-  const { concave, flares, material, nose, origem, page, pro, size = 24, tail, tamanho, wells } = JSON.parse(request.body)
+  const { concave, flares, material, nose, origem, page, pro, recorte, size = 24, tail, tamanho, wells } = JSON.parse(request.body)
   let query = supabase
     .from('shape')
     .select(`
@@ -25,6 +25,7 @@ export default async function (request, response) {
   if (nose) { query = query.eq('nose', true) }
   if (origem?.length) { query = query.in('marca.origem.valor', origem) }
   if (pro) { query = query.eq('pro_model', true) }
+  if (recorte) { query = query.eq('cut_outs', true) }
   if (tail) { query = query.eq('tail', true) }
   if (tamanho?.length) { query = query.in('tamanho.valor', tamanho) }
   if (wells) { query = query.eq('wheel_wells', true) }
