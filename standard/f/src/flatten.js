17802b4d9bff71a6d96360605bd73abdd3c5df38
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-22 18:20:38 -0300

    refactor(f): flatten deve dar a possibilidade de determinar os niveis

--- standard/f/src/flatten.js
+++ standard/f/src/flatten.js
@@ -1,6 +1,7 @@
+import arity from './arity'
 import curry from './curry'
 
-const flatten = (array) =>
-  array.flat(Infinity)
+const flatten = (array, depth = Infinity) =>
+  array.flat(depth)
 
-export default curry(flatten)
+export default curry(arity(1, flatten))
