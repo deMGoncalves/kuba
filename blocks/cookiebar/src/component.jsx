54137700545e9e1e93cbc902bd99c932d45150c5
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-22 19:24:05 -0300

    fix(cookiebar): erro de digitacao no texto de aceitar

--- blocks/cookiebar/src/component.jsx
+++ blocks/cookiebar/src/component.jsx
@@ -7,6 +7,6 @@ export default (cookiebar) =>
   <aside className={style.cookiebar} opened:isTruthy={cookiebar.opened}>
     <text.Strong className={style.cookiebar__strong} master darker xs bold>Política de Privacidade</text.Strong>
     <text.P className={style.cookiebar__p} master xxxs>Utilizamos cookies para oferecer melhor experiência, melhorar o desempenho, analisar como você interage em nosso site e personalizar conteúdos.</text.P>
-    <button.Primary className={style.cookiebar__button} onClick={() => cookiebar.aceitar()} bold>Acitar Cookies</button.Primary>
+    <button.Primary className={style.cookiebar__button} onClick={() => cookiebar.aceitar()} bold>Aceitar Cookies</button.Primary>
     <button.naked.Master className={style.cookiebar__button} onClick={() => cookiebar.recusar()} darker bold>Recusar Cookies</button.naked.Master>
   </aside>
