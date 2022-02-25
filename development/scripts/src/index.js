3178d9ed4705e31ecfd11577d5ba7832c23ccbfb
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-25 16:03:47 -0300

    refactor(script): removendo os script que nao eh de geracao

--- development/scripts/src/index.js
+++ development/scripts/src/index.js
@@ -20,21 +20,4 @@ program
   .option('-f, --folder [folder]', 'Caminho dentro do package em que o component sera criado', '')
   .action(require('./component'))
 
-program
-  .command('formatter [package]')
-  .description('Formata o codigo de um pacote')
-  .option('--no-stylelint', 'Não executar o stylelint')
-  .action(require('./formatter'))
-
-program
-  .command('serve [package]')
-  .description('Serve um pacote')
-  .action(require('./serve'))
-
-program
-  .command('test [package]')
-  .description('Testa um pacote')
-  .option('-w, --watch [watch]', 'Manten o teste ativo')
-  .action(require('./test'))
-
 program.parse(process.argv)
