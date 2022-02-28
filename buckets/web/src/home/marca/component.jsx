24ca39eacd8d154933bce1645f269209f503b5cf
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-28 19:50:23 -0300

    fix(material): quando nao tiver valor, nao deve ser exibido o bloco

--- buckets/web/src/home/marca/component.jsx
+++ buckets/web/src/home/marca/component.jsx
@@ -5,7 +5,7 @@ import text from '@kuba/text'
 import style from './style'
 
 export default (material) =>
-  <container.Section className={style.material}>
+  <container.Section className={style.material} empty:isTruthy={material.empty}>
     <text.H2 className={style.material__h2} master darker md bold>{material.valor}</text.H2>
     <Metro className={style.material__metro}>
       {material.shapes}
