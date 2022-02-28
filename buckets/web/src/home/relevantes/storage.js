aeff35b3df11133fc72206bdab186db2cac74f75
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-28 17:47:40 -0300

    feat(home): inclusa da preferencia do tamanho para os destaques

--- buckets/web/src/home/relevantes/storage.js
+++ buckets/web/src/home/relevantes/storage.js
@@ -6,9 +6,20 @@ import * as settings from '@kuba/settings'
 const onError = f.dunder.onError
 const onResponse = f.dunder.onResponse
 
+const tamanho = JSON.parse(f.or(localStorage.getItem('_ml.tamanho'), '{}'))
+
 const effect = middleware((target) => (
   http
-    .post(`${settings.api.url}/shape/shelf`, { size: 4, page: 1 })
+    .post(`${settings.api.url}/shape/shelf`, {
+      tamanho: f
+        .from(tamanho)
+        .pipe(f.entries)
+        .pipe(f.filter(f.__, f.compose(f.different(0), f.prop('[1]'))))
+        .pipe(f.map(f.__, f.prop('[0]')))
+        .done(),
+      size: 4,
+      page: 1
+    })
     .then(response => response.json())
     .then(({ data, error }) => (
       error
