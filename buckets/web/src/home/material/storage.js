b721552d12a75857085dfd75c64e87d2f701913e
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-28 17:57:46 -0300

    refactor(home): troca do material para maple canadense

--- buckets/web/src/home/material/storage.js
+++ buckets/web/src/home/material/storage.js
@@ -11,7 +11,7 @@ const tamanho = JSON.parse(f.or(localStorage.getItem('_ml.tamanho'), '{}'))
 const effect = middleware((target) => (
   http
     .post(`${settings.api.url}/shape/shelf`, {
-      material: ['Maple Norte Americano'],
+      material: ['Maple Canadense'],
       tamanho: f
         .from(tamanho)
         .pipe(f.entries)
