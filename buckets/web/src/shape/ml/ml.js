6560e69210128fdc716fe1f36ecabde7f11fba1d
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-28 18:53:59 -0300

    feat(ml): agora vamos acompanhar as marcas mais acessados

--- buckets/web/src/shape/ml/ml.js
+++ buckets/web/src/shape/ml/ml.js
@@ -1,10 +1,11 @@
 import * as f from '@kuba/f'
 import { before } from '@kuba/middleware'
+import marca from './marca'
 import material from './material'
 import tamanho from './tamanho'
 
 const onView = before(function (shape) {
-  f.idle(f.chain(material, tamanho))(shape)
+  f.idle(f.chain(marca, material, tamanho))(shape)
   return [shape]
 })
 
