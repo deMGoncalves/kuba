2cc6c0dfcc56a69404cf6e9793d6a96b4c57f4ff
deMGoncalves <cleber.engineer@hotmail.com>
2022-02-28 17:54:53 -0300

    feat(home): inclusao da preferencia de tamanho na recomendacao dos pro

--- buckets/web/src/home/pro/storage.js
+++ buckets/web/src/home/pro/storage.js
@@ -6,10 +6,18 @@ import * as settings from '@kuba/settings'
 const onError = f.dunder.onError
 const onResponse = f.dunder.onResponse
 
+const tamanho = JSON.parse(f.or(localStorage.getItem('_ml.tamanho'), '{}'))
+
 const effect = middleware((target) => (
   http
     .post(`${settings.api.url}/shape/shelf`, {
       pro: true,
+      tamanho: f
+        .from(tamanho)
+        .pipe(f.entries)
+        .pipe(f.filter(f.__, f.compose(f.different(0), f.prop('[1]'))))
+        .pipe(f.map(f.__, f.prop('[0]')))
+        .done(),
       size: 4,
       page: 1
     })
