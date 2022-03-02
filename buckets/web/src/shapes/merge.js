c234dd81440b87063d823cb66e6e0ea0b0061f54
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-02 18:46:23 -0300

    chore(web): nao usamos esta pagina neste package

--- buckets/web/src/shapes/merge.js
+++
@@ -1,7 +0,0 @@
-import * as f from '@kuba/f'
-import global from '@kuba/global'
-
-export default (shapes, target) =>
-  f.equal(target.page, 1)
-    ? shapes
-    : [...global.shapes, ...shapes]
--- buckets/web/src/shapes/more/component.jsx
+++
@@ -1,6 +0,0 @@
-import h from '@kuba/h'
-import text from '@kuba/text'
-import style from './style'
-
-export default (more) =>
-  <text.Button className={style.more} onClick={() => more.next()} visible={more.visible} master darker xxxs bold>{more.content}</text.Button>
