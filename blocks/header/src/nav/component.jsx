c57b8d6713a533a65b2a6341f9ada5e72a3a654b
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-02 18:43:12 -0300

    refactor(header): movendo a pagina de marcas para o modulo marcas

--- blocks/header/src/nav/component.jsx
+++ blocks/header/src/nav/component.jsx
@@ -11,7 +11,7 @@ export default (nav) =>
     <nav className={[style.nav, nav.className]} opened:isTruthy={nav.opened}>
       <link.Master className={style.nav__link} href={urlFor('home')} onClick:prevent={() => nav.redirectTo('home')} darker xxxs bold>Home</link.Master>
       <link.Master className={style.nav__link} href={urlFor('shapes.home')} onClick:prevent={() => nav.redirectTo('shapes.home')} darker xxxs bold>Shapes</link.Master>
-      <link.Master className={style.nav__link} href={urlFor('marcas')} onClick:prevent={() => nav.redirectTo('marcas')} darker xxxs bold>Marcas</link.Master>
+      <link.Master className={style.nav__link} href={urlFor('marcas.home')} onClick:prevent={() => nav.redirectTo('marcas.home')} darker xxxs bold>Marcas</link.Master>
     </nav>
     <Hide when={nav.opened}>
       <button.icon.Bars className={style.nav__button} onClick={() => nav.open()} />
