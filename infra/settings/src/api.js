1c6e630c62aa54968dd578063f0a93a1d3c35e5c
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-04 10:06:46 -0300

    refactor(settings): agora a api tera uma versao

--- infra/settings/src/api.js
+++ infra/settings/src/api.js
@@ -1,5 +1,5 @@
 import env from '@kuba/env'
 
 export default {
-  url: env.isProd ? `//${location.hostname}/api` : `//${location.hostname}:${location.port}/api`
+  url: env.isProd ? `//${location.hostname}/api/v1` : `//${location.hostname}:${location.port}/api/v1`
 }
