0eb7e59df18df09a3b2547708915390e7d3180e9
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-08 22:41:01 -0300

    perf(web): vou deixar apenas o primeiro bloco sincrono

--- buckets/web/src/home/component.jsx
+++ buckets/web/src/home/component.jsx
@@ -1,14 +1,13 @@
 import h, { Fragment } from '@kuba/h'
 import Lazy from '@kuba/lazy'
 import Site from '@kuba/site'
-import Material from './material'
 import Relevantes from './relevantes'
 
 export default () =>
   <Site>
     <Fragment slot='main'>
       <Relevantes />
-      <Material />
+      <Lazy require={() => import('./material' /* webpackChunkName: 'material' */)} />
       <Lazy require={() => import('./marca' /* webpackChunkName: 'marca' */)} />
       <Lazy require={() => import('./pro' /* webpackChunkName: 'pro' */)} />
     </Fragment>
