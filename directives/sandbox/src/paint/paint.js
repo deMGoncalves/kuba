01aee80241e63cc7be97744a6f989fe7c58e726a
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-14 19:18:11 -0300

    refactor(paint): dividindo as responsabilidades da construcao das tags

--- directives/sandbox/src/paint/paint.js
+++ directives/sandbox/src/paint/paint.js
@@ -1,10 +1,10 @@
-import h, { Fragment } from '@kuba/h'
 import * as f from '@kuba/f'
+import fragment from './fragment'
+import tag from './tag'
+import text from './text'
 
-const paint = function (tag) {
-  if (f.equal(tag.type, 1)) return h(tag.name, tag.props, ...f.map(tag.children, paint))
-  if (f.equal(tag.type, 3)) return tag.content
-  if (f.equal(tag.type, 11)) return h(Fragment, {}, ...f.map(tag.children, paint))
-}
-
-export default paint
+export default f.cond(
+  [tag.is, tag.execute],
+  [text.is, text.execute],
+  [fragment.is, fragment.execute]
+)
