1231ac8de08433e3d40b8c4ca506bbdf938c4791
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-15 14:17:50 -0300

    refactor(sandbox): agora posso renderizar elemento fora do sandbox

--- directives/sandbox/src/render/render.js
+++ directives/sandbox/src/render/render.js
@@ -1,7 +1,9 @@
 import * as f from '@kuba/f'
+import outside from './outside'
 import self from './self'
 
 export default (sandbox) =>
   f.cond(
-    [self.is, self.execute(sandbox)]
+    [self.is, self.execute(sandbox)],
+    [outside.is, outside.execute]
   )
