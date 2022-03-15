d7865a7c7062b00d3dded3379850a2d4621cdd36
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-15 10:04:11 -0300

    refactor(sandbox): usando o processo de render

--- directives/sandbox/src/connect.js
+++ directives/sandbox/src/connect.js
@@ -1,19 +1,11 @@
 import h, { Fragment } from '@kuba/h'
-import * as f from '@kuba/f'
 import middleware from '@kuba/middleware'
-import paint from './paint'
+import render from './render'
 
 const connect = function (sandbox) {
   const worker = sandbox.require()
 
-  worker.addEventListener('message', function (event) {
-    f.and(
-      f.equal('render', event.data?.action),
-      f.equal('self', event.data?.payload?.element)
-    ) && (
-      sandbox.render(...f.map(event.data?.payload?.children, paint))
-    )
-  })
+  worker.addEventListener('message', render(sandbox))
 }
 
 export default middleware(connect)
