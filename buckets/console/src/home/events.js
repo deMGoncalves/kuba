5a1bc2d165099818e7216c1ea495f8c1e3fb5946
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-16 20:24:10 -0300

    refactor(console): prefira trabalhar com deconstructor

--- buckets/console/src/home/events.js
+++ buckets/console/src/home/events.js
@@ -1,7 +1,8 @@
 import * as f from '@kuba/f'
 import middleware from '@kuba/middleware'
 
-const onEvent = f.dunder.onEvent
+const { onEvent } = f.dunder
+
 const events = middleware((target) => (
   setTimeout(() => target[onEvent](), 1000)
 ))
