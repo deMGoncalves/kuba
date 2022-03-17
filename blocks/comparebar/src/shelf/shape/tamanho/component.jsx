51f37b727db4b9d22ed411756c09ad8da8c3d5fe
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-17 14:41:14 -0300

    refactor(tamanho): ordenacao dos imports

--- blocks/comparebar/src/shelf/shape/tamanho/component.jsx
+++ blocks/comparebar/src/shelf/shape/tamanho/component.jsx
@@ -1,7 +1,7 @@
 import h from '@kuba/h'
 import icon from '@kuba/icon'
-import text from '@kuba/text'
 import style from './style'
+import text from '@kuba/text'
 
 export default (tamanho) =>
   <div className={style.tamanho}>
