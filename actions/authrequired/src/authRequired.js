9b58839a15b8abcbdd43b2675d7cc8cd17c8ce16
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-17 10:30:49 -0300

    refactor(authrequired): usando o http para fazer o fetch

--- actions/authrequired/src/authRequired.js
+++ actions/authrequired/src/authRequired.js
@@ -2,10 +2,13 @@ import * as f from '@kuba/f'
 import * as settings from '@kuba/settings'
 import { urlFor } from '@kuba/router'
 import headers from './headers'
+import http from '@kuba/http'
 import middleware from '@kuba/middleware'
 
 const authRequired = () =>
-  fetch(`${settings.api.url}/auth/user`, { headers })
+  http
+    .post(`${settings.api.url}/auth/user`)
+    .headers(headers)
     .then(response => response.json())
     .then(f.prop('data.aud'))
     .then(f.different('authenticated'))
