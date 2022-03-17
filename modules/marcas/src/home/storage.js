8b6fdcdcb88d171e0c8a50f74112a8cab7ccfa5a
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-16 23:41:04 -0300

    refactor(marcas): organizando os imports

--- modules/marcas/src/home/storage.js
+++ modules/marcas/src/home/storage.js
@@ -1,8 +1,8 @@
 import * as f from '@kuba/f'
+import * as settings from '@kuba/settings'
 import { setGlobal } from '@kuba/global'
 import http from '@kuba/http'
 import middleware from '@kuba/middleware'
-import * as settings from '@kuba/settings'
 
 const storage = () =>
   http
