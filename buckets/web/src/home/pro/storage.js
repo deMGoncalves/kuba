b9c549ecdda6e492e9bab80b080e066b82d919fa
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-16 22:52:27 -0300

    refactor(pro): organizacao dos imports

--- buckets/web/src/home/pro/storage.js
+++ buckets/web/src/home/pro/storage.js
@@ -1,7 +1,7 @@
 import * as f from '@kuba/f'
+import * as settings from '@kuba/settings'
 import http from '@kuba/http'
 import middleware from '@kuba/middleware'
-import * as settings from '@kuba/settings'
 
 const onError = f.dunder.onError
 const onResponse = f.dunder.onResponse
