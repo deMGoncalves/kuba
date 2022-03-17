52ce7816e13bd022cef8185d5574f430b74e132d
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-16 23:09:42 -0300

    refactor(marca): organizacao dos imports

--- buckets/web/src/marca/storage.js
+++ buckets/web/src/marca/storage.js
@@ -1,8 +1,8 @@
 import * as f from '@kuba/f'
+import * as settings from '@kuba/settings'
+import { params } from '@kuba/router'
 import http from '@kuba/http'
 import middleware from '@kuba/middleware'
-import { params } from '@kuba/router'
-import * as settings from '@kuba/settings'
 
 const onError = f.dunder.onError
 const onResponse = f.dunder.onResponse
