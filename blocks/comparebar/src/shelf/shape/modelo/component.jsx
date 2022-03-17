48d7efb1b77d28a8b94e1ae7d8c5f3b8d84d45b3
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-17 14:28:31 -0300

    refactor(modelo): ordenacao dos imports

--- blocks/comparebar/src/shelf/shape/modelo/component.jsx
+++ blocks/comparebar/src/shelf/shape/modelo/component.jsx
@@ -1,6 +1,6 @@
 import h from '@kuba/h'
-import text from '@kuba/text'
 import style from './style'
+import text from '@kuba/text'
 
 export default (modelo) =>
   <text.H1 className={style.modelo} master darker xxxs>{modelo.valor}</text.H1>
