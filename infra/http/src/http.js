238526c6b75518f1ee1b42ba79d1776ee9d31a13
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-16 22:14:45 -0300

    refactor(http): troca da logica para um pipe

--- infra/http/src/http.js
+++ infra/http/src/http.js
@@ -1,8 +1,38 @@
-import headers from './headers'
+import * as f from '@kuba/f'
 
 export default new Proxy({}, {
   get (_, method) {
-    return (url, body = {}) =>
-      fetch(url, { body: JSON.stringify(body), headers: new Headers(headers), method })
+    return function (url) {
+      const init = {
+        method
+      }
+
+      return {
+        body (target) {
+          f.assign(init, {
+            body: JSON.stringify(target)
+          })
+          return this
+        },
+
+        headers (target) {
+          f.assign(init, {
+            headers: new Headers(target)
+          })
+          return this
+        },
+
+        then (callback) {
+          return fetch(url, init).then(callback)
+        },
+
+        mode (target) {
+          f.assign(init, {
+            mode: target
+          })
+          return this
+        }
+      }
+    }
   }
 })
