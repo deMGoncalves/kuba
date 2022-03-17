63f731c62b13d886327eb97710d5ef8aa97309d1
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-16 21:20:50 -0300

    feat(supabase): servico para criar conexao com o supabase

--- infra/supabase/src/index.js
+++ infra/supabase/src/index.js
@@ -1,8 +1,1 @@
-import { createClient } from '@supabase/supabase-js'
-import options from './options'
-
-export default createClient(
-  process.env.API_URL,
-  process.env.API_KEY,
-  options
-)
+export { default as createClient } from './createClient'
