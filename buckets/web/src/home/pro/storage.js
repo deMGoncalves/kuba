59e9f5754f807eab5168fff6d8b861cd5058a827
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-16 23:04:16 -0300

    refactor(pro): nova forma de se trabalhar com o http

--- buckets/web/src/home/pro/storage.js
+++ buckets/web/src/home/pro/storage.js
@@ -8,7 +8,8 @@ const tamanho = JSON.parse(f.or(localStorage.getItem('_ml.tamanho'), '{}'))
 
 const effect = middleware((target) => (
   http
-    .post(`${settings.api.url}/shape/shelf`, {
+    .post(`${settings.api.url}/shape/shelf`)
+    .body({
       pro: true,
       tamanho: f
         .from(tamanho)
