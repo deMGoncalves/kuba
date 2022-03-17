3b218a7bd7b58c0a635ce52c8932a469ee4adba6
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-17 10:32:05 -0300

    refactor(authrequired): usando o redirect to para redirecionar

--- actions/authrequired/src/authRequired.js
+++ actions/authrequired/src/authRequired.js
@@ -1,6 +1,6 @@
 import * as f from '@kuba/f'
 import * as settings from '@kuba/settings'
-import { urlFor } from '@kuba/router'
+import { redirectTo } from '@kuba/router'
 import headers from './headers'
 import http from '@kuba/http'
 import middleware from '@kuba/middleware'
@@ -12,6 +12,6 @@ const authRequired = () =>
     .then(response => response.json())
     .then(f.prop('data.aud'))
     .then(f.different('authenticated'))
-    .then(anonymous => (anonymous && location.assign(urlFor('signin'))))
+    .then(anonymous => (anonymous && redirectTo('signin')))
 
 export default middleware(authRequired)
