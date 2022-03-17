637f1d3ef1ee76d7ba47d1df7defff3ffcc9cc40
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-17 10:43:06 -0300

    refactor(hook): padronizacao do pacote

--- actions/hook/src/index.js
+++ actions/hook/src/index.js
@@ -1,18 +1,1 @@
-import * as f from '@kuba/f'
-
-export default (handler) =>
-  function (target, method, descriptor) {
-    const spy = f.magic(f.random())
-
-    handler(function (...args) {
-      return target[spy](...args)
-    })
-
-    Object.defineProperty(target, spy, {
-      value: function () {
-        return Reflect.apply(this[method], this, arguments)
-      }
-    })
-
-    return descriptor
-  }
+export { default } from './hook'
