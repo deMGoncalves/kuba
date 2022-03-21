e492245c4a787b3e6c80a48a0dcb1fbf36563087
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-21 20:56:55 -0300

    chore(icon): nao usamos o help

--- elements/help/src/component.jsx
+++
@@ -1,9 +0,0 @@
-import h from '@kuba/h'
-import text from '@kuba/text'
-import style from './style'
-
-export default (helper) =>
-  <div className={[style.helper__tooltip, helper.className]}>
-    <span className={style.helper__span}>{helper.text}</span>
-    <text.Label className={style.helper__icon} bold xxxs>?</text.Label>
-  </div>
