06cb0f2d6b610f33ab78719b05dd07079f61e481
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-21 12:51:41 -0300

    refactor(tail): organizando os imports

--- blocks/filter/src/tail/component.jsx
+++ blocks/filter/src/tail/component.jsx
@@ -1,6 +1,6 @@
 import h, { Fragment } from '@kuba/h'
-import tag from '@kuba/tag'
 import style from './style'
+import tag from '@kuba/tag'
 
 export default (tail) =>
   <tag.Master className={style.tail} onClick={() => tail.toggle()} selected:isTruthy={tail.selected} darker medium>
