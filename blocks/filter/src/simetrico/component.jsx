749c74275e8540d4022351149d8e630e787ad037
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-21 12:49:04 -0300

    refactor(simetrico): organizando os imports

--- blocks/filter/src/simetrico/component.jsx
+++ blocks/filter/src/simetrico/component.jsx
@@ -1,6 +1,6 @@
 import h, { Fragment } from '@kuba/h'
-import tag from '@kuba/tag'
 import style from './style'
+import tag from '@kuba/tag'
 
 export default (simetrico) =>
   <tag.Master className={style.simetrico} onClick={() => simetrico.toggle()} selected:isTruthy={simetrico.selected} darker medium>
