becadf4c39d165f53a811b397a354a935eeb2672
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-21 12:56:31 -0300

    refactor(wells): organizando os imports

--- blocks/filter/src/wells/component.jsx
+++ blocks/filter/src/wells/component.jsx
@@ -1,6 +1,6 @@
 import h, { Fragment } from '@kuba/h'
-import tag from '@kuba/tag'
 import style from './style'
+import tag from '@kuba/tag'
 
 export default (wells) =>
   <tag.Master className={style.wells} onClick={() => wells.toggle()} selected:isTruthy={wells.selected} darker medium>
