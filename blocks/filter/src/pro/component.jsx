416eb0b7bb30ca2d00a925c82b60512ac88a6cc6
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-21 12:46:55 -0300

    refactor(pro): organizando os imports

--- blocks/filter/src/pro/component.jsx
+++ blocks/filter/src/pro/component.jsx
@@ -1,6 +1,6 @@
 import h, { Fragment } from '@kuba/h'
-import tag from '@kuba/tag'
 import style from './style'
+import tag from '@kuba/tag'
 
 export default (pro) =>
   <tag.Master className={style.pro} onClick={() => pro.toggle()} selected:isTruthy={pro.selected} darker medium>
