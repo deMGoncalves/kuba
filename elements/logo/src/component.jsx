6047ee1684d537ee40b72a4851f3a30b86c7f92e
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-21 21:56:04 -0300

    refactor(logo): padronizacao dos imports

--- elements/logo/src/component.jsx
+++ elements/logo/src/component.jsx
@@ -1,12 +1,12 @@
 import h from '@kuba/h'
-import style from './style'
 import height from './height'
+import style from './style'
 import width from './width'
 
 export default (logo) =>
   <img
-    alt={logo.name}
     className={[style.logo, logo.className]}
+    alt={logo.name}
     height={height(logo)}
     large={logo.large}
     loading='lazy'
