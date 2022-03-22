f657217ac03c312dd8052cb07fd260b7862abe98
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:59:17 -0300

    refactor(gtm): movendo o disparo do pixel e evento no processo idle

--- pixels/gtm/src/pixel.js
+++ pixels/gtm/src/pixel.js
@@ -6,14 +6,24 @@ import env from '@kuba/env'
 import h, { render } from '@kuba/h'
 import schema from './schema.json'
 
-f.and(agent.isUser, env.isProd, accepted) && (
+const pixel = f.idle(() =>
   render(
     document.head,
     <script src={schema.src} async />
-  ),
+  )
+)
 
+const event = f.idle(() =>
   dataLayer.push({
     'gtm.kuba': new Date().getTime(),
     event: 'gtm.js'
   })
+)
+
+f.and(
+  agent.isUser,
+  env.isProd,
+  accepted) && (
+  pixel(),
+  event()
 )
