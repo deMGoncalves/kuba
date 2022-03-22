8d652e2175536b7cca972e131ca573318944966a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:45:11 -0300

    refactor(events): padronizacao do uso do pacote http

--- buckets/web/src/shape/events.js
+++ buckets/web/src/shape/events.js
@@ -1,17 +1,15 @@
 import * as f from '@kuba/f'
-import * as settings from '@kuba/settings'
 import { after } from '@kuba/middleware'
 import { params } from '@kuba/router'
-import http from '@kuba/http'
+import http, { api } from '@kuba/http'
 
 const views = JSON.parse(f.or(sessionStorage.getItem('_kuba.views'), '{}'))
 
 const onView = after(function (output) {
   f.isFalsy(views[params.shape]) && (
     http
-      .post(`${settings.api.url}/shape/view`, { slug: params.shape })
-      .then(response => response.json())
-      .then(({ error }) => (
+      .post(`${api.url}/shape/view`, { slug: params.shape })
+      .json(({ error }) => (
         f.isNil(error) && (
           f.assign(views, { [params.shape]: f.T() }),
           sessionStorage.setItem('_kuba.views', JSON.stringify(views))
