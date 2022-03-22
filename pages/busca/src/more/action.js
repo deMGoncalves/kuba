69b907194038b350192ca20a880a6672fa233cfc
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:50:55 -0300

    refactor(action): padronizacao do uso do pacote http

--- pages/busca/src/more/action.js
+++ pages/busca/src/more/action.js
@@ -1,15 +1,13 @@
 import * as f from '@kuba/f'
-import global, { setGlobal } from '@kuba/global'
-import http from '@kuba/http'
 import { after } from '@kuba/middleware'
 import { args } from '@kuba/router'
-import * as settings from '@kuba/settings'
+import global, { setGlobal } from '@kuba/global'
+import http, { api } from '@kuba/http'
 
 const next = after(function (output) {
   http
-    .post(`${settings.api.url}/shape/search`, { page: this.page, q: args.q })
-    .then(response => response.json())
-    .then(({ data: shapes, error }) => (
+    .post(`${api.url}/shape/search`, { page: this.page, q: args.q })
+    .json(({ data: shapes, error }) => (
       f.not(error) && setGlobal({
         shapes: [...global.shapes, ...shapes]
       })
