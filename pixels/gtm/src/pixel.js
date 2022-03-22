fc18b1c81b6e83799b0de4391e17a1970a362d71
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:56:35 -0300

    refactor(gtm): movendo a configuracao do gtm para o proprio pacote

--- pixels/gtm/src/pixel.js
+++ pixels/gtm/src/pixel.js
@@ -1,15 +1,15 @@
-import h, { render } from '@kuba/h'
 import * as f from '@kuba/f'
-import agent from '@kuba/agent'
-import env from '@kuba/env'
-import { gtm } from '@kuba/settings'
 import accepted from './accepted'
+import agent from '@kuba/agent'
 import dataLayer from './dataLayer'
+import env from '@kuba/env'
+import h, { render } from '@kuba/h'
+import schema from './schema.json'
 
 f.and(agent.isUser, env.isProd, accepted) && (
   render(
     document.head,
-    <script src={`https://www.googletagmanager.com/gtm.js?id=${gtm.id}`} async />
+    <script src={schema.src} async />
   ),
 
   dataLayer.push({
