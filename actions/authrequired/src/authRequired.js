766f727efe36355a1eba4368482dfffd9cf6ba0f
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:27:49 -0300

    refactor(authrequired): agora a configuracao da api fica no pacote http

--- actions/authrequired/src/authRequired.js
+++ actions/authrequired/src/authRequired.js
@@ -1,13 +1,12 @@
 import * as f from '@kuba/f'
-import * as settings from '@kuba/settings'
 import { redirectTo } from '@kuba/router'
 import headers from './headers'
-import http from '@kuba/http'
+import http, { api } from '@kuba/http'
 import middleware from '@kuba/middleware'
 
 const authRequired = () =>
   http
-    .post(`${settings.api.url}/auth/user`)
+    .post(`${api.url}/auth/user`)
     .headers(headers)
     .then(response => response.json())
     .then(f.prop('data.aud'))
