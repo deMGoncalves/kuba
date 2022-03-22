4950fe188b091b31767ecc9836d1f66ae993518c
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:48:02 -0300

    refactor(storage): padronizacao do pacote http

--- modules/marcas/src/home/storage.js
+++ modules/marcas/src/home/storage.js
@@ -1,14 +1,12 @@
 import * as f from '@kuba/f'
-import * as settings from '@kuba/settings'
 import { setGlobal } from '@kuba/global'
-import http from '@kuba/http'
+import http, { api } from '@kuba/http'
 import middleware from '@kuba/middleware'
 
 const storage = () =>
   http
-    .post(`${settings.api.url}/marca/shelf`)
-    .then(response => response.json())
-    .then(({ data: marcas, error }) => (
+    .post(`${api.url}/marca/shelf`)
+    .json(({ data: marcas, error }) => (
       f.not(error) && setGlobal({ marcas })
     ))
 
