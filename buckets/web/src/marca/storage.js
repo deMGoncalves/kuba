8c463a515ece47ea91ece6fa9d8369b5117d4b69
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:42:22 -0300

    refactor(storage): padronizacao do uso do http

--- buckets/web/src/marca/storage.js
+++ buckets/web/src/marca/storage.js
@@ -1,17 +1,15 @@
 import * as f from '@kuba/f'
-import * as settings from '@kuba/settings'
 import { params } from '@kuba/router'
-import http from '@kuba/http'
+import http, { api } from '@kuba/http'
 import middleware from '@kuba/middleware'
 
 const { onError, onResponse } = f.dunder
 
 const storage = middleware((target) =>
   http
-    .post(`${settings.api.url}/marca`)
+    .post(`${api.url}/marca`)
     .body({ slug: params.marca })
-    .then(response => response.json())
-    .then(({ data, error }) => (
+    .json(({ data, error }) => (
       error
         ? target[onError]()
         : target[onResponse](data)
