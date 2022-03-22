0c882b482c9f7293762f827a6ccb5840610afafb
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:49:37 -0300

    refactor(storage): padronizacao do uso do pacote http

--- pages/busca/src/storage.js
+++ pages/busca/src/storage.js
@@ -1,17 +1,15 @@
 import * as f from '@kuba/f'
-import * as settings from '@kuba/settings'
 import { args } from '@kuba/router'
-import http from '@kuba/http'
+import http, { api } from '@kuba/http'
 import middleware from '@kuba/middleware'
 
 const { onError, onResponse } = f.dunder
 
 const storage = middleware((target) =>
   http
-    .post(`${settings.api.url}/shape/search`)
+    .post(`${api.url}/shape/search`)
     .body({ page: 1, q: args.q })
-    .then(response => response.json())
-    .then(({ data, error }) => (
+    .json(({ data, error }) => (
       error
         ? target[onError]()
         : target[onResponse](data)
