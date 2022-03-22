eff15d66fdce72668d9a63ff6925b39bec2d2499
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 16:52:30 -0300

    refactor(settings): as configuracoes da api ficaram no pacote http

--- infra/settings/src/api.js
+++
@@ -1,5 +0,0 @@
-import env from '@kuba/env'
-
-export default {
-  url: env.isProd ? `//${location.hostname}/api/v1` : `//${location.hostname}:${location.port}/api/v1`
-}
--- infra/settings/src/index.js
+++ infra/settings/src/index.js
@@ -1,4 +1,3 @@
-export { default as api } from './api'
 export { default as app } from './app'
 export { default as gtm } from './gtm'
 export { default as hotjar } from './hotjar'
