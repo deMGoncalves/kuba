193a7e4f42647447037fa7de138520863a846a16
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-21 23:30:32 -0300

    refactor(formadata): simplificando a logica do filtro

--- filters/formdata/src/handler.js
+++ filters/formdata/src/handler.js
@@ -3,12 +3,5 @@ import * as f from '@kuba/f'
 export default (listener) =>
   (e) => (
     e.preventDefault(),
-    listener(
-      f
-        .from([...e.target])
-        .pipe(f.filter(f.__, f.prop('name')))
-        .pipe(f.map(f.__, ({ name, value, unmaskedValue }) => [name, f.or(unmaskedValue, value)]))
-        .pipe(f.pairs)
-        .done()
-    )
+    listener(f.pairs([...new FormData(e.target)]))
   )
