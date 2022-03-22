64180a87821c4e3ea68c6574b8a9286c4a88c204
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-03-22 17:01:17 -0300

    refactor(settings): configuraca do gtm fica no proprio pacote

--- infra/settings/src/gtm.js
+++
@@ -1,3 +0,0 @@
-export default {
-  id: 'GTM-T9MKXPH'
-}
--- infra/settings/src/index.js
+++ infra/settings/src/index.js
@@ -1,5 +1,4 @@
 export { default as app } from './app'
-export { default as gtm } from './gtm'
 export { default as hotjar } from './hotjar'
 export { default as lang } from './lang'
 export { default as lomadee } from './lomadee'
