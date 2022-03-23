eede3137ea9dbe428d9204983f5bb060a7c946a0
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-23 08:18:49 -0300

    refactor(settings): nao usamos mais o hotjar dentro do settings

--- infra/settings/src/hotjar.js
+++
@@ -1,4 +0,0 @@
-export default {
-  hjid: '2005194',
-  hjsv: 6
-}
--- infra/settings/src/index.js
+++ infra/settings/src/index.js
@@ -1,4 +1,3 @@
 export { default as app } from './app'
-export { default as hotjar } from './hotjar'
 export { default as lang } from './lang'
 export { default as lomadee } from './lomadee'
