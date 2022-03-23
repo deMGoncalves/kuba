3b079e423829c0a158b2b699a85a1490a7c1b475
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-23 09:46:14 -0300

    refactor(sw): padronizacao do pacote

--- pixels/sw/src/index.js
+++ pixels/sw/src/index.js
@@ -1,10 +1,1 @@
-import * as f from '@kuba/f'
-import agent from '@kuba/agent'
-import env from '@kuba/env'
-import register from './register'
-
-f.and(
-  agent.isUser,
-  env.isProd,
-  f.has('serviceWorker', navigator)
-) && register()
+import './pixel'
