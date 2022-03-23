5c266f3cefdef4272201518bb2299f030c5aa123
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-23 10:23:44 -0300

    refactor(prop): preciso receber o erro do catch

--- standard/f/src/prop.js
+++ standard/f/src/prop.js
@@ -4,7 +4,9 @@ const prop = (path, object) => {
   /* eslint no-new-func: "off" */
   try {
     return (new Function('x', `return x${/^\[/.test(path) ? '' : '.'}${path}`))(object)
-  } catch (_error) {}
+  } catch (_error) {
+    return undefined
+  }
 }
 
 export default curry(prop)
