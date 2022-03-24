32e5262f39266116a89b85a177bcd37b637b9e87
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-24 08:20:13 -0300

    refactor(filter): nomeando os grupos de seletores

--- standard/h/src/tag/attributes/filter/mapper.js
+++ standard/h/src/tag/attributes/filter/mapper.js
@@ -3,6 +3,6 @@ import * as f from '@kuba/f'
 export default (name) =>
   f
     .from(name)
-    .pipe(f.exec(/([a-z-]+):?([a-z]+)?$/i))
+    .pipe(f.exec(/(?<attribute>[a-z-]+):?(?<filter>[a-z]+)?$/i))
     .pipe(f.slice(f.__, 1, 3))
     .done()
