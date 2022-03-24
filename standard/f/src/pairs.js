78da78026a9c32f024a290f54cc9aa6cb52eb0bc
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-24 10:28:08 -0300

    refactor(pairs): atribuicoes devem ficar dentro de parenteses

--- standard/f/src/pairs.js
+++ standard/f/src/pairs.js
@@ -2,6 +2,6 @@ import curry from './curry'
 import reduce from './reduce'
 
 const pairs = (array) =>
-  reduce(array, (x, [a, b]) => (x[a] = b, x), {})
+  reduce(array, (x, [a, b]) => ((x[a] = b), x), {})
 
 export default curry(pairs)
