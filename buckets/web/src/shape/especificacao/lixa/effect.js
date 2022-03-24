16acce307e7e1772afa8adc5e655609ce1887ad1
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-24 14:38:02 -0300

    refactor(lixa): usar deconstructor para arrays e objetos

--- buckets/web/src/shape/especificacao/lixa/effect.js
+++ buckets/web/src/shape/especificacao/lixa/effect.js
@@ -2,7 +2,7 @@ import * as f from '@kuba/f'
 import global, { useEffect } from '@kuba/global'
 import middleware from '@kuba/middleware'
 
-const onChange = f.dunder.onChange
+const { onChange } = f.dunder
 
 const effect = middleware((target) => (
   useEffect(() => target[onChange](global.shape.lixa))
