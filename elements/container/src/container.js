4bfb4097d7dd6f759f54932d05c15b20ce081666
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:38:30 -0300

    refactor(container): encapsulando a logica dentro de um grupo de instrucoes

--- elements/container/src/container.js
+++ elements/container/src/container.js
@@ -4,7 +4,7 @@ export default new Proxy({}, {
   get: (_, tag) =>
     styled[tag]`
       margin: 0 auto;
-      max-width: ${(props) => props.fluid ? 'auto' : '987px'};
+      max-width: ${(props) => (props.fluid ? 'auto' : '987px')};
       padding: 0 var(--spacing_inset-xs);
       position: relative;
       width: 100%;
