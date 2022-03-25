4fb1b16282444f383729e45bc4e28aeba5a30147
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:00:27 -0300

    refactor(styled): encapsulando a logica dentro de um grupo de instrucoes

--- standard/styled/src/styled.js
+++ standard/styled/src/styled.js
@@ -1,10 +1,12 @@
 import * as f from '@kuba/f'
+
 import style from './style'
 import tag from './tag'
 
 export default new Proxy({}, {
-  get: (_, tagName) =>
+  get: (_, tagName) => (
     f.equal(tagName, 'style')
       ? style
       : tag(tagName)
+  )
 })
