8cf6d5de9dc3a1c2ef4264898325c7ed4c6c13cd
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:11:40 -0300

    refactor(dunder): encapsulando a logica dentro de um grupo de instrucoes

--- standard/f/src/dunder.js
+++ standard/f/src/dunder.js
@@ -2,8 +2,11 @@ import curry from './curry'
 import has from './has'
 import magic from './magic'
 
-const evaluate = (name, target) =>
-  has(magic(name), target) ? target[magic(name)]() : target
+const evaluate = (name, target) => (
+  has(magic(name), target)
+    ? target[magic(name)]()
+    : target
+)
 
 const dunder = new Proxy(curry(evaluate), {
   get: (_target, method) => magic(method)
