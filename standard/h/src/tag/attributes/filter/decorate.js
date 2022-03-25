d083cbee91875ef50530b7b012b388bb5deedb94
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:04:29 -0300

    refactor(filter): encapsulando a logica dentro de um grupo de instrucoes

--- standard/h/src/tag/attributes/filter/decorate.js
+++ standard/h/src/tag/attributes/filter/decorate.js
@@ -1,9 +1,10 @@
 import * as f from '@kuba/f'
 import filters from '@kuba/h/src/filters'
 
-const decorate = (handler, value) =>
+const decorate = (handler, value) => (
   filters.has(handler)
     ? filters.get(handler)(value)
     : value
+)
 
 export default f.curry(decorate)
