267d6cdc18fd11432f47478e257f39e80b4ef55c
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:07:30 -0300

    refactor(is): encapsulando a logica dentro de um grupo de instrucoes

--- standard/f/src/is.js
+++ standard/f/src/is.js
@@ -4,9 +4,10 @@ import F from './F'
 import isNil from './isNil'
 import or from './or'
 
-const is = (Klass, value) =>
+const is = (Klass, value) => (
   isNil(value)
     ? F()
     : or(equal(value.constructor, Klass), value instanceof Klass)
+)
 
 export default curry(is)
