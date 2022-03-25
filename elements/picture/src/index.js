b555ba52686a1c112bda358da97e6fdbf9413523
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:29:09 -0300

    refactor(picture): encapsulando a logica dentro de um grupo de instrucoes

--- elements/picture/src/index.js
+++ elements/picture/src/index.js
@@ -2,7 +2,8 @@ import * as f from '@kuba/f'
 import skeleton from '@kuba/skeleton'
 import component from './component'
 
-export default (props, children) =>
+export default (props, children) => (
   f.len([...children])
     ? component(props, children)
     : skeleton.picture(props)
+)
