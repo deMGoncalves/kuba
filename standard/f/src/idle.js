8acdf761f0c58df811b2c3e7a28f2916b325dc4f
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:09:02 -0300

    refactor(idle): encapsulando a logica dentro de um grupo de instrucoes

--- standard/f/src/idle.js
+++ standard/f/src/idle.js
@@ -1,9 +1,10 @@
 import curry from './curry'
 
 const idle = (func) =>
-  (...args) =>
+  (...args) => (
     'requestIdleCallback' in window
       ? window.requestIdleCallback(() => func(...args))
       : window.setTimeout(() => func(...args), 0)
+  )
 
 export default curry(idle)
