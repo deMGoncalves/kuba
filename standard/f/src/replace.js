08095ed92dca8cfbe4e45dc7fb0f556593c1ff0f
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-25 14:05:50 -0300

    refactor(replace): encapsulando a logica dentro de um grupo de instrucoes

--- standard/f/src/replace.js
+++ standard/f/src/replace.js
@@ -3,9 +3,10 @@ import indexOf from './indexOf'
 import is from './is'
 import splice from './splice'
 
-const replace = (literal, x, y) =>
+const replace = (literal, x, y) => (
   is(String, literal)
     ? literal.replace(x, y)
     : splice(literal, indexOf(literal, x), 1, y)
+)
 
 export default curry(replace)
