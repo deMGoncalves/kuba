590eb2beb98bc4836dbc8e0e35d1b62f4784fb2b
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-28 17:40:07 -0300

    refactor(flex): simplificando a construcao da variacao flex

--- pages/shape/src/especificacao/flex.js
+++ pages/shape/src/especificacao/flex.js
@@ -1,10 +1,10 @@
 import * as f from '@kuba/f'
 import { paint, repaint } from '@kuba/h'
-import component from '@kuba/shape/src/especificacao/template'
 import effect from './effect'
 import setData from './setData'
+import template from './template'
 
-@paint(component)
+@paint(template)
 @effect
 class Flex {
   #pristine
@@ -27,10 +27,10 @@ class Flex {
   }
 
   @repaint
-  [effect.onChange] (valor) {
+  [effect.onChange] (shape) {
     this.#pristine = f.F()
-    this.#valor = valor
-    setData(valor)
+    this.#valor = shape.flex?.valor
+    setData(this)
     return this
   }
 }
