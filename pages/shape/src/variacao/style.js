784a6648e784f7d2791d7d3961bbd558b0b2cceb
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-28 15:53:30 -0300

    refactor(variacao): template para padronizar os tamanhos

--- pages/shape/src/variacao/style.js
+++ pages/shape/src/variacao/style.js
@@ -23,4 +23,24 @@ export default styled.style`
       grid-template-columns: repeat(3, 1fr);
     }
   }
+
+  .variacao__section {
+    align-items: flex-start;
+    background-color: #fff;
+    border-radius: var(--border-radius-sm);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-lg);
+    padding: var(--spacing_inset-xs);
+  }
+
+  .variacao__dl {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-quarck);
+  }
+
+  .variacao__text {
+    line-height: var(--line-height-lg) !important;
+  }
 `
---
+++ pages/shape/src/variacao/template.jsx
@@ -0,0 +1,12 @@
+import h from '@kuba/h'
+import style from './style'
+import text from '@kuba/text'
+
+export default (variacao) =>
+  <section className={style.variacao__section}>
+    <text.Data master darker xs bold>{variacao.valor}</text.Data>
+    <dl className={style.variacao__dl}>
+      <text.Dt className={style.variacao__text} master darker xxxs medium>Tamanho</text.Dt>
+      <text.Dd className={style.variacao__text} master xxxs>Medida em polegadas do comprimento de desenvolvido do shape</text.Dd>
+    </dl>
+  </section>
