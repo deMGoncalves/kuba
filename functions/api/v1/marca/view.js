d3d7aa9ec0bedba611ebd4661a4f3612437d98fa
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-28 23:51:31 -0300

    refactor(view): simplificando o codigo para tentativa de deixar o codigo bonito

--- functions/api/v1/marca/view.js
+++ functions/api/v1/marca/view.js
@@ -2,21 +2,18 @@ import { createClient } from '@kuba/supabase'
 
 export async function onRequestPost (context) {
   const supabase = createClient(context)
-
-  const {
-    slug
-  } = await context.request.json()
+  const params = await context.request.json()
 
   const { data } = await supabase
     .from('marca')
     .select('views')
-    .eq('slug', slug)
+    .eq('slug', params.slug)
     .single()
 
   const { error } = await supabase
     .from('marca')
     .update({ views: data.views + 1 })
-    .eq('slug', slug)
+    .eq('slug', params.slug)
 
   return new Response(JSON.stringify({ error }))
 }
