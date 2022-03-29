5243c68856dbb554c17a076b2b8bb99e2f217b06
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-28 23:44:35 -0300

    refactor(marca): tentativa de deixar o codigo simples

--- functions/api/v1/marca/index.js
+++ functions/api/v1/marca/index.js
@@ -2,18 +2,12 @@ import { createClient } from '@kuba/supabase'
 
 export async function onRequestPost (context) {
   const supabase = createClient(context)
-
-  const {
-    slug
-  } = await context.request.json()
+  const params = await context.request.json()
 
   const { data, error } = await supabase
     .from('marca')
-    .select(`
-      *,
-      origem (*)
-    `)
-    .eq('slug', slug)
+    .select('*, origem (*)')
+    .eq('slug', params.slug)
     .single()
 
   return new Response(JSON.stringify({ data, error }))
