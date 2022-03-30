4e3d9a7cb93cba0e98d1852047a657150eccf9a5
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-30 13:11:06 -0300

    refactor(especificacao): este component foi substituido por toppings

--- blocks/filter/src/especificacao.js
+++
@@ -1,8 +0,0 @@
-import styled from '@kuba/styled'
-
-export default styled.div`
-  border-left: var(--border-width-hairline) solid var(--color-master-light);
-  display: flex;
-  gap: var(--spacing_inset-xs);
-  padding-left: var(--spacing_inset-xs);
-`
