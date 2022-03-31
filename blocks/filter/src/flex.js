5b3d48c59e619dc88d2e218c7d131e9578f93cd7
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-31 14:50:39 -0300

    refactor(flex): precisamos apenas extender o select

---
+++ blocks/filter/src/flex.js
@@ -0,0 +1,47 @@
+import * as f from '@kuba/f'
+import { paint, repaint } from '@kuba/h'
+import schema from './schema.json'
+import Select, { component, Option, scroll } from './select'
+
+@paint(component)
+class Flex extends Select {
+  #options
+
+  get descricao () {
+    return 'Qual nível de flexibilidade você prefere?'
+  }
+
+  get key () {
+    return 'flex'
+  }
+
+  get nome () {
+    return 'Flex'
+  }
+
+  get options () {
+    return this.#options ??= f.map(schema.flex, Option.create(this))
+  }
+
+  @repaint
+  @scroll.unlock
+  close () {
+    super.close()
+    return this
+  }
+
+  @repaint
+  @scroll.lock
+  open () {
+    super.open()
+    return this
+  }
+
+  @repaint
+  [Option.onChange] () {
+    super[Option.onChange]()
+    return this
+  }
+}
+
+export default Flex
--- blocks/filter/src/flex/header/component.jsx
+++
@@ -1,9 +0,0 @@
-import h from '@kuba/h'
-import style from './style'
-import text from '@kuba/text'
-
-export default () =>
-  <header className={style.header}>
-    <text.Strong className={style.header__strong} master darker xxxs medium>Flex</text.Strong>
-    <text.P className={style.header__p} master darker xs bold>Qual nível de flexibilidade você prefere?</text.P>
-  </header>
