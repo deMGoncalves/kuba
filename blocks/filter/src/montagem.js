cc433bf6cf3e1d8b1c637e4f5baf1bf7931a6d8b
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-31 15:15:05 -0300

    refactor(montagem): precisamos apenas extendeer o select

---
+++ blocks/filter/src/montagem.js
@@ -0,0 +1,47 @@
+import * as f from '@kuba/f'
+import { paint, repaint } from '@kuba/h'
+import schema from './schema.json'
+import Select, { component, Option, scroll } from './select'
+
+@paint(component)
+class Montagem extends Select {
+  #options
+
+  get descricao () {
+    return 'Escolha a maneira como o truck é anexado ao shape'
+  }
+
+  get key () {
+    return 'montagem'
+  }
+
+  get nome () {
+    return 'Montagem'
+  }
+
+  get options () {
+    return this.#options ??= f.map(schema.montagem, Option.create(this))
+  }
+
+  @repaint
+  @scroll.unlock
+  close () {
+    super.close()
+    return this
+  }
+
+  @repaint
+  @scroll.lock
+  open () {
+    super.open()
+    return this
+  }
+
+  @repaint
+  [Option.onChange] () {
+    super[Option.onChange]()
+    return this
+  }
+}
+
+export default Montagem
--- blocks/filter/src/montagem/header/component.jsx
+++
@@ -1,9 +0,0 @@
-import h from '@kuba/h'
-import style from './style'
-import text from '@kuba/text'
-
-export default () =>
-  <header className={style.header}>
-    <text.Strong className={style.header__strong} master darker xxxs medium>Flex</text.Strong>
-    <text.P className={style.header__p} master darker xs bold>Escolha a maneira como o truck é anexado ao shape</text.P>
-  </header>
