b614903d78ea80beeae57af68f1fa1b13a96ae5c
deMGoncalves <cleber.engineer@hotmail.com>
2022-03-31 21:49:39 -0300

    refactor(material): precisamos apenas extender o shelf

--- buckets/web/src/home/ml.js
+++ buckets/web/src/home/ml.js
@@ -1,6 +1,7 @@
 import * as f from '@kuba/f'
 
 const marca = JSON.parse(f.or(localStorage.getItem('_ml.marca'), '{}'))
+const material = JSON.parse(f.or(localStorage.getItem('_ml.material'), '{}'))
 const tamanho = JSON.parse(f.or(localStorage.getItem('_ml.tamanho'), '{}'))
 
 export default {
@@ -10,6 +11,16 @@ export default {
       .pipe(f.entries)
       .pipe(f.reduce(f.__, (x, y) => f.gte(x[1], y[1]) ? x : y, []))
       .pipe(f.prop('[0]'))
+      .done()
+  },
+
+  get material () {
+    return f
+      .from(material)
+      .pipe(f.entries)
+      .pipe(f.reduce(f.__, (x, y) => f.gte(x[1], y[1]) ? x : y, []))
+      .pipe(f.prop('[0]'))
+      .pipe(f.or(f.__, 'Maple Canadense'))
       .done()
   },
 
