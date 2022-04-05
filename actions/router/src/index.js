e129d37418b4df65049df51f5e2aaf230820662d
deMGoncalves <cleber.engineer@hotmail.com>
2022-04-05 12:35:28 -0300

    chore(router): agora consigo saber o hash atual da pagina

--- actions/router/src/index.js
+++ actions/router/src/index.js
@@ -1,6 +1,7 @@
 export { default } from './router'
 export { default as args } from './args'
 export { default as blueprint } from './blueprint'
+export { default as hash } from './hash'
 export { default as params } from './params'
 export { default as redirectTo } from './redirectTo'
 export { default as urlFor } from './urlFor'
