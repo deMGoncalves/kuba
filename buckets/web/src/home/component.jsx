b41dbb7855b4394111f7b5f90ee9b89c9306bf93
deMGoncalves <cleber.engineer@hotmail.com>
2022-04-17 18:10:23 -0300

    refactor(home): nao preciso fazer o carregamento async

--- buckets/web/src/home/component.jsx
+++ buckets/web/src/home/component.jsx
@@ -1,14 +1,19 @@
 import h, { Fragment } from '@kuba/h'
 import Hero from './hero'
-import Lazy from '@kuba/lazy'
+import Marca from './marca'
+import Material from './material'
+import Pro from './pro'
 import Site from '@kuba/site'
+import Zone from '@kuba/zone'
 
 export default () =>
   <Site>
     <Fragment slot='main'>
       <Hero />
-      <Lazy require={() => import('./material' /* webpackChunkName: 'material' */)} />
-      <Lazy require={() => import('./marca' /* webpackChunkName: 'marca' */)} />
-      <Lazy require={() => import('./pro' /* webpackChunkName: 'pro' */)} />
+      <Material />
+      <Zone>
+        <Marca />
+        <Pro />
+      </Zone>
     </Fragment>
   </Site>
