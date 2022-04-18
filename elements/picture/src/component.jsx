7d65e8d7ab4e28d319f5ca00c614acc8a5fc314c
deMGoncalves <cleber.engineer@hotmail.com>
2022-04-18 00:53:06 -0300

    perf(picture): nem todas as imagens devem ser lazy

--- elements/picture/src/component.jsx
+++ elements/picture/src/component.jsx
@@ -6,5 +6,5 @@ import style from './style'
 export default (props, children) =>
   <picture className={[style.picture, props.className]} onClick={props.onClick}>
     {children}
-    <img className={style.picture__img} loading='lazy' alt={f.or(props.alt, settings.app.name)} height={props.height} width={props.width} />
+    <img className={style.picture__img} loading='auto' alt={f.or(props.alt, settings.app.name)} height={props.height} width={props.width} />
   </picture>
