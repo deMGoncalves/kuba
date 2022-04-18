a76c4c286015dbf868ab1be81a23d6eaa19aa4ad
deMGoncalves <cleber.engineer@hotmail.com>
2022-04-18 00:22:04 -0300

    refactor(comparar): troca do enderedo de imagem

--- pages/comparar/src/thumbnail/thumbnail.js
+++ pages/comparar/src/thumbnail/thumbnail.js
@@ -5,11 +5,11 @@ import component from './component'
 @paint(component)
 class Thumbnail {
   get master () {
-    return Shape.master?.thumbnail
+    return `https://ik.imagekit.io/deMGoncalves/${Shape.master?.thumbnail}?tr=w-457.5,h-571.867`
   }
 
   get slave () {
-    return Shape.slave?.thumbnail
+    return `https://ik.imagekit.io/deMGoncalves/${Shape.slave?.thumbnail}?tr=w-457.5,h-571.867`
   }
 }
 
