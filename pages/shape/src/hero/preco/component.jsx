6197a045c6ff6e1db34bca2ef2d1b6ed8cf6223a
deMGoncalves <cleber.engineer@hotmail.com>
2022-04-19 09:54:54 -0300

    style(price): aviso do preco nao tem contraste

--- pages/shape/src/hero/preco/component.jsx
+++ pages/shape/src/hero/preco/component.jsx
@@ -9,7 +9,7 @@ export default (preco) =>
       <div className={style.preco}>
         <text.Span className={style.preco__text} master darker xxxs medium>Preço</text.Span>
         <text.Data className={style.preco__text} master darker md bold>{preco.valor}</text.Data>
-        <text.P className={style.preco__text} danger dark xxxs>Preço sujeito a alteração no site indicado</text.P>
+        <text.P className={style.preco__text} danger darker xxxs>Preço sujeito a alteração no site indicado</text.P>
       </div>
     </Hide>
   </>
