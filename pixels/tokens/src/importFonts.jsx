7cf561fd6fd44873de023b97d5f4802bc66f4813
deMGoncalves <cleber.engineer@hotmail.com>
2022-04-27 09:48:53 -0300

    refactor(tokens): os preconnect deve ficar no markup

--- pixels/tokens/src/importFonts.jsx
+++ pixels/tokens/src/importFonts.jsx
@@ -5,8 +5,6 @@ agent.isUser && (
   render(
     document.head,
     <>
-      <link rel='preconnect' href='https://fonts.googleapis.com' />
-      <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
       <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap' />
       <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Squada+One:wght@400;500;700&display=swap' />
     </>
