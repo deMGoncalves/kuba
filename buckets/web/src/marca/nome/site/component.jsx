98f00c562ae60b859ad2ae6046aaa7bcebaf8054
deMGoncalves <cleber.engineer@hotmail.com>
2022-04-27 17:44:33 -0300

    chore(web): agora a pagina marca fica no workspace pages

--- buckets/web/src/marca/nome/site/component.jsx
+++
@@ -1,7 +0,0 @@
-import h from '@kuba/h'
-import link from '@kuba/link'
-
-export default (site) =>
-  <link.Primary uid='site' href={site.href} target='_blank' dark xxxs medium>
-    {site.content}
-  </link.Primary>
