13552b592116ce427695799fb90242e72c78964b
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-03 11:22:07 -0300

    refactor(marca): agora usaremos o discovery para persistir a calibragem da marca

--- pages/shape/src/ml/marca.js
+++ pages/shape/src/ml/marca.js
@@ -1,23 +1,6 @@
-import * as f from '@kuba/f'
+import discovery from '@kuba/discovery'
 
-export default (shape) => {
-  const ml = JSON.parse(f.or(localStorage.getItem('_ml.marca'), '{}'))
-  const champion = shape.marca.nome
-  const losers = f
-    .from(ml)
-    .pipe(f.keys)
-    .pipe(f.filter(f.__, f.different(champion)))
-    .done()
-
-  f.forEach(losers, (loser) => (
-    f.assign(ml, {
-      [loser]: f.max(f.dec(f.or(ml[loser], 0)), 0)
-    })
-  ))
-
-  f.assign(ml, {
-    [champion]: f.min(f.add(f.or(ml[champion], 0), 3), 9)
-  })
-
-  localStorage.setItem('_ml.marca', JSON.stringify(ml))
-}
+export default (shape) =>
+  discovery
+    .serie('marca')
+    .toScore([shape.marca.nome])
