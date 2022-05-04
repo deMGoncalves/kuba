1cc2eac281e66f40544cfdd83ef9a94ee67bd70a
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-04 11:57:49 -0300

    refactor(relacionado): so sera carregado quando o effect e zone estiver ok

--- pages/shape/src/relacionado/storage.js
+++ pages/shape/src/relacionado/storage.js
@@ -3,30 +3,40 @@ import global, { useEffect } from '@kuba/global'
 import http, { api } from '@kuba/http'
 import middleware from '@kuba/middleware'
 
-const { onError, onResponse } = f.dunder
+const { didMount, onError, onResponse } = f.dunder
 
-const storage = middleware((target) => (
-  useEffect(() => (
-    http
-      .post(`${api.url}/shape/shelf`)
-      .body({
-        material: f
-          .from(global.shape.material)
-          .pipe(f.or(f.__, []))
-          .pipe(f.map(f.__, f.prop('valor')))
-          .done(),
-        tamanho: [global.shape.tamanho.valor],
-        size: 4,
-        page: 1
-      })
-      .json()
-      .then(({ data, error }) => (
-        error
-          ? target[onError]()
-          : target[onResponse](data)
-      ))
+const storage = middleware((target) => {
+  const mount = new Promise((resolve) => (
+    target[didMount] = resolve
   ))
-))
+
+  const effect = new Promise((resolve) =>
+    useEffect(resolve)
+  )
+
+  Promise
+    .all([mount, effect])
+    .then(() => (
+      http
+        .post(`${api.url}/shape/shelf`)
+        .body({
+          material: f
+            .from(global.shape.material)
+            .pipe(f.or(f.__, []))
+            .pipe(f.map(f.__, f.prop('valor')))
+            .done(),
+          tamanho: [global.shape.tamanho.valor],
+          size: 4,
+          page: 1
+        })
+        .json()
+        .then(({ data, error }) => (
+          error
+            ? target[onError]()
+            : target[onResponse](data)
+        ))
+    ))
+})
 
 f.assign(storage, {
   onError,
