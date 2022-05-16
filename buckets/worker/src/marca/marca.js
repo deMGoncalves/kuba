0c4d0a79e10f323f5ecc740d87ef309e42694710
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-16 09:32:57 -0300

    chore(worker): configuracao para tentar fazer o deploy

--- buckets/worker/src/marca/marca.js
+++ buckets/worker/src/marca/marca.js
@@ -1,9 +1,6 @@
-import { createClient } from '@kuba/supabase'
+import supabase from '@kuba/supabase'
 
 export default async function (request) {
-  const env = { API_KEY, API_URL }
-  const supabase = createClient({ env })
-
   const { data, error } = await supabase
     .from('marca')
     .select('*, origem (*)')
