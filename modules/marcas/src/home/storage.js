db66f79001cfd66ba965c6f9ea71c102f0a0f65c
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-16 16:56:32 -0300

    refactor(marcas): troca da api e padronizacao de resposta

--- modules/marcas/src/home/storage.js
+++ modules/marcas/src/home/storage.js
@@ -1,14 +1,23 @@
 import * as f from '@kuba/f'
-import { setGlobal } from '@kuba/global'
 import http, { api } from '@kuba/http'
 import middleware from '@kuba/middleware'
 
-const storage = () =>
-  http
-    .post(`${api.url}/marca/shelf`)
-    .json()
-    .then(({ data: marcas, error }) => (
-      f.not(error) && setGlobal({ marcas })
-    ))
+const { onError, onResponse } = f.dunder
 
-export default middleware(storage)
+const storage = middleware((target) =>
+  http
+    .get(`${api.worker}/marca/shelf`)
+    .json()
+    .then(({ data, error }) => (
+      error
+        ? target[onError]()
+        : target[onResponse](data)
+    ))
+)
+
+f.assign(storage, {
+  onError,
+  onResponse
+})
+
+export default storage
