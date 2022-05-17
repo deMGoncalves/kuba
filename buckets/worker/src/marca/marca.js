bde8fa4d8e0d53de8e7e03f9f77c7d995ab5401a
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-17 10:52:57 -0300

    refactor(worker): padronizando o codigo

--- buckets/worker/src/marca/marca.js
+++ buckets/worker/src/marca/marca.js
@@ -1,10 +1,11 @@
 import supabase from '@kuba/supabase'
 
 export default async function (request) {
+  const { slug } = request.params
   const { data, error } = await supabase
     .from('marca')
     .select('*, origem (*)')
-    .eq('slug', request.params.slug)
+    .eq('slug', slug)
     .single()
 
   return new Response(JSON.stringify({ data, error }))
