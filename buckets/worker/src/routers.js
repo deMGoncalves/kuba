638bdc47645fdc9932425d979ba3a27581612eb2
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-17 09:03:30 -0300

    refactor(worker): configuracao da rota para o servico de view

--- buckets/worker/src/routers.js
+++ buckets/worker/src/routers.js
@@ -6,6 +6,7 @@ const router = Router()
 router.get('/marca/shelf', marca.shelf)
 router.get('/marca/sitemap.xml', marca.sitemap)
 router.get('/marca/:slug', marca)
+router.get('/marca/:slug/view', marca.view)
 
 router.all('*', () =>
   new Response('404, not found!', { status: 404 })
