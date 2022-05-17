fc18ab818cbcc561d51bcf74bac652db6e70c907
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-17 12:09:21 -0300

    refactor(marca): nao usaremos a paginacao

--- pages/marca/src/component.jsx
+++ pages/marca/src/component.jsx
@@ -1,7 +1,6 @@
 import Breadcrumb from './breadcrumb'
 import Descricao from './descricao'
 import h, { Fragment } from '@kuba/h'
-import More from './more'
 import Nome from './nome'
 import Shelf from './shelf'
 import Site from '@kuba/site'
@@ -12,7 +11,6 @@ export default () =>
     <Fragment slot='main'>
       <Nome />
       <Shelf />
-      <More />
       <Descricao />
     </Fragment>
   </Site>
--- pages/marca/src/more/component.jsx
+++
@@ -1,6 +0,0 @@
-import h from '@kuba/h'
-import style from './style'
-import text from '@kuba/text'
-
-export default (more) =>
-  <text.Button className={style.more} onClick={() => more.next()} visible={more.visible} master darker xxxs bold>{more.content}</text.Button>
