5dd9a7cfd6a598ea282781e85e0de92ad58fd364
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-20 07:51:04 -0300

    chore(echo): movendo o pacote para o workspace infra

--- actions/echo/src/blob.js
+++
@@ -1,4 +0,0 @@
-export default new Blob(
-  ['self.addEventListener(\'message\', e => self.postMessage(e.data))'],
-  { type: 'application/javascript' }
-)
--- actions/echo/src/index.js
+++
@@ -1,7 +0,0 @@
-import emit from './emit'
-import on from './on'
-
-export default {
-  emit,
-  on
-}
