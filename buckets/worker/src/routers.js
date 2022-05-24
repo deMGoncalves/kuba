6119716e2204656ca5fb463d00875657bf2d3d56
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-24 16:03:12 -0300

    refactor(worker): removendo as rotas do contexto de marca e shape

--- buckets/worker/src/routers.js
+++ buckets/worker/src/routers.js
@@ -5,12 +5,10 @@ import shelf from './shelf'
 
 const router = Router()
 
-router.get('/marca/shelf', marca.shelf)
 router.get('/marca/sitemap.xml', marca.sitemap)
 router.get('/marca/:slug', marca)
 router.get('/marca/:slug/view', marca.view)
 
-router.post('/shape/shelf', shape.shelf)
 router.get('/shape/search', shape.search)
 router.get('/shape/sitemap.xml', shape.sitemap)
 router.get('/shape/marca/:slug', shape.marca)
