45af0fc32a739414a69a3bb729d7f903d30372a9
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-24 15:59:03 -0300

    refactor(marca): nao usaremos este servico neste contexto

--- buckets/worker/src/marca/index.js
+++ buckets/worker/src/marca/index.js
@@ -1,11 +1,9 @@
 import * as f from '@kuba/f'
 import marca from './marca'
-import shelf from './shelf'
 import sitemap from './sitemap.xml'
 import view from './view'
 
 f.assign(marca, {
-  shelf,
   sitemap,
   view
 })
