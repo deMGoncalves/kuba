216c87ecf76b556ed4002a0b10065f40496ab58c
deMGoncalves <cleber.engineer@hotmail.com>
2022-05-24 15:42:36 -0300

    refactor(shape): nao termos este servico neste contexto

--- buckets/worker/src/shape/index.js
+++ buckets/worker/src/shape/index.js
@@ -2,14 +2,12 @@ import * as f from '@kuba/f'
 import marca from './marca'
 import search from './search'
 import shape from './shape'
-import shelf from './shelf'
 import sitemap from './sitemap.xml'
 import view from './view'
 
 f.assign(shape, {
   marca,
   search,
-  shelf,
   sitemap,
   view
 })
