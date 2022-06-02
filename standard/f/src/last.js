579cbeeaaebeb966854b5569199059853af709c8
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-02 10:16:29 -0300

    feat(f): inclusao do comportamento do dunder method no last

--- standard/f/src/last.js
+++ standard/f/src/last.js
@@ -1,8 +1,11 @@
 import curry from './curry'
 import dec from './dec'
+import dunder from './dunder'
 import len from './len'
 
-const last = (array) =>
-  array[dec(len(array))]
+const evaluate = dunder('last')
+
+const last = (target) =>
+  evaluate(target)[dec(len(target))]
 
 export default curry(last)
