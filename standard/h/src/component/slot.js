4971432438a6eb0b24c62dfd4786fef47bc51265
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-06-05 14:46:52 -0300

    refactor(h): padronizacao a construcao dos components

--- standard/h/src/component/slot.js
+++ standard/h/src/component/slot.js
@@ -1,10 +1,7 @@
 import * as f from '@kuba/f'
 
 export default (children) =>
-  new Proxy(
-    children,
-    {
-      get: (target, name) =>
-        f.or(target[name], f.filter(target, f.compose(f.equal(name), f.prop('slot'))))
-    }
-  )
+  new Proxy(children, {
+    get: (target, name) =>
+      f.or(target[name], f.filter(target, f.compose(f.equal(name), f.prop('slot'))))
+  })
