d5e739dd64d00d06d9aeaa6eddf805e800517673
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-06-05 10:46:40 -0300

    refactor(h): separando o evaluate do reflow

--- standard/h/src/element/children/reflow/reflow.js
+++ standard/h/src/element/children/reflow/reflow.js
@@ -1,31 +1,14 @@
 import * as f from '@kuba/f'
-import Added from './added'
-import Different from './different'
-import DifferentEntity from './differentEntity'
-import Equal from './equal'
-import EqualEntity from './equalEntity'
-import EqualUID from './equalUID'
-import Removed from './removed'
+import evaluate from './evaluate'
 
-export default function (children) {
-  const evaluate = f.cond(
-    [Added.is, Added.exec(children)],
-    [Removed.is, Removed.exec(children)],
-    [Different.is, Different.exec(children)],
-    [EqualUID.is, EqualUID.exec(children)],
-    [EqualEntity.is, EqualEntity.exec(children)],
-    [DifferentEntity.is, DifferentEntity.exec(children)],
-    [Equal.is, Equal.exec(children)]
-  )
-
-  return new Promise((resolve) => (
+export default (children, newChildren) =>
+  new Promise((resolve) => (
     f
-      .from([...arguments])
+      .from([children, newChildren])
       .pipe(f.map(f.__, f.toArray))
       .pipe(f.apply(f.zip))
-      .pipe(f.forEach(f.__, f.apply(evaluate)))
+      .pipe(f.forEach(f.__, f.apply(evaluate(children))))
       .done(),
 
     resolve()
   ))
-}
