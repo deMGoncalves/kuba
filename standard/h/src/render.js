9fcfaea48d7f2c2c8907d0eb04811edd11e1b4a1
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-06-05 10:17:08 -0300

    refactor(h): padronizacao do codigo

--- standard/h/src/render.js
+++ standard/h/src/render.js
@@ -1,9 +1,9 @@
 import * as f from '@kuba/f'
 
-const render = (element, ...children) => {
+const render = (element, ...children) => (
   Promise
     .all(f.map(children, child => child.mount()))
     .then(f.frame(children => element.append(...children)))
-}
+)
 
 export default f.curry(f.arity(2, render))
