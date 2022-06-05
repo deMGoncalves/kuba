2171927c89611a5912779e3724737bfcd045ec9a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-06-05 10:13:38 -0300

    refactor(h): colocar apenas o append em frame

--- standard/h/src/render.js
+++ standard/h/src/render.js
@@ -3,7 +3,7 @@ import * as f from '@kuba/f'
 const render = (element, ...children) => {
   Promise
     .all(f.map(children, child => child.mount()))
-    .then(children => element.append(...children))
+    .then(f.frame(children => element.append(...children)))
 }
 
-export default f.curry(f.arity(2, f.frame(render)))
+export default f.curry(f.arity(2, render))
