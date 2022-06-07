c294e8d36ca75a9480d9e9e2ed2af8c68a8e9a62
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-07 09:28:09 -0300

    perf(h): inclusao do mapper na memoizacao

--- standard/h/src/element/attributes/mapper.js
+++ standard/h/src/element/attributes/mapper.js
@@ -1,9 +1,12 @@
 import * as f from '@kuba/f'
 
-export default (props) =>
-  new Map(
-    f.filter(
-      f.entries(props),
-      f.compose(f.not, f.test(/^(?<attributes>className|is|slot|uid|on[A-Z].+)$/), f.prop('[0]'))
-    )
+const mapper = f.memoize((props) => (
+  f.filter(
+    f.entries(props),
+    f.compose(f.not, f.test(/^(?<attributes>className|is|slot|uid|on[A-Z].+)$/), f.prop('[0]'))
   )
+))
+
+export default (props) => (
+  new Map(mapper(props))
+)
