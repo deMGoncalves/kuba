9369b6577512e32d6d640decd9df55d3ea8a90a7
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-07 09:01:33 -0300

    perf(avatar): inclusao do memoize no merge da props com o style

--- elements/avatar/src/merge.js
+++ elements/avatar/src/merge.js
@@ -1,2 +1,13 @@
-export default (props, style) =>
-  ({ ...props, className: [style.avatar, style[props.small && 'small'], style[props.large && 'large'], props.className] })
+import * as f from '@kuba/f'
+
+const merge = (props, style) => ({
+  ...props,
+  className: [
+    style.avatar,
+    style[props.small && 'small'],
+    style[props.large && 'large'],
+    props.className
+  ]
+})
+
+export default f.memoize(merge)
