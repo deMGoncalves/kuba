973a5c00c3c764b18a21b8af85faf20e89629cd2
Cleber / <cleber.engineer@hotmail.com>
2022-06-07 00:48:13 -0300

    Update mapper.js

--- standard/h/src/element/attributes/filter/mapper.js
+++ standard/h/src/element/attributes/filter/mapper.js
@@ -1,8 +1,11 @@
 import * as f from '@kuba/f'
 
-export default (name) =>
+const mapper = (name) => (
   f
     .from(name)
     .pipe(f.exec(/(?<attribute>[a-z-]+):?(?<filter>[a-z]+)?$/i))
     .pipe(f.slice(f.__, 1, 3))
     .done()
+)
+
+export default f.memoize(mapper)
