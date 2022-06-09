8e4a1d4f24f756a64b34bb6db9fc2585e73b5157
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-09 10:29:19 -0300

    style(f): padroniacao da taxonomia da funcao arity

--- standard/f/src/arity.js
+++ standard/f/src/arity.js
@@ -1,11 +1,11 @@
 import curry from './curry'
 
-const arity = (n, func) => {
+const arity = (n, target) => {
   switch (n) {
-    case 0: return () => func()
-    case 1: return function (_a) { return func(...arguments) }
-    case 2: return function (_a, _b) { return func(...arguments) }
-    case 3: return function (_a, _b, _c) { return func(...arguments) }
+    case 0: return () => target()
+    case 1: return function (_a) { return target(...arguments) }
+    case 2: return function (_a, _b) { return target(...arguments) }
+    case 3: return function (_a, _b, _c) { return target(...arguments) }
     default: throw new Error('O primeiro argumento para arity deve ser um número inteiro não negativo e não superior a três')
   }
 }
