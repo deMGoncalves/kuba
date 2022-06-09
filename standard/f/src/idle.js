1082e369b4b12798d76ea2cf1bf7d348ccdc9fe8
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-09 12:14:50 -0300

    style(f): padronizacao da taxonomida da funcao idle

--- standard/f/src/idle.js
+++ standard/f/src/idle.js
@@ -1,10 +1,10 @@
 import curry from './curry'
 
-const idle = (func) =>
+const idle = (target) =>
   (...args) => (
     'requestIdleCallback' in window
-      ? window.requestIdleCallback(() => func(...args))
-      : window.setTimeout(() => func(...args), 0)
+      ? window.requestIdleCallback(() => target(...args))
+      : window.setTimeout(() => target(...args), 0)
   )
 
 export default curry(idle)
