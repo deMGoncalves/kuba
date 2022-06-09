0840373310247cf6a8d53e2c5a1527404aa15880
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-09 10:15:31 -0300

    style(f): padronizacao da taxonomida da funcao add

--- standard/f/src/add.js
+++ standard/f/src/add.js
@@ -2,7 +2,7 @@ import arity from './arity'
 import curry from './curry'
 import reduce from './reduce'
 
-const add = (x, ...y) =>
-  reduce(y, (a, b) => a + b, x)
+const add = (x, ...args) =>
+  reduce(args, (a, b) => a + b, x)
 
 export default curry(arity(2, add))
