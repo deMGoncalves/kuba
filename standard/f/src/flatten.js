1c57024da5f151da93f8d2a62638c8be2d2c42cc
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-09 11:54:28 -0300

    style(f): padronizacao da taxonomia da funcao flatten

--- standard/f/src/flatten.js
+++ standard/f/src/flatten.js
@@ -1,7 +1,7 @@
 import arity from './arity'
 import curry from './curry'
 
-const flatten = (array, depth = Infinity) =>
-  array.flat(depth)
+const flatten = (target, depth = Infinity) =>
+  target?.flat?.(depth)
 
 export default curry(arity(1, flatten))
