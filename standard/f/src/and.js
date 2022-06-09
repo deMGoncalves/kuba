27c3eb3d76fac0aedfac210bf95dba3da0285a96
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-09 10:21:36 -0300

    style(f): padronizacao da taxonomia da funcao and

--- standard/f/src/and.js
+++ standard/f/src/and.js
@@ -2,7 +2,7 @@ import arity from './arity'
 import curry from './curry'
 import reduce from './reduce'
 
-const and = (x, ...y) =>
-  reduce(y, (a, b) => a && b, x)
+const and = (x, ...args) =>
+  reduce(args, (a, b) => a && b, x)
 
 export default curry(arity(2, and))
