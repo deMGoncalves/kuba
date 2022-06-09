a11c06bab96f3481fc11c41307dee5ebcb5b82f1
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-09 10:41:23 -0300

    style(f): padronizacao da taxonomia da funcao concat

--- standard/f/src/concat.js
+++ standard/f/src/concat.js
@@ -2,7 +2,7 @@ import arity from './arity'
 import curry from './curry'
 import reduce from './reduce'
 
-const concat = (x, ...y) =>
-  reduce(y, (a, b) => a.concat(b), x)
+const concat = (x, ...args) =>
+  reduce(args, (a, b) => a.concat(b), x)
 
 export default curry(arity(2, concat))
