3f88933d981814e2b9269d98e1d9d74dd35f2094
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-10 12:19:55 -0300

    style(f): padronizacao da taxonomia da funcao unshifit

--- standard/f/src/unshift.js
+++ standard/f/src/unshift.js
@@ -2,7 +2,8 @@ import always from './always'
 import arity from './arity'
 import curry from './curry'
 
-const unshift = (array, ...args) =>
-  always(array)(array.unshift(...args))
+const unshift = (target, ...args) => (
+  always(target)(target.unshift(...args))
+)
 
 export default curry(arity(2, unshift))
