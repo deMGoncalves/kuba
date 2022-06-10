c6c52bedb7425a1b9f9ad1d6292ea793fa51da64
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-10 12:22:21 -0300

    style(f): padronizacao da taxonomia da funcao zip

--- standard/f/src/zip.js
+++ standard/f/src/zip.js
@@ -4,7 +4,8 @@ import map from './map'
 import max from './max'
 import repeat from './repeat'
 
-const zip = (x, y) =>
+const zip = (x, y) => (
   map(repeat(null, max(len(x), len(y))), (_, i) => [x[i], y[i]])
+)
 
 export default curry(zip)
