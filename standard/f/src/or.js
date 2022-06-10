d0821740d7bf8117339f2f34d8be7e2de8a7043d
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-06-09 23:02:02 -0300

    style(f): padronizacao da taxonomia da funcao or

--- standard/f/src/or.js
+++ standard/f/src/or.js
@@ -1,7 +1,8 @@
 import arity from './arity'
 import curry from './curry'
 
-const or = (x, ...args) =>
-  args.reduce((a, b) => a || b, x)
+const or = (x, ...target) => (
+  target?.reduce((a, b) => a || b, x)
+)
 
 export default curry(arity(2, or))
