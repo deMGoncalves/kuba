61b8c6b73ab1dcf72f2d20386dee235601206841
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-10 12:17:29 -0300

    style(f): padronizacao da taxonommia da funcao to string

--- standard/f/src/toString.js
+++ standard/f/src/toString.js
@@ -1,7 +1,8 @@
 import arity from './arity'
 import curry from './curry'
 
-const toString = (value, radix) =>
-  value.toString(radix)
+const toString = (target, radix) => (
+  target.toString(radix)
+)
 
 export default curry(arity(1, toString))
