5e935ccab52ab2843a14e3d612359b02628b616f
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-10 12:52:00 -0300

    refactor(f): inclusao de codigo defensivo para a funcao un shift

--- standard/f/src/unshift.js
+++ standard/f/src/unshift.js
@@ -3,7 +3,7 @@ import arity from './arity'
 import curry from './curry'
 
 const unshift = (target, ...args) => (
-  always(target)(target.unshift(...args))
+  always(target)(target?.unshift?.(...args))
 )
 
 export default curry(arity(2, unshift))
