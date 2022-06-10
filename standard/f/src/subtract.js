87d073c92d6130ddd07bf693a93aa21a131f6754
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-10 12:09:17 -0300

    style(f): padronizacao da taxonomida da funcao subtract

--- standard/f/src/subtract.js
+++ standard/f/src/subtract.js
@@ -2,7 +2,8 @@ import arity from './arity'
 import curry from './curry'
 import reduce from './reduce'
 
-const subtract = (x, ...y) =>
-  reduce(y, (a, b) => a - b, x)
+const subtract = (x, ...args) => (
+  reduce(args, (a, b) => a - b, x)
+)
 
 export default curry(arity(2, subtract))
