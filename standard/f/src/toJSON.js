60fbdc865a6e6e30ce77dc52a08677b617daabb3
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-15 14:30:03 -0300

    feat(f): inclusao da funco toJSON

---
+++ standard/f/src/toJSON.js
@@ -0,0 +1,10 @@
+import curry from './curry'
+import dunder from './dunder'
+import is from './is'
+
+const toJSON = (target) =>
+  is(String, target)
+    ? JSON.parse(target)
+    : dunder('toJSON', target)
+
+export default curry(toJSON)
