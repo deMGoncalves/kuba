c2674a29ca0e91f7256fd89fdb516303fc8500ef
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-06-20 15:28:16 -0300

    refactor(f): no internal nao podemos trabalhar com dunders

--- standard/f/src/first.js
+++ standard/f/src/first.js
@@ -1,4 +1,16 @@
 import curry from './curry'
-import first from './internal/first'
+import dunder from './dunder'
+import has from './has'
+import magic from './magic'
+import _first from './internal/first'
+
+const containsDunderFirstIn = has(magic('first'))
+const evaluate = dunder('first')
+
+const first = (target) => (
+  containsDunderFirstIn(target)
+    ? evaluate(target)
+    : _first(target)
+)
 
 export default curry(first)
--- standard/f/src/internal/first.js
+++ standard/f/src/internal/first.js
@@ -1,14 +1,5 @@
-import dunder from '../dunder'
-import has from '../has'
-import magic from './magic'
-
-const containsDunderFirstIn = has(magic('first'))
-const evaluate = dunder('first')
-
 const first = (target) => (
-  containsDunderFirstIn(target)
-    ? evaluate(target)
-    : target?.[0]
+  target?.[0]
 )
 
 export default first
