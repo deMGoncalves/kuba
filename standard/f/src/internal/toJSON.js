c1041048e4f6bb70ff7b4de6c20823124cdf160a
Cleber de Moraes Goncalves <cleber.engineer@hotmail.com>
2022-06-20 16:17:59 -0300

    refactor(f): removendo referencias ao dunder

--- standard/f/src/internal/toJSON.js
+++ standard/f/src/internal/toJSON.js
@@ -1,10 +1,5 @@
-import dunder from '../dunder'
-import is from './is'
-
 const toJSON = (target) => (
-  is(String, target)
-    ? JSON.parse(target)
-    : dunder('toJSON', target)
+  JSON.parse(target)
 )
 
 export default toJSON
--- standard/f/src/toJSON.js
+++ standard/f/src/toJSON.js
@@ -1,4 +1,14 @@
 import curry from './curry'
-import toJSON from './internal/toJSON'
+import dunder from './dunder'
+import is from './is'
+import _toJSON from './internal/toJSON'
+
+const evaluate = dunder('toJSON')
+
+const toJSON = (target) => (
+  is(String, target)
+    ? _toJSON(target)
+    : evaluate(target)
+)
 
 export default curry(toJSON)
