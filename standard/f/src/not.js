f9e1a35594f3f9efdc62df043e173499fe8fe48a
deMGoncalves <cleber.engineer@hotmail.com>
2022-06-21 09:30:26 -0300

    feat(f): agora conseguimos negar qualquer funcao

--- standard/f/src/not.js
+++ standard/f/src/not.js
@@ -1,4 +1,11 @@
+import * as f from '@kuba/f'
+import arity from './arity'
 import curry from './curry'
-import not from './internal/not'
+import _not from './internal/not'
 
-export default curry(not)
+const not = new Proxy(curry(_not), {
+  get: (_, key) =>
+    curry(arity(f[key].length, (...args) => _not(f[key](...args))))
+})
+
+export default not
