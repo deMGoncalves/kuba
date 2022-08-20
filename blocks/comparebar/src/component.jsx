918089be59709b68ad46a50c2a92e07a0664527a
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-20 19:27:25 -0300

    chore(comparebar): contrato para abrir e fechar o sidebar

--- blocks/comparebar/src/component.jsx
+++ blocks/comparebar/src/component.jsx
@@ -1,3 +1,4 @@
+import * as f from '@kuba/f'
 import Back from './back'
 import Footer from './footer'
 import h from '@kuba/h'
@@ -6,10 +7,18 @@ import Shelf from './shelf'
 import Side from '@kuba/side'
 import style from './style'
 
-export default (comparebar) =>
-  <Side className={style.comparebar} onClose={() => comparebar.close()} opened={comparebar.opened}>
+const opened = f.dunder('opened')
+
+const component = (comparebar) =>
+  <Side className={style.comparebar} onClose={() => comparebar.close()} opened={opened(comparebar)}>
     <Back onClick={() => comparebar.close()} />
     <Header />
     <Shelf />
     <Footer />
   </Side>
+
+f.assign(component, {
+  opened: f.dunder.opened
+})
+
+export default component
