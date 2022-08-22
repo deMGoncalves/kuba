010da7f2525470898179aae8dfdff35ebf977624
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-22 15:51:16 -0300

    refactor(footer): a validacao da exibicao do footer deve ser um contrato

--- blocks/comparebar/src/footer/component.jsx
+++ blocks/comparebar/src/footer/component.jsx
@@ -1,12 +1,21 @@
+import * as f from '@kuba/f'
 import h from '@kuba/h'
 import icon from '@kuba/icon'
 import link from '@kuba/link'
 import style from './style'
 
-export default (footer) =>
-  <footer className={style.footer} valid:isTruthy={footer.valid}>
+const valid = f.dunder('valid')
+
+const component = (footer) =>
+  <footer className={style.footer} valid:isTruthy={valid(footer)}>
     <link.Master className={style.footer__link} href={footer.href} onClick={() => footer.redirect()} darker xxxs bold>
       Ver comparação
       <icon.ArrowLongRight />
     </link.Master>
   </footer>
+
+f.assign(component, {
+  valid: f.dunder.valid
+})
+
+export default component
