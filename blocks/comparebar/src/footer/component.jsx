28c7220a572b8939ca56b85c21b487991604189b
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-23 10:46:37 -0300

    chore(comparebar): textos devem ficar no i18n e nao no component

--- blocks/comparebar/src/footer/component.jsx
+++ blocks/comparebar/src/footer/component.jsx
@@ -1,5 +1,6 @@
 import * as f from '@kuba/f'
 import h from '@kuba/h'
+import i18n from './i18n'
 import icon from '@kuba/icon'
 import link from '@kuba/link'
 import style from './style'
@@ -9,7 +10,7 @@ const valid = f.dunder('valid')
 const component = (footer) =>
   <footer className={style.footer} valid:isTruthy={valid(footer)}>
     <link.Master className={style.footer__link} href={footer.href} onClick={() => footer.redirect()} darker xxxs bold>
-      Ver comparação
+      {i18n.text}
       <icon.ArrowLongRight />
     </link.Master>
   </footer>
