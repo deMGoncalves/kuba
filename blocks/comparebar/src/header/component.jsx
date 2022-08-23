3a8fd5ccb5555326816de7fe36cb6bd047cbe979
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-23 11:01:01 -0300

    chore(comparebar): o title e description devem ficar no arquivo i18n

--- blocks/comparebar/src/header/component.jsx
+++ blocks/comparebar/src/header/component.jsx
@@ -1,9 +1,10 @@
 import h from '@kuba/h'
+import i18n from './i18n'
 import style from './style'
 import text from '@kuba/text'
 
 export default () =>
   <header className={style.header}>
-    <text.Strong className={style.header__strong} master darker xxxs medium>Comparar</text.Strong>
-    <text.P className={style.header__p} master darker xs bold>Selecione dois shapes para fazer uma comparação</text.P>
+    <text.Strong className={style.header__strong} master darker xxxs medium>{i18n.title}</text.Strong>
+    <text.P className={style.header__p} master darker xs bold>{i18n.description}</text.P>
   </header>
