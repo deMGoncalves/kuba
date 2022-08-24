21cc12e624febb978b7395a95d32f21493fe92fd
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-24 20:59:20 -0300

    chore(tipo): movendo o tipo para um contexto separado

---
+++ blocks/filter/src/tipo/i18n.js
@@ -0,0 +1,11 @@
+import i18n from '@kuba/i18n'
+import { hub } from '@kuba/lang'
+
+export default i18n({
+	descricao: {
+		[hub.PT]: 'Que tipo de shape é você quer?'
+	},
+	nome: {
+		[hub.PT]: 'Tipo'
+	}
+})
