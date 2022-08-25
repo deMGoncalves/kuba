37ff2ccba7b74626f83c342fdf5ae00169f7be69
DijalmaCavalcante <dijalma0412@gmail.com>
2022-08-25 14:45:06 -0300

    chore(shapes): criando i18n para colocar titulo e subtitulo da pagina de pro model de shapes

---
+++ modules/shapes/src/pro/title/i18n.js
@@ -0,0 +1,15 @@
+import i18n from '@kuba/i18n'
+import { hub } from '@kuba/lang'
+
+export default i18n({
+  title: {
+    [hub.PT]: 'Shapes',
+    [hub.EN]: 'Shapes',
+    [hub.ES]: 'Shapes'
+  },
+  description: {
+    [hub.PT]: 'Pro Model',
+    [hub.EN]: 'Pro Model',
+    [hub.ES]: 'Pro Model'
+  }
+})
