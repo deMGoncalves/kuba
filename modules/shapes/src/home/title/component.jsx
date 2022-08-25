c2a58bc5446b63b38795c61aeea0fff4bae77e83
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-25 08:05:23 -0300

    chore(shapes): os textos devem ficar no i18n

--- modules/shapes/src/home/title/component.jsx
+++ modules/shapes/src/home/title/component.jsx
@@ -1,10 +1,11 @@
 import h from '@kuba/h'
 import container from '@kuba/container'
+import i18n from './i18n.js'
 import style from './style'
 import text from '@kuba/text'
 
 export default () =>
   <container.Section className={style.title}>
-    <text.Span className={style.title__span} master xxxs darker medium>Shapes</text.Span>
-    <text.H1 className={style.title__h1} master darker md bold>Escolha o melhor shape para seu setup</text.H1>
+    <text.Span className={style.title__span} master xxxs darker medium>{i18n.title}</text.Span>
+    <text.H1 className={style.title__h1} master darker md bold>{i18n.description}</text.H1>
   </container.Section>
---
+++ modules/shapes/src/home/title/i18n.js
@@ -0,0 +1,11 @@
+import i18n from '@kuba/i18n'
+import { hub } from '@kuba/lang'
+
+export default i18n({
+	title: {
+		[hub.PT]: 'Shapes'
+	},
+  title: {
+		[hub.PT]: 'Escolha o melhor shape para seu setup'
+  }
+})
