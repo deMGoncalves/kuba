cfde949cb588dffda6bc7ed0ff4b282ea08d5e8c
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-25 09:07:35 -0300

    fix(shapes): erro na configuracao das keys do arquivo i18n

--- modules/shapes/src/home/title/i18n.js
+++ modules/shapes/src/home/title/i18n.js
@@ -2,10 +2,10 @@ import i18n from '@kuba/i18n'
 import { hub } from '@kuba/lang'
 
 export default i18n({
+	description: {
+		[hub.PT]: 'Escolha o melhor shape para seu setup'
+  },
 	title: {
 		[hub.PT]: 'Shapes'
-	},
-  title: {
-		[hub.PT]: 'Escolha o melhor shape para seu setup'
-  }
+	}
 })
