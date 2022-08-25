a077c6dcf154e6d98cc9b9e0b8338cba17a3b5be
DijalmaCavalcante <dijalma0412@gmail.com>
2022-08-24 21:51:43 -0300

    chore(title): chamando i18n para dentro do component da home

--- modules/shapes/src/home/title/component.jsx
+++ modules/shapes/src/home/title/component.jsx
@@ -2,9 +2,10 @@ import h from '@kuba/h'
 import container from '@kuba/container'
 import style from './style'
 import text from '@kuba/text'
+import i18n from './i18n'
 
 export default () =>
   <container.Section className={style.title}>
-    <text.Span className={style.title__span} master xxxs darker medium>Shapes</text.Span>
-    <text.H1 className={style.title__h1} master darker md bold>Escolha o melhor shape para seu setup</text.H1>
+    <text.Span className={style.title__span} master xxxs darker medium>{i18n.title}</text.Span>
+    <text.H1 className={style.title__h1} master darker md bold>{i18n.description}</text.H1>
   </container.Section>
