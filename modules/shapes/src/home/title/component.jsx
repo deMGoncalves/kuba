4db58443503ddd243a46a2303e8b54b1c31ad12c
deMGoncalves <cleber.engineer@hotmail.com>
2022-08-29 10:58:38 -0300

    refactor(shapes): o bloco title estava com os dados duplicado

--- modules/shapes/src/home/title/component.jsx
+++ modules/shapes/src/home/title/component.jsx
@@ -1,11 +1,10 @@
 import h from '@kuba/h'
 import container from '@kuba/container'
-import i18n from './i18n.js'
 import style from './style'
 import text from '@kuba/text'
 
-export default () =>
+export default (_, children) =>
   <container.Section className={style.title}>
-    <text.Span className={style.title__span} master xxxs darker medium>{i18n.title}</text.Span>
-    <text.H1 className={style.title__h1} master darker md bold>{i18n.description}</text.H1>
+    <text.Span className={style.title__span} master xxxs darker medium>{children.title}</text.Span>
+    <text.H1 className={style.title__h1} master darker md bold>{children.description}</text.H1>
   </container.Section>
