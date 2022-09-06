67c0621452ccfa67364423d7791f7bc588d31306
deMGoncalves <cleber.engineer@hotmail.com>
2022-09-06 10:06:48 -0300

    style(filter): erro de identacao

--- blocks/filter/src/tipo/i18n.js
+++ blocks/filter/src/tipo/i18n.js
@@ -2,10 +2,10 @@ import i18n from '@kuba/i18n'
 import { hub } from '@kuba/lang'
 
 export default i18n({
-	descricao: {
-		[hub.PT]: 'Que tipo de shape é você quer?'
-	},
-	nome: {
-		[hub.PT]: 'Tipo'
-	}
+  descricao: {
+    [hub.PT]: 'Que tipo de shape é você quer?'
+  },
+  nome: {
+    [hub.PT]: 'Tipo'
+  }
 })
