1c8111c44cb76e61dac03346284c14859d16c121
deMGoncalves <cleber.engineer@hotmail.com>
2022-09-11 11:46:13 -0300

    refactor(scraper): padronizacao do codigo

--- buckets/scraper/src/index.js
+++ buckets/scraper/src/index.js
@@ -3,7 +3,9 @@ const shape = require('./shape')
 const Scraper = require('./scraper')
 
 ;(async () => {
-	const pagina = await Pagina.do(shape)
-
-	Scraper.acessa(pagina).baixarHtml().tirarScreenshot()
+  // Scraper acessa a pagina do shape, baixa o html e tira um screenshot
+  const pagina = await Pagina.do(shape)
+  const scraper = await Scraper.acessa(pagina)
+  await scraper.baixarHtml()
+  await scraper.tirarScreenshot()
 })()
