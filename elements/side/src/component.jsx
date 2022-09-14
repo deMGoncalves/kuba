e9b3efb613cec765cc934e58a9e84687eddfe787
deMGoncalves <cleber.engineer@hotmail.com>
2022-09-14 16:51:33 -0300

    refactor(side): nao usaremos mais os filtros do h

--- elements/side/src/component.jsx
+++ elements/side/src/component.jsx
@@ -1,10 +1,11 @@
 import h, { Fragment } from '@kuba/h'
+import isTruthy from './isTruthy'
 import style from './style'
 
 export default (props, children) =>
   <>
-    <aside className={[style.side, props.className]} opened:isTruthy={props.opened}>
+    <aside className={[style.side, props.className]} opened={isTruthy(props.opened)}>
       {children}
     </aside>
-    <div className={style.side__overlayer} onClick={props.onClose} opened:isTruthy={props.opened} />
+    <div className={style.side__overlayer} onClick={props.onClose} opened={isTruthy(props.opened)} />
   </>
