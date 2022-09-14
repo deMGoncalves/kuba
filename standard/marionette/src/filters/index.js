491648bc7336d2d77c8f844ef9122ef32cb193bf
deMGoncalves <cleber.engineer@hotmail.com>
2022-09-14 13:54:59 -0300

    chore(marionette): filtro para montar um objeto com os dados do form

--- standard/marionette/src/filters/index.js
+++ standard/marionette/src/filters/index.js
@@ -1,9 +1,11 @@
+import formData from './formData'
 import isTruthy from './isTruthy'
 import prevent from './prevent'
 import stop from './stop'
 import value from './value'
 
 export default {
+  formData,
   isTruthy,
   prevent,
   stop,
