8797de403e9144c9ac6757f84cd5bb44722b051b
deMGoncalves <cleber.engineer@hotmail.com>
2022-09-15 11:20:17 -0300

    refactor(comparebar): usando o component do marionette para facilitar os contratos

--- blocks/comparebar/src/footer/component.jsx
+++ blocks/comparebar/src/footer/component.jsx
@@ -1,24 +1,27 @@
-import * as f from '@kuba/f'
+// import * as f from '@kuba/f'
+import { component } from '@kuba/marionette'
 import h from '@kuba/h'
 import i18n from './i18n'
 import icon from '@kuba/icon'
 import link from '@kuba/link'
 import style from './style'
 
-const href = f.dunder('href')
-const valid = f.dunder('valid')
+// const href = f.dunder('href')
+// const valid = f.dunder('valid')
 
-const component = (footer) =>
-  <footer className={style.footer} valid={valid(footer)}>
-    <link.Master className={style.footer__link} href={href(footer)} onClick={() => footer.redirect()} darker xxxs bold>
+// const component = (footer) =>
+export default component((footer) => (
+  <footer className={style.footer} valid={footer.valid}>
+    <link.Master className={style.footer__link} onClick={() => footer.redirect()} darker xxxs bold>
       {i18n.text}
       <icon.ArrowLongRight />
     </link.Master>
   </footer>
+))
 
-f.assign(component, {
-  href: f.dunder.href,
-  valid: f.dunder.valid
-})
+// f.assign(component, {
+//   href: f.dunder.href,
+//   valid: f.dunder.valid
+// })
 
-export default component
+// export default component
