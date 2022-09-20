996f62e6b81c3b68041b14ceed93624fb987f20b
deMGoncalves <cleber.engineer@hotmail.com>
2022-09-20 14:55:30 -0300

    refactor(cookiebar): devemos usar o component do marionette para escrever contratos

--- blocks/cookiebar/src/component.jsx
+++ blocks/cookiebar/src/component.jsx
@@ -1,22 +1,15 @@
-import * as f from '@kuba/f'
+import { component } from '@kuba/marionette'
 import button from '@kuba/button'
 import h from '@kuba/h'
 import i18n from './i18n'
 import style from './style'
 import text from '@kuba/text'
 
-const opened = f.dunder('opened')
-
-const component = (cookiebar) =>
-  <aside className={style.cookiebar} opened={opened(cookiebar)}>
+export default component((cookiebar) => (
+  <aside className={style.cookiebar} opened={cookiebar.opened()}>
     <text.Strong className={style.cookiebar__strong} master darker xs bold>{i18n.title}</text.Strong>
     <text.P className={style.cookiebar__p} master xxxs>{i18n.description}</text.P>
     <button.Primary className={style.cookiebar__button} onClick={() => cookiebar.aceitar()} bold>{i18n.allow}</button.Primary>
     <button.naked.Master className={style.cookiebar__button} onClick={() => cookiebar.recusar()} darker bold>{i18n.deny}</button.naked.Master>
   </aside>
-
-f.assign(component, {
-  opened: f.dunder.opened
-})
-
-export default component
+))
