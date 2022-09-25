646cd5e2a09e862dc01ca385d28d6007a1a0d7dd
deMGoncalves <cleber.engineer@hotmail.com>
2022-09-24 22:24:26 -0300

    refactor(cookiebar): nao usamos esta validacao

--- blocks/cookiebar/src/cookie/component.jsx
+++ blocks/cookiebar/src/cookie/component.jsx
@@ -3,4 +3,4 @@ import i18n from './i18n'
 import style from './style'
 
 export default (cookie) =>
-  <button className={style.cookie} onClick={() => cookie.open()} visible:isTruthy={cookie.visible}>{i18n.text}</button>
+  <button className={style.cookie} onClick={() => cookie.open()}>{i18n.text}</button>
