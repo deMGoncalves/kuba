46e92fc9b8bc25943afce43419e0cbbb6708fd81
deMGoncalves <cleber.engineer@hotmail.com>
2022-10-07 19:23:49 -0300

    refactor(filter): os filtros devem ficar no escopo da class

--- blocks/filter/src/toppings/template.jsx
+++ blocks/filter/src/toppings/template.jsx
@@ -3,6 +3,6 @@ import style from './style'
 import tag from '@kuba/tag'
 
 export default (topping) =>
-  <tag.Master className={style.toppings__tag} onClick={() => topping.toggle()} selected:isTruthy={topping.selected} darker medium>
+  <tag.Master className={style.toppings__tag} onClick={() => topping.toggle()} selected={topping.selected} darker medium>
     {topping.nome}
   </tag.Master>
