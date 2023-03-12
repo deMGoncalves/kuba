47811ac7081e56ee94fab21f5347ad6435263242
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-12 17:49:43 -0300

    refactor(themecolor): movendo para o index.html

--- .prelude/pixel/themeColor/src/component.jsx
+++
@@ -1,9 +0,0 @@
-import h from '@kuba/h'
-
-function component () {
-  return (
-    <meta name='theme-color' content='#ffffff' />
-  )
-}
-
-export default component
--- .prelude/pixel/themeColor/src/render.jsx
+++
@@ -1,4 +0,0 @@
-import h, { render } from '@kuba/h'
-import ThemeColor from './component'
-
-render(document.head, <ThemeColor />)
