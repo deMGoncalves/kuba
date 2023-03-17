facdc30e387d893e7801acf69304f59584261f84
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-17 15:56:39 -0300

    chore(telephone): inclusao das validacoes

--- component/telephone/src/component.jsx
+++ component/telephone/src/component.jsx
@@ -1,10 +1,23 @@
+import Choose from '@kuba/choose'
 import h from '@kuba/h'
-import Input, { Label } from '@kuba/input'
+import Input, { Label, Supporting } from '@kuba/input'
 
-function component () {
+function component (telephone) {
   return (
-    <Input id='telephone' type='tel' name='telephone'>
+    <Input
+      type='tel'
+      id='telephone'
+      name='telephone'
+      required
+      onBlur={(e) => telephone.onChange(e)}
+      onChange={(e) => telephone.onChange(e)}
+      onInvalid={(e) => telephone.onChange(e)}
+      value={telephone.value}
+    >
       <Label>Telephone</Label>
+      <Choose when={telephone.state}>
+        <Supporting equal='required'>Telephone is required</Supporting>
+      </Choose>
     </Input>
   )
 }
--- component/telephone/src/telephone.jsx
+++ component/telephone/src/telephone.jsx
@@ -1,10 +1,34 @@
-import { paint } from '@kuba/h'
+import * as filter from '@kuba/filter'
+import * as validator from '@kuba/validator'
+import { paint, repaint } from '@kuba/h'
 import component from './component'
 import imask from '@kuba/imask'
 
 @paint(component)
 @imask
 class Telephone {
+  #state
+  #value
+
+  get state () {
+    return (this.#state ??= '')
+  }
+
+  get value () {
+    return (this.#value ??= '')
+  }
+
+  constructor (props) {
+    this.#value = props.value
+  }
+
+  @filter.value
+  @validator.required
+  onChange (value) {
+    this.#value = value
+    return this
+  }
+
   [imask.settings] () {
     return {
       element: this[paint.rootElement]().querySelector('input'),
@@ -15,6 +39,12 @@ class Telephone {
         ]
       }
     }
+  }
+
+  @repaint
+  [validator.outlet] (state) {
+    this.#state = state
+    return this
   }
 }
 
