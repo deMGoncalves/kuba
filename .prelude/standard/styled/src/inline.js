d17e05b37e026a82b41dc08af6fd2c6435322b62
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-23 10:17:35 -0300

    refactor(styled): removendo quebras e duplicacao de espaco

--- .prelude/standard/styled/src/inline.js
+++ .prelude/standard/styled/src/inline.js
@@ -6,7 +6,7 @@ export default {
     (strings, ...expressions) =>
       (props, children) => {
         const values = expressions.map((e) => e?.(props))
-        const style = f.zip(strings, values).flat(Infinity).join('').trim()
+        const style = f.zip(strings, values).flat(Infinity).join('').replace(/[\n ]+/g, ' ').trim()
         return h(key, { style, ...props }, ...children)
       }
 }
--- .prelude/standard/styled/src/internal.js
+++ .prelude/standard/styled/src/internal.js
@@ -2,7 +2,7 @@ import * as f from '@kuba/f'
 import h, { render } from '@kuba/h'
 
 export default (strings, ...expressions) => {
-  const textContent = f.zip(strings, expressions).flat(Infinity).join('').trim()
+  const textContent = f.zip(strings, expressions).flat(Infinity).join('').replace(/[\n ]+/g, ' ').trim()
   render(
     document.head,
     <style>{textContent}</style>
