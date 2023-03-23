94aa960484e9d882f148dd9ee569d2d6d52ee043
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-23 10:28:29 -0300

    fix(props): nao estava validando se o property estava no props

--- .prelude/standard/props/src/props.js
+++ .prelude/standard/props/src/props.js
@@ -1,13 +1,16 @@
 function props (ClassRef) {
   return new Proxy(
     function () {
+      const [props] = arguments
       const instance = (this instanceof ClassRef)
         ? new ClassRef(...arguments)
         : ClassRef(...arguments)
 
       const traps = {
         get (target, key) {
-          const prop = Reflect.get(target, key)
+          const prop = Reflect.has(target, key)
+            ? Reflect.get(target, key)
+            : Reflect.get(props, key)
           return prop?.bind?.(target) ?? prop
         },
 
