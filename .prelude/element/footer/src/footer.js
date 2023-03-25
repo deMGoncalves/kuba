edf985907acc5c79814bf9e0afdb7c5d65ed4d51
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-25 18:29:18 -0300

    chore(footer): este pacote nao fica no workspace element

--- .prelude/element/footer/src/footer.js
+++
@@ -1,9 +0,0 @@
-import styled from '@kuba/styled'
-
-export default styled.footer`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  gap: var(--spacing_inset-sm);
-  width: 100%;
-`
