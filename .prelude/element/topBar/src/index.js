804919ea95fd060f7bf5eaa480fa92443edbb3c2
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-25 19:32:15 -0300

    chore(topbar): nao preciso deste elemento

--- .prelude/element/topBar/src/index.js
+++
@@ -1,4 +0,0 @@
-export { default } from './component'
-export { default as Headline } from './headline'
-export { default as Leading } from './leading'
-export { default as Trailing } from './trailing'
