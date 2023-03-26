38e1f44ff38dc51847f94169c57269091bdbe11a
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-26 07:34:14 -0300

    chore(hoem): inclusao da navegacao na pagina

--- site/home/src/component.jsx
+++ site/home/src/component.jsx
@@ -2,11 +2,13 @@ import './style'
 import h, { Fragment } from '@kuba/h'
 import Header from '@kuba/header'
 import Main from '@kuba/main'
+import Navigation from '@kuba/navigation'
 
 function component () {
   return (
     <>
       <Header />
+      <Navigation />
       <Main />
     </>
   )
