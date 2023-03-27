2d1c63e8494c042f731ce8b9be49abb15ff00b3d
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-27 16:44:08 -0300

    chore(navigation): deve fechar a navegacao quando a media for maior 768px

--- component/navigation/src/action.js
+++ component/navigation/src/action.js
@@ -1,10 +1,12 @@
 import echo from '@kuba/echo'
 import magic from '@kuba/magic'
+import media from '@kuba/media'
 import middleware from '@kuba/middleware'
 
 const action = middleware(function (navigation) {
-  echo.on('navigation:close', () => (navigation[action.toggle]?.(false)))
-  echo.on('navigation:open', () => (navigation[action.toggle]?.(true)))
+  echo.on('navigation:close', () => (navigation[action.toggle](false)))
+  echo.on('navigation:open', () => (navigation[action.toggle](true)))
+  media('(min-width: 768px').in(() => navigation[action.toggle](false))
 })
 
 Object.assign(action, {
