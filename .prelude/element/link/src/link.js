8446b92ad02653af9f54999d442c463a18e72fed
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-27 15:44:17 -0300

    chore(link): o peso da font nao precisa ser bold

--- .prelude/element/link/src/link.js
+++ .prelude/element/link/src/link.js
@@ -4,6 +4,6 @@ export default styled.a`
   color: var(--color-master-dark);
   font-family: var(--font-family-base);
   font-size: var(--font-size-xxs);
-  font-weight: var(--font-weight-bold);
+  font-weight: var(--font-weight-medium);
   line-height: inherit;
 `
