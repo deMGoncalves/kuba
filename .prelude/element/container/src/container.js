86f6ca5f0122a6575869e8956d30d0fbe0b07286
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-28 01:11:05 -0300

    chore(container): delimitando o tamanho do container

--- .prelude/element/container/src/container.js
+++ .prelude/element/container/src/container.js
@@ -4,7 +4,7 @@ export default new Proxy({}, {
   get: (_target, key) =>
     styled[key]`
       margin: 0 auto;
-      max-width: 1024px;
+      max-width: 1200px;
       padding: 0 var(--spacing_inset-xs);
       position: relative;
       width: 100%;
