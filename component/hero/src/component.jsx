b42812d4e3e6aa742b7c3795e0aada9e1a783eee
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-28 15:52:24 -0300

    chore(hero): estilizacao da estrutura

--- component/hero/src/component.jsx
+++ component/hero/src/component.jsx
@@ -1,11 +1,16 @@
 import './style'
+import Button from '@kuba/button'
 import container from '@kuba/container'
 import h from '@kuba/h'
+import text from '@kuba/text'
 
 function component () {
   return (
     <section className='hero'>
-      <container.Div className='hero__container' />
+      <container.Div className='hero__container'>
+        <text.H1 className='hero__text' master dark md highlight bold>Iaculis nunc sed augue lacus viverra vitae</text.H1>
+        <Button.Secondary>Sed vulputate</Button.Secondary>
+      </container.Div>
     </section>
   )
 }
--- component/hero/src/style.js
+++ component/hero/src/style.js
@@ -6,20 +6,36 @@ export default styled`
   }
 
   .hero__container {
-    aspect-ratio: 4 / 3;
+    align-items: center;
+    aspect-ratio: 3 / 4;
     background-color: var(--color-master-lightest);
     border-radius: var(--border-radius-sm);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing_inset-md);
+    justify-content: flex-end;
+    padding: var(--spacing_inset-md) var(--spacing_inset-xs) !important;
   }
 
-  @media (min-width: 768px) {
+  @media (min-width: 481px) {
+    .hero__container {
+      aspect-ratio: 5 / 4;
+    }
+  }
+
+  @media (min-width: 769px) {
     .hero__container {
       aspect-ratio: 16 / 9;
     }
   }
 
-  @media (min-width: 960px) {
+  @media (min-width: 961px) {
     .hero__container {
       aspect-ratio: 21 / 9;
     }
+  }
+
+  .hero__text {
+    text-align: center;
   }
 `
