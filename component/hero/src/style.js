77c0ab09d13476aeeebbb726163b5eab0e732c05
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-29 11:00:30 -0300

    chore(hero): ajuste nas medidas do aspect ratio

--- component/hero/src/style.js
+++ component/hero/src/style.js
@@ -7,35 +7,35 @@ export default styled`
 
   .hero__banner {
     align-items: center;
-    aspect-ratio: 3 / 4;
+    aspect-ratio: 480 / 600;
     background-color: var(--color-master-lightest);
     border-radius: var(--border-radius-sm);
     display: flex;
     flex-direction: column;
-    gap: var(--spacing_inset-md);
+    gap: var(--spacing_inset-sm);
     justify-content: flex-end;
-    padding: var(--spacing_inset-md) 0;
+    padding: var(--spacing_inset-md);
   }
 
   @media (min-width: 481px) {
     .hero__banner {
-      aspect-ratio: 5 / 4;
+      aspect-ratio: 768 / 550;
     }
   }
 
   @media (min-width: 769px) {
     .hero__banner {
-      aspect-ratio: 16 / 9;
+      aspect-ratio: 960 / 500;
     }
   }
 
   @media (min-width: 961px) {
     .hero__banner {
-      aspect-ratio: 21 / 9;
+      aspect-ratio: 1200 / 500;
     }
   }
 
-  .hero__text {
+  .hero__title{
     text-align: center;
   }
 `
