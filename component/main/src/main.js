21b5b58a46133fcc2421050e5067519a58aebafc
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-30 17:49:20 -0300

    feat(main): component para estrutura principal e de espacamentos

---
+++ component/main/src/main.js
@@ -0,0 +1,10 @@
+import styled from '@kuba/styled'
+
+export default styled.main`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  gap: var(--spacing_inset-lg);
+  justify-content: flex-start;
+  padding: var(--spacing_inset-xs) 0;
+`
