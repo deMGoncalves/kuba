2f09bc4113ecf50ce3bd527222194f6ab613f1a2
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-30 10:44:48 -0300

    refactor(text): recalibrando os espacamentos entre linhas

--- .prelude/element/text/src/text.js
+++ .prelude/element/text/src/text.js
@@ -8,7 +8,7 @@ const text = new Proxy({}, {
       font-family: ${p.family};
       font-size: ${p.size};
       font-weight: ${p.weight};
-      line-height: var(--line-height-md);
+      line-height: var(--line-height-sm);
       text-align: ${p.align};
     `
   }
