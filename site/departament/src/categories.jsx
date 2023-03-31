8e6a6ae0e50bdb99486a888e54010a279243608f
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-31 10:41:03 -0300

    refactor(departament): neste momento nao vamos usar o metro

--- site/departament/src/categories.jsx
+++ site/departament/src/categories.jsx
@@ -1,25 +1,10 @@
 import container from '@kuba/container'
 import h from '@kuba/h'
-import Metro from '@kuba/metro'
 
 function component () {
   return (
-    <section style='width: 100%'>
-      <container.Div>
-        <Metro>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-          <div>asdf</div>
-        </Metro>
-      </container.Div>
+    <section>
+      <container.Div />
     </section>
   )
 }
