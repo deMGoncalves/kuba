214699f65b3c31f00e7a5906cb8a3873c714105e
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-31 12:33:41 -0300

    chore(hero): vamos eliminar texto que nao tem necessidade

--- component/hero/src/component.jsx
+++ component/hero/src/component.jsx
@@ -2,14 +2,12 @@ import './style'
 import Button from '@kuba/button'
 import container from '@kuba/container'
 import h from '@kuba/h'
-import text from '@kuba/text'
 
 function component (props) {
   return (
     <section className='hero'>
       <container.Div>
         <div className='hero__banner' half={props.half}>
-          <text.H1 master dark md highlight bold center>Iaculis nunc sed augue lacus viverra vitae</text.H1>
           <Button.Secondary>Sed vulputate</Button.Secondary>
         </div>
       </container.Div>
