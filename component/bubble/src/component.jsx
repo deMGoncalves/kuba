b002f50bd7dce443ea744623bd98876672ffbea4
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-31 12:32:30 -0300

    chore(bubble): estilizacao do component

--- component/bubble/src/component.jsx
+++ component/bubble/src/component.jsx
@@ -1,9 +1,13 @@
 import './style'
 import h from '@kuba/h'
+import text from '@kuba/text'
 
-function component () {
+function component (props, children) {
   return (
-    <section />
+    <section className={['bubble', props.className]}>
+      <img className='bubble__img' src={props.src} alt={props.alt ?? 'kuba'} loading='auto'/>
+      <text.H1 master xxxs dark medium center>{children}</text.H1>
+    </section>
   )
 }
 
