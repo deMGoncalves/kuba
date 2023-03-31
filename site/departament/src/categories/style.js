a06353a8e853a7766330721afc228caf047f9579
deMGoncalves <cleber.engineer@hotmail.com>
2023-03-31 16:32:34 -0300

    refactor(categories): diagramacao das quebras de layout

--- site/departament/src/categories/style.js
+++ site/departament/src/categories/style.js
@@ -9,30 +9,48 @@ export default styled`
   }
 
   .categories__bubble {
-    min-width: calc(40% - 14px);
+    min-width: calc(66.66% - 14px);
   }
 
-  @media (min-width: 481px) {
+  @media (min-width: 395px) {
+    .categories__bubble {
+      min-width: calc(40% - 14px);
+    }
+  }
+
+  @media (min-width: 543px) {
     .categories__bubble {
       min-width: calc(28.57% - 14px);
     }
   }
 
-  @media (min-width: 769px) {
+  @media (min-width: 691px) {
     .categories__bubble {
       min-width: calc(22.22% - 14px);
     }
   }
 
-  @media (min-width: 961px) {
+  @media (min-width: 839px) {
     .categories__bubble {
       min-width: calc(18.18% - 14px);
     }
   }
 
-  @media (min-width: 1200px) {
+  @media (min-width: 989px) {
     .categories__bubble {
-      min-width: calc(12.5% - 14px);
+      min-width: calc(15.38% - 14px);
+    }
+  }
+
+  @media (min-width: 1135px) {
+    .categories__bubble {
+      min-width: calc(13.33% - 14px);
+    }
+  }
+
+  @media (min-width: 1217px) {
+    .categories__bubble {
+      min-width: calc(12.50% - 14px);
     }
   }
 `
