dd4ba9169af5e886a967d1548d4d573506396746
cleiton-carlos <cleitoncarlos.programmer@gmail.com>
2023-03-30 23:50:42 -0300

    chore: adicionado link para a home

--- component/header/src/nav.jsx
+++ component/header/src/nav.jsx
@@ -6,6 +6,7 @@ import Link from '@kuba/link'
 function component () {
   return (
     <nav className='header__nav'>
+      <Link href={urlFor('home')}>Home</Link>
       <Link href={urlFor('departament', { departament: 'cabelos' })}>Cabelos</Link>
       <Link href={urlFor('departament', { departament: 'perfumes-e-perfumaria' })}>Perfumes e Perfumaria</Link>
       <Link href={urlFor('departament', { departament: 'maquiagem' })}>Maquiagem</Link>
