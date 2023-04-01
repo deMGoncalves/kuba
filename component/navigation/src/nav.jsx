eb3ad449f9a9669326da5788ddec913f5d75c673
deMGoncalves <cleber.engineer@hotmail.com>
2023-04-01 18:38:55 -0300

    chore(nav): inclusao das marcas

--- component/navigation/src/nav.jsx
+++ component/navigation/src/nav.jsx
@@ -13,6 +13,7 @@ function component () {
       <Link className='navigation__link' href={urlFor('departament', { departament: 'labios' })}>Lábios</Link>
       <Link className='navigation__link' href={urlFor('departament', { departament: 'kits-e-paletas' })}>Kits e Paletas</Link>
       <Link className='navigation__link' href={urlFor('departament', { departament: 'acessorios' })}>Acessórios</Link>
+      <Link className="navigation__link" href={urlFor('home')}>Marcas</Link>
     </nav>
   )
 }
